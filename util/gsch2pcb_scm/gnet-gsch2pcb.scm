ea8406a69d76aaf04329e7eee252535ee5173327
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2015-07-18 03:58:38 +0000

    [util] -Import: Vladimir's gnet-gsch2pcb (no-m4 variant)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@253 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ util/gsch2pcb_scm/gnet-gsch2pcb.scm
@@ -0,0 +1,54 @@
+;;; -*-scheme-*-
+;;;
+
+;;; gEDA - GPL Electronic Design Automation
+;;; gnetlist - gEDA Netlist
+;;; Copyright (C) 1998-2010 Ales Hvezda
+;;;
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+;;; MA 02111-1301 USA.
+
+;;  gsch2pcb format  (based on PCBboard format by JM Routoure & Stefan Petersen)
+;;  Bill Wilson    billw@wt.net
+;;  6/17/2003
+
+;; Simplified (thrown out m4 support) specially for Igor2 by vzh
+
+;; Splits a string with space separated words and returns a list
+;; of the words (still as strings).
+(define (gsch2pcb:split-to-list the-string)
+  (filter!
+   (lambda (x) (not (string=? "" x)))
+   (string-split the-string #\space)))
+
+;; Write the footprint for the package `refdes' to `port'.
+(define (gsch2pcb:write-value-footprint refdes port)
+
+  (let ((value (gnetlist:get-package-attribute refdes "value"))
+         (footprint (gsch2pcb:split-to-list
+                     (gnetlist:get-package-attribute refdes "footprint"))))
+
+    (format port "PKG(~A,~A,~A)\n" (string-join footprint " ") refdes value)))
+
+;; Write the footprints for all the refdes' in `lst'.
+(define (gsch2pcb:write-value-footprints port lst)
+  (for-each (lambda (x) (gsch2pcb:write-value-footprint x port)) lst))
+
+
+(define (gsch2pcb output-filename)
+  (begin
+    (set-current-output-port (gnetlist:output-port output-filename))
+    ;; don't use m4
+    (gsch2pcb:write-value-footprints (current-output-port) packages)))
