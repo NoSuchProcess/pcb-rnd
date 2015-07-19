19233c60d872aa719dcb222f41669182d3dfd808
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2015-07-19 13:35:08 +0000

    [scconfig] -Fix: generate actions_list.h with if()s around gui - register only the one gui that's actually in use
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@320 1d61de99-560f-4dba-8957-1e505d42358b

--- src/Makefile.in.mod/gen_core_lists.sh
+++ src/Makefile.in.mod/gen_core_lists.sh
@@ -1,7 +1,46 @@
 #!/bin/sh
 for f in $*
 do
+	echo "___name___ $f"
 	cat $f
-done | grep "^REGISTER"
+done | awk '
+BEGIN {
+	q = "\""
+}
 
+/^___name___/ {
+	fullname = $2
+	basename = $2
+	sub("/[^/]*$", "", basename)
+}
+
+/^type=/ {
+	type = $0
+	sub("^type=", "", type)
+	TYPE[basename] = type
+	print "// ", type, basename
+}
+
+
+/^REGISTER/ {
+	LIST[basename] = LIST[basename] $0 "\n"
+}
+
+END {
+	for(n in LIST) {
+		hn = n
+		sub("^hid/", "", hn)
+#		if (hn in HIDNAME_FIXUP)
+#			hn = HIDNAME_FIXUP[hn]
+
+		print "/* " n  " (" TYPE[n] ") */"
+		if (TYPE[n] == "gui")
+			print "if ((gui != NULL) && (strcmp(gui->name, " q hn q ") == 0)) {"
+		print LIST[n]
+		if (TYPE[n] == "gui")
+			print "}"
+	}
+}
+
+'
 
