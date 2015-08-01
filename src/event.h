1cd77d5cf885b2ba3e9f7af1cb7be74b1a3f15ad
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2015-08-01 07:19:31 +0000

    [core] -Add: comment the event API
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@516 1d61de99-560f-4dba-8957-1e505d42358b

--- src/event.h
+++ src/event.h
@@ -5,6 +5,7 @@ typedef enum {
 	EVENT_last       /* not a real event */
 } event_id_t;
 
+/* Argument types in event's argv[] */
 typedef enum {
 	ARG_INT,
 	ARG_DOUBLE,
@@ -12,6 +13,7 @@ typedef enum {
 } event_argtype_t;
 
 
+/* An argument is its type and value */
 typedef struct {
 	event_argtype_t type;
 	union {
@@ -21,10 +23,19 @@ typedef struct {
 	} d;
 } event_arg_t;
 
+/* Event callback prototype */
 typedef void (event_handler_t)(int argc, event_arg_t *argv[]);
 
+/* Bind: add a handler to the call-list of an event; the cookie is also remembered
+   so that mass-unbind is easier later. */
 void event_bind(event_id_t ev, event_handler_t *handler, void *cookie);
+
+/* Unbind: remove a handler from an event */
 void event_unbind(event_id_t ev, event_handler_t *handler);
+
+/* Unbind by cookie: remove all handlers from an event matching the cookie */
 void event_unbind_cookie(event_id_t ev, void *cookie);
+
+/* Unbind all by cookie: remove all handlers from all events matching the cookie */
 void event_unbind_allcookie(void *cookie);
 
