dfbfbe23597e66f6cfe100fff92b6d302d838f19
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2015-08-02 08:16:17 +0000

    [gpmi] -Add: script load dialogs with module heuristics
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@549 1d61de99-560f-4dba-8957-1e505d42358b

--- src/pcb-gpmi/gpmi_plugin/manage_scripts.c
+++ src/pcb-gpmi/gpmi_plugin/manage_scripts.c
@@ -30,8 +30,8 @@ do { \
 
 static script_info_t *choose_script(char **operations, int *operation)
 {
-	HID_Attribute attr[10];
-	HID_Attr_Val result[10];
+	HID_Attribute attr[3];
+	HID_Attr_Val result[3];
 	const char **scrl;
 	script_info_t *i;
 	int n;
@@ -73,7 +73,68 @@ static script_info_t *choose_script(char **operations, int *operation)
 
 static script_info_t *load_script(void)
 {
-	return NULL;
+	char *fn, *ext, *guess;
+	script_info_t *info;
+	int default_mod = -1;
+	HID_Attribute attr[3];
+	HID_Attr_Val result[3];
+	char *exts[] = {
+		".tcl",    "tcl",
+		".lua",    "lua",
+		".awk",    "mawk",
+		".mawk",   "mawk",
+		".py",     "python",
+		".scm",    "scheme",
+		".rb",     "ruby",
+		".ruby",   "ruby",
+		".st",     "stutter",
+		".pas",    "ghli",
+		".pl",     "pearl",
+		".php",    "php",
+		NULL,      NULL
+	};
+	char *modules[] = { "tcl", "lua", "mawk", "python","scheme", "ruby",
+	                    "stutter", "ghli", "pearl", "php", NULL };
+
+
+	fn = gui->fileselect("Load script", "Load a GPMI script", NULL, NULL, "gpmi_load_script", HID_FILESELECT_READ);
+
+	if (fn == NULL)
+		return NULL;
+
+	ext = strrchr(fn, '.');
+	if (ext != NULL) {
+		char **s, **i;
+		/* find the extension in the extension->module pairs */
+		for(s = exts; s[0] != NULL; s+=2)
+			if (strcmp(ext, s[0]) == 0)
+				break;
+
+		/* if found, look up the "default enum value" for that module */
+		if (s[1] != NULL) {
+			int n;
+			for(i = modules, n = 0; *i != NULL; i++,n++) {
+				if (strcmp(*i, s[1]) == 0) {
+					default_mod = n;
+					break;
+				}
+			}
+		}
+	}
+
+	attr_make_enum(&attr[0],  "module", "Select a GPMI module to interpret the script", modules, default_mod);
+
+	if (gui->attribute_dialog(attr, 1, result, "GPMI manage scripts - select module", "Select one of GPMI modules to interpret the script"))
+		return NULL;
+
+	if (result[0].int_value < 0)
+		return NULL;
+
+	printf("HAH %s %d %s\n", fn, result[0].int_value, modules[result[0].int_value]);
+	info = hid_gpmi_load_module(modules[result[0].int_value], fn, NULL);
+	if (info == NULL)
+		gui->report_dialog("GPMI script load", "Error loading the script.\nPlease consult the message log for details.");
+	return info;
 }
 
 void gpmi_hid_manage_scripts(void)
