b330615e1a09c8f1ddb85ebd20c7ecf2cf74360d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2015-08-15 12:59:30 +0000

    [rosetta] -Add: untested python example for 30_move
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@650 1d61de99-560f-4dba-8957-1e505d42358b

--- doc-rnd/gpmi/rosetta/30_move/ex.py
+++ doc-rnd/gpmi/rosetta/30_move/ex.py
@@ -1,8 +1,20 @@
+# TODO: unfinished/untested
+
 PkgLoad("pcb-rnd-gpmi/actions", 0);
 PkgLoad("pcb-rnd-gpmi/dialogs", 0);
+PkgLoad("pcb-rnd-gpmi/layout", 0);
 
 def ev_action(id, name, argc, x, y):
-	dialog_log("Hello world!\n");
+	dx = action_arg(0) * mm2pcb_multiplier()
+	dy = action_arg(1) * mm2pcb_multiplier()
+	num_objs = layout_search_selected("mv_search", "OM_ANY")
+	for n in xrange(0, num_objs):
+		print n
+		obj_ptr = layout_search_get("mv_search", n)
+		layout_obj_move(obj_ptr, "OC_OBJ", dx, dy)
+	layout_search_free("mv_search")
+
 
 Bind("ACTE_action", "ev_action");
 action_register("hello", "", "log hello world", "hello()");
+action_register("mv", "", "move selected objects by dx and dy mm", "mv(dx,dy)");
