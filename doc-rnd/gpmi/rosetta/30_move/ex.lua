6069fba9cd9c8d39f35fd320ecce60fbf567569a
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2015-08-16 04:03:15 +0000

    [rosetta] -Fix: 30_move lua: use local variables
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@666 1d61de99-560f-4dba-8957-1e505d42358b

--- doc-rnd/gpmi/rosetta/30_move/ex.lua
+++ doc-rnd/gpmi/rosetta/30_move/ex.lua
@@ -2,10 +2,10 @@ PkgLoad("pcb-rnd-gpmi/actions", 0);
 PkgLoad("pcb-rnd-gpmi/layout", 0);
 
 function ev_action(id, name, argc, x, y)
-	dx = action_arg(0) * mm2pcb_multiplier()
-	dy = action_arg(1) * mm2pcb_multiplier()
+	local dx = action_arg(0) * mm2pcb_multiplier()
+	local dy = action_arg(1) * mm2pcb_multiplier()
 
-	num_objs = layout_search_selected("mv_search", "OM_ANY")
+	local num_objs = layout_search_selected("mv_search", "OM_ANY")
 	for n=0,num_objs+1
 	do
 		obj_ptr = layout_search_get("mv_search", n)
