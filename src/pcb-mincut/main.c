1a521a810fb5f3d5ac8f647b2c9c2a2c47ce5b47
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2015-09-03 05:10:59 +0000

    [mincut] -Add: rule to run tests, test code emits the solution on stdout
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@904 1d61de99-560f-4dba-8957-1e505d42358b

--- src/pcb-mincut/main.c
+++ src/pcb-mincut/main.c
@@ -4,15 +4,22 @@
 #include "load.h"
 #include "solve.h"
 
+#define strempty(s) ((s) == NULL ? "" : (s))
 
 int main()
 {
+	int *best;
 	gr_t *g;
+	int n;
+
 	g = load(stdin);
 	if (g == NULL) {
 		fprintf(stderr, "Failed to load input, exiting\n");
 		exit(1);
 	}
-	solve(g);
+	best = solve(g);
+	for(n = 0; best[n*2] != -1; n++)
+		printf("%s-%s\n", strempty(g->node2name[best[n*2+0]]), strempty(g->node2name[best[n*2+1]]));
+
 	return 0;
 }
