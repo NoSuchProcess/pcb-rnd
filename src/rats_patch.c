250f67ff13371608bdf610cdef9c72c5792087fc
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2015-09-19 07:57:48 +0000

    [ba] -Add: missing license banner
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@933 1d61de99-560f-4dba-8957-1e505d42358b

--- src/rats_patch.c
+++ src/rats_patch.c
@@ -1,3 +1,37 @@
+/* $Id$ */
+
+/*
+ *                            COPYRIGHT
+ *
+ *  PCB, interactive printed circuit board design
+ *  Copyright (C) 2015 Tibor 'Igor2' Palinkas
+ * 
+ *  This module, rats.c, was written and is Copyright (C) 1997 by harry eaton
+ *  this module is also subject to the GNU GPL as described below
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+/* Change History:
+ * Started 6/10/97
+ * Added support for minimum length rat lines 6/13/97
+ * rat lines to nearest line/via 8/29/98
+ * support for netlist window 10/24/98
+ */
+
 #include "rats_patch.h"
 
 static void rats_patch_remove(PCBTypePtr pcb, rats_patch_line_t *n, int do_free);
