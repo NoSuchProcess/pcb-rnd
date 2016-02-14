0b72b9ebe78ee61818eb29f9f467a43fbeb72920
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-02-14 06:43:44 +0000

    [pinnum] -Fix: export pin num change function's prototype in the header
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@1008 1d61de99-560f-4dba-8957-1e505d42358b

--- src/change.h
+++ src/change.h
@@ -113,6 +113,7 @@ bool ClrObjectOctagon (int, void *, void *, void *);
 void *ChangeObjectName (int, void *, void *, void *, char *);
 void *QueryInputAndChangeObjectName (int, void *, void *, void *, int);
 void ChangePCBSize (Coord, Coord);
+void *ChangeObjectPinnum (int Type, void *Ptr1, void *Ptr2, void *Ptr3, char *Name);
 
 /* Change the specified text on an element, either on the board (give
    PCB, PCB->Data) or in a buffer (give NULL, Buffer->Data).  The old
