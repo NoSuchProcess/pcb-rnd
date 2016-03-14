c6f1272c237c5258b562659f7bc347c118156a06
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-03-14 09:00:01 +0000

    [leak] -Fix: print cookie as text
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@1266 1d61de99-560f-4dba-8957-1e505d42358b

--- src/hid/common/actions.c
+++ src/hid/common/actions.c
@@ -434,7 +434,7 @@ void hid_actions_uninit(void)
 	for (e = htsp_first(all_actions); e; e = htsp_next(all_actions, e)) {
 		hid_cookie_action_t *ca = e->value;
 		if (ca->cookie != NULL)
-			fprintf(stderr, "WARNING: hid_actions_uninit: action '%s' with cookie %p left registered, check your plugins!\n", e->key, ca->cookie);
+			fprintf(stderr, "WARNING: hid_actions_uninit: action '%s' with cookie '%s' left registered, check your plugins!\n", e->key, ca->cookie);
 		free(e->key);
 		free(e->value);
 	}
