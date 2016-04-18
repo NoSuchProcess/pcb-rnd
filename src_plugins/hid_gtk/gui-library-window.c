49737c65c50f1077d232bcbfe0a7cbc20ebfcac1
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-04-18 04:14:53 +0000

    [[library_t] -Fix: gtk hid shouldn't print location of non-existent entries
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@1597 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/hid_gtk/gui-library-window.c
+++ src_plugins/hid_gtk/gui-library-window.c
@@ -423,10 +423,10 @@ static void library_window_preview_refresh(GhidLibraryWindow * library_window, c
 				g_string_append(pt, name);
 			}
 		}
+		g_string_append(pt, "\nLocation:\n ");
+		g_string_append(pt, entry->data.fp.loc_info);
+		g_string_append(pt, "\n");
 	}
-	g_string_append(pt, "\nLocation:\n ");
-	g_string_append(pt, entry->data.fp.loc_info);
-	g_string_append(pt, "\n");
 	gtk_label_set_text(GTK_LABEL(library_window->preview_text), g_string_free(pt, FALSE));
 }
 
