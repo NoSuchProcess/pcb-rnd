cfea25d26dd1f713d2c4e746b816edae61200510
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-04-25 05:48:51 +0000

    [conf] -Add: conf generator script that takes a .h struct and generates a series of macro calls
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@1675 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ scconfig/gen_conf.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# TODO: rewrite this in C for better portability
+
+awk '
+	BEGIN {
+		level = -1
+		q = "\""
+		cm = ","
+	}
+	/[{]/ { level++ }
+
+	/struct.*/ {
+		name = $0
+		sub(".*struct[ \t]*", "", name)
+		sub("[ \t{].*", "", name)
+		if (level == 0)
+			path = ""
+		else
+			path = path "/" name
+	}
+
+	/CFT_/ {
+		if (level < 1)
+			next
+		name=$0
+		sub("^.*CFT_", "CFN_", name)
+		sub(";.*", "", name)
+		type=name
+		sub("[ \t]*[^ \t]*$", "", type)
+		sub("[^ \t]*[ \t]*", "", name)
+		array = (name ~ "[[]")
+		if (array)
+			sub("[[].*$", "", name)
+		id = path
+		sub("^/", "", id)
+		gsub("/", ".", id)
+		id = id "." name
+		printf("conf_reg(%-36s %d, %-16s %-25s %s)\n", id cm, array, type cm, q path q cm, q name q)
+		
+	}
+
+	/[}]/ {
+		level--
+		sub("[/][^/]*$", "", path)
+	}
+
+
+	END {
+		if (level != -1)
+			print "Error: unbalanced braces" > "/dev/stderr"
+	}
+'
