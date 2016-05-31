28662c5500c4c91380591c8420ce1f1bc9bdd37b
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-05-31 18:15:41 +0000

    [io_pcb] -Add: main plugin source
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@1763 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src_plugins/io_pcb/io_pcb.c
@@ -0,0 +1,42 @@
+/*
+ *                            COPYRIGHT
+ *
+ *  PCB, interactive printed circuit board design
+ *  Copyright (C) 2016 Tibor 'Igor2' Palinkas
+ * 
+ *  This module, io_pcb, was written and is Copyright (C) 2016 by Tibor Palinkas
+ *  this module is also subject to the GNU GPL as described below
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include "config.h"
+#include "global.h"
+#include "plugins.h"
+
+/*
+#include "data.h"
+#include "action_helper.h"
+#include "conf.h"
+#include "error.h"
+*/
+
+pcb_uninit_t hid_io_pcb_init(void)
+{
+#warning TODO: API: register io using HOOK_REGISTER
+	return NULL;
+}
+
