6144cef95d80c01de28e81a924f09eb81bae170c
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-06-02 03:06:47 +0000

    [conf] -Add: mincut config header
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@1783 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src_plugins/mincut/rats_mincut_conf.h
@@ -0,0 +1,15 @@
+#ifndef PCB_MINCUT_CONF_H
+#define PCB_MINCUT_CONF_H
+
+#include "globalconst.h"
+#include "conf.h"
+
+typedef struct {
+	struct plugins {
+		struct mincut {
+			CFT_BOOLEAN enable;         /* Enable calculating mincut on shorts (rats_mincut.c) when non-zero */
+		} mincut;
+	} plugins;
+} conf_mincut_t;
+
+#endif
