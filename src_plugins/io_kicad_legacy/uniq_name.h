87454a6bfc434270ec6bc7ac330288b49f62299f
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-08-21 09:01:52 +0000

    [io_kicad_legacy] -Add: uniq name lib (for generating .mod footprint names)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@2663 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src_plugins/io_kicad_legacy/uniq_name.h
@@ -0,0 +1,27 @@
+#ifndef PCB_UNIQ_NAME_H
+#define PCB_UNIQ_NAME_H
+
+#include <genht/htsp.h>
+
+typedef struct unm_s {
+	const char *unnamed;     /* name to use when orig_name is NULL */
+	const char *suffix_sep;  /* separator for the suffix appended to generate unique names */
+	htsp_t seen;             /* hash for storing names already handed out */
+	unsigned long int ctr;   /* duplication counter - this will become the suffix for duplicated items */
+} unm_t;
+
+/* Initialize a new group of unique names */
+void unm_init(unm_t *state);
+
+/* Free all memory claimed by the group */
+void unm_uninit(unm_t *state);
+
+/* Generate and return a unique name:
+    - if orig_name is NULL, generate an unnamed item
+    - if orig_name is not-NULL and is unseen so far, return a copy of orig_name
+    - if orig_name is not-NULL and has been already seen, return a modified version
+   Strings returned are newly allocated and can be used until unm_uninit()
+   is called on state. */
+const char *unm_name(unm_t *state, const char *orig_name);
+
+#endif
