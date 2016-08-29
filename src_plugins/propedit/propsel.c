e256b4efab923f87b6dc775ac5fb1829c77a26ad
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-08-29 03:46:01 +0000

    [propedit] -Add: also map attrbitues of objects
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@2779 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/propedit/propsel.c
+++ src_plugins/propedit/propsel.c
@@ -32,9 +32,12 @@ typedef struct {
 #define map_chk_skip(ctx, obj) \
 	if (!TEST_FLAG(PCB_FLAG_SELECTED, obj)) return;
 
+#define type2field_String string
 #define type2field_Coord coord
 #define type2field_Angle angle
 #define type2field_int i
+
+#define type2TYPE_String PCB_PROPT_STRING
 #define type2TYPE_Coord PCB_PROPT_COORD
 #define type2TYPE_Angle PCB_PROPT_ANGLE
 #define type2TYPE_int PCB_PROPT_INT
@@ -46,11 +49,44 @@ do { \
 	pcb_props_add(((map_ctx_t *)ctx)->props, name, type2TYPE_ ## type, v); \
 } while(0)
 
+static void map_attr(void *ctx, const AttributeListType *list)
+{
+	int i, bl = 0;
+	char small[256];
+	char *big = NULL;
+
+	small[0] = 'a';
+	small[1] = '/';
+
+	for (i = 0; i < list->Number; i++) {
+		int len = strlen(list->List[i].name);
+		char *nm;
+		if (len >= sizeof(small)-3) {
+			if (len > bl) {
+				bl = len + 128;
+				if (big != NULL)
+					free(big);
+				big = malloc(bl);
+				big[0] = 'a';
+				big[1] = '/';
+			}
+			nm = big;
+		}
+		else
+			nm = small;
+
+		map_add_prop(ctx, nm, String, list->List[i].value);
+	}
+	if (big != NULL)
+		free(big);
+}
+
 static void map_line_cb(void *ctx, PCBType *pcb, LayerType *layer, LineType *line)
 {
 	map_chk_skip(ctx, line);
 	map_add_prop(ctx, "p/trace/thickness", Coord, line->Thickness);
 	map_add_prop(ctx, "p/trace/clearance", Coord, line->Clearance);
+	map_attr(ctx, &line->Attributes);
 }
 
 static void map_arc_cb(void *ctx, PCBType *pcb, LayerType *layer, ArcType *arc)
@@ -62,6 +98,7 @@ static void map_arc_cb(void *ctx, PCBType *pcb, LayerType *layer, ArcType *arc)
 	map_add_prop(ctx, "p/arc/height",      Coord, arc->Height);
 	map_add_prop(ctx, "p/arc/angle/start", Angle, arc->StartAngle);
 	map_add_prop(ctx, "p/arc/angle/delta", Angle, arc->Delta);
+	map_attr(ctx, &arc->Attributes);
 }
 
 static void map_text_cb(void *ctx, PCBType *pcb, LayerType *layer, TextType *text)
@@ -69,29 +106,34 @@ static void map_text_cb(void *ctx, PCBType *pcb, LayerType *layer, TextType *tex
 	map_chk_skip(ctx, text);
 	map_add_prop(ctx, "p/text/scale", int, text->Scale);
 	map_add_prop(ctx, "p/text/rotation", int, text->Direction);
+	map_attr(ctx, &text->Attributes);
 }
 
 static void map_poly_cb(void *ctx, PCBType *pcb, LayerType *layer, PolygonType *poly)
 {
 	map_chk_skip(ctx, poly);
+	map_attr(ctx, &poly->Attributes);
 }
 
 static void map_eline_cb(void *ctx, PCBType *pcb, ElementType *element, LineType *line)
 {
 	map_chk_skip(ctx, line);
 	map_line_cb(ctx, pcb, NULL, line);
+	map_attr(ctx, &line->Attributes);
 }
 
 static void map_earc_cb(void *ctx, PCBType *pcb, ElementType *element, ArcType *arc)
 {
 	map_chk_skip(ctx, arc);
 	map_arc_cb(ctx, pcb, NULL, arc);
+	map_attr(ctx, &arc->Attributes);
 }
 
 static void map_etext_cb(void *ctx, PCBType *pcb, ElementType *element, TextType *text)
 {
 	map_chk_skip(ctx, text);
 	map_text_cb(ctx, pcb, NULL, text);
+	map_attr(ctx, &text->Attributes);
 }
 
 static void map_epin_cb(void *ctx, PCBType *pcb, ElementType *element, PinType *pin)
@@ -101,12 +143,14 @@ static void map_epin_cb(void *ctx, PCBType *pcb, ElementType *element, PinType *
 	map_add_prop(ctx, "p/pin/clearance", Coord, pin->Clearance);
 	map_add_prop(ctx, "p/pin/mask",      Coord, pin->Mask);
 	map_add_prop(ctx, "p/pin/hole",      Coord, pin->DrillingHole);
+	map_attr(ctx, &pin->Attributes);
 }
 
 static void map_epad_cb(void *ctx, PCBType *pcb, ElementType *element, PadType *pad)
 {
 	map_chk_skip(ctx, pad);
 	map_add_prop(ctx, "p/pad/mask",      Coord, pad->Mask);
+	map_attr(ctx, &pad->Attributes);
 }
 
 
@@ -118,6 +162,7 @@ static void map_via_cb(void *ctx, PCBType *pcb, PinType *via)
 	map_add_prop(ctx, "p/via/clearance", Coord, via->Clearance);
 	map_add_prop(ctx, "p/via/mask",      Coord, via->Mask);
 	map_add_prop(ctx, "p/via/hole",      Coord, via->DrillingHole);
+	map_attr(ctx, &via->Attributes);
 }
 
 void pcb_propsel_map_core(htsp_t *props)
