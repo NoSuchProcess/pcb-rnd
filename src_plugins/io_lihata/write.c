a27c7d1aec6c147e29dd17802f71649d1ab52431
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-09-01 04:30:22 +0000

    [io_lihata] -Add: write metadata (name of the board) if non-empty
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@2844 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_lihata/write.c
+++ src_plugins/io_lihata/write.c
@@ -26,10 +26,26 @@
 #include "plugins.h"
 #include "plug_io.h"
 
+static void build_board_meta(PCBType *pcb, lht_doc_t *brd)
+{
+	lht_node_t *meta, *field;
+
+	meta = lht_dom_node_alloc(LHT_HASH, "meta");
+	lht_dom_hash_put(brd->root, meta);
+
+	if (pcb->Name != NULL) {
+		field = lht_dom_node_alloc(LHT_TEXT, "board_name");
+		field->data.text.value = pcb_strdup(pcb->Name);
+		lht_dom_hash_put(meta, field);
+	}
+}
+
 static lht_doc_t *build_board(PCBType *pcb)
 {
 	lht_doc_t *brd = lht_dom_init();
 	brd->root = lht_dom_node_alloc(LHT_HASH, "pcb-rnd-board-v1");
+	build_board_meta(pcb, brd);
+	return brd;
 }
 
 int io_lihata_write_pcb(plug_io_t *ctx, FILE * FP)
