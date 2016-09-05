450866c594bd7a9db11316cd617614b32e1876a8
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-09-05 06:33:09 +0000

    [io_lihata] -Add: parse board metadata and attributes
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@2933 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_lihata/read.c
+++ src_plugins/io_lihata/read.c
@@ -35,17 +35,128 @@
 #include "compat_misc.h"
 #include "macro.h"
 #include "error.h"
+#include "misc.h"
+#include "misc_util.h"
+
+static int parse_attributes(AttributeListType *list, lht_node_t *nd)
+{
+	lht_node_t *n;
+	lht_dom_iterator_t it;
+
+	if (nd->type != LHT_HASH)
+		return -1;
+
+	for(n = lht_dom_first(&it, nd); n != NULL; n = lht_dom_next(&it)) {
+		if (n->type == LHT_TEXT)
+			AttributePutToList(list, n->name, n->data.text.value, 0);
+	}
+
+	return 0;
+}
+
+/* Load the (duplicated) string value of a text node into res. Return 0 on success */
+static int parse_text(char **res, lht_node_t *nd)
+{
+	if ((nd == NULL) || (nd->type != LHT_TEXT))
+		return -1;
+	*res = pcb_strdup(nd->data.text.value);
+	return 0;
+}
+
+/* Load the Coord value of a text node into res. Return 0 on success */
+static int parse_coord(Coord *res, lht_node_t *nd)
+{
+	double tmp;
+	bool success;
+
+	if ((nd == NULL) || (nd->type != LHT_TEXT))
+		return -1;
+	
+	tmp = GetValueEx(nd->data.text.value, NULL, NULL, NULL, NULL, &success);
+	if (!success)
+		return -1;
+
+	*res = tmp;
+	return 0;
+}
+
+static int parse_meta(PCBType *pcb, lht_node_t *nd)
+{
+	lht_node_t *grp;
+
+	if (nd->type != LHT_HASH)
+		return -1;
+
+	parse_text(&pcb->Name, lht_dom_hash_get(nd, "meta"));
+
+	grp = lht_dom_hash_get(nd, "grid");
+	if ((grp != NULL) && (grp->type == LHT_HASH)) {
+		parse_coord(&pcb->GridOffsetX, lht_dom_hash_get(grp, "offs_x"));
+		parse_coord(&pcb->GridOffsetY, lht_dom_hash_get(grp, "offs_y"));
+		parse_coord(&pcb->Grid, lht_dom_hash_get(grp, "spacing"));
+	}
+
+	grp = lht_dom_hash_get(nd, "size");
+	if ((grp != NULL) && (grp->type == LHT_HASH)) {
+		parse_coord(&pcb->MaxWidth, lht_dom_hash_get(grp, "x"));
+		parse_coord(&pcb->MaxHeight, lht_dom_hash_get(grp, "y"));
+	}
+
+	grp = lht_dom_hash_get(nd, "drc");
+	if ((grp != NULL) && (grp->type == LHT_HASH)) {
+		parse_coord(&pcb->Bloat, lht_dom_hash_get(grp, "bloat"));
+		parse_coord(&pcb->Shrink, lht_dom_hash_get(grp, "shrink"));
+		parse_coord(&pcb->minWid, lht_dom_hash_get(grp, "min_width"));
+		parse_coord(&pcb->minSlk, lht_dom_hash_get(grp, "min_silk"));
+		parse_coord(&pcb->minDrill, lht_dom_hash_get(grp, "min_drill"));
+		parse_coord(&pcb->minRing, lht_dom_hash_get(grp, "min_ring"));
+	}
+
+	return 0;
+}
+
+static DataType *parse_data(lht_node_t *nd)
+{
+	return 1;
+}
+
+static int parse_board(PCBType *pcb, lht_node_t *nd)
+{
+	lht_node_t *sub;
+
+	if ((nd->type != LHT_HASH) || (strcmp(nd->name, "pcb-rnd-board-v1"))) {
+		Message("Not a board lihata.\n");
+		return -1;
+	}
+
+	sub = lht_dom_hash_get(nd, "attributes");
+	if ((sub != NULL) && (parse_attributes(&pcb->Attributes, sub) != 0))
+		return -1;
+
+	sub = lht_dom_hash_get(nd, "meta");
+	if ((sub != NULL) && (parse_meta(pcb, sub) != 0))
+		return -1;
+
+	sub = lht_dom_hash_get(nd, "data");
+	if ((sub != NULL) && ((pcb->Data = parse_data(sub)) == NULL))
+		return -1;
+
+	return 0;
+}
 
 int io_lihata_parse_pcb(plug_io_t *ctx, PCBTypePtr Ptr, const char *Filename, conf_role_t settings_dest)
 {
+	int res;
 	char *errmsg;
 	lht_doc_t *doc = lht_dom_load(Filename, &errmsg);
 
 	if (doc == NULL) {
-		Message("Error loading '%s': %s\n", errmsg);
+		Message("Error loading '%s': %s\n", Filename, errmsg);
 		return -1;
 	}
 
-	return 0;
+	res = parse_board(Ptr, doc->root);
+	lht_dom_uninit(doc);
+	return res;
 }
 
