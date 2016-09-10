2906efc9f6ac38b379c75467967f7e10ac85031c
hzeller <hzeller@1d61de99-560f-4dba-8957-1e505d42358b>
2016-09-10 20:36:11 +0000

    o Const correctness: use const element in hash table.
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@3102 1d61de99-560f-4dba-8957-1e505d42358b

--- src/ht_element.h
+++ src/ht_element.h
@@ -2,7 +2,7 @@
 #define PCB_HT_ELEMENT_H
 
 /* hash instance */
-typedef ElementType *htep_key_t;
+typedef const ElementType *htep_key_t;
 typedef int htep_value_t;
 #define HT(x) htep_ ## x
 #include <genht/ht.h>
