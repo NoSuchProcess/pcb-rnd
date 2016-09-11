f66330f3fd8f8afde810e3fbca30c1938e3555bf
hzeller <hzeller@1d61de99-560f-4dba-8957-1e505d42358b>
2016-09-11 18:47:44 +0000

    o Fix some trailing whitespace.
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@3187 1d61de99-560f-4dba-8957-1e505d42358b

--- src/funchash.h
+++ src/funchash.h
@@ -3,7 +3,7 @@
  *
  *  PCB-rnd, interactive printed circuit board design
  *  Copyright (C) 2016 Tibor 'Igor2' Palinkas
- * 
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
