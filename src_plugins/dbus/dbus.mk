a2e78cd0ffcb6f8f31a482690ee7172af3c8dda3
hzeller <hzeller@1d61de99-560f-4dba-8957-1e505d42358b>
2016-09-12 18:21:51 +0000

    [dbus] Generation of dbus introspect XML: make that a const char*
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@3280 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/dbus/dbus.mk
+++ src_plugins/dbus/dbus.mk
@@ -4,7 +4,7 @@ append /local/pcb/RULES  [@
 # dbus
 dbus-introspect.h : dbus.xml Makefile
 	echo '/* AUTOMATICALLY GENERATED FROM dbus.xml DO NOT EDIT */' > $@.tmp
-	echo "static char *pcb_dbus_introspect_xml ="  > $@.tmp
+	echo "static const char *pcb_dbus_introspect_xml ="  > $@.tmp
 	sed 's/\\/\\\\/g; s/"/\\"/g; s/^/"/; s/$$/"/' < dbus.xml >> $@.tmp
 	echo ";" >> $@.tmp
 	mv $@.tmp $@
