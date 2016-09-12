e036021c05aa0911590935b99a6acf25a36c7677
hzeller <hzeller@1d61de99-560f-4dba-8957-1e505d42358b>
2016-09-12 01:16:55 +0000

    o document the confusing fact that the parameter to   tempfile_unlink() is also being deleted.
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@3230 1d61de99-560f-4dba-8957-1e505d42358b

--- src/compat_fs.h
+++ src/compat_fs.h
@@ -3,4 +3,8 @@ char *GetWorkingDirectory(char *);
 int pcb_mkdir(const char *path, int mode);
 int pcb_spawnvp(const char **argv);
 char *tempfile_name_new(char *name);
+
+/* remove temporary file and _also_ free the memory for name
+ * (this fact is a little confusing)
+ */
 int tempfile_unlink(char *name);
