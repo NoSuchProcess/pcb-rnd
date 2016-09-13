c1e4d07a1fdf6b4ba9d33f1312c2ee9ddcdc1527
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-09-13 05:14:34 +0000

    [loghid] -Add: get loghid compilable (init/uninit, plugins_feature entry)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@3295 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src_plugins/loghid/loghid.c
@@ -0,0 +1,48 @@
+/*
+ *                            COPYRIGHT
+ *
+ *  PCB, interactive printed circuit board design
+ *  Copyright (C) 1994,1995,1996 Thomas Nau
+ *  Copyright (C) 1997, 1998, 1999, 2000, 2001 Harry Eaton
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  Contact addresses for paper mail and Email:
+ *  Harry Eaton, 6697 Buttonhole Ct, Columbia, MD 21044, USA
+ *  haceaton@aplcomm.jhuapl.edu
+ *
+ */
+#include "config.h"
+#include "global.h"
+#include "conf.h"
+#include "data.h"
+#include "action_helper.h"
+#include "change.h"
+#include "error.h"
+#include "undo.h"
+#include "plugins.h"
+#include "hid_actions.h"
+
+static const char *loghid_cookie = "loghid plugin";
+
+static void hid_loghid_uninit(void)
+{
+	hid_remove_actions_by_cookie(loghid_cookie);
+}
+
+pcb_uninit_t hid_loghid_init(void)
+{
+	return hid_loghid_uninit;
+}
