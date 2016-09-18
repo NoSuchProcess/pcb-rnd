5a57103951bf593aa5fc5b77ee7c9c2bc702563c
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-09-18 11:14:15 +0000

    [propedit] -Fix: truncate trailing zeros to keep numbers shorter
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@3452 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/propedit/props.c
+++ src_plugins/propedit/props.c
@@ -193,8 +193,8 @@ const char *propedit_sprint_val(pcb_prop_type_t type, pcb_propval_t val)
 	b = buff[buff_idx];
 	switch(type) {
 		case PCB_PROPT_STRING: return val.string; break;
-		case PCB_PROPT_COORD:  pcb_snprintf(b, 128, "%$mm\n%$ml", val.coord, val.coord); break;
-		case PCB_PROPT_ANGLE:  sprintf(b, "%f", val.angle); break;
+		case PCB_PROPT_COORD:  pcb_snprintf(b, 128, "%.06$mm\n%.06$ml", val.coord, val.coord); break;
+		case PCB_PROPT_ANGLE:  sprintf(b, "%.2f", val.angle); break;
 		case PCB_PROPT_INT:    sprintf(b, "%d", val.i); break;
 		default: strcpy(b, "<unknown type>");
 	}
