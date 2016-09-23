f6a7c296873fd16c49d090233c5063899842c3a8
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-09-23 04:27:51 +0000

    [core] -Fix: reindent polyarea.h
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@3539 1d61de99-560f-4dba-8957-1e505d42358b

--- src/polyarea.h
+++ src/polyarea.h
@@ -25,13 +25,13 @@
 #ifndef	PCB_POLYAREA_H
 #define	PCB_POLYAREA_H
 
-	typedef int BOOLp;
+typedef int BOOLp;
 
 #ifndef FALSE
-	enum {
-		FALSE = 0,
-		TRUE = 1
-	};
+enum {
+	FALSE = 0,
+	TRUE = 1
+};
 #endif
 
 #define PLF_DIR 1
@@ -47,98 +47,98 @@
 #endif
 
 
-	typedef Coord vertex[2];			/* longing point representation of
+typedef Coord vertex[2];				/* longing point representation of
 																   coordinates */
-	typedef vertex Vector;
+typedef vertex Vector;
 
 #define VertexEqu(a,b) (memcmp((a),(b),sizeof(Vector))==0)
 #define VertexCpy(a,b) memcpy((a),(b),sizeof(Vector))
 
 
-	extern Vector vect_zero;
+extern Vector vect_zero;
 
-	enum {
-		err_no_memory = 2,
-		err_bad_parm = 3,
-		err_ok = 0
-	};
+enum {
+	err_no_memory = 2,
+	err_bad_parm = 3,
+	err_ok = 0
+};
 
 
-	typedef struct CVCList CVCList;
-	typedef struct VNODE VNODE;
-	struct CVCList {
-		double angle;
-		VNODE *parent;
-		CVCList *prev, *next, *head;
-		char poly, side;
-	};
-	struct VNODE {
-		VNODE *next, *prev, *shared;
-		struct {
-			unsigned int status:3;
-			unsigned int mark:1;
-		} Flags;
-		CVCList *cvc_prev;
-		CVCList *cvc_next;
-		Vector point;
-	};
+typedef struct CVCList CVCList;
+typedef struct VNODE VNODE;
+struct CVCList {
+	double angle;
+	VNODE *parent;
+	CVCList *prev, *next, *head;
+	char poly, side;
+};
+struct VNODE {
+	VNODE *next, *prev, *shared;
+	struct {
+		unsigned int status:3;
+		unsigned int mark:1;
+	} Flags;
+	CVCList *cvc_prev;
+	CVCList *cvc_next;
+	Vector point;
+};
 
-	typedef struct PLINE PLINE;
-	struct PLINE {
-		Coord xmin, ymin, xmax, ymax;
-		PLINE *next;
-		VNODE head;
-		unsigned int Count;
-		double area;
-		rtree_t *tree;
-		pcb_bool is_round;
-		Coord cx, cy;
-		Coord radius;
-		struct {
-			unsigned int status:3;
-			unsigned int orient:1;
-		} Flags;
-	};
+typedef struct PLINE PLINE;
+struct PLINE {
+	Coord xmin, ymin, xmax, ymax;
+	PLINE *next;
+	VNODE head;
+	unsigned int Count;
+	double area;
+	rtree_t *tree;
+	pcb_bool is_round;
+	Coord cx, cy;
+	Coord radius;
+	struct {
+		unsigned int status:3;
+		unsigned int orient:1;
+	} Flags;
+};
 
-	PLINE *poly_NewContour(Vector v);
+PLINE *poly_NewContour(Vector v);
 
-	void poly_IniContour(PLINE * c);
-	void poly_ClrContour(PLINE * c);	/* clears list of vertices */
-	void poly_DelContour(PLINE ** c);
+void poly_IniContour(PLINE * c);
+void poly_ClrContour(PLINE * c);	/* clears list of vertices */
+void poly_DelContour(PLINE ** c);
 
-	BOOLp poly_CopyContour(PLINE ** dst, PLINE * src);
+BOOLp poly_CopyContour(PLINE ** dst, PLINE * src);
 
-	void poly_PreContour(PLINE * c, BOOLp optimize);	/* prepare contour */
-	void poly_InvContour(PLINE * c);	/* invert contour */
+void poly_PreContour(PLINE * c, BOOLp optimize);	/* prepare contour */
+void poly_InvContour(PLINE * c);	/* invert contour */
 
-	VNODE *poly_CreateNode(Vector v);
+VNODE *poly_CreateNode(Vector v);
 
-	void poly_InclVertex(VNODE * after, VNODE * node);
-	void poly_ExclVertex(VNODE * node);
+void poly_InclVertex(VNODE * after, VNODE * node);
+void poly_ExclVertex(VNODE * node);
 
 /**********************************************************************/
 
-	typedef struct POLYAREA POLYAREA;
-	struct POLYAREA {
-		POLYAREA *f, *b;
-		PLINE *contours;
-		rtree_t *contour_tree;
-	};
+typedef struct POLYAREA POLYAREA;
+struct POLYAREA {
+	POLYAREA *f, *b;
+	PLINE *contours;
+	rtree_t *contour_tree;
+};
 
-	BOOLp poly_M_Copy0(POLYAREA ** dst, const POLYAREA * srcfst);
-	void poly_M_Incl(POLYAREA ** list, POLYAREA * a);
+BOOLp poly_M_Copy0(POLYAREA ** dst, const POLYAREA * srcfst);
+void poly_M_Incl(POLYAREA ** list, POLYAREA * a);
 
-	BOOLp poly_Copy0(POLYAREA ** dst, const POLYAREA * src);
-	BOOLp poly_Copy1(POLYAREA * dst, const POLYAREA * src);
+BOOLp poly_Copy0(POLYAREA ** dst, const POLYAREA * src);
+BOOLp poly_Copy1(POLYAREA * dst, const POLYAREA * src);
 
-	BOOLp poly_InclContour(POLYAREA * p, PLINE * c);
-	BOOLp poly_ExclContour(POLYAREA * p, PLINE * c);
+BOOLp poly_InclContour(POLYAREA * p, PLINE * c);
+BOOLp poly_ExclContour(POLYAREA * p, PLINE * c);
 
 
-	BOOLp poly_ChkContour(PLINE * a);
+BOOLp poly_ChkContour(PLINE * a);
 
-	BOOLp poly_CheckInside(POLYAREA * c, Vector v0);
-	BOOLp Touching(POLYAREA * p1, POLYAREA * p2);
+BOOLp poly_CheckInside(POLYAREA * c, Vector v0);
+BOOLp Touching(POLYAREA * p1, POLYAREA * p2);
 
 /**********************************************************************/
 
@@ -147,34 +147,34 @@
 /* checks whether point lies within contour
 independently of its orientation */
 
-	int poly_InsideContour(PLINE * c, Vector v);
-	int poly_ContourInContour(PLINE * poly, PLINE * inner);
-	POLYAREA *poly_Create(void);
+int poly_InsideContour(PLINE * c, Vector v);
+int poly_ContourInContour(PLINE * poly, PLINE * inner);
+POLYAREA *poly_Create(void);
 
-	void poly_Free(POLYAREA ** p);
-	void poly_Init(POLYAREA * p);
-	void poly_FreeContours(PLINE ** pl);
-	BOOLp poly_Valid(POLYAREA * p);
+void poly_Free(POLYAREA ** p);
+void poly_Init(POLYAREA * p);
+void poly_FreeContours(PLINE ** pl);
+BOOLp poly_Valid(POLYAREA * p);
 
-	enum PolygonBooleanOperation {
-		PBO_UNITE,
-		PBO_ISECT,
-		PBO_SUB,
-		PBO_XOR
-	};
+enum PolygonBooleanOperation {
+	PBO_UNITE,
+	PBO_ISECT,
+	PBO_SUB,
+	PBO_XOR
+};
 
-	double vect_dist2(Vector v1, Vector v2);
-	double vect_det2(Vector v1, Vector v2);
-	double vect_len2(Vector v1);
+double vect_dist2(Vector v1, Vector v2);
+double vect_det2(Vector v1, Vector v2);
+double vect_len2(Vector v1);
 
-	int vect_inters2(Vector A, Vector B, Vector C, Vector D, Vector S1, Vector S2);
+int vect_inters2(Vector A, Vector B, Vector C, Vector D, Vector S1, Vector S2);
 
-	int poly_Boolean(const POLYAREA * a, const POLYAREA * b, POLYAREA ** res, int action);
-	int poly_Boolean_free(POLYAREA * a, POLYAREA * b, POLYAREA ** res, int action);
-	int poly_AndSubtract_free(POLYAREA * a, POLYAREA * b, POLYAREA ** aandb, POLYAREA ** aminusb);
-	int SavePOLYAREA(POLYAREA * PA, char *fname);
+int poly_Boolean(const POLYAREA * a, const POLYAREA * b, POLYAREA ** res, int action);
+int poly_Boolean_free(POLYAREA * a, POLYAREA * b, POLYAREA ** res, int action);
+int poly_AndSubtract_free(POLYAREA * a, POLYAREA * b, POLYAREA ** aandb, POLYAREA ** aminusb);
+int SavePOLYAREA(POLYAREA * PA, char *fname);
 
 /* calculate the bounding box of a POLYAREA and save result in b */
-	void poly_bbox(POLYAREA * p, BoxType * b);
+void poly_bbox(POLYAREA * p, BoxType * b);
 
-#endif   /* PCB_POLYAREA_H */
+#endif /* PCB_POLYAREA_H */
