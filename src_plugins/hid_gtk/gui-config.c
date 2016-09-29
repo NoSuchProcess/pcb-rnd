558cfbea8cd5cd8bef22df04fc5ab69511a7900c
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-09-29 06:02:09 +0000

    [conf] -Add: gtk preferences config pov: create button creates the new node and redraws the window (selection is lost)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@3664 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/hid_gtk/gui-config.c
+++ src_plugins/hid_gtk/gui-config.c
@@ -2370,7 +2370,18 @@ static void config_auto_reset_cb(GtkButton *btn, void *data)
 
 static void config_auto_create_cb(GtkButton *btn, void *data)
 {
-	printf("create!\n");
+	gds_t s;
+	conf_native_t *nat = auto_tab_widgets.nat;
+	conf_role_t role = config_auto_get_edited_role();
+
+	/* create the config node in the source lht */
+	gds_init(&s);
+	conf_print_native_field((conf_pfn)pcb_append_printf, &s, 0, &nat->val, nat->type, &nat->prop[0], 0);
+	conf_set(role, nat->hash_path, -1, s.array, POL_OVERWRITE);
+	gds_uninit(&s);
+
+	config_page_update_auto(nat);
+#warning also save
 }
 
 
