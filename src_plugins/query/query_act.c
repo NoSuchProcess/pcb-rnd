6fcdaf9d6f9b423ea0962dd21182e14fcd2dc199
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-10-07 04:03:21 +0000

    [query] -Add: query action: eval
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@3834 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/query/query_act.c
+++ src_plugins/query/query_act.c
@@ -25,6 +25,7 @@
 #include "global.h"
 #include "query.h"
 #include "query_y.h"
+#include "query_exec.h"
 
 static const char query_action_syntax[] =
 	"query(dump, expr) - dry run: compile and dump an expression\n"
@@ -41,10 +42,35 @@ static int query_action(int argc, const char **argv, Coord x, Coord y)
 
 	if (strcmp(cmd, "dump") == 0) {
 		pcb_qry_node_t *prg = NULL;
-		printf("Script: '%s'\n", argv[1]);
+		printf("Script dump: '%s'\n", argv[1]);
 		pcb_qry_set_input(argv[1]);
 		qry_parse(&prg);
 		pcb_qry_dump_tree(" ", prg);
+		return 0;
+	}
+
+	if (strcmp(cmd, "eval") == 0) {
+		pcb_qry_node_t *prg = NULL;
+		pcb_qry_exec_t ec;
+		pcb_qry_val_t res;
+
+		printf("Script eval: '%s'\n", argv[1]);
+		pcb_qry_set_input(argv[1]);
+		qry_parse(&prg);
+
+		if (prg == NULL) {
+			printf("Compilation error.\n");
+			return -1;
+		}
+
+		pcb_qry_init(&ec, prg);
+		if (pcb_qry_eval(&ec, prg, &res) == 0) {
+			printf("result: %d\n", pcb_qry_is_true(&res));
+		}
+		else
+			printf("Execution error.\n");
+/*		pcb_qry_uninit(&ec);*/
+
 		return 0;
 	}
 
