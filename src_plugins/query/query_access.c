6a098e17f1a0c8a1cec96a53c99ee073e1ddaac7
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-10-11 06:18:32 +0000

    [query] -Add: listing objects fill in parent of obj_any
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@3967 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/query/query_access.c
+++ src_plugins/query/query_access.c
@@ -29,77 +29,79 @@
 #include "misc.h"
 #include "layer.h"
 
-#define APPEND(_ctx_, _type_, _obj_) \
+#define APPEND(_ctx_, _type_, _obj_, _parenttype_, _parent_) \
 do { \
 	pcb_objlist_t *lst = (pcb_objlist_t *)_ctx_; \
 	pcb_obj_t *o = malloc(sizeof(pcb_obj_t)); \
 	o->type = _type_; \
 	o->data.any = _obj_; \
+	o->parent_type = _parenttype_; \
+	o->parent.any = _parent_; \
 	pcb_objlist_append(lst, o); \
 } while(0)
 
 static int list_layer_cb(void *ctx, PCBType *pcb, LayerType *layer, int enter)
 {
 	if (enter)
-		APPEND(ctx, PCB_OBJ_LAYER, layer);
+		APPEND(ctx, PCB_OBJ_LAYER, layer, PCB_PARENT_DATA, pcb->Data);
 	return 0;
 }
 
 static void list_line_cb(void *ctx, PCBType *pcb, LayerType *layer, LineType *line)
 {
-	APPEND(ctx, PCB_OBJ_LINE, line);
+	APPEND(ctx, PCB_OBJ_LINE, line, PCB_PARENT_LAYER, layer);
 }
 
 static void list_arc_cb(void *ctx, PCBType *pcb, LayerType *layer, ArcType *arc)
 {
-	APPEND(ctx, PCB_OBJ_ARC, arc);
+	APPEND(ctx, PCB_OBJ_ARC, arc, PCB_PARENT_LAYER, layer);
 }
 
 static void list_text_cb(void *ctx, PCBType *pcb, LayerType *layer, TextType *text)
 {
-	APPEND(ctx, PCB_OBJ_TEXT, text);
+	APPEND(ctx, PCB_OBJ_TEXT, text, PCB_PARENT_LAYER, layer);
 }
 
 static void list_poly_cb(void *ctx, PCBType *pcb, LayerType *layer, PolygonType *poly)
 {
-	APPEND(ctx, PCB_OBJ_POLYGON, poly);
+	APPEND(ctx, PCB_OBJ_POLYGON, poly, PCB_PARENT_LAYER, layer);
 }
 
 static int list_element_cb(void *ctx, PCBType *pcb, ElementType *element, int enter)
 {
 	if (enter)
-		APPEND(ctx, PCB_OBJ_ELEMENT, element);
+		APPEND(ctx, PCB_OBJ_ELEMENT, element, PCB_PARENT_DATA, pcb->Data);
 	return 0;
 }
 
 static void list_eline_cb(void *ctx, PCBType *pcb, ElementType *element, LineType *line)
 {
-	APPEND(ctx, PCB_OBJ_ELINE, line);
+	APPEND(ctx, PCB_OBJ_ELINE, line, PCB_PARENT_ELEMENT, element);
 }
 
 static void list_earc_cb(void *ctx, PCBType *pcb, ElementType *element, ArcType *arc)
 {
-	APPEND(ctx, PCB_OBJ_EARC, arc);
+	APPEND(ctx, PCB_OBJ_EARC, arc, PCB_PARENT_ELEMENT, element);
 }
 
 static void list_etext_cb(void *ctx, PCBType *pcb, ElementType *element, TextType *text)
 {
-	APPEND(ctx, PCB_OBJ_ETEXT, text);
+	APPEND(ctx, PCB_OBJ_ETEXT, text, PCB_PARENT_ELEMENT, element);
 }
 
 static void list_epin_cb(void *ctx, PCBType *pcb, ElementType *element, PinType *pin)
 {
-	APPEND(ctx, PCB_OBJ_PIN, pin);
+	APPEND(ctx, PCB_OBJ_PIN, pin, PCB_PARENT_ELEMENT, element);
 }
 
 static void list_epad_cb(void *ctx, PCBType *pcb, ElementType *element, PadType *pad)
 {
-	APPEND(ctx, PCB_OBJ_PAD, pad);
+	APPEND(ctx, PCB_OBJ_PAD, pad, PCB_PARENT_ELEMENT, element);
 }
 
 static void list_via_cb(void *ctx, PCBType *pcb, PinType *via)
 {
-	APPEND(ctx, PCB_OBJ_VIA, via);
+	APPEND(ctx, PCB_OBJ_VIA, via, PCB_PARENT_DATA, pcb->Data);
 }
 
 void pcb_qry_list_all(pcb_qry_val_t *lst, pcb_objtype_t mask)
@@ -213,6 +215,9 @@ static int field_line(pcb_obj_t *obj, pcb_qry_node_t *fld, pcb_qry_val_t *res, c
 		if (strcmp(s2, "y2") == 0)  PCB_QRY_RET_INT(res, l->Point1.Y);
 		if (strcmp(s2, "thickness") == 0)  PCB_QRY_RET_INT(res, l->Thickness);
 		if (strcmp(s2, "clearance") == 0)  PCB_QRY_RET_INT(res, l->Clearance);
+	}
+
+	if (strcmp(s1, "layer") == 0) {
 	}
 
 	PCB_QRY_RET_INV(res);
