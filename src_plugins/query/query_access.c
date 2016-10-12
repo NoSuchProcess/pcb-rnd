168a5338f4e111558c51e5165abf26c44b911c01
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-10-12 02:28:15 +0000

    [query] -Change: reorder accessor code so pin and pads will be able to call element functions without forward decl
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@3982 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/query/query_access.c
+++ src_plugins/query/query_access.c
@@ -375,42 +375,6 @@ static int field_rat(pcb_obj_t *obj, pcb_qry_node_t *fld, pcb_qry_val_t *res)
 	PCB_QRY_RET_INV(res);
 }
 
-static int field_pad(pcb_obj_t *obj, pcb_qry_node_t *fld, pcb_qry_val_t *res)
-{
-	PadType *p = obj->data.pad;
-	const char *s1, *s2;
-
-	fld2str_req(s1, fld, 0);
-	fld2str_opt(s2, fld, 1);
-
-	if ((s1[0] == 'a') && (s1[1] == '\0'))
-		PCB_QRY_RET_STR(res, AttributeGetFromList(&p->Attributes, s2));
-
-	if ((s1[0] == 'p') && (s1[1] == '\0')) {
-		if (strcmp(s2, "x1") == 0)  PCB_QRY_RET_INT(res, p->Point1.X);
-		if (strcmp(s2, "y1") == 0)  PCB_QRY_RET_INT(res, p->Point1.Y);
-		if (strcmp(s2, "x2") == 0)  PCB_QRY_RET_INT(res, p->Point1.X);
-		if (strcmp(s2, "y2") == 0)  PCB_QRY_RET_INT(res, p->Point1.Y);
-		if (strcmp(s2, "thickness") == 0)  PCB_QRY_RET_INT(res, p->Thickness);
-		if (strcmp(s2, "clearance") == 0)  PCB_QRY_RET_INT(res, p->Clearance);
-		if (strcmp(s2, "mask") == 0)  PCB_QRY_RET_INT(res, p->Mask);
-		if (strcmp(s2, "name") == 0)  PCB_QRY_RET_STR(res, p->Name);
-		if (strcmp(s2, "number") == 0)  PCB_QRY_RET_STR(res, p->Number);
-	}
-
-	if (strcmp(s1, "layer") == 0)
-		return layer_of_obj(fld->next, res, PCB_LYT_COPPER | (TEST_FLAG(PCB_FLAG_ONSOLDER, p) ? PCB_LYT_BOTTOM : PCB_LYT_TOP));
-
-	if (s2 != NULL)
-		PCB_QRY_RET_INV(res);
-
-	if (strcmp(s1, "element") != 0) {
-#warning TODO: call the element
-	}
-
-	PCB_QRY_RET_INV(res);
-}
-
 static int field_via(pcb_obj_t *obj, pcb_qry_node_t *fld, pcb_qry_val_t *res)
 {
 	PinType *p = obj->data.via;
@@ -435,21 +399,6 @@ static int field_via(pcb_obj_t *obj, pcb_qry_node_t *fld, pcb_qry_val_t *res)
 	PCB_QRY_RET_INV(res);
 }
 
-static int field_pin(pcb_obj_t *obj, pcb_qry_node_t *fld, pcb_qry_val_t *res)
-{
-	const char *s1, *s2;
-
-	fld2str_req(s1, fld, 0);
-	fld2str_opt(s2, fld, 1);
-
-	if (strcmp(s1, "element") == 0) {
-		if (s2 != NULL)
-			PCB_QRY_RET_INV(res);
-#warning TODO: call the element
-		PCB_QRY_RET_INV(res);
-	}
-	return field_via(obj, fld, res);
-}
 
 
 static int field_element(pcb_obj_t *obj, pcb_qry_node_t *fld, pcb_qry_val_t *res)
@@ -563,6 +512,59 @@ static int field_etext(pcb_obj_t *obj, pcb_qry_node_t *fld, pcb_qry_val_t *res)
 
 	return field_text(obj, fld, res);
 }
+
+static int field_pin(pcb_obj_t *obj, pcb_qry_node_t *fld, pcb_qry_val_t *res)
+{
+	const char *s1, *s2;
+
+	fld2str_req(s1, fld, 0);
+	fld2str_opt(s2, fld, 1);
+
+	if (strcmp(s1, "element") == 0) {
+		if (s2 != NULL)
+			PCB_QRY_RET_INV(res);
+#warning TODO: call the element
+		PCB_QRY_RET_INV(res);
+	}
+	return field_via(obj, fld, res);
+}
+
+static int field_pad(pcb_obj_t *obj, pcb_qry_node_t *fld, pcb_qry_val_t *res)
+{
+	PadType *p = obj->data.pad;
+	const char *s1, *s2;
+
+	fld2str_req(s1, fld, 0);
+	fld2str_opt(s2, fld, 1);
+
+	if ((s1[0] == 'a') && (s1[1] == '\0'))
+		PCB_QRY_RET_STR(res, AttributeGetFromList(&p->Attributes, s2));
+
+	if ((s1[0] == 'p') && (s1[1] == '\0')) {
+		if (strcmp(s2, "x1") == 0)  PCB_QRY_RET_INT(res, p->Point1.X);
+		if (strcmp(s2, "y1") == 0)  PCB_QRY_RET_INT(res, p->Point1.Y);
+		if (strcmp(s2, "x2") == 0)  PCB_QRY_RET_INT(res, p->Point1.X);
+		if (strcmp(s2, "y2") == 0)  PCB_QRY_RET_INT(res, p->Point1.Y);
+		if (strcmp(s2, "thickness") == 0)  PCB_QRY_RET_INT(res, p->Thickness);
+		if (strcmp(s2, "clearance") == 0)  PCB_QRY_RET_INT(res, p->Clearance);
+		if (strcmp(s2, "mask") == 0)  PCB_QRY_RET_INT(res, p->Mask);
+		if (strcmp(s2, "name") == 0)  PCB_QRY_RET_STR(res, p->Name);
+		if (strcmp(s2, "number") == 0)  PCB_QRY_RET_STR(res, p->Number);
+	}
+
+	if (strcmp(s1, "layer") == 0)
+		return layer_of_obj(fld->next, res, PCB_LYT_COPPER | (TEST_FLAG(PCB_FLAG_ONSOLDER, p) ? PCB_LYT_BOTTOM : PCB_LYT_TOP));
+
+	if (s2 != NULL)
+		PCB_QRY_RET_INV(res);
+
+	if (strcmp(s1, "element") != 0) {
+#warning TODO: call the element
+	}
+
+	PCB_QRY_RET_INV(res);
+}
+
 
 /***/
 
