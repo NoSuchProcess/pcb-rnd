0d4f2f83419bb5782bf5cf8ff6a9e581affdba93
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-10-12 06:21:16 +0000

    [query] -Add: language generated sources so the plugin is compilable without bison and flex
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@4000 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src_plugins/query/query_y.h
@@ -0,0 +1,92 @@
+/* A Bison parser, made by GNU Bison 3.0.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_QRY_QUERY_Y_H_INCLUDED
+# define YY_QRY_QUERY_Y_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int qry_debug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    T_LET = 258,
+    T_ASSERT = 259,
+    T_RULE = 260,
+    T_LIST = 261,
+    T_INVALID = 262,
+    T_OR = 263,
+    T_AND = 264,
+    T_EQ = 265,
+    T_NEQ = 266,
+    T_GTEQ = 267,
+    T_LTEQ = 268,
+    T_NL = 269,
+    T_UNIT = 270,
+    T_STR = 271,
+    T_QSTR = 272,
+    T_INT = 273,
+    T_DBL = 274
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE YYSTYPE;
+union YYSTYPE
+{
+#line 71 "query_y.y" /* yacc.c:1909  */
+
+	char *s;
+	Coord c;
+	double d;
+	const Unit *u;
+	pcb_qry_node_t *n;
+
+#line 82 "query_y.h" /* yacc.c:1909  */
+};
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE qry_lval;
+
+int qry_parse (pcb_qry_node_t **prg_out);
+
+#endif /* !YY_QRY_QUERY_Y_H_INCLUDED  */
