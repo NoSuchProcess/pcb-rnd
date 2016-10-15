01cf419d1b0e93287b6bc7e7b25ec39008d0527c
erich <erich@1d61de99-560f-4dba-8957-1e505d42358b>
2016-10-15 06:19:27 +0000

    [io_kicad_legacy] now have uniqeuly numbered netnames working in $EQUIPOT summary sections
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@4087 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_kicad_legacy/write.c
+++ src_plugins/io_kicad_legacy/write.c
@@ -832,6 +832,7 @@ LibraryMenuTypePtr pcb_netlist_find_net4pinname(PCBTypePtr pcb, const char *pin)
 int write_kicad_legacy_equipotential_netlists(FILE * FP, PCBTypePtr Layout)
 {
         int n; /* code mostly lifted from netlist.c */ 
+	int netNumber;
 
 	/* first we write a default netlist for the 0 net, which is for unconnected pads in pcbnew */
 	fputs("$EQUIPOT\n",FP);
@@ -840,14 +841,14 @@ int write_kicad_legacy_equipotential_netlists(FILE * FP, PCBTypePtr Layout)
 	fputs("$EndEQUIPOT\n",FP);
 
 	/* now we step through any available netlists and generate descriptors */
-        for (n = 0; n < Layout->NetlistLib[NETLIST_EDITED].MenuN; n++) {
+        for (n = 0, netNumber = 1; n < Layout->NetlistLib[NETLIST_EDITED].MenuN; n++) {
                 LibraryMenuTypePtr menu = &Layout->NetlistLib[NETLIST_EDITED].Menu[n];
                 int p;
-                for (p = 0; p < menu->EntryN; p++) {
+                for (p = 0; p < menu->EntryN; p++, netNumber ++) {
                         LibraryEntryTypePtr netlist = &menu->Entry[p];
 			if (netlist != NULL) {
 				fputs("$EQUIPOT\n",FP);
-				fprintf(FP, "Na %d \"goes here\"\n", (p+1) ); /*, '!'pcb_netlist_name(netlist));  netlist 0 is used for unconnected pads  */
+				fprintf(FP, "Na %d \"goes here\"\n", netNumber ); /*, '!'pcb_netlist_name(netlist));  netlist 0 is used for unconnected pads  */
 				fputs("St ~\n", FP);
 				fputs("$EndEQUIPOT\n",FP);
 			} 
