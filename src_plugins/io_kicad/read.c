389964e1ad858a9de05ba918a3e7af257d67c040
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-10-20 17:43:43 +0000

    [io_kicad] -Add: blank read.[ch]
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@4168 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src_plugins/io_kicad/read.c
@@ -0,0 +1,36 @@
+/*
+ *														COPYRIGHT
+ *
+ *	pcb-rnd, interactive printed circuit board design
+ *	Copyright (C) 2016 Tibor 'Igor2' Palinkas
+ *	Copyright (C) 2016 Erich S. Heinzle
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program; if not, write to the Free Software
+ *	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *	Contact addresses for paper mail and Email:
+ *	Thomas Nau, Schlehenweg 15, 88471 Baustetten, Germany
+ *	Thomas.Nau@rz.uni-ulm.de
+ *
+ */
+#include <math.h>
+#include "plug_io.h"
+#include "error.h"
+#include "data.h"
+#include "read.h"
+#include "layer.h"
+#include "const.h"
+#include "netlist.h"
+
+
