668f721de2a23f0ebe80233e376e0c3ba968c81c
erich <erich@1d61de99-560f-4dba-8957-1e505d42358b>
2016-10-23 02:04:17 +0000

    [io_kicad] gr_line now being parsed, no helper functions
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@4208 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_kicad/read.c
+++ src_plugins/io_kicad/read.c
@@ -217,34 +217,78 @@ static int kicad_parse_gr_text(read_state_t *st, gsxl_node_t *subtree)
 /* kicad_pcb/gr_line */
 static int kicad_parse_gr_line(read_state_t *st, gsxl_node_t *subtree)
 {
+	gsxl_node_t *n;
+	unsigned long tally = 0;
+
 	if (subtree->str != NULL) {
-
-		printf("gr_line element being parsed: '%s'\n", subtree->str);
-
-/*		if (subtree != NULL) {
-			kicad_parse_start(st, subtree);
+		printf("gr_line being parsed: '%s'\n", subtree->str);		
+		for(n = subtree; n != NULL; n = n->next) {
+			if (n->str != NULL && strcmp("start", n->str) == 0) {
+					SEEN_NO_DUP(tally, 0);
+					if (n->children != NULL && n->children->str != NULL) {
+						pcb_printf("gr_line start at x: '%s'\n", (n->children->str));
+						SEEN_NO_DUP(tally, 1); /* same as ^= 1 was */
+					} else {
+						return -1;
+					}
+					if (n->children->next != NULL && n->children->next->str != NULL) {
+						pcb_printf("gr_line start at y: '%s'\n", (n->children->next->str));
+						SEEN_NO_DUP(tally, 2);	
+					} else {
+						return -1;
+					}
+			} else if (n->str != NULL && strcmp("end", n->str) == 0) {
+					SEEN_NO_DUP(tally, 3);
+					if (n->children != NULL && n->children->str != NULL) {
+						pcb_printf("gr_line end at x: '%s'\n", (n->children->str));
+						SEEN_NO_DUP(tally, 4);
+					} else {
+						return -1;
+					}
+					if (n->children->next != NULL && n->children->next->str != NULL) {
+						pcb_printf("gr_line end at y: '%s'\n", (n->children->next->str));
+						SEEN_NO_DUP(tally, 5);	
+					} else {
+						return -1;
+					}
+			} else if (n->str != NULL && strcmp("layer", n->str) == 0) {
+					SEEN_NO_DUP(tally, 6);
+					if (n->children != NULL && n->children->str != NULL) {
+						pcb_printf("gr_line layer: '%s'\n", (n->children->str));
+						SEEN_NO_DUP(tally, 7);
+					} else {
+						return -1;
+					}
+			} else if (n->str != NULL && strcmp("width", n->str) == 0) {
+					SEEN_NO_DUP(tally, 8);
+					if (n->children != NULL && n->children->str != NULL) {
+						pcb_printf("gr_line width: '%s'\n", (n->children->str));
+						SEEN_NO_DUP(tally, 9);
+					} else {
+						return -1;
+					}
+			} else if (n->str != NULL && strcmp("angle", n->str) == 0) { /* unlikely to be used or seen */
+					SEEN_NO_DUP(tally, 10);
+					if (n->children != NULL && n->children->str != NULL) {
+						pcb_printf("gr_line angle: '%s'\n", (n->children->str));
+						SEEN_NO_DUP(tally, 11);
+					} else {
+						return -1;
+					}
+			} else if (n->str != NULL && strcmp("net", n->str) == 0) { /* unlikely to be used or seen */
+					SEEN_NO_DUP(tally, 12);
+					if (n->children != NULL && n->children->str != NULL) {
+						pcb_printf("gr_line net: '%s'\n", (n->children->str));
+						SEEN_NO_DUP(tally, 13);
+					} else {
+						return -1;
+					}
+			} else {
+				return -1;
+			}
 		}
-		if (subtree->next != NULL) {
-			kicad_parse_end(st, subtree->next);
-		}
-		if (subtree->next->next->next->next == NULL) {
-			if (subtree->next->next != NULL) {
-				kicad_parse_layer(st, subtree->next->next);
-			}
-			if (subtree->next->next->next != NULL) {
-				kicad_parse_width(st, subtree->next->next->next);
-			}
-		} else {
-			if (subtree->next->next != NULL) {
-				kicad_parse_angle(st, subtree->next->next);
-			}
-			if (subtree->next->next->next != NULL) {
-				kicad_parse_layer(st, subtree->next->next->next);
-			}
-			if (subtree->next->next->next->next != NULL) {
-				kicad_parse_width(st, subtree->next->next->next->next);
-			}
-		} */
+	}
+	if (tally >= 0) { /* need start, end, layer, thickness at a minimum */
 		return 0;
 	}
 	return -1;
