b113bff2729a74fef5c87f97f11194d5ace97565
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-11-07 07:52:19 +0000

    [remote] -Add: test program parses the input [remote] -Add: basic test messages
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@4668 1d61de99-560f-4dba-8957-1e505d42358b

--- tests/remote/main.c
+++ tests/remote/main.c
@@ -12,7 +12,8 @@ int main()
 	memset(&pctx, 0, sizeof(pctx));
 
 	for(;;) {
-		int res, c = getchar();
+		parse_res_t res;
+		int c = getchar();
 		if (c == EOF)
 			return -1;
 		if (c == '\n') {
@@ -22,7 +23,17 @@ int main()
 		else
 			col++;
 		res = parse_char(&pctx, c);
-		if (res < 0)
-			printf("parse error at %d:%d\n", line, col);
+		switch(res) {
+			case PRES_ERR:
+				printf("parse error at %d:%d\n", line, col);
+				return 1;
+			case PRES_GOT_MSG:
+				printf("msg '%s'\n", pctx.pcmd);
+				proto_node_print(pctx.targ, 1);
+				printf("\n");
+				break;
+			case PRES_PROCEED:
+				break;
+		}
 	}
 }
