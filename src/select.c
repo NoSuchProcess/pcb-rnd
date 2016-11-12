08b625803f90dbe23dc8bf7d9fc693d6e78086b1
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-11-12 04:23:53 +0000

    [core] -Fix: macro based coord swap in selection box canonicalization: we'll need more of these swaps for the solder-side bug
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@4749 1d61de99-560f-4dba-8957-1e505d42358b

--- src/select.c
+++ src/select.c
@@ -220,24 +220,27 @@ static long int *ListBlock_(BoxTypePtr Box, pcb_bool Flag, int *len)
 	int used = 0, alloced = 0;
 	long int *list = NULL;
 
+#define swap(a,b) \
+do { \
+	Coord tmp; \
+	tmp = a; \
+	a = b; \
+	b = tmp; \
+} while(0)
+
 	/* Make sure our negative box is canonical: always from bottom-right to top-left
 	   This limits all possible box coordinate orders to two, one for positive and
 	   one for negative. */
 	if (IS_BOX_NEGATIVE(Box)) {
-		Coord tmp;
-		if (Box->X1 < Box->X2) {
-			tmp = Box->X1;
-			Box->X1 = Box->X2;
-			Box->X2 = tmp;
-		}
-		if (Box->Y1 < Box->Y2) {
-			tmp = Box->Y1;
-			Box->Y1 = Box->Y2;
-			Box->Y2 = tmp;
-		}
+		if (Box->X1 < Box->X2)
+			swap(Box->X1, Box->X2);
+		if (Box->Y1 < Box->Y2)
+			swap(Box->Y1, Box->Y2);
 	}
 
-	/*pcb_printf("box: %mm %mm - %mm %mm %d\n", Box->X1, Box->Y1, Box->X2, Box->Y2, IS_BOX_NEGATIVE(Box));*/
+#undef swap
+
+pcb_printf("box: %mm %mm - %mm %mm    [ %d ]\n", Box->X1, Box->Y1, Box->X2, Box->Y2, IS_BOX_NEGATIVE(Box));
 
 /* append an object to the return list OR set the flag if there's no list */
 #define append(undo_type, p1, obj) \
