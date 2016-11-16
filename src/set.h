308cd79f6e4563997d6c333672dfc5e7e98dc863
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-11-16 06:55:00 +0000

    [unravel] -Remove: prototypes of non-existing set functions
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@5011 1d61de99-560f-4dba-8957-1e505d42358b

--- src/set.h
+++ src/set.h
@@ -33,7 +33,6 @@
 
 void SetTextScale(int);
 void SetGrid(pcb_coord_t, pcb_bool);
-void SetZoom(double);
 void SetLineSize(pcb_coord_t);
 void SetViaSize(pcb_coord_t, pcb_bool);
 void SetViaDrillingHole(pcb_coord_t, pcb_bool);
@@ -43,7 +42,6 @@ void SetBufferNumber(int);
 void SetMode(int);
 void SetCrosshairRangeToBuffer(void);
 void SetLocalRef(pcb_coord_t, pcb_coord_t, pcb_bool);
-void RedrawZoom(pcb_coord_t, pcb_coord_t);
 void SaveMode(void);
 void RestoreMode(void);
 
