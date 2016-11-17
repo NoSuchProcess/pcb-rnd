af75ad7366c6bf589767df5151bd44f4f041858b
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-11-17 06:46:08 +0000

    [unravel2] -Cleanup: indentation
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@5044 1d61de99-560f-4dba-8957-1e505d42358b

--- src/globalconst.h
+++ src/globalconst.h
@@ -38,72 +38,68 @@
 /* frame between the groundplane and the copper or mask - noone seems
    to remember what these two are for; changing them may have unforeseen
    side effects. */
-#define	GROUNDPLANEFRAME        PCB_MIL_TO_COORD(15)
+#define GROUNDPLANEFRAME        PCB_MIL_TO_COORD(15)
 #define MASKFRAME               PCB_MIL_TO_COORD(3)
 
 /* ---------------------------------------------------------------------------
  * some limit specifications
  */
-#define LARGE_VALUE		(COORD_MAX / 2 - 1) /* maximum extent of board and elements */
- 
-#define	MAX_LAYER		16	/* max number of layer, check source */
-					/* code for more changes, a *lot* more changes */
-#define	MIN_LINESIZE		PCB_MIL_TO_COORD(0.01)	/* thickness of lines */
-#define	MAX_LINESIZE		LARGE_VALUE
-#define MIN_ARCSIZE		PCB_MIL_TO_COORD(0.01)
-#define MAX_ARCSIZE		LARGE_VALUE
-#define	MIN_TEXTSCALE		10	/* scaling of text objects in percent */
-#define	MAX_TEXTSCALE		10000
-#define	MIN_PINORVIASIZE	PCB_MIL_TO_COORD(20)	/* size of a pin or via */
-#define	MIN_PINORVIAHOLE	PCB_MIL_TO_COORD(4)	/* size of a pins or vias drilling hole */
-#define	MAX_PINORVIASIZE	LARGE_VALUE
-#define	MIN_PINORVIACOPPER	PCB_MIL_TO_COORD(4)	/* min difference outer-inner diameter */
-#define	MIN_PADSIZE		PCB_MIL_TO_COORD(1)	/* min size of a pad */
-#define	MAX_PADSIZE		LARGE_VALUE   /* max size of a pad */
-#define	MIN_DRC_VALUE		PCB_MIL_TO_COORD(0.1)
-#define	MAX_DRC_VALUE		PCB_MIL_TO_COORD(500)
-#define	MIN_DRC_SILK		PCB_MIL_TO_COORD(1)
-#define	MAX_DRC_SILK		PCB_MIL_TO_COORD(30)
-#define	MIN_DRC_DRILL		PCB_MIL_TO_COORD(1)
-#define	MAX_DRC_DRILL		PCB_MIL_TO_COORD(50)
-#define	MIN_DRC_RING		0
-#define	MAX_DRC_RING		PCB_MIL_TO_COORD(100)
-#define	MIN_GRID		1
-#define	MAX_GRID		PCB_MIL_TO_COORD(1000)
-#define	MAX_FONTPOSITION	255	/* upper limit of characters in my font */
+#define LARGE_VALUE      (COORD_MAX / 2 - 1) /* maximum extent of board and elements */
 
-#define	MAX_COORD		LARGE_VALUE	/* coordinate limits */
-#define	MIN_SIZE		PCB_MIL_TO_COORD(10)	/* lowest width and height of the board */
-#define	MAX_BUFFER		5	/* number of pastebuffers */
-					/* additional changes in menu.c are */
-					/* also required to select more buffers */
+#define MAX_LAYER        16    /* max number of layer, check source code for more changes, a *lot* more changes */
+#define MIN_LINESIZE     PCB_MIL_TO_COORD(0.01)	/* thickness of lines */
+#define MAX_LINESIZE     LARGE_VALUE
+#define MIN_ARCSIZE      PCB_MIL_TO_COORD(0.01)
+#define MAX_ARCSIZE      LARGE_VALUE
+#define MIN_TEXTSCALE    10 /* scaling of text objects in percent */
+#define MAX_TEXTSCALE    10000
+#define MIN_PINORVIASIZE PCB_MIL_TO_COORD(20)	/* size of a pin or via */
+#define MIN_PINORVIAHOLE PCB_MIL_TO_COORD(4)	/* size of a pins or vias drilling hole */
+#define MAX_PINORVIASIZE LARGE_VALUE
+#define MIN_PINORVIACOPPER PCB_MIL_TO_COORD(4)	/* min difference outer-inner diameter */
+#define MIN_PADSIZE      PCB_MIL_TO_COORD(1)	/* min size of a pad */
+#define MAX_PADSIZE      LARGE_VALUE   /* max size of a pad */
+#define MIN_DRC_VALUE    PCB_MIL_TO_COORD(0.1)
+#define MAX_DRC_VALUE    PCB_MIL_TO_COORD(500)
+#define MIN_DRC_SILK     PCB_MIL_TO_COORD(1)
+#define MAX_DRC_SILK     PCB_MIL_TO_COORD(30)
+#define MIN_DRC_DRILL    PCB_MIL_TO_COORD(1)
+#define MAX_DRC_DRILL    PCB_MIL_TO_COORD(50)
+#define MIN_DRC_RING     0
+#define MAX_DRC_RING     PCB_MIL_TO_COORD(100)
+#define MIN_GRID         1
+#define MAX_GRID         PCB_MIL_TO_COORD(1000)
+#define MAX_FONTPOSITION 255 /* upper limit of characters in my font */
 
-#define	DEFAULT_DRILLINGHOLE	40	/* default inner/outer ratio for */
-					/* pins/vias in percent */
+#define MAX_COORD        LARGE_VALUE /* coordinate limits */
+#define MIN_SIZE         PCB_MIL_TO_COORD(10) /* lowest width and height of the board */
+#define MAX_BUFFER       5 /* number of pastebuffers additional changes in menu.c are also required to select more buffers */
+
+#define DEFAULT_DRILLINGHOLE 40 /* default inner/outer ratio for pins/vias in percent */
 
 #if MAX_LINESIZE > MAX_PINORVIASIZE	/* maximum size value */
-#define	MAX_SIZE	MAX_LINESIZE
+#define MAX_SIZE MAX_LINESIZE
 #else
-#define	MAX_SIZE	MAX_PINORVIASIZE
+#define MAX_SIZE MAX_PINORVIASIZE
 #endif
 
-#ifndef	MAXPATHLEN			/* maximum path length */
-#ifdef	PATH_MAX
-#define	MAXPATHLEN	PATH_MAX
+#ifndef MAXPATHLEN   /* maximum path length */
+#ifdef PATH_MAX
+#define MAXPATHLEN PATH_MAX
 #else
-#define	MAXPATHLEN	2048
+#define MAXPATHLEN 2048
 #endif
 #endif
 
-#define	MAX_LINE_POINT_DISTANCE		0	/* maximum distance when searching */
+#define MAX_LINE_POINT_DISTANCE		0	/* maximum distance when searching */
 						/* line points */
-#define	MAX_POLYGON_POINT_DISTANCE	0	/* maximum distance when searching */
+#define MAX_POLYGON_POINT_DISTANCE	0	/* maximum distance when searching */
 						/* polygon points */
-#define	MAX_ELEMENTNAMES		3	/* number of supported names of */
+#define MAX_ELEMENTNAMES		3	/* number of supported names of */
 						/* an element */
 #define MAX_NETLIST_LINE_LENGTH		255	/* maximum line length for netlist files */
-#define	MAX_MODESTACK_DEPTH		16	/* maximum depth of mode stack */
-#define	MIN_GRID_DISTANCE		4	/* minimum distance between point */
+#define MAX_MODESTACK_DEPTH		16	/* maximum depth of mode stack */
+#define MIN_GRID_DISTANCE		4	/* minimum distance between point */
 						/* to enable grid drawing */
 	/* size of diamond element mark */
 #define EMARK_SIZE	PCB_MIL_TO_COORD (10)
