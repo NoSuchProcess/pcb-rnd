c3d4987d9550773d70da2cc48c0a9d42a1f77da3
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-11-18 05:25:00 +0000

    [util] -Fix: better action renaming
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@5079 1d61de99-560f-4dba-8957-1e505d42358b

--- util/devhelpers/renamea.sh
+++ util/devhelpers/renamea.sh
@@ -7,4 +7,4 @@ lc=`echo $na | tr "[A-Z]" "[a-z]"`
 ./renameo.sh "$1"            "pcb_act_$na"
 ./renameo.sh "${lc}_help"    "pcb_acth_$na"
 ./renameo.sh "${lc}_syntax"  "pcb_acts_$na"
-./renameo.sh "AFAIL($lc)"    "PCB_ACT_FAIL($na)"
+./rename.sh "s/\\([^a-zA-Z0-9_]\\)AFAIL[(]$lc[)]/\\1PCB_ACT_FAIL($na)/g"
