cb6e042afb83ab117ceb1cbe6e1d79d17a0c5dd0
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-11-25 07:48:10 +0000

    [tests] -Add: export test script (no cmp yet)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@5270 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ tests/RTT/Export.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+TRUNK=../..
+if test -z "$pcb_rnd_bin"
+then
+	pcb_rnd_bin="$TRUNK/src/pcb-rnd"
+fi
+
+fmt_args=""
+
+set_fmt_args()
+{
+	case "$fmt" in
+		png)
+			fmt_args="--dpi 1200"
+			ext=.png
+			;;
+	esac
+}
+
+cmp_fmt()
+{
+	local ref="$1" out="$2"
+	case "$fmt" in
+		png)
+			echo "$ref" "$out"
+	esac
+}
+
+while test $# -gt 0
+do
+	case "$1"
+	in
+		-f|-x) fmt=$2; shift 1;;
+		-b) pcb_rnd_bin=$2; shift 1;;
+		*)
+			if test -z "$fn"
+			then
+				fn="$1"
+			else
+				echo "unknown switch $1; try --help" >&2
+				exit 1
+			fi
+			;;
+	esac
+	shift 1
+done
+
+if test -z "$fmt"
+then
+	echo "need a format" >&2
+fi
+
+set_fmt_args
+
+$pcb_rnd_bin -x "$fmt" $fmt_args "$fn"
+
+base=${fn%%.pcb}
+ref_fn=ref/$base$ext
+fmt_fn=$base$ext
+out_fn=out/$base$ext
+if test -f "$fmt_fn"
+then
+	mv $fmt_fn out
+fi
+cmp_fmt "$ref_fn" "$out_fn"
