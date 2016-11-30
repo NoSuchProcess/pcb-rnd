3352083258eb32336b790e4cb71c5cacb70fe05c
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-11-30 04:35:37 +0000

    [layer] -Del: obsolete comment
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@5417 1d61de99-560f-4dba-8957-1e505d42358b

--- src/layer.c
+++ src/layer.c
@@ -485,10 +485,6 @@ pcb_bool pcb_layer_is_paste_empty(pcb_side_t side)
 	return paste_empty;
 }
 
-/***********************************************************************
- * Layer Group Functions
- */
-
 pcb_layergrp_id_t MoveLayerToGroup(pcb_layer_id_t layer, pcb_layergrp_id_t group)
 {
 	pcb_layergrp_id_t prev, i, j;
