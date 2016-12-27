1b3542009aa39b99234eae94cd05fb23df8fd884
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2016-12-27 13:04:01 +0000

    [io_lihata] -Fix: don't change PCB->Filename on the backup .pcb save
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@5951 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_lihata/write.c
+++ src_plugins/io_lihata/write.c
@@ -788,7 +788,7 @@ int io_lihata_write_pcb(pcb_plug_io_t *ctx, FILE * FP, const char *old_filename,
 		
 		orig_fn = PCB->Filename;
 		PCB->Filename = NULL;
-		fprintf(stderr, "NOTE: io_lihata_write_pcb will save to '%s' but first saves in '%s': res=%d (expected: 0)\n", new_filename, pcb_fn, pcb_hid_actionl("SaveTo", "LayoutAs", pcb_fn, "pcb", NULL));
+		fprintf(stderr, "NOTE: io_lihata_write_pcb will save to '%s' but first saves in '%s': res=%d (expected: 0)\n", new_filename, pcb_fn, pcb_save_pcb(pcb_fn, "pcb"));
 		free(pcb_fn);
 		
 		/* restore these because SaveTo() has changed them */
