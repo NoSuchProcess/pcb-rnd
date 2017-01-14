92a091a3eaa89f8b92ef05819fbcfb5ed66a8f12
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-01-14 18:39:57 +0000

    [gtk] -Change: make box compat static inline based so gtk3 can set homogenous
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@6266 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_gtk_common/compat.h
+++ src_plugins/lib_gtk_common/compat.h
@@ -1,9 +1,31 @@
-/* hbox/vbox creation, similar to gtk2's, but we don't use homogenous */
+/* hbox/vbox creation, similar to gtk2's */
 #ifdef PCB_GTK3
-#	define GTKC_HBOX_NEW(spacing) gtk_box_new(GTK_ORIENTATION_HORIZONTAL, spacing)
-#	define GTKC_VBOX_NEW(spacing) gtk_box_new(GTK_ORIENTATION_VERTICAL, spacing)
+static inline GtkWidget *gtkc_hbox_new(gboolean homogenous, gint spacing)
+{
+	GtkBox *box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, spacing);
+	gtk_box_set_homogenous(box, homogenous);
+	return GTK_WIDGET(box);
+}
+
+static inline GtkWidget *gtkc_vbox_new(gboolean homogenous, gint spacing)
+{
+	GtkBox *box = gtk_box_new(TK_ORIENTATION_VERTICAL, spacing);
+	gtk_box_set_homogenous(box, homogenous);
+	return GTK_WIDGET(box);
+}
+
 #else
-#	define GTKC_HBOX_NEW(spacing) gtk_hbox_new(false, spacing)
-#	define GTKC_VBOX_NEW(spacing) gtk_vbox_new(false, spacing)
+/* gtk2 */
+
+static inline GtkWidget *gtkc_hbox_new(gboolean homogenous, gint spacing)
+{
+	return gtk_hbox_new(homogenous, spacing);
+}
+
+static inline GtkWidget *gtkc_vbox_new(gboolean homogenous, gint spacing)
+{
+	return gtk_vbox_new(homogenous, spacing);
+}
+
 #endif
 
