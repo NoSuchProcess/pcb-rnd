ddd8eff71313f6f6b6dc0dc3de5ceede5315a120
avigne <avigne@1d61de99-560f-4dba-8957-1e505d42358b>
2017-01-17 18:22:41 +0000

    [gtk] -Change: from a basic dialog to a gtk_about_dialog
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@6333 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_gtk_common/dlg_about.c
+++ src_plugins/lib_gtk_common/dlg_about.c
@@ -3,7 +3,7 @@
  *
  *  PCB, interactive printed circuit board design
  *  Copyright (C) 1994,1995,1996 Thomas Nau
- *  pcb-rnd Copyright (C) 2017 Tibor 'Igor2' Palinkas
+ *  pcb-rnd Copyright (C) 2017 Tibor 'Igor2' Palinkas, Alain Vigne
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -25,22 +25,33 @@
  *
  */
 
-/* This code was originally written by Bill Wilson for the PCB Gtk port. */
-
 #include "config.h"
 
 #include "dlg_about.h"
+#include "compat_nls.h"
 #include "build_run.h"
 
-void pcb_gtk_dlg_about(GtkWidget *top_window)
+void pcb_gtk_dlg_about(GtkWidget * top_window)
 {
-	GtkWidget *dialog = gtk_message_dialog_new(
-		GTK_WINDOW(top_window),
-		(GtkDialogFlags) (GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT),
-		GTK_MESSAGE_INFO, GTK_BUTTONS_OK, "%s", pcb_get_infostr()
-	);
+	GtkWidget *w = gtk_about_dialog_new();
+	GtkAboutDialog *about = GTK_ABOUT_DIALOG(w);
+	char *s_info = pcb_get_infostr();
+	const gchar *authors[] = { "Igor2", "Alain", NULL };
 
-	gtk_dialog_run(GTK_DIALOG(dialog));
-	gtk_widget_destroy(dialog);
+	gtk_about_dialog_set_program_name(about, "pcb-rnd");
+	gtk_about_dialog_set_version(about, VERSION);
+	/*FIXME: pcb_author()  not good here because gchar ** expected : */
+	/*gtk_about_dialog_set_authors (about, pcb_author()); */
+	gtk_about_dialog_set_authors(about, authors);
+	gtk_about_dialog_set_copyright (about, "FIXME : text of the license ...");
+	/* in GTK3:
+  gtk_about_dialog_set_license_type(about, GTK_LICENSE_GPL_2_0);*/
+	/*FIXME: Refactor the string w.r.t. the dialog */
+	gtk_about_dialog_set_comments(about, s_info);
+	gtk_about_dialog_set_website(about, "http://repo.hu/projects/pcb-rnd/");
+	gtk_about_dialog_set_documenters(about, authors);
+	gtk_about_dialog_set_translator_credits(about, _("translator-credits"));
+
+	gtk_dialog_run(GTK_DIALOG(w));
+	gtk_widget_destroy(w);
 }
-
