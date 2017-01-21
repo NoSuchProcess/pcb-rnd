fc095f1af529f346b00951a9f0f3eb4c71deba46
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-01-21 10:47:35 +0000

    [draw_sect] -Add: low level draw helpers (line, hatched box, clipped line)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@6384 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/draw_csect/draw_csect.c
+++ src_plugins/draw_csect/draw_csect.c
@@ -30,19 +30,115 @@
 #include "plugins.h"
 #include "stub_draw_csect.h"
 
-static void draw_csect(pcb_hid_gc_t gc)
+#include "obj_text_draw.h"
+#include "obj_line_draw.h"
+
+/* Draw a text at x;y sized scale percentage */
+static void dtext(int x, int y, int scale, int dir, const char *txt)
 {
 	pcb_text_t t;
 
-	pcb_gui->set_color(gc, PCB->ElementColor);
-
-	t.X = 0;
-	t.Y = 0;
-	t.TextString = "Board cross section";
-	t.Direction = 0;
-	t.Scale = 150;
+	t.X = PCB_MM_TO_COORD(x);
+	t.Y = PCB_MM_TO_COORD(y);
+	t.TextString = txt;
+	t.Direction = dir;
+	t.Scale = scale;
 	t.Flags = pcb_no_flags();
 	DrawTextLowLevel(&t, 0);
+}
+
+/* draw a line of a specific thickness */
+static void dline(int x1, int y1, int x2, int y2, float thick)
+{
+	pcb_line_t l;
+	l.Point1.X = PCB_MM_TO_COORD(x1);
+	l.Point1.Y = PCB_MM_TO_COORD(y1);
+	l.Point2.X = PCB_MM_TO_COORD(x2);
+	l.Point2.Y = PCB_MM_TO_COORD(y2);
+	l.Thickness = PCB_MM_TO_COORD(thick);
+	_draw_line(&l);
+}
+
+
+/* draw a line clipped with two imaginary vertical lines at cx1 and cx2 */
+static void dline_vclip(int x1, int y1, int x2, int y2, float thick, int cx1, int cx2)
+{
+	if (cx2 < cx1) { /* make sure cx2 > cx1 */
+		int tmp;
+		tmp = cx1;
+		cx1 = cx2;
+		cx2 = tmp;
+	}
+
+	if (x2 == x1) { /* do not clip vertical lines */
+		if ((x1 >= cx1) && (x1 <= cx2))
+			dline(x1, y1, x2, y2, thick);
+		return;
+	}
+
+	if (x2 < x1) { /* make sure x2 > x1 */
+		int tmp;
+		tmp = x1;
+		x1 = x2;
+		x2 = tmp;
+		tmp = y1;
+		y1 = y2;
+		y2 = tmp;
+	}
+
+	/* clip */
+	if (x2 > cx2) {
+		y2 = (double)(cx2 - x1) / (double)(x2 - x1) * (double)(y2 - y1) + y1;
+		x2 = cx2;
+	}
+	if (x1 < cx1) {
+		y1 = (double)(cx1 - x1) / (double)(x2 - x1) * (double)(y2 - y1) + y1;
+		x1 = cx1;
+	}
+
+	dline(x1, y1, x2, y2, thick);
+}
+
+
+/* Draw a 45-deg hactched box wihtout the rectangle;
+   if step > 0 use \ hatching, else use / */
+static void hatch_box(int x1, int y1, int x2, int y2, float thick, int step)
+{
+	int n, h = y2 - y1;
+
+	if (step > 0)
+		for(n = x1 - h; n <= x2; n += step)
+			dline_vclip(n, y1, n+h, y2, thick,  x1, x2);
+	else
+		for(n = x2; n >= x1-h; n += step)
+			dline_vclip(n+h, y1, n, y2, thick,  x1, x2);
+}
+
+/* draw a hatched rectangle; to turn off hatching in a directon set the
+   corresponding step to 0 */
+static void dhrect(int x1, int y1, int x2, int y2, float thick_rect, float thick_hatch, int step_fwd, int step_back)
+{
+
+	dline(x1, y1, x2, y1, thick_rect);
+	dline(x2, y1, x2, y2, thick_rect);
+	dline(x1, y2, x2, y2, thick_rect);
+	dline(x1, y1, x1, y2, thick_rect);
+
+	if (step_fwd > 0)
+		hatch_box(x1, y1, x2, y2, thick_hatch, +step_fwd);
+
+	if (step_back > 0)
+		hatch_box(x1, y1, x2, y2, thick_hatch, -step_back);
+}
+
+
+/* Draw the cross-section layer */
+static void draw_csect(pcb_hid_gc_t gc)
+{
+	pcb_gui->set_color(gc, PCB->ElementColor);
+
+	dtext(0, 0, 500, 0, "Board cross section");
+	dhrect(0, 20, 100, 40,  1, 0.5,  5, 5);
 }
 
 
