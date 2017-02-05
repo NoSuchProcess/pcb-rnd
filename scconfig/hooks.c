09ee43ce1959055be4952d20d597c3571f1c7dd8
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-02-05 02:55:18 +0000

    [scconfig] -Fix: use -std=c99 even when not configured with --debug
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@6691 1d61de99-560f-4dba-8957-1e505d42358b

--- scconfig/hooks.c
+++ scconfig/hooks.c
@@ -281,8 +281,7 @@ int hook_detect_host()
 /* until we rewrite the generators in C */
 	require("fstools/awk",  0, 1);
 
-	if (istrue(get("/local/pcb/debug")))
-		require("cc/argstd/*", 0, 0);
+	require("cc/argstd/*", 0, 0);
 
 	require("cc/func_attr/unused/*", 0, 0);
 	require("cc/inline", 0, 0);
