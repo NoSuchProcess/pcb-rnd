ff8b0a72cdf8ebad9bdf51bc17d2739a8ded740d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-02-05 14:57:13 +0000

    [gtk] -Move: more attribute dialog code to common
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@6707 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_gtk_common/dlg_attribute.h
+++ src_plugins/lib_gtk_common/dlg_attribute.h
@@ -3,6 +3,8 @@
 
 int ghid_attribute_dialog(GtkWidget *top_window, pcb_hid_attribute_t * attrs, int n_attrs, pcb_hid_attr_val_t * results, const char *title, const char *descr);
 
+void pcb_gtk_dlg_attributes(GtkWidget *top_window, const char *owner, pcb_attribute_list_t * attrs);
+
 
 
 
