f8f781208557434fa35761661a4a7d7680e2a3e5
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-02-05 14:52:27 +0000

    [gtk] -Move: progress dialog to common
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@6705 1d61de99-560f-4dba-8957-1e505d42358b

--- scconfig/Rev.h
+++ scconfig/Rev.h
@@ -1,1 +1,1 @@
-static const int myrev = 6703;
+static const int myrev = 6705;
---
+++ src_plugins/lib_gtk_common/dlg_progress.h
@@ -0,0 +1,3 @@
+#include <gtk/gtk.h>
+
+int pcb_gtk_dlg_progress(GtkWidget *top_window, int so_far, int total, const char *message);
