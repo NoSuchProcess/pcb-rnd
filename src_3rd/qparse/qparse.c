a7607eaacc6456efadc00aa995c7021e44653267
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-02-09 05:57:23 +0000

    [qparse] -Update: copyright year
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@6786 1d61de99-560f-4dba-8957-1e505d42358b

--- src_3rd/qparse/qparse.c
+++ src_3rd/qparse/qparse.c
@@ -1,6 +1,6 @@
 /*
     libgpmi - General Package/Module Interface - qparse package
-    Copyright (C) 2006-2007 Tibor 'Igor2' Palinkas
+    Copyright (C) 2006-2007, 2017 Tibor 'Igor2' Palinkas
   
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
