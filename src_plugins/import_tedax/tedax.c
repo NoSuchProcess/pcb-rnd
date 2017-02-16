c4e530ce4bf4f5c353a0ca47489b3576d113a10a
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-02-16 06:51:07 +0000

    [import_tedax] -Add: load and apply networks and elements
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@6922 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/import_tedax/tedax.c
+++ src_plugins/import_tedax/tedax.c
@@ -28,6 +28,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <genht/htsp.h>
+#include <genht/hash.h>
+
 #include "board.h"
 #include "data.h"
 #include "error.h"
@@ -38,6 +41,7 @@
 #include "hid_actions.h"
 #include "plugins.h"
 #include "hid.h"
+
 
 static const char *tedax_cookie = "tedax importer";
 
@@ -53,28 +57,6 @@ static const char *tedax_cookie = "tedax importer";
 		if (*end == '\\') \
 			return -1; \
 	} while(0)
-
-typedef struct {
-	char *refdes;
-	char *value;
-	char *footprint;
-} symattr_t;
-
-#define null_empty(s) ((s) == NULL ? "" : (s))
-
-static void sym_flush(symattr_t *sattr)
-{
-	if (sattr->refdes != NULL) {
-/*		pcb_trace("tedax sym: refdes=%s val=%s fp=%s\n", sattr->refdes, sattr->value, sattr->footprint);*/
-		if (sattr->footprint == NULL)
-			pcb_message(PCB_MSG_ERROR, "tedax: not importing refdes=%s: no footprint specified\n", sattr->refdes);
-		else
-			pcb_hid_actionl("ElementList", "Need", null_empty(sattr->refdes), null_empty(sattr->footprint), null_empty(sattr->value), NULL);
-	}
-	free(sattr->refdes); sattr->refdes = NULL;
-	free(sattr->value); sattr->value = NULL;
-	free(sattr->footprint); sattr->footprint = NULL;
-}
 
 static int tedax_getline(FILE *f, char *buff, int buff_size, char *argv[], int argv_size)
 {
@@ -124,12 +106,30 @@ static int tedax_getline(FILE *f, char *buff, int buff_size, char *argv[], int a
 	return -1; /* can't get here */
 }
 
+#define null_empty(s) ((s) == NULL ? "" : (s))
+
+typedef struct {
+	char *value;
+	char *footprint;
+} fp_t;
+
+static void *htsp_get2(htsp_t *ht, const char *key, size_t size)
+{
+	void *res = htsp_get(ht, key);
+	if (res == NULL) {
+		res = calloc(size, 1);
+		htsp_set(ht, pcb_strdup(key), res);
+	}
+	return res;
+}
+
 static int tedax_parse_net(FILE *fn)
 {
 	char line[520];
 	char *argv[16];
 	int argc;
-	symattr_t sattr;
+	htsp_t fps;
+	htsp_entry_t *e;
 
 	/* look for the header */
 	if (tedax_getline(fn, line, sizeof(line), argv, sizeof(argv)/sizeof(argv[0])) < 2)
@@ -137,28 +137,50 @@ static int tedax_parse_net(FILE *fn)
 	if ((argv[1] == NULL) || (strcmp(argv[0], "tEDAx") != 0) || (strcmp(argv[1], "v1") != 0))
 		return -1;
 
+	htsp_init(&fps, strhash, strkeyeq);
 
-	memset(&sattr, 0, sizeof(sattr));
-
-	pcb_hid_actionl("ElementList", "start", NULL);
 	pcb_hid_actionl("Netlist", "Freeze", NULL);
 	pcb_hid_actionl("Netlist", "Clear", NULL);
 
-
 	while((argc = tedax_getline(fn, line, sizeof(line), argv, sizeof(argv)/sizeof(argv[0]))) >= 0) {
 		int n;
-		printf("line %d\n", argc);
-		for(n = 0; n < argc; n++)
-			printf(" '%s'\n", argv[n]);
-/*		argc = qparse2(s, &argv, QPARSE_DOUBLE_QUOTE | QPARSE_SINGLE_QUOTE);*/
-/*		if ((argc > 1) && (strcmp(argv[0], "COMPONENT") == 0)) {*/
+		if (strcmp(argv[0], "footprint") == 0) {
+			fp_t *fp = htsp_get2(&fps, argv[1], sizeof(fp_t));
+			fp->footprint = pcb_strdup(argv[2]);
+		}
+		else if (strcmp(argv[0], "value") == 0) {
+			fp_t *fp = htsp_get2(&fps, argv[1], sizeof(fp_t));
+			fp->value = pcb_strdup(argv[2]);
+		}
+		else if (strcmp(argv[0], "conn") == 0) {
+			char id[512];
+			sprintf(id, "%s-%s", argv[2], argv[3]);
+			pcb_hid_actionl("Netlist", "Add",  argv[1], id, NULL);
+		}
 	}
-
-	sym_flush(&sattr);
 
 	pcb_hid_actionl("Netlist", "Sort", NULL);
 	pcb_hid_actionl("Netlist", "Thaw", NULL);
+
+	pcb_hid_actionl("ElementList", "start", NULL);
+	for (e = htsp_first(&fps); e; e = htsp_next(&fps, e)) {
+		fp_t *fp = e->value;
+
+		pcb_trace("tedax fp: refdes=%s val=%s fp=%s\n", e->key, fp->value, fp->footprint);
+		if (fp->footprint == NULL)
+			pcb_message(PCB_MSG_ERROR, "tedax: not importing refdes=%s: no footprint specified\n", e->key);
+		else
+			pcb_hid_actionl("ElementList", "Need", null_empty(e->key), null_empty(fp->footprint), null_empty(fp->value), NULL);
+
+		free(e->key);
+		free(fp->value);
+		free(fp->footprint);
+		free(fp);
+	}
+
 	pcb_hid_actionl("ElementList", "Done", NULL);
+
+	htsp_uninit(&fps);
 
 	return 0;
 }
