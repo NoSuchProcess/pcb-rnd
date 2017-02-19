61964705e2f303b71bb2e39e74419c1e98d37aac
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-02-19 14:35:05 +0000

    [font] -Add: draw_fontsel infra
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@7049 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src_plugins/draw_fontsel/draw_fontsel.c
@@ -0,0 +1,54 @@
+/*
+ *                            COPYRIGHT
+ *
+ *  pcb-rnd, interactive printed circuit board design
+ *  Copyright (C) 2017 Tibor 'Igor2' Palinkas
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  Contact addresses for paper mail and Email:
+ *  Thomas Nau, Schlehenweg 15, 88471 Baustetten, Germany
+ *  Thomas.Nau@rz.uni-ulm.de
+ *
+ */
+
+/* printing routines */
+#include "config.h"
+
+#include <time.h>
+
+#include "board.h"
+#include "build_run.h"
+#include "data.h"
+#include "draw.h"
+#include "obj_all.h"
+#include "plugins.h"
+/*#include "stub_draw_fontsel.h"*/
+
+#include "obj_text_draw.h"
+
+static void pcb_draw_fontsel(pcb_hid_gc_t gc)
+{
+}
+
+static void hid_draw_fontsel_uninit(void)
+{
+}
+
+pcb_uninit_t hid_draw_fontsel_init(void)
+{
+/*	pcb_stub_draw_fontsel = DrawFab;*/
+	return hid_draw_fontsel_uninit;
+}
