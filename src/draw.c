a06df0598f5c370da53ec8cbf2390d98706d4643
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-02-20 19:09:47 +0000

    [font] -Fix: missing else made draw code think fontsel can't be drawn
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@7087 1d61de99-560f-4dba-8957-1e505d42358b

--- src/draw.c
+++ src/draw.c
@@ -688,7 +688,7 @@ void pcb_hid_expose_layer(pcb_hid_t *hid, const pcb_hid_expose_ctx_t *e)
 			pcb_gui->end_layer();
 		}
 	}
-	if (lflg & PCB_LYT_FONTSEL) {
+	else if (lflg & PCB_LYT_FONTSEL) {
 		if ((pcb_layer_gui_set_vlayer(PCB_VLY_FONTSEL, 0)) || (e->force)) {
 			pcb_stub_draw_fontsel(Output.fgGC);
 			pcb_gui->end_layer();
