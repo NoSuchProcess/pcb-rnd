e81a7beb7c3f503cfdbc21f3675d0e66780d11f1
erich <erich@1d61de99-560f-4dba-8957-1e505d42358b>
2017-02-23 14:11:12 +0000

    [export_fidocadj] Fine tuned fidocadj text export angle, dimensions, placement
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@7150 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/export_fidocadj/fidocadj.c
+++ src_plugins/export_fidocadj/fidocadj.c
@@ -241,14 +241,22 @@ static void fidocadj_do_export(pcb_hid_attr_val_t * options)
 			pcb_coord_t x0 = text->X, sx = text->BoundingBox.X2 - text->BoundingBox.X1;
 			pcb_coord_t y0 = text->Y, sy = text->BoundingBox.Y2 - text->BoundingBox.Y1;
 			switch(text->Direction) {
-				case 0: x0 += sx; break;
-				case 1: y0 += sy; break;
-				case 2: x0 -= sx; break;
-				case 3: y0 -= sy; break;
+				case 0:
+					/*x0 += sx;*/
+					break;
+				case 1: 
+					/*y0 += sy;*/
+					break;
+				case 2: 
+					/*x0 -= sx;*/
+					break;
+				case 3: 
+					/*y0 -= sy;*/
+					break;
 			}
-			fprintf(f, "TY %ld %ld %ld %ld %d 0 %d * ",
-				crd(x0), crd(y0), crd(sx), crd(sy),
-				text->Direction, fidoly);
+			fprintf(f, "TY %ld %ld %ld %ld %d 1 %d * ", /* 1 = bold */
+				crd(x0), crd(y0), 2*crd(sy)/5, 2*crd(sx)/5, /* scaling voodoo */
+				90*(text->Direction), fidoly);
 			for(s = text->TextString; *s != '\0'; s++) {
 				if (*s == ' ')
 					fprintf(f, "++");
