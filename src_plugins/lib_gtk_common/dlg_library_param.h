d02a9e2a40da6b81528d5072bf773487d21d9a21
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-02-26 09:51:23 +0000

    [gtk] -Fix: more clear API doc around --help
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@7259 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_gtk_common/dlg_library_param.h
+++ src_plugins/lib_gtk_common/dlg_library_param.h
@@ -1,7 +1,7 @@
 #include "plug_footprint.h"
 #include "dlg_library.h"
 
-/** Parse the help of a parametric footprint, offer GUI to change parameters.
+/** Parse the "--help" of a parametric footprint, offer GUI to change parameters.
     If filter_txt is non-NULL, it contains initial parameters from the user.
     Returns a dynamically allocated parametric footprint command */
 char *pcb_gtk_library_param_ui(pcb_gtk_library_t *library_window, pcb_fplibrary_t *entry, const char *filter_txt);
