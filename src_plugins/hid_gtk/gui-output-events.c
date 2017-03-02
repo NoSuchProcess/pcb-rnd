3c9092d34ab5edba0f2c08277e93475ca6201b6d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-03-02 07:13:27 +0000

    [gtk] -Change: copyright norice on gui-output-events: the remaining few functions are all config event handlers written by me earlier
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@7355 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/hid_gtk/gui-output-events.c
+++ src_plugins/hid_gtk/gui-output-events.c
@@ -3,6 +3,7 @@
  *
  *  PCB, interactive printed circuit board design
  *  Copyright (C) 1994,1995,1996,1997,1998,1999 Thomas Nau
+ *  pcb-rnd Copyright (C) 2016, 2017 Tibor 'Igor2' Palinkas
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -23,8 +24,6 @@
  *  Thomas.Nau@rz.uni-ulm.de
  *
  */
-
-/* This file written by Bill Wilson for the PCB Gtk port */
 
 #include "config.h"
 #include "conf_core.h"
