605f13b80a59eb7269bded2479a0836bcfc60b0f
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-03-05 10:32:12 +0000

    [tedax] -Add: code to write pads (no geo yet)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@7440 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_tedax/footprint.c
+++ src_plugins/io_tedax/footprint.c
@@ -23,14 +23,56 @@
  */
 
 #include "config.h"
+#include <stdio.h>
 
 #include "footprint.h"
 
 #include "unit.h"
+#include "error.h"
+#include "data.h"
+#include "board.h"
+#include "obj_elem.h"
+
+static int fp_save(pcb_data_t *data, const char *fn)
+{
+	FILE *f;
+	f = fopen(fn, "w");
+	if (f == NULL) {
+		pcb_message(PCB_MSG_ERROR, "can't open %s for writing\n", fn);
+		return -1;
+	}
+	fprintf(f, "tedax v1\n");
+
+	PCB_ELEMENT_LOOP(data)
+	{
+		fprintf(f, "begin footprint v1 %s\n", element->Name->TextString);
+		PCB_PAD_LOOP(element)
+		{
+			char *lloc, *pnum = pad->Number, buff[64];
+			lloc="??";
+			if ((pnum == NULL) || (*pnum == '\0')) {
+				sprintf(buff, "%p", (void *)pad);
+				pnum = buff;
+				if (pnum[1] == 'x')
+					pnum+=2;
+			}
+			fprintf(f, "term %s %s signal %s\n", pnum, pnum, pad->Name);
+			fprintf(f, "polygon %s copper %s ---\n", lloc, pnum);
+		}
+		PCB_END_LOOP;
+		fprintf(f, "end footprint\n");
+		break;
+	}
+	PCB_END_LOOP;
+
+	fclose(f);
+
+	return 0;
+}
 
 const char pcb_acts_Savetedax[] = "SaveTedax(type, filename)";
 const char pcb_acth_Savetedax[] = "Saves the specific type of data in a tEDAx file";
 int pcb_act_Savetedax(int argc, const char **argv, pcb_coord_t x, pcb_coord_t y)
 {
-
+	return fp_save(PCB->Data, argv[1]);
 }
