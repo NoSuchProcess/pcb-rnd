6e5c1e982be41b792d3256991da1218c86ac133f
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-03-10 08:20:51 +0000

    [gtk] -Cleanup: remove another bunch of prorotypes of hidden render functions
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@7545 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/hid_gtk/render.h
+++ src_plugins/hid_gtk/render.h
@@ -4,20 +4,6 @@
 
 void ghid_cancel_lead_user(void);
 
-void ghid_gdk_set_special_colors(conf_native_t *cfg);
-
-gboolean ghid_gdk_drawing_area_expose_cb(GtkWidget *widget, GdkEventExpose *ev, void *port);
-void ghid_gdk_port_drawing_realize_cb(GtkWidget *widget, gpointer data);
-
-int ghid_gdk_set_layer_group(pcb_layergrp_id_t group, pcb_layer_id_t layer, unsigned int flags, int is_empty);
-
-void ghid_gl_set_special_colors(conf_native_t *cfg);
-
-gboolean ghid_gl_drawing_area_expose_cb(GtkWidget *widget, GdkEventExpose *ev, void *port);
-void ghid_gl_port_drawing_realize_cb(GtkWidget *widget, gpointer data);
-
-int ghid_gl_set_layer_group(pcb_layergrp_id_t group, pcb_layer_id_t layer, unsigned int flags, int is_empty);
-
 /* the only entry points we should see */
 void ghid_gdk_install(pcb_gtk_common_t *common, pcb_hid_t *hid);
 void ghid_gl_install(pcb_gtk_common_t *common, pcb_hid_t *hid);
