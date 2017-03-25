840d69fdd6428a0391a74687c69bb7d90c8d9701
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-03-25 10:37:27 +0000

    [eagle] -Add: dispatch-parse root and <drawing> children
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@7869 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_eagle/read.c
+++ src_plugins/io_eagle/read.c
@@ -35,6 +35,64 @@
 #include "conf.h"
 #include "error.h"
 
+typedef struct read_state_s {
+	xmlDoc *doc;
+	xmlNode *root;
+	pcb_board_t *pcb;
+} read_state_t;
+
+typedef struct {
+	const char *node_name;
+	int (*parser)(read_state_t *st, xmlNode *subtree);
+} dispatch_t;
+
+/* Search the dispatcher table for subtree->str, execute the parser on match
+   with the children ("parameters") of the subtree */
+static int eagle_dispatch(read_state_t *st, xmlNode *subtree, const dispatch_t *disp_table)
+{
+	const dispatch_t *d;
+	const xmlChar *name;
+
+	/* do not tolerate empty/NIL node */
+	if (subtree->name == NULL)
+		return -1;
+
+	if (subtree->type == XML_TEXT_NODE)
+		name = "@text";
+	else
+		name = subtree->name;
+
+	for(d = disp_table; d->node_name != NULL; d++)
+		if (xmlStrcmp((xmlChar *)d->node_name, name) == 0)
+			return d->parser(st, subtree->children);
+
+	pcb_message(PCB_MSG_ERROR, "eagle: unknown node: '%s'\n", name);
+	/* node name not found in the dispatcher table */
+	return -1;
+}
+
+/* Take each children of tree and execute them using eagle_dispatch
+   Useful for procssing nodes that may host various subtrees of different
+   nodes ina  flexible way. Return non-zero if any subtree processor failed. */
+static int eagle_foreach_dispatch(read_state_t *st, xmlNode *tree, const dispatch_t *disp_table)
+{
+	xmlNode *n;
+
+	for(n = tree; n != NULL; n = n->next)
+		if (eagle_dispatch(st, n, disp_table) != 0)
+			return -1;
+
+	return 0; /* success */
+}
+
+/* No-op: ignore the subtree */
+static int eagle_read_nop(read_state_t *st, xmlNode *subtree)
+{
+	return 0;
+}
+
+
+
 int io_eagle_test_parse_pcb(pcb_plug_io_t *ctx, pcb_board_t *Ptr, const char *Filename, FILE *f)
 {
 	char line[1024];
@@ -53,13 +111,34 @@ int io_eagle_test_parse_pcb(pcb_plug_io_t *ctx, pcb_board_t *Ptr, const char *Fi
 	return 0;
 }
 
-int io_eagle_read_pcb(pcb_plug_io_t *ctx, pcb_board_t *Ptr, const char *Filename, conf_role_t settings_dest)
+
+static int eagle_read_drawing(read_state_t *st, xmlNode *subtree)
+{
+	static const dispatch_t disp[] = { /* possible children of <drawing> */
+		{"settings",  eagle_read_nop},
+		{"grid",      eagle_read_nop},
+		{"layers",    eagle_read_nop},
+		{"board",     eagle_read_nop},
+		{"@text",     eagle_read_nop},
+		{NULL, NULL}
+	};
+	return eagle_foreach_dispatch(st, subtree->children, disp);
+}
+
+int io_eagle_read_pcb(pcb_plug_io_t *ctx, pcb_board_t *pcb, const char *Filename, conf_role_t settings_dest)
 {
 	xmlDoc *doc;
 	xmlNode *root;
 	xmlChar *ver;
 	char *end;
-	int v1, v2, v3;
+	int v1, v2, v3, res;
+	read_state_t st;
+	static const dispatch_t disp[] = { /* possible children of root */
+		{"drawing",        eagle_read_drawing},
+		{"compatibility",  eagle_read_nop},
+		{"@text",          eagle_read_nop},
+		{NULL, NULL}
+	};
 
 	doc = xmlReadFile(Filename, NULL, 0);
 	if (doc == NULL) {
@@ -100,8 +179,13 @@ int io_eagle_read_pcb(pcb_plug_io_t *ctx, pcb_board_t *Ptr, const char *Filename
 	}
 	pcb_message(PCB_MSG_DEBUG, "Loading eagle board version %d.%d.%d\n", v1, v2, v3);
 
+	st.doc = doc;
+	st.root = root;
+	st.pcb = pcb;
 
-	pcb_trace("Houston, the Eagle has landed.\n");
+	res = eagle_foreach_dispatch(&st, root->children, disp);
+
+	pcb_trace("Houston, the Eagle has landed. %d\n", res);
 	goto err; /* until we really parse */
 
 	xmlFreeDoc(doc);
