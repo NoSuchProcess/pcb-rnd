26d0e9150f70c61cac906e6a35d587ba7a08231b
avigne <avigne@1d61de99-560f-4dba-8957-1e505d42358b>
2017-04-17 09:44:28 +0000

    [gtk3] -Fix: typo.
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@8313 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/hid_gtk3_cairo/gtkhid-main.c
+++ src_plugins/hid_gtk3_cairo/gtkhid-main.c
@@ -33,7 +33,7 @@ void gtk3_cairo_parse_arguments(int *argc, char ***argv)
 	gtkhid_parse_arguments(argc, argv);
 }
 
-int pplg_check_ver_gtk3_cairo(int ver_needed) { return 0; }
+int pplg_check_ver_hid_gtk3_cairo(int ver_needed) { return 0; }
 
 void pplg_uninit_hid_gtk3_cairo(void)
 {
