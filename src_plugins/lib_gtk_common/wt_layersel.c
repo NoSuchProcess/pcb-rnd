fff812d6f7e80e4f2a5aea31b9d006412f64231e
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-04-29 05:57:21 +0000

    [layersel] -Fix: proper group len administration for the virtual group so that layers are drawn and updated when the group is open
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@8579 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_gtk_common/wt_layersel.c
+++ src_plugins/lib_gtk_common/wt_layersel.c
@@ -418,7 +418,8 @@ static void layersel_populate(pcb_gtk_layersel_t *ls)
 		pcb_gtk_ls_grp_t *lsg = &ls->lsg_virt;
 		int n;
 
-		lsg->layer = calloc(sizeof(pcb_gtk_ls_lyr_t), 5);
+		ls->grp_virt.len = 5;
+		lsg->layer = calloc(sizeof(pcb_gtk_ls_lyr_t), ls->grp_virt.len);
 		gtk_box_pack_start(GTK_BOX(ls->grp_box), build_group_start(ls, lsg, "Virtual", 0, &ls->grp_virt), FALSE, FALSE, 0);
 		gtk_box_pack_start(GTK_BOX(lsg->layers), build_layer(lsg, &lsg->layer[0], "Pins/Pads", -1, &conf_core.appearance.color.pin), FALSE, FALSE, 1);
 		gtk_box_pack_start(GTK_BOX(lsg->layers), build_layer(lsg, &lsg->layer[1], "Vias", -1, &conf_core.appearance.color.via), FALSE, FALSE, 1);
@@ -426,7 +427,7 @@ static void layersel_populate(pcb_gtk_layersel_t *ls)
 		gtk_box_pack_start(GTK_BOX(lsg->layers), build_layer(lsg, &lsg->layer[3], "Rats", -1, &conf_core.appearance.color.rat), FALSE, FALSE, 1);
 		gtk_box_pack_start(GTK_BOX(lsg->layers), build_layer(lsg, &lsg->layer[4], "All-silk", -1, &conf_core.appearance.color.element), FALSE, FALSE, 1);
 
-		for(n = 0; n < 5; n++)
+		for(n = 0; n < ls->grp_virt.len; n++)
 			lsg->layer[n].ev_selected = ev_lyr_no_select;
 
 		build_group_finish(lsg);
