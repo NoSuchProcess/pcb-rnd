609b8f3ad90ee99bf5d8476c249a293cfb9ae2cd
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-04-30 03:53:06 +0000

    [layer] -Fix: printf format string is %ld for layer id
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@8632 1d61de99-560f-4dba-8957-1e505d42358b

--- src/layer_vis.c
+++ src/layer_vis.c
@@ -97,7 +97,7 @@ int pcb_layervis_change_group_vis(pcb_layer_id_t Layer, int On, pcb_bool ChangeS
 		On = !PCB->Data->Layer[Layer].On;
 
 	if (conf_core.rc.verbose)
-		printf("pcb_layervis_change_group_vis(Layer=%d, On=%d, ChangeStackOrder=%d)\n", Layer, On, ChangeStackOrder);
+		printf("pcb_layervis_change_group_vis(Layer=%ld, On=%d, ChangeStackOrder=%d)\n", Layer, On, ChangeStackOrder);
 
 	/* decrement 'i' to keep stack in order of layergroup */
 	if ((group = pcb_layer_get_group(PCB, Layer)) >= 0) {
