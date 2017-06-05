2f9bbe58f17b7d6c0d68a797a5783fcd0168dd3e
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-06-05 13:07:41 +0000

    [gtk] -Fix: board-preview sets board extents as drawing size for proper zoom
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@9156 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_gtk_common/wt_preview.c
+++ src_plugins/lib_gtk_common/wt_preview.c
@@ -39,6 +39,7 @@
 #include "wt_preview.h"
 
 #include "copy.h"
+#include "board.h"
 #include "data.h"
 #include "draw.h"
 #include "move.h"
@@ -565,7 +566,10 @@ GtkWidget *pcb_gtk_preview_layer_new(pcb_gtk_common_t *com, pcb_gtk_init_drawing
 GtkWidget *pcb_gtk_preview_board_new(pcb_gtk_common_t *com, pcb_gtk_init_drawing_widget_t init_widget, pcb_gtk_preview_expose_t expose)
 {
 	GtkWidget *w = pcb_gtk_preview_generic_new(com, init_widget, expose, -1, NULL);
+	pcb_gtk_preview_t *p = (pcb_gtk_preview_t *)w;
 	g_object_set(G_OBJECT(w), "kind", PCB_GTK_PREVIEW_BOARD, NULL);
+	p->view.height = PCB->MaxHeight;
+	p->view.width = PCB->MaxWidth;
 	return w;
 }
 
