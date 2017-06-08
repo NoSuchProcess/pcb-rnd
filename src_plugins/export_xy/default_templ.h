58d17291e109af0e32708a0c55646ba4c386e49b
erich <erich@1d61de99-560f-4dba-8957-1e505d42358b>
2017-06-08 14:24:01 +0000

    [export_xy] added TM220A pick and place machine export
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@9236 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/export_xy/default_templ.h
+++ src_plugins/export_xy/default_templ.h
@@ -21,4 +21,18 @@ static const char *templ_gxyrs_hdr =
 	"#Designator X-Loc Y-Loc Rotation Side Type X-Size Y-Size Value Footprint\n"
 	"# --------------------------------------------\n";
 
-static const char *templ_gxyrs_elem = NULL;
+static const char *templ_gxyrs_elem =
+	"%elem.name%,%elem.x%,%elem.y%,%elem.rot%,%elem.side%,%elem.descr%\n";
+
+static const char *templ_TM220A_hdr = 
+	"#pcb-rnd TM220A xyrs version 1.0\n"
+	"#Date: %UTC%\n"
+	"#Author: %author%\n"
+	"#Title: %title% - pcb-rnd gxyrs\n"
+	"#Placement data:\n"
+	"#RefDes, top/bottom, X, Y, rotation\n"
+	"# X,Y in %suffix%.  rotation in degrees.\n"
+        "# --------------------------------------------\n";
+
+static const char *templ_TM220A_elem =
+	"%elem.name%,%elem.side%,%elem.x%,%elem.y%,%elem.rot%\n";
