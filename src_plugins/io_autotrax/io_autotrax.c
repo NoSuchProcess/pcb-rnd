fdd25b3c66ac427abf94dafee455a1545a6c1207
erich <erich@1d61de99-560f-4dba-8957-1e505d42358b>
2017-06-13 15:49:45 +0000

    [io_autotrax] preliminaries for autotrax IO
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@9470 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src_plugins/io_autotrax/io_autotrax.c
@@ -0,0 +1,115 @@
+/*
+ *                            COPYRIGHT
+ *
+ *  pcb-rnd, interactive printed circuit board design
+ *
+ *  autotrax IO plugin - plugin coordination
+ *  pcb-rnd Copyright (C) 2017 Tibor 'Igor2' Palinkas
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include "config.h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "footprint.h"
+
+#include "board.h"
+#include "netlist.h"
+#include "conf_core.h"
+#include "buffer.h"
+#include "plugins.h"
+#include "hid.h"
+#include "hid_actions.h"
+#include "action_helper.h"
+#include "compat_misc.h"
+
+
+static const char *autotrax_cookie = "autotrax IO";
+
+static const char pcb_acts_Saveautotrax[] = "SaveAutotrax(type, filename)";
+static const char pcb_acth_Saveautotrax[] = "Saves the specific type of data in an autotrax file. Type can be: board-footprints";
+
+int io_autotrax_fmt(pcb_plug_io_t *ctx, pcb_plug_iot_t typ, int wr, const char *fmt)
+{
+	if (strcmp(ctx->description, fmt) == 0)
+		return 200;
+
+	if ((strcmp(fmt, "autotra") != 0) ||
+		((typ & (~(PCB_IOT_FOOTPRINT | PCB_IOT_BUFFER | PCB_IOT_PCB))) != 0))
+		return 0;
+
+	return 100;
+}
+
+static int pcb_act_Saveautotrax(int argc, const char **argv, pcb_coord_t x, pcb_coord_t y)
+{
+	const char *fname, *type = argv[0];
+	if (argc < 1)t
+		PCB_ACT_FAIL(Saveautotrax);
+
+	fname = (argc > 1) ? argv[1] : NULL;
+
+	if (pcb_strcasecmp(type, "board-footprints") == 0)
+		return tedax_fp_save(PCB->Data, argv[1]);
+
+	PCB_ACT_FAIL(Saveautotrax);
+}
+
+PCB_REGISTER_ACTIONS(autotrax_action_list, autotrax_cookie)
+
+int pplg_check_ver_io_autotrax(int ver_needed) { return 0; }
+
+void pplg_uninit_io_autotrax(void)
+{
+	pcb_hid_remove_actions_by_cookie(autotrax_cookie);
+}
+
+#include "dolists.h"
+
+int pplg_init_io_autotrax(void)
+{
+
+	/* register the IO hook */
+	io_autotrax.plugin_data = NULL;
+	io_autotrax.fmt_support_prio = io_autotrax_fmt;
+	io_autotrax.test_parse_pcb = io_autotrax_test_parse_pcb;
+	io_autotrax.parse_pcb = io_autotrax_read_pcb;
+	io_autotrax.parse_element = NULL;
+	io_autotrax.parse_font = NULL;
+	io_autotrax.write_buffer = io_autotrax_write_buffer;
+	io_autotrax.write_element = io_autotrax_write_element;
+	io_autotrax.write_pcb = io_autotrax_write_pcb;
+	io_autotrax.default_fmt = "autotrax";
+	io_autotrax.description = "Autotrax";
+	io_autotrax.save_preference_prio = 80;
+	io_autotrax.default_extension = ".PCB";
+	io_autotrax.fp_extension = ".PCB";
+	io_autotrax.mime_type = "application/x-autotrax-pcb";
+
+
+	PCB_HOOK_REGISTER(pcb_plug_io_t, pcb_plug_io_chain, &io_autotrax);
+
+	PCB_REGISTER_ACTIONS(autotrax_action_list, autotrax_cookie);
+
+	/* TODO: Alloc plugin-globals here. */
+
+	return 0;
+}
+
