5e49f391e2757537f3da15eff454ed63a06f69a7
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-06-17 10:08:58 +0000

    [subc] -Fix: subc dup_at sets subc parent properly
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@9523 1d61de99-560f-4dba-8957-1e505d42358b

--- src/obj_subc.c
+++ src/obj_subc.c
@@ -195,6 +195,7 @@ pcb_subc_t *pcb_subc_dup_at(pcb_board_t *pcb, pcb_data_t *dst, pcb_subc_t *src, 
 	int n;
 	pcb_subc_t *sc = pcb_subc_alloc();
 	PCB_SET_PARENT(sc->data, subc, sc);
+	PCB_SET_PARENT(sc, data, dst);
 	pcb_subclist_append(&dst->subc, sc);
 
 	sc->BoundingBox.X1 = sc->BoundingBox.Y1 = PCB_MAX_COORD;
