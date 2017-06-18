a0a425df0dd6d7f2650f52d02a31550bcf383c84
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-06-18 14:41:56 +0000

    [io_eagle] -Add: license text
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@9602 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_eagle/egb_tree.c
+++ src_plugins/io_eagle/egb_tree.c
@@ -1,3 +1,31 @@
+/*
+ *														COPYRIGHT
+ *
+ *	pcb-rnd, interactive printed circuit board design
+ *	Copyright (C) 2017 Tibor 'Igor2' Palinkas
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program; if not, write to the Free Software
+ *	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *	Contact addresses for paper mail and Email:
+ *	Thomas Nau, Schlehenweg 15, 88471 Baustetten, Germany
+ *	Thomas.Nau@rz.uni-ulm.de
+ *
+ */
+
+/* Eagle binary tree store */
+
 #include <stdlib.h>
 #include <genht/hash.h>
 #include "egb_tree.h"
--- src_plugins/io_eagle/egb_tree.h
+++ src_plugins/io_eagle/egb_tree.h
@@ -1,4 +1,31 @@
-/* eagle binary file tree representation */
+/*
+ *														COPYRIGHT
+ *
+ *	pcb-rnd, interactive printed circuit board design
+ *	Copyright (C) 2017 Tibor 'Igor2' Palinkas
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program; if not, write to the Free Software
+ *	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *	Contact addresses for paper mail and Email:
+ *	Thomas Nau, Schlehenweg 15, 88471 Baustetten, Germany
+ *	Thomas.Nau@rz.uni-ulm.de
+ *
+ */
+
+/* Eagle binary tree store */
+
 #ifndef PCB_EGB_TREE_H
 #define PCB_EGB_TREE_H
 #include <stdio.h>
