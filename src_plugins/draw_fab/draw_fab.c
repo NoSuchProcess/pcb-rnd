f1be4dc1bcd018c9e818b637fcfaab10fad9c2cf
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-06-20 18:22:22 +0000

    [subc] -Fix: draw_fab considers outline in subc (rtree instead of list)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@9666 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/draw_fab/draw_fab.c
+++ src_plugins/draw_fab/draw_fab.c
@@ -259,7 +259,7 @@ static void DrawFab(pcb_hid_gc_t gc)
 	yoff = -TEXT_LINE;
 	for (found = 0, i = 0; i < pcb_max_layer; i++) {
 		pcb_layer_t *l = LAYER_PTR(i);
-		if ((pcb_layer_flags(PCB, i) & PCB_LYT_OUTLINE) && (linelist_length(&l->Line) || arclist_length(&l->Arc))) {
+		if ((pcb_layer_flags(PCB, i) & PCB_LYT_OUTLINE) && (!PCB_RTREE_EMPTY(l->line_tree) || !PCB_RTREE_EMPTY(l->arc_tree))) {
 			found = 1;
 			break;
 		}
