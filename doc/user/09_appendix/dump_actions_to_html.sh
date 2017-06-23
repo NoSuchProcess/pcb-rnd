bd8947c94b20c7cc4bb9a12891dcc048d45e1647
miloh <miloh@1d61de99-560f-4dba-8957-1e505d42358b>
2017-06-23 06:21:37 +0000

    [doc] -edit: use Posix shell shebang, remove excess comments
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@9732 1d61de99-560f-4dba-8957-1e505d42358b

--- doc/user/09_appendix/dump_actions_to_html.sh
+++ doc/user/09_appendix/dump_actions_to_html.sh
@@ -1,24 +1,11 @@
-#! /bin/bash
-# bash script to dump pcb-rnd actions and parse output a html table for the
-# docs
+#!/bin/sh
+
+# dumps pcb-rnd actions into a simple html table for the docs 
 #
 #errata: pcb_dump_actions function in src/hid_actions.c
-	#printf("A%s\n", ca->action->name);
-	#dump_string('D', desc);
-	#dump_string('S', synt);
-
-# we want to sort the pcb-rnd actions and process them into a captioned html
-# table with three header columns: Action Name, Description, Syntax 
-#
-# if the line starts with "A", strip the letter and enclose the rest of the
-# line in a <tr> tag
-# if the next line starts with a D, strip the letter and enclose the rest of
-# the line in a <td> tag, add newline and start the <td> tag for any syntax content
-# any following lines that start with an S are printed in the <td> 
-# repeat
-
-#TODO: use pcb-rnd version used to generate the table in table header 
-#TODO: fix escape parsing in syntax sections 
+#        printf("A%s\n", ca->action->name);
+#        dump_string('D', desc);
+#        dump_string('S', synt);
 
 cat table_opener.html > action_reference.html 
 
