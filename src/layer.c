10be61a085c9cc9770fc630f2feacc0561f056a4
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-06-27 06:35:09 +0000

    [layer] -Fix: don't segfault on resolving a bound layer ID without binding, rather assert
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@9867 1d61de99-560f-4dba-8957-1e505d42358b

--- src/layer.c
+++ src/layer.c
@@ -159,7 +159,7 @@ pcb_layer_id_t pcb_layer_id(pcb_data_t *Data, pcb_layer_t *Layer)
 {
 	if (Layer->parent != Data) {
 		/* the only case this makes sense is when we are resolving a bound layer */
-		if (!PCB_LAYER_IS_REAL(Layer))
+		if ((!PCB_LAYER_IS_REAL(Layer)) && (Layer->meta.bound.real != NULL))
 			return pcb_layer_id(Data, Layer->meta.bound.real);
 		assert(!"pcb_layer_id: invalid layer id for bound layer - pelase report this bug");
 		return -1;
