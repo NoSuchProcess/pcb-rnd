a638251eed1bcbc01de4e39533df83fca08f3c84
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-07-06 18:21:14 +0000

    [io_pcb] -Fix: make sure there's a top copper layer and a bottom copper layer when loading an .fp file directly (reported by celem)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@10096 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_pcb/file.c
+++ src_plugins/io_pcb/file.c
@@ -675,6 +675,15 @@ void PostLoadElementPCB()
 	yyPCB->MaxWidth = e->BoundingBox.X2;
 	yyPCB->MaxHeight = e->BoundingBox.Y2;
 	yyPCB->is_footprint = 1;
+
+	{
+		pcb_layergrp_id_t gid;
+		assert(pcb_layergrp_list(PCB, PCB_LYT_TOP | PCB_LYT_COPPER, &gid, 1) == 1);
+		pcb_layer_create(gid, "top copper");
+		assert(pcb_layergrp_list(PCB, PCB_LYT_BOTTOM | PCB_LYT_COPPER, &gid, 1) == 1);
+		pcb_layer_create(gid, "bottom copper");
+	}
+
 }
 
 
