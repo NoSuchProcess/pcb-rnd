b3daf79df78c91ff3be6d9856398457561cf6816
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-07-08 15:33:38 +0000

    [core] -Fix: RipUp element doesn't read crosshair directly but asks the GUI (so it works from menu)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@10191 1d61de99-560f-4dba-8957-1e505d42358b

--- src/object_act.c
+++ src/object_act.c
@@ -803,8 +803,9 @@ static int pcb_act_RipUp(int argc, const char **argv, pcb_coord_t x, pcb_coord_t
 		case F_Element:
 			{
 				void *ptr1, *ptr2, *ptr3;
-
-				if (pcb_search_screen(pcb_crosshair.X, pcb_crosshair.Y, PCB_TYPE_ELEMENT, &ptr1, &ptr2, &ptr3) != PCB_TYPE_NONE) {
+				pcb_coord_t gx, gy;
+				pcb_gui->get_coords("Click on an element", &gx, &gy);
+				if (pcb_search_screen(gx, gy, PCB_TYPE_ELEMENT, &ptr1, &ptr2, &ptr3) != PCB_TYPE_NONE) {
 					Note.Buffer = conf_core.editor.buffer_number;
 					pcb_buffer_set_number(PCB_MAX_BUFFER - 1);
 					pcb_buffer_clear(PCB, PCB_PASTEBUFFER);
