2188e55e0078cc84febfd409301131062638d842
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-07-24 03:54:53 +0000

    [io_lihata] -Change: mark .pcb file name pattern an obsolete setting
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@10408 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_lihata/lht_conf.h
+++ src_plugins/io_lihata/lht_conf.h
@@ -6,7 +6,7 @@
 typedef struct {
 	const struct plugins {
 		const struct io_lihata {
-			CFT_STRING aux_pcb_pattern;  /* file name pattern to use when generating the .pcb backup */
+			CFT_STRING aux_pcb_pattern;  /* [obsolete] file name pattern to use when generating the .pcb backup */
 		} io_lihata;
 	} plugins;
 } conf_io_lihata_t;
