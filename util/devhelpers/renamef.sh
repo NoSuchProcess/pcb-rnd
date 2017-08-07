3fdbe68c20af8d3deeb48311062a290ed3b383f4
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-08-07 07:04:36 +0000

    [util] -Fix: rename function script: don't rename struct members
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@10641 1d61de99-560f-4dba-8957-1e505d42358b

--- util/devhelpers/renamef.sh
+++ util/devhelpers/renamef.sh
@@ -2,7 +2,7 @@
 
 # Rename a function and log
 
-./rename.sh "s/\\([^a-zA-Z0-9_]\\)${1}[ \t]*[(]/\\1$2(/g"
+./rename.sh "s/\\([^a-zA-Z0-9_>.]\\)${1}[ \t]*[(]/\\1$2(/g"
 ./rename.sh "s/^${1}[ \t]*[(]/$2(/g"
 
 echo "$1 -> $2" >> doc/developer/renames
