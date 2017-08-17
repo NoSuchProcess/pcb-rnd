3cdd522125d98ea795f948600cd058256c5bca99
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-08-17 04:11:22 +0000

    [loghid] -Fix: help text references to the gtk gui hid with its full name
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@10874 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/loghid/loghid.c
+++ src_plugins/loghid/loghid.c
@@ -86,7 +86,7 @@ static int loghid_usage(const char *topic)
 	fprintf(stderr, "\nloghid command line arguments:\n\n");
 	pcb_hid_usage(loghid_attribute_list, NUM_OPTIONS);
 	fprintf(stderr, "\n");
-	fprintf(stderr, "Usage: pcb-rnd [generic_options] --gui loghid-gui --target-hid gtk foo.pcb\n");
+	fprintf(stderr, "Usage: pcb-rnd [generic_options] --gui loghid-gui --target-hid gtk2_gdk foo.pcb\n");
 	fprintf(stderr, "Usage: pcb-rnd [generic_options] --x loghid-exp --target-hid png foo.pcb\n");
 	fprintf(stderr, "\n");
 	return 0;
