16116d7cf70fc95d731dba7347e1c44e2ee266f7
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-08-28 17:57:09 +0000

    [export_dxf] -Add: low level drawing functions
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@11127 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src_plugins/export_dxf/dxf_draw.c
@@ -0,0 +1,50 @@
+static void dxf_draw_handle(dxf_ctx_t *ctx)
+{
+	ctx->handle++;
+	fprintf(ctx->f, "5\n%lu\n", ctx->handle);
+}
+
+static void dxf_draw_line_props(dxf_ctx_t *ctx)
+{
+	fprintf(ctx->f, "100\nAcDbEntity\n");
+	fprintf(ctx->f, "8\n0\n"); /* layer name */
+	fprintf(ctx->f, "6\nByLayer\n"); /* linetype name */
+	fprintf(ctx->f, "62\n256\n"); /* color; 256=ByLayer */
+	fprintf(ctx->f, "370\n-1\n"); /* lineweight enum */
+}
+
+static void dxf_draw_line(dxf_ctx_t *ctx, pcb_line_t *l)
+{
+	pcb_coord_t z = 20;
+	fprintf(ctx->f, "0\nLINE\n");
+	dxf_draw_handle(ctx);
+	dxf_draw_line_props(ctx);
+	fprintf(ctx->f, "100\nAcDbLine\n");
+	pcb_fprintf(ctx->f, "10\n%mm %mm %mm\n", l->Point1.X, z, l->Point1.Y);
+	pcb_fprintf(ctx->f, "11\n%mm %mm %mm\n", l->Point2.X, z, l->Point2.Y);
+}
+
+static void dxf_draw_circle(dxf_ctx_t *ctx, pcb_coord_t x, pcb_coord_t y, pcb_coord_t r)
+{
+	pcb_coord_t z = 20;
+	fprintf(ctx->f, "0\nCIRCLE\n");
+	dxf_draw_handle(ctx);
+	dxf_draw_line_props(ctx);
+	fprintf(ctx->f, "100\nAcDbCircle\n");
+	pcb_fprintf(ctx->f, "10\n%mm %mm %mm\n", x, z, y);
+	pcb_fprintf(ctx->f, "30\n%mm\n", r);
+}
+
+static void dxf_draw_arc(dxf_ctx_t *ctx, pcb_arc_t *arc)
+{
+	pcb_coord_t z = 20;
+	fprintf(ctx->f, "0\nARC\n");
+	dxf_draw_handle(ctx);
+	dxf_draw_line_props(ctx);
+	fprintf(ctx->f, "100\nAcDbCircle\n");
+	pcb_fprintf(ctx->f, "10\n%mm %mm %mm\n", arc->X, z, arc->Y);
+	pcb_fprintf(ctx->f, "30\n%mm\n", (arc->Width + arc->Height)/2);
+	fprintf(ctx->f, "100\nAcDbArc\n");
+	fprintf(ctx->f, "50\n%f\n", arc->StartAngle);
+	fprintf(ctx->f, "51\n%f\n", arc->Delta);
+}
