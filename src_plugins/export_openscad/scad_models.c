37f1b55138ade1f8d7a0036947349020867e3770
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-09-01 14:50:16 +0000

    [openscad] -Add: license banners
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@11211 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/export_openscad/scad_models.c
+++ src_plugins/export_openscad/scad_models.c
@@ -1,3 +1,25 @@
+ /*
+  *                            COPYRIGHT
+  *
+  *  pcb-rnd, interactive printed circuit board design
+  *  Copyright (C) 2017 Tibor 'Igor2' Palinkas
+  *
+  *  This program is free software; you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation; either version 2 of the License, or
+  *  (at your option) any later version.
+  *
+  *  This program is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *  GNU General Public License for more details.
+  *
+  *  You should have received a copy of the GNU General Public License
+  *  along with this program; if not, write to the Free Software
+  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+  *
+  */
+
 static void scad_insert_model(htsp_t *models, const char *name, pcb_coord_t x0, pcb_coord_t y0)
 {
 	FILE *fin;
