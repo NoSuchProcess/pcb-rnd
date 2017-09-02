657023104d16ec1f4c966cf4e5e754b0c80db701
avigne <avigne@1d61de99-560f-4dba-8957-1e505d42358b>
2017-09-02 14:05:36 +0000

    [gtk3] -Cleanup: use_mask() can be removed now.
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@11258 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/hid_gtk3_cairo/gtkhid-cairo.c
+++ src_plugins/hid_gtk3_cairo/gtkhid-cairo.c
@@ -628,64 +628,6 @@ static void ghid_cairo_set_drawing_mode(pcb_composite_op_t op, pcb_bool direct, 
 
 static void ghid_cairo_use_mask(pcb_mask_op_t use_it)
 {
-	static int mask_seq_id = 0;
-	//GdkRGBA color;
-	render_priv_t *priv = gport->render_priv;
-
-	//if (use_it == cur_mask)
-		return;
-
-	switch (use_it) {
-	case HID_MASK_OFF:
-		//priv->drawable = priv->pixmap;
-		mask_seq = 0;
-		break;
-
-	case HID_MASK_BEFORE:
-		/* The HID asks not to receive this mask type, so warn if we get it */
-		g_return_if_reached();
-
-	case HID_MASK_INIT:
-		start_subcomposite();
-		//ghid_mask_setup(priv);
-		mask_seq = 0;
-
-		/* clear the mask */
-		//color.alpha = 0.0;
-		//gdk_gc_set_foreground(priv->mask_gc, &color);
-		//gdk_draw_rectangle(priv->drawable, priv->mask_gc, TRUE, 0, 0, gport->view.canvas_width, gport->view.canvas_height);
-		break;
-
-	case HID_MASK_CLEAR:
-		cairo_set_operator(priv->cr, CAIRO_OPERATOR_CLEAR);
-		//color.alpha = 0.0;
-		//gdk_gc_set_foreground(priv->mask_gc, &color);
-		break;
-
-	case HID_MASK_SET:
-		cairo_set_operator(priv->cr, CAIRO_OPERATOR_SOURCE);
-		//color.alpha = 1.0;
-		//gdk_gc_set_foreground(priv->mask_gc, &color);
-		break;
-
-	case HID_MASK_AFTER:
-		priv->cr = priv->cr_target;
-		//cairo_set_operator(priv->cr, CAIRO_OPERATOR_OVER);
-		//cairo_set_source_surface(priv->cr, priv->surf_layer, 0, 0);
-		cairo_mask_surface(priv->cr, priv->surf_layer, 0, 0);
-		cairo_fill(priv->cr);
-		//cairo_paint_with_alpha(priv->cr, conf_core.appearance.layer_alpha);
-		//cairo_paint_with_alpha(priv->cr, 1.0);
-
-		mask_seq_id++;
-		if (!mask_seq_id)
-			mask_seq_id = 1;
-		mask_seq = mask_seq_id;
-
-		//priv->drawable = priv->pixmap;
-		break;
-	}
-	cur_mask = use_it;
 }
 
 
