7c4ee7c9e7611c73db79bf91f3b4b5b5f15e4c7a
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-09-05 07:16:40 +0000

    [hid] -Add: DAD: dynamic attribute dialog macros
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@11331 1d61de99-560f-4dba-8957-1e505d42358b

--- src/hid_attrib.h
+++ src/hid_attrib.h
@@ -1,6 +1,7 @@
 #ifndef PCB_HID_ATTRIB_H
 #define PCB_HID_ATTRIB_H
 
+#include <assert.h>
 #include "hid.h"
 
 /* Used for HID attributes (exporting and printing, mostly).
@@ -43,8 +44,9 @@ typedef enum pcb_hids_e {
 #define PCB_HATT_IS_COMPOSITE(type) \
 	(((type) >= PCB_HATT_BEGIN_HBOX) && ((type) < PCB_HATT_END))
 
-/* field name in struct pcb_hid_attribute_s */
-#define pcb_hatt_flags max_val
+/* alternative field names in struct pcb_hid_attribute_s */
+#define pcb_hatt_flags       max_val
+#define pcb_hatt_table_cols  min_val
 
 struct pcb_hid_attribute_s {
 	const char *name;
@@ -90,4 +92,120 @@ extern pcb_hid_attr_node_t *hid_attr_nodes;
 
 void pcb_hid_usage(pcb_hid_attribute_t * a, int numa);
 void pcb_hid_usage_option(const char *name, const char *help);
+
+
+/* Helpers for building dynamic attribute dialogs (DAD) */
+#define PCB_DAD_DECL(table) \
+	pcb_hid_attribute_t *table = NULL; \
+	int table ## _len = 0; \
+	int table ## _alloced = 0;
+
+#define PCB_DAD_FREE(table) \
+do { \
+	int __n__; \
+	for(__n__ = 0; __n__ < table ## _len; __n__++) { \
+		PCB_DAD_FREE_FIELD(table, __n__); \
+	} \
+	free(table); \
+} while(0)
+
+
+#define PCB_DAD_BEGIN(table, item_type) \
+	PCB_DAD_ALLOC(table, item_type);
+
+#define PCB_DAD_BEGIN_TABLE(table, cols) \
+do { \
+	PCB_DAD_BEGIN(table, PCB_HATT_BEGIN_TABLE) \
+	PCB_DAD_SET(table, pcb_hatt_table_cols, cols) \
+} while(0)
+
+#define PCB_DAD_BEGIN_HBOX(table)      PCB_DAD_BEGIN(table, PCB_HATT_BEGIN_HBOX)
+#define PCB_DAD_BEGIN_VBOX(table)      PCB_DAD_BEGIN(table, PCB_HATT_BEGIN_VBOX)
+#define PCB_DAD_END(table)             PCB_DAD_BEGIN(table, PCB_HATT_END)
+#define PCB_DAD_COMPFLAG(table, val)   PCB_DAD_SET(table, pcb_hatt_flags, val)
+
+#define PCB_DAD_LABEL(table, text) \
+do { \
+	PCB_DAD_ALLOC(table, PCB_HATT_LABEL); \
+	PCB_DAD_SET(table, name, text); \
+} while(0);
+
+#define PCB_DAD_INTEGER(table, label) \
+do { \
+	PCB_DAD_ALLOC(table, PCB_HATT_INTEGER); \
+	PCB_DAD_SET(table, name, label); \
+} while(0);
+
+#define PCB_DAD_MINVAL(table, val)       PCB_DAD_SET(table, min_val, val)
+#define PCB_DAD_MAXVAL(table, val)       PCB_DAD_SET(table, max_val, val)
+#define PCB_DAD_DEFAULT(table, val)      PCB_DAD_SET_VAL(table, default_val, val)
+#define PCB_DAD_MINMAX(table, min, max)  (PCB_DAD_SET(table, min_val, min),PCB_DAD_SET(table, max_val, max))
+
+
+/* DAD internals (do not use directly) */
+#define PCB_DAD_ALLOC(table, item_type) \
+	do { \
+		if (table ## _len >= table ## _alloced) { \
+			table ## _alloced += 16; \
+			table = realloc(table, sizeof(table[0]) * table ## _alloced); \
+		} \
+		memset(&table[table ## _len], 0, sizeof(table[0])); \
+		table[table ## _len].type = item_type; \
+		table ## _len++; \
+	} while(0);
+
+#define PCB_DAD_SET(table, field, value) \
+	table[table ## _len - 1].field = (value)
+
+#define PCB_DAD_SET_VAL(table, field, val) \
+do { \
+	switch(table[table ## _len - 1].type) { \
+		case PCB_HATT_LABEL: \
+			assert(0); \
+			break; \
+		case PCB_HATT_INTEGER: \
+		case PCB_HATT_BOOL: \
+		case PCB_HATT_ENUM: \
+		case PCB_HATT_MIXED: \
+		case PCB_HATT_COORD: \
+		case PCB_HATT_UNIT: \
+			table[table ## _len - 1].field.int_value = (int)val; \
+			break; \
+		case PCB_HATT_REAL: \
+			table[table ## _len - 1].field.real_value = (double)val; \
+			break; \
+		case PCB_HATT_STRING: \
+		case PCB_HATT_PATH: \
+			table[table ## _len - 1].field.str_value = (char *)val; \
+			break; \
+		case PCB_HATT_BEGIN_HBOX: \
+		case PCB_HATT_BEGIN_VBOX: \
+		case PCB_HATT_BEGIN_TABLE: \
+		case PCB_HATT_END: \
+			assert(0); \
+	} \
+} while(0)
+
+#define PCB_DAD_FREE_FIELD(table, field) \
+do { \
+	switch(table[table ## _len - 1].type) { \
+		case PCB_HATT_LABEL: \
+		case PCB_HATT_INTEGER: \
+		case PCB_HATT_BOOL: \
+		case PCB_HATT_ENUM: \
+		case PCB_HATT_MIXED: \
+		case PCB_HATT_COORD: \
+		case PCB_HATT_UNIT: \
+		case PCB_HATT_REAL: \
+		case PCB_HATT_STRING: \
+		case PCB_HATT_PATH: \
+			break; \
+		case PCB_HATT_BEGIN_HBOX: \
+		case PCB_HATT_BEGIN_VBOX: \
+		case PCB_HATT_BEGIN_TABLE: \
+		case PCB_HATT_END: \
+			break; \
+	} \
+} while(0)
+
 #endif
