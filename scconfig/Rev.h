7ec2bfb6626905fc24677f6dbf1ee394737de4ef
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-09-11 03:43:24 +0000

    [dialogs] -Add: new plugin for moving dialog boxes to
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@11438 1d61de99-560f-4dba-8957-1e505d42358b

--- scconfig/Rev.h
+++ scconfig/Rev.h
@@ -1,1 +1,1 @@
-static const int myrev = 11396;
+static const int myrev = 11436;
---
+++ src_plugins/dialogs/dialogs.c
@@ -0,0 +1,62 @@
+/*
+ *                            COPYRIGHT
+ *
+ *  pcb-rnd, interactive printed circuit board design
+ *  Copyright (C) 2017 Tibor 'Igor2' Palinkas
+ *
+ *  This module, dialogs, was written and is Copyright (C) 2017 by Tibor Palinkas
+ *  this module is also subject to the GNU GPL as described below
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include "config.h"
+#include "hid.h"
+#include "hid_attrib.h"
+#include "hid_dad.h"
+
+/*
+static const char d1_syntax[] = "d1()\n";
+static const char d1_help[] = "debug action for development";
+static int pcb_act_d1(int argc, const char **argv, pcb_coord_t x, pcb_coord_t y)
+{
+	printf("D1!\n");
+	return 0;
+}
+*/
+
+pcb_hid_action_t dialogs_action_list[] = {
+/*	{"integrity", 0, pcb_act_integrity,
+	 integrity_help, integrity_syntax}*/
+};
+
+static const char *dialogs_cookie = "dialogs plugin";
+
+PCB_REGISTER_ACTIONS(dialogs_action_list, dialogs_cookie)
+
+int pplg_check_ver_dialogs(int ver_needed) { return 0; }
+
+void pplg_uninit_dialogs(void)
+{
+	pcb_hid_remove_actions_by_cookie(dialogs_cookie);
+}
+
+#include "dolists.h"
+int pplg_init_dialogs(void)
+{
+	PCB_REGISTER_ACTIONS(dialogs_action_list, dialogs_cookie)
+	return 0;
+}
