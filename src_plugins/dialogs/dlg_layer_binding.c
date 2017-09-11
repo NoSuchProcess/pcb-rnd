60f1d745b158ab3cb083164b52d507406f0e7a6e
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-09-11 05:57:06 +0000

    [subc] -Add: layer binding dialog: save data and subc pointer in the shared context
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@11449 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/dialogs/dlg_layer_binding.c
+++ src_plugins/dialogs/dlg_layer_binding.c
@@ -38,14 +38,17 @@ typedef struct {
 typedef struct {
 	const char **layer_names;
 	lb_widx_t *widx;
+	pcb_data_t *data;
+	pcb_subc_t *subc;
 } lb_ctx_t;
 
 static const char pcb_acts_LayerBinding[] = "LayerBinding(object)\nLayerBinding(selected)\nLayerBinding(buffer)\n";
 static const char pcb_acth_LayerBinding[] = "Change the layer binding.";
 static int pcb_act_LayerBinding(int argc, const char **argv, pcb_coord_t x, pcb_coord_t y)
 {
-	pcb_data_t *data = NULL;
-	pcb_subc_t *subc = NULL;
+	lb_ctx_t ctx;
+
+	memset(&ctx, 0, sizeof(ctx));
 
 	if ((argc == 0) || (pcb_strcasecmp(argv[0], "object") == 0)) {
 		int type;
@@ -56,8 +59,8 @@ static int pcb_act_LayerBinding(int argc, const char **argv, pcb_coord_t x, pcb_
 			pcb_message(PCB_MSG_ERROR, "No subc under the cursor\n");
 			return -1;
 		}
-		subc = ptr2;
-		data = subc->data;
+		ctx.subc = ptr2;
+		ctx.data = ctx.subc->data;
 	}
 	else if (pcb_strcasecmp(argv[0], "selected") == 0) {
 #warning subc TODO
@@ -74,11 +77,10 @@ static int pcb_act_LayerBinding(int argc, const char **argv, pcb_coord_t x, pcb_
 
 	{ /* interactive mode */
 		int n;
-		lb_ctx_t ctx;
 
 		PCB_DAD_DECL(dlg);
 
-		ctx.widx = malloc(sizeof(lb_widx_t) * data->LayerN);
+		ctx.widx = malloc(sizeof(lb_widx_t) * ctx.data->LayerN);
 		ctx.layer_names = calloc(sizeof(char *), PCB->Data->LayerN+1);
 		for(n = 0; n < PCB->Data->LayerN; n++)
 			ctx.layer_names[n] = PCB->Data->Layer[n].meta.real.name;
@@ -86,7 +88,7 @@ static int pcb_act_LayerBinding(int argc, const char **argv, pcb_coord_t x, pcb_
 
 
 		PCB_DAD_BEGIN_TABLE(dlg, 2);
-		for(n = 0; n < data->LayerN; n++) {
+		for(n = 0; n < ctx.data->LayerN; n++) {
 			lb_widx_t *w = ctx.widx+n;
 			/* left side */
 			PCB_DAD_BEGIN_VBOX(dlg);
