b5ca7d1c17442b6e5d815c845c77244ccd1c4f78
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-09-15 10:59:24 +0000

    [export_stat] -Add: use safe_fs for opening the output
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@11553 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/export_stat/stat.c
+++ src_plugins/export_stat/stat.c
@@ -43,6 +43,7 @@
 #include "pcb-printf.h"
 #include "compat_misc.h"
 #include "plug_io.h"
+#include "safe_fs.h"
 
 #include "hid.h"
 #include "hid_nogui.h"
@@ -143,7 +144,7 @@ static void stat_do_export(pcb_hid_attr_val_t * options)
 	if (!filename)
 		filename = "pcb.stat.lht";
 
-	f = fopen(filename, "w");
+	f = pcb_fopen(filename, "w");
 	if (!f) {
 		perror(filename);
 		return;
