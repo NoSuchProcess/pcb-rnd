063d11efab51eaf6cb03139bf834d9562ad4b1fc
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-09-22 06:20:49 +0000

    [poly] -Fix: wrong invalidate-draw call for flag change
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@11701 1d61de99-560f-4dba-8957-1e505d42358b

--- src/obj_poly.c
+++ src/obj_poly.c
@@ -484,12 +484,12 @@ void *pcb_polyop_insert_point(pcb_opctx_t *ctx, pcb_layer_t *Layer, pcb_polygon_
 	return (&Polygon->Points[ctx->insert.idx]);
 }
 
-/* changes the clearance flag of a line */
+/* changes the clearance flag of a polygon */
 void *pcb_polyop_change_join(pcb_opctx_t *ctx, pcb_layer_t *Layer, pcb_polygon_t *poly)
 {
 	if (PCB_FLAG_TEST(PCB_FLAG_LOCK, poly))
 		return (NULL);
-	pcb_line_invalidate_erase(poly);
+	pcb_poly_invalidate_erase(poly);
 	if (PCB_FLAG_TEST(PCB_FLAG_CLEARPOLYPOLY, poly)) {
 		pcb_undo_add_obj_to_clear_poly(PCB_TYPE_POLYGON, Layer, poly, poly, pcb_false);
 		pcb_poly_restore_to_poly(PCB->Data, PCB_TYPE_POLYGON, Layer, poly);
@@ -500,7 +500,7 @@ void *pcb_polyop_change_join(pcb_opctx_t *ctx, pcb_layer_t *Layer, pcb_polygon_t
 		pcb_undo_add_obj_to_clear_poly(PCB_TYPE_POLYGON, Layer, poly, poly, pcb_true);
 		pcb_poly_clear_from_poly(PCB->Data, PCB_TYPE_POLYGON, Layer, poly);
 	}
-	pcb_line_invalidate_draw(Layer, poly);
+	pcb_poly_invalidate_draw(Layer, poly);
 	return poly;
 }
 
