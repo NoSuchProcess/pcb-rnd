7e94b6885f8496c3cc3a7eb2e3e0b07bd7d9ab1a
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-09-23 16:02:38 +0000

    [loghid] -Fix: allow screen to be NULL in drawing mode set
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@11733 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/loghid/hid-logger.c
+++ src_plugins/loghid/hid-logger.c
@@ -83,7 +83,10 @@ static void log_destroy_gc(pcb_hid_gc_t gc)
 
 static void log_set_drawing_mode(pcb_composite_op_t op, pcb_bool direct, const pcb_box_t *screen)
 {
-	pcb_fprintf(out_, "set_drawing_mode(%d,%d,[%mm;%mm,%mm;%mm])\n", op, direct, screen->X1, screen->Y1, screen->X2, screen->Y2);
+	if (screen != NULL)
+		pcb_fprintf(out_, "set_drawing_mode(%d,%d,[%mm;%mm,%mm;%mm])\n", op, direct, screen->X1, screen->Y1, screen->X2, screen->Y2);
+	else
+		pcb_fprintf(out_, "set_drawing_mode(%d,%d,[NULL-screen])\n", op, direct);
 	delegatee_->set_drawing_mode(op, direct, screen);
 }
 
