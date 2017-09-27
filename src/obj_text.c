727478e9eb3e9e07d9ee9691bd3e50a6e7887250
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-09-27 05:07:20 +0000

    [core] -Fix: don't change the ID when text is put on a layer - instead, invent the ID when the text is created (to prevent unintentional ID changes, e.g. on lihata load)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@11816 1d61de99-560f-4dba-8957-1e505d42358b

--- src/obj_text.c
+++ src/obj_text.c
@@ -98,6 +98,7 @@ pcb_text_t *pcb_text_new(pcb_layer_t *Layer, pcb_font_t *PCBFont, pcb_coord_t X,
 	text->Scale = Scale;
 	text->TextString = pcb_strdup(TextString);
 	text->fid = PCBFont->id;
+	text->ID = pcb_create_ID_get();
 
 	pcb_add_text_on_layer(Layer, text, PCBFont);
 
@@ -130,7 +131,6 @@ void pcb_add_text_on_layer(pcb_layer_t *Layer, pcb_text_t *text, pcb_font_t *PCB
 {
 	/* calculate size of the bounding box */
 	pcb_text_bbox(PCBFont, text);
-	text->ID = pcb_create_ID_get();
 	if (!Layer->text_tree)
 		Layer->text_tree = pcb_r_create_tree(NULL, 0, 0);
 	pcb_r_insert_entry(Layer->text_tree, (pcb_box_t *) text, 0);
