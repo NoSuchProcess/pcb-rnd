dd6cdcdfa0fe5b27b47da5422cf71fede2a94e39
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-10-14 17:03:28 +0000

    [padstack] -Add: padstack hash eq
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@12090 1d61de99-560f-4dba-8957-1e505d42358b

--- src/obj_padstack.c
+++ src/obj_padstack.c
@@ -56,8 +56,58 @@ unsigned int pcb_padstack_hash(const pcb_padstack_proto_t *p)
 	return ret;
 }
 
+static int pcb_padstack_shape_eq(const pcb_padstack_shape_t *sh1, const pcb_padstack_shape_t *sh2)
+{
+	int n;
+
+	if (sh1->layer_mask != sh2->layer_mask) return 0;
+	if (sh1->comb != sh2->comb) return 0;
+	if (sh1->clearance != sh2->clearance) return 0;
+	if (sh1->shape != sh2->shape) return 0;
+
+	switch(sh1->shape) {
+		case PCB_PSSH_POLY:
+			if (sh1->data.poly.len != sh2->data.poly.len) return 0;
+			for(n = 0; n < sh1->data.poly.len; n++) {
+				if (sh1->data.poly.pt[n].X != sh2->data.poly.pt[n].X) return 0;
+				if (sh1->data.poly.pt[n].Y != sh2->data.poly.pt[n].Y) return 0;
+			}
+			break;
+		case PCB_PSSH_LINE:
+			if (sh1->data.line.x1 != sh2->data.line.x1) return 0;
+			if (sh1->data.line.x2 != sh2->data.line.x2) return 0;
+			if (sh1->data.line.y1 != sh2->data.line.y1) return 0;
+			if (sh1->data.line.y2 != sh2->data.line.y2) return 0;
+			if (sh1->data.line.thickness != sh2->data.line.thickness) return 0;
+			if (sh1->data.line.square != sh2->data.line.square) return 0;
+			break;
+		case PCB_PSSH_CIRC:
+			if (sh1->data.circ.x != sh2->data.circ.x) return 0;
+			if (sh1->data.circ.y != sh2->data.circ.y) return 0;
+			if (sh1->data.circ.dia != sh2->data.circ.dia) return 0;
+			break;
+	}
+
+	return 1;
+}
+
 int pcb_padstack_eq(const pcb_padstack_proto_t *p1, const pcb_padstack_proto_t *p2)
 {
-	return 0;
+	int n1, n2;
+
+	if (p1->hdia != p2->hdia) return 0;
+	if (p1->htop != p2->htop) return 0;
+	if (p1->hbottom != p2->hbottom) return 0;
+	if (p1->len != p2->len) return 0;
+
+	for(n1 = 0; n1 < p1->len; n1++) {
+		for(n2 = 0; n1 < p2->len; n2++)
+			if (pcb_padstack_shape_eq(p1->shape + n1, p2->shape + n2))
+				goto found;
+		return 0;
+		found:;
+	}
+
+	return 1;
 }
 
