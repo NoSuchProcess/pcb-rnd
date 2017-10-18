329028fd911c86e6dfdfe1be93608f875d17388a
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-10-18 06:48:08 +0000

    [fp_fs] -Add: silently ignore .lht and .subc.lht suffix when matching unsuffixed
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@12170 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/fp_fs/fp_fs.c
+++ src_plugins/fp_fs/fp_fs.c
@@ -286,8 +286,8 @@ static int fp_search_cb(void *cookie, const char *subdir, const char *name, pcb_
 	fp_search_t *ctx = (fp_search_t *) cookie;
 	if ((strncmp(ctx->target, name, ctx->target_len) == 0) && ((! !ctx->parametric) == (type == PCB_FP_PARAMETRIC))) {
 		const char *suffix = name + ctx->target_len;
-		/* ugly heuristics: footprint names may end in .fp or .ele */
-		if ((*suffix == '\0') || (pcb_strcasecmp(suffix, ".fp") == 0) || (pcb_strcasecmp(suffix, ".ele") == 0)) {
+		/* ugly heuristics: footprint names may end in .fp or .ele or .subc.lht or .lht */
+		if ((*suffix == '\0') || (pcb_strcasecmp(suffix, ".fp") == 0) || (pcb_strcasecmp(suffix, ".ele") == 0) || (pcb_strcasecmp(suffix, ".subc.lht") == 0) || (pcb_strcasecmp(suffix, ".lht") == 0)) {
 			ctx->path = pcb_strdup(subdir);
 			ctx->real_name = pcb_strdup(name);
 			return 1;
