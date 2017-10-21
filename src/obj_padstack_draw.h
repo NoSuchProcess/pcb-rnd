ca5ed1b1242837dbf47ca9996726d8b6f121de60
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-10-21 06:54:24 +0000

    [padstack] -Add: header with prototypes for drawing
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@12243 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src/obj_padstack_draw.h
@@ -0,0 +1,31 @@
+/*
+ *                            COPYRIGHT
+ *
+ *  pcb-rnd, interactive printed circuit board design
+ *  Copyright (C) 2017 Tibor 'Igor2' Palinkas
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+/* Include rtree.h for these */
+#ifdef PCB_RTREE_H
+pcb_r_dir_t pcb_pin_draw_callback(const pcb_box_t *b, void *cl);
+pcb_r_dir_t pcb_pin_clear_callback(const pcb_box_t *b, void *cl);
+#endif
+
+void pcb_pin_draw(pcb_pin_t *pin, pcb_bool draw_hole);
+void pcb_pin_invalidate_erase(pcb_pin_t *Pin);
+void pcb_pin_invalidate_draw(pcb_pin_t *Pin);
