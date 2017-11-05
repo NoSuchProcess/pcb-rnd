617109e738d32aa2d56542c70c4d5ea27836e588
avigne <avigne@1d61de99-560f-4dba-8957-1e505d42358b>
2017-11-05 11:08:36 +0000

    [gtk] -Doc: API.
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@12633 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_gtk_common/in_keyboard.h
+++ src_plugins/lib_gtk_common/in_keyboard.h
@@ -18,20 +18,25 @@ typedef enum {
 	SHIFT_CONTROL_MOD1_PRESSED = PCB_M_Shift | PCB_M_Ctrl | PCB_M_Mod1
 } ModifierKeysState;
 
-/** */
+/** \return TRUE if \p ksym is SHIFT or CTRL modifier key. */
 gboolean ghid_is_modifier_key_sym(gint ksym);
 
-/** Returns key modifier state */
+/** \return key modifier state of \p drawing_area if \p state is NULL, otherwise, corresponding \p state key modifier. */
 ModifierKeysState ghid_modifier_keys_state(GtkWidget *drawing_area, GdkModifierType *state);
 
-/** Handle user keys in the output drawing area. */
+/** Handle user key events of the output drawing area. */
 gboolean ghid_port_key_press_cb(GtkWidget *drawing_area, GdkEventKey *kev, gpointer data);
 
 extern pcb_hid_cfg_keys_t ghid_keymap;
 
 extern GdkModifierType pcb_gtk_glob_mask;
 
+/** \return the keyval corresponding to \p desc key name. \p len is not used. */
 unsigned short int ghid_translate_key(const char *desc, int len);
+
+/** Return in \p out -a string of max. \p out_len chars-, the GDK name corresponding to \p key_char key value.
+    \return 0 upon successful completion, -1 otherwise.
+*/
 int ghid_key_name(unsigned short int key_char, char *out, int out_len);
 
 #endif
