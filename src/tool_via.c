b1bf1ca93c5cf44df288688748e990e8a6be9aba
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-11-10 04:02:14 +0000

    [tool] -Fix: indentation glitch introduced in r12720
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@12721 1d61de99-560f-4dba-8957-1e505d42358b

--- src/tool_via.c
+++ src/tool_via.c
@@ -76,14 +76,14 @@ void pcb_tool_via_notify_mode(void)
 		if ((via = pcb_via_new(PCB->Data, Note.X, Note.Y,
 													conf_core.design.via_thickness, 2 * conf_core.design.clearance,
 													0, conf_core.design.via_drilling_hole, NULL, pcb_no_flags())) != NULL) {
-		pcb_obj_add_attribs(via, PCB->pen_attr);
-		pcb_undo_add_obj_to_create(PCB_TYPE_VIA, via, via, via);
-		if (pcb_gui->shift_is_pressed())
-			pcb_chg_obj_thermal(PCB_TYPE_VIA, via, via, via, PCB->ThermStyle, INDEXOFCURRENT);
-		pcb_undo_inc_serial();
-		pcb_via_invalidate_draw(via);
-		pcb_draw();
-	}
+			pcb_obj_add_attribs(via, PCB->pen_attr);
+			pcb_undo_add_obj_to_create(PCB_TYPE_VIA, via, via, via);
+			if (pcb_gui->shift_is_pressed())
+				pcb_chg_obj_thermal(PCB_TYPE_VIA, via, via, via, PCB->ThermStyle, INDEXOFCURRENT);
+			pcb_undo_inc_serial();
+			pcb_via_invalidate_draw(via);
+			pcb_draw();
+		}
 	}
 }
 
