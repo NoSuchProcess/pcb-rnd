28d9fed33ee7df7b9220151b959b2de181190ecf
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-11-19 14:48:46 +0000

    [io_eagle] -Change: enable by default (buildin)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@12905 1d61de99-560f-4dba-8957-1e505d42358b

--- scconfig/plugins.h
+++ scconfig/plugins.h
@@ -90,7 +90,7 @@ plugin_def("export_xy",        "xy (centroid) pcb_exporter",      sbuildin, 1)
 
 plugin_header("\nIO plugins (file formats):\n")
 plugin_def("io_autotrax",      "autotrax (freeware PCB CAD)",     sbuildin, 1)
-plugin_def("io_eagle",         "Eagle's xml format",              sdisable, 0)
+plugin_def("io_eagle",         "Eagle's xml format",              sbuildin, 1)
 plugin_def("io_hyp",           "hyperlynx .hyp loader",           sbuildin, 1)
 plugin_def("io_kicad_legacy",  "Kicad's legacy format",           sbuildin, 1)
 plugin_def("io_kicad",         "Kicad's s-expr format",           sbuildin, 1)
