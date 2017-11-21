ce69c0188947768905738e6626b3eb7fbaf0c683
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-11-21 06:49:00 +0000

    [scconfig] -Fix: map_plugins should sort deps for reproducible output
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@12916 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/map_plugins.sh
+++ src_plugins/map_plugins.sh
@@ -50,7 +50,7 @@ END { print "\n" }
 
 ' > ../scconfig/plugins.h
 
-$PUPLUG findpups . "" 'plugin_dep("%N", "%m")\n' >> ../scconfig/plugins.h
+$PUPLUG findpups . "" 'plugin_dep("%N", "%m")\n' | sort >> ../scconfig/plugins.h
 
 
 # Generate the plugin list
