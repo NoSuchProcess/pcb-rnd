835abd938bd1201774958bb7291afbc00c345fd7
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-11-25 04:26:29 +0000

    -Revert: part of r12980 - do not attempt to update the text bounding box if there's no PCB available,  because there won't be font either
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@13004 1d61de99-560f-4dba-8957-1e505d42358b

--- src/obj_text.c
+++ src/obj_text.c
@@ -650,13 +650,14 @@ void pcb_text_update(pcb_layer_t *layer, pcb_text_t *text)
 	pcb_data_t *data = layer->parent;
 	pcb_board_t *pcb = pcb_data_get_top(data);
 
-	if (pcb != NULL)
-		pcb_poly_restore_to_poly(data, PCB_TYPE_TEXT, layer, text);
+	if (pcb == NULL)
+		return
+
+	pcb_poly_restore_to_poly(data, PCB_TYPE_TEXT, layer, text);
 	pcb_r_delete_entry(layer->text_tree, (pcb_box_t *) text);
 	pcb_text_bbox(pcb_font(pcb, text->fid, 1), text);
 	pcb_r_insert_entry(layer->text_tree, (pcb_box_t *) text, 0);
-	if (pcb != NULL)
-		pcb_poly_clear_from_poly(data, PCB_TYPE_TEXT, layer, text);
+	pcb_poly_clear_from_poly(data, PCB_TYPE_TEXT, layer, text);
 }
 
 void *pcb_textop_change_flag(pcb_opctx_t *ctx, pcb_layer_t *Layer, pcb_text_t *Text)
