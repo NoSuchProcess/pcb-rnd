7a8de0e4e8f5c79c659cb4a86de3bbc8d65bcf56
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-11-26 10:10:34 +0000

    [padstack] -Add: when mirroring data, also mirror padstacks
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@13049 1d61de99-560f-4dba-8957-1e505d42358b

--- src/data.c
+++ src/data.c
@@ -412,6 +412,11 @@ void pcb_data_mirror(pcb_data_t *data, pcb_coord_t y_offs)
 		pcb_via_mirror(data, via, y_offs);
 	}
 	PCB_END_LOOP;
+	PCB_PADSTACK_LOOP(data);
+	{
+		pcb_pstk_mirror(padstack, y_offs);
+	}
+	PCB_END_LOOP;
 	PCB_SUBC_LOOP(data);
 	{
 		pcb_subc_mirror(data, subc, y_offs);
