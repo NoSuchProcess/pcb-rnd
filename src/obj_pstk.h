19cae8765880bf924b664ba431c6f72c74647c35
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-12-01 05:11:07 +0000

    [core] -Fix: wrong API comments: high level pstk and subc moves are not undoable
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@13099 1d61de99-560f-4dba-8957-1e505d42358b

--- src/obj_pstk.h
+++ src/obj_pstk.h
@@ -164,7 +164,7 @@ void pcb_pstk_proto_del_shape_idx(pcb_pstk_proto_t *proto, int idx);
 /* Mirror a padstack (useful for sending to the other side - set swap_side to 1 in that case) */
 void pcb_pstk_mirror(pcb_pstk_t *ps, pcb_coord_t y_offs, int swap_side);
 
-/* Undoable move (op wrapper) */
+/* High level move (op wrapper; no undo) */
 void pcb_pstk_move(pcb_pstk_t *ps, pcb_coord_t dx, pcb_coord_t dy, pcb_bool more_to_come);
 
 /* Low level move - updates only the coordinates and the bbox */
