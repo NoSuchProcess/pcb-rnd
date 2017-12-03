343b734bfc2722e4a03e78935c5287cace98963a
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-12-03 05:44:59 +0000

    [io_eagle] -Add: dru line parser
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@13168 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_eagle/parse_dru.c
+++ src_plugins/io_eagle/parse_dru.c
@@ -29,6 +29,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <genvector/gds_char.h>
 
 int pcb_eagle_dru_test_parse(FILE *f)
 {
@@ -63,3 +64,63 @@ int pcb_eagle_dru_test_parse(FILE *f)
 
 	return 1;
 }
+
+/* eat up whitespace after the '=' */
+static void eat_up_ws(FILE *f)
+{
+	for(;;) {
+		int c = fgetc(f);
+		if (c == EOF)
+			return;
+		if (!isspace(c)) {
+			ungetc(c, f);
+			return;
+		}
+	}
+}
+
+void pcb_eagle_dru_parse_line(FILE *f, gds_t *buff, char **key, char **value)
+{
+	long n, keyo = -1, valo = -1;
+	gds_truncate(buff, 0);
+	*key = NULL;
+	*value = NULL;
+	for(;;) {
+		int c = fgetc(f);
+		if (c == EOF)
+			break;
+		if ((c == '\r') || (c == '\n')) {
+			if (buff->used == 0) /* ignore leading newlines */
+				continue;
+			break;
+		}
+		if (isspace(c) && (keyo < 0)) /* ignore leading spaces */
+			continue;
+
+		/* have key, don't have val, found sep */
+		if ((keyo >= 0) && (valo < 0) && (c == '=')) {
+			for(n = buff->used-1; n >= 0; n--) {
+				if (!isspace(buff->array[n]))
+					break;
+				buff->array[n] = '\0';
+			}
+			gds_append(buff, '\0');
+			valo = buff->used;
+			eat_up_ws(f);
+		}
+		else
+			gds_append(buff, c);
+
+		/* set key offset */
+		if (keyo < 0)
+			keyo = 0;
+	}
+
+	gds_append(buff, '\0');
+
+	if (keyo >= 0)
+		*key = buff->array + keyo;
+	if (valo >= 0)
+		*value = buff->array + valo;
+}
+
