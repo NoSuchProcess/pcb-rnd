f5457bb52b510c1e0c67d80a1283759eac66c61e
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2017-12-04 07:31:59 +0000

    [propedit] -Fix: always list padstack hole, even if 0, so the user has a chance to change it
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@13245 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/propedit/propsel.c
+++ src_plugins/propedit/propsel.c
@@ -186,8 +186,7 @@ static void map_pstk_cb(void *ctx, pcb_board_t *pcb, pcb_pstk_t *ps)
 
 	proto = pcb_pstk_get_proto(ps);
 	map_add_prop(ctx, "p/padstack/clearance", pcb_coord_t, ps->Clearance/2);
-	if (proto->hdia > 0)
-		map_add_prop(ctx, "p/padstack/hole", pcb_coord_t, proto->hdia);
+	map_add_prop(ctx, "p/padstack/hole", pcb_coord_t, proto->hdia);
 
 	map_attr(ctx, &ps->Attributes);
 }
