38fb129de377f28026e678fa178bf6bfa335ff34
ade <ade@1d61de99-560f-4dba-8957-1e505d42358b>
2018-01-18 14:40:14 +0000

    [core] -Add: Draw the current 'pen' when in line tool mode and a line has not yet been placed.
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@14085 1d61de99-560f-4dba-8957-1e505d42358b

--- src/tool_line.c
+++ src/tool_line.c
@@ -331,6 +331,24 @@ void pcb_tool_line_draw_attached(void)
 			pcb_route_draw_drc(&pcb_crosshair.Route,pcb_crosshair.GC);
 		pcb_gui->set_color(pcb_crosshair.GC, conf_core.appearance.color.crosshair);
 	}
+	else {
+		/* Draw a circle (0 length line) to show where the line will be placed */
+    if(CURRENT)
+			pcb_gui->set_color(pcb_crosshair.GC,CURRENT->meta.real.color);
+
+		pcb_draw_wireframe_line(pcb_crosshair.GC,
+														pcb_crosshair.X, pcb_crosshair.Y,
+														pcb_crosshair.X, pcb_crosshair.Y, 
+														conf_core.design.line_thickness,0 );
+
+		if(conf_core.editor.show_drc) {
+			pcb_gui->set_color(pcb_crosshair.GC,conf_core.appearance.color.cross);
+			pcb_draw_wireframe_line(pcb_crosshair.GC,
+															pcb_crosshair.X, pcb_crosshair.Y,
+															pcb_crosshair.X, pcb_crosshair.Y, 
+															conf_core.design.line_thickness + (2 * PCB->Bloat),0 );
+		}
+	}
 }
 
 pcb_bool pcb_tool_line_undo_act(void)
