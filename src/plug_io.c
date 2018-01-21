2e36ae9ea6d86dbc655c495a5a13ecbd56510ed3
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-01-21 09:43:45 +0000

    [core] -Fix: issue a layers changed event before layer vis reset on board creation so that the layer selector is not reading into already free'd memory
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@14162 1d61de99-560f-4dba-8957-1e505d42358b

--- src/plug_io.c
+++ src/plug_io.c
@@ -497,6 +497,7 @@ static int real_load_pcb(const char *Filename, const char *fmt, pcb_bool revert,
 		}
 
 		/* have to be called after pcb_board_resize() so vis update is after a board changed update */
+		pcb_event(PCB_EVENT_LAYERS_CHANGED, NULL);
 		pcb_layervis_reset_stack();
 
 		/* enable default font if necessary */
