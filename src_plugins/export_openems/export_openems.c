be4fbe9ac8a86e49a5b7908812664b9bbb0b5279
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-02-11 08:51:49 +0000

    [openems] -Add: prepare for rendering square cap lines as polygons
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@14670 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/export_openems/export_openems.c
+++ src_plugins/export_openems/export_openems.c
@@ -465,13 +465,23 @@ static void openems_fill_rect(pcb_hid_gc_t gc, pcb_coord_t x1, pcb_coord_t y1, p
 static void openems_draw_line(pcb_hid_gc_t gc, pcb_coord_t x1, pcb_coord_t y1, pcb_coord_t x2, pcb_coord_t y2)
 {
 	wctx_t *ctx = ems_ctx;
-	long oid = ctx->oid++;
 
-#warning TODO: convert square cap line to poly? Maybe in core?
-
-	pcb_fprintf(ctx->f, "points%ld(1, 1) = %mm; points%ld(2, 1) = %mm;\n", oid, x1, oid, -y1);
-	pcb_fprintf(ctx->f, "points%ld(1, 2) = %mm; points%ld(2, 2) = %mm;\n", oid, x2, oid, -y2);
-	pcb_fprintf(ctx->f, "CSX = AddPcbrndTrace(CSX, PCBRND, %d, points%ld, %mm, 0);\n", ctx->clayer, oid, gc->width);
+	if (gc->cap == Square_Cap) {
+		pcb_coord_t x[4], y[4];
+		pcb_line_t tmp;
+		tmp.Point1.X = x1;
+		tmp.Point1.Y = y1;
+		tmp.Point2.X = x2;
+		tmp.Point2.Y = y2;
+		pcb_sqline_to_rect(&tmp, x, y);
+/*		openems_fill_polygon_offs(gc, 4, x, y, 0, 0);*/
+	}
+	else {
+		long oid = ctx->oid++;
+		pcb_fprintf(ctx->f, "points%ld(1, 1) = %mm; points%ld(2, 1) = %mm;\n", oid, x1, oid, -y1);
+		pcb_fprintf(ctx->f, "points%ld(1, 2) = %mm; points%ld(2, 2) = %mm;\n", oid, x2, oid, -y2);
+		pcb_fprintf(ctx->f, "CSX = AddPcbrndTrace(CSX, PCBRND, %d, points%ld, %mm, 0);\n", ctx->clayer, oid, gc->width);
+	}
 }
 
 static void openems_draw_rect(pcb_hid_gc_t gc, pcb_coord_t x1, pcb_coord_t y1, pcb_coord_t x2, pcb_coord_t y2)
