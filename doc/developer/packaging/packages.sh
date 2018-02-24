efe8c5d76aaf61d60caf36465093c5752d9e662a
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-02-24 18:58:54 +0000

    [doc] -Add: script to calcualte package deps
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@14959 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ doc/developer/packaging/packages.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+proot=../../../src_plugins
+
+for n in $proot/*/*.pup
+do
+	pkg=`basename $n`
+	sed "s/^/$pkg /" < $n
+done | awk '
+	{ pkg = $1; sub("[.]pup$", "", pkg) }
+	($2 == "$package") { PKG[$3] = PKG[$3] " " pkg; PLUGIN[pkg] = $3 }
+	($2 == "dep") { PLUGIN_DEP[pkg] = PLUGIN_DEP[pkg] " " $3 }
+
+	function add_dep(pkg, depson,    ds)
+	{
+		if (pkg != depson) {
+			ds = pkg "::" depson
+			if (!(ds in DEP_SEEN)) {
+				DEP_SEEN[ds] = 1
+				PKG_DEP[pkg] = PKG_DEP[pkg] " " depson
+			}
+		}
+	}
+
+	END {
+		for(pkg in PKG)
+			add_dep(pkg, "(core)")
+
+		for(plg in PLUGIN_DEP) {
+			v = split(PLUGIN_DEP[plg], A, " ")
+			pkg = PLUGIN[plg]
+			if (pkg == "") continue
+			for(n = 1; n <= v; n++) {
+				if (A[n] == "") continue
+				depson = PLUGIN[A[n]]
+				if (depson == "")
+					depson = "!!!" A[n]
+				add_dep(pkg, depson)
+			}
+		}
+
+		
+		for(pkg in PKG) {
+			print pkg "     depends_on    " PKG_DEP[pkg]
+		}
+	}
+'
