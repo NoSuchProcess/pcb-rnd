03a8c51e338283dde3f5ea8dc88630cdb0cf4ea3
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-02-25 14:09:22 +0000

    [doc] -Add: package descriptions and metapackage
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@14976 1d61de99-560f-4dba-8957-1e505d42358b

--- doc/developer/packaging/packages.sh
+++ doc/developer/packaging/packages.sh
@@ -1,21 +1,44 @@
 #!/bin/sh
 proot=../../../src_plugins
 
+meta_deps="core io-standard io-alien hid-gtk2-dl hid-gtk2-gdk export export-sim export-extra auto extra cloud"
+
 (echo '
 <html>
 <body>
-<table border=1>
-<tr><th> package <th> depends on (packages) <th> consists of (plugins)
 '
 
 for n in $proot/*/*.pup
 do
 	pkg=`basename $n`
 	sed "s/^/$pkg /" < $n
-done | awk '
-	{ pkg = $1; sub("[.]pup$", "", pkg) }
-	($2 == "$package") { PKG[$3] = PKG[$3] " " pkg; PLUGIN[pkg] = $3 }
-	($2 == "dep") { PLUGIN_DEP[pkg] = PLUGIN_DEP[pkg] " " $3 }
+done | awk -v "meta_deps=$meta_deps" '
+	BEGIN {
+		while((getline < "desc") == 1) {
+			if ($0 ~ "^@") {
+				pkg=$0
+				sub("^@", "", pkg)
+				getline SHORT[pkg] < "desc"
+				continue
+			}
+			LONG[pkg] = LONG[pkg] $0 " "
+		}
+	}
+
+	{
+		pkg = $1;
+		sub("[.]pup$", "", pkg)
+		if (pkg == "(core)") pkg="core"
+		
+
+		val=$3
+		if (val == "(core)") val="core"
+		pkg = "pcb-rnd-" pkg
+		val = "pcb-rnd-" val
+	}
+
+	($2 == "$package") { PKG[val] = PKG[val] " " pkg; PLUGIN[pkg] = val; IFILES[val] = IFILES[val] " " pkg ".pup " pkg ".so" }
+	($2 == "dep") { PLUGIN_DEP[pkg] = PLUGIN_DEP[pkg] " " val }
 
 	function add_dep(pkg, depson,    ds)
 	{
@@ -29,9 +52,12 @@ done | awk '
 	}
 
 	END {
+	
+		# everything depends on core
 		for(pkg in PKG)
-			add_dep(pkg, "(core)")
+			add_dep(pkg, "pcb-rnd-core")
 
+		# calculate dependencies
 		for(plg in PLUGIN_DEP) {
 			v = split(PLUGIN_DEP[plg], A, " ")
 			pkg = PLUGIN[plg]
@@ -45,17 +71,32 @@ done | awk '
 			}
 		}
 
-		
+		PKG_DEP["core"] = ""
+		PKG_DEP["pcb-rnd"] = meta_deps
+		PKG["pcb-rnd"] = "&lt;metapackage&gt;"
+
+		print "<h3> Package summary and dependencies </h3>"
+		print "<table border=1>"
+		print "<tr><th> package <th> depends on (packages) <th> consists of (plugins)"
+
+		for(pkg in PKG)
+			print "<tr><th>" pkg "<td>" PKG_DEP[pkg] "<td>" PKG[pkg]
+		print "</table>"
+
+		print "<h3> Package description and files </h3>"
+		print "<table border=1>"
+		print "<tr><th> package <th> files <th> short <th> long"
 		for(pkg in PKG) {
-			print pkg "|" PKG_DEP[pkg] "|" PKG[pkg]
+			if (SHORT[pkg] == "") SHORT[pkg] = "&nbsp;"
+			if (LONG[pkg] == "") LONG[pkg] = "&nbsp;"
+			print "<tr><th>" pkg "<td>" IFILES[pkg] "<td>" SHORT[pkg]  "<td>" LONG[pkg]
 		}
+		print "</table>"
+
 	}
-' | sort | awk -F "[|]" '
-	{ print "<tr><th>" $1 "<td>" $2 "<td>" $3 }
 '
 
 echo '
-</table>
 </body>
 </html>
 ') > packages.html
