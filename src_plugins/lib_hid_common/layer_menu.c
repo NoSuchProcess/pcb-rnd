dfd491e3be7a22439b4dd6f32fdecf349b855ec1
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-04-03 03:45:48 +0000

    [lib_hid_common] -Add: prepare for the new anchored layer view/pick selector menus
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@16104 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_hid_common/layer_menu.c
+++ src_plugins/lib_hid_common/layer_menu.c
@@ -32,23 +32,40 @@
 #include "layer.h"
 #include "layer_grp.h"
 #include "pcb-printf.h"
+#include "hid_cfg.h"
+#include "hid.h"
 
-void pcb_layer_menu_create(const char *path_prefix, const char *cookie)
+typedef struct {
+	const char *anch;
+	int view;
+} ly_ctx_t;
+
+static void layer_install_menu(void *ctx_, pcb_hid_cfg_t *cfg, lht_node_t *node, char *path)
 {
-	char path[1024], *bn;
-	int plen = strlen(path_prefix), len_avail = sizeof(path) - plen;
+	ly_ctx_t *ctx = ctx_;
+	int plen = strlen(path);
+	int len_avail = 125;
+	char *end = path + plen;
+	pcb_menu_prop_t props;
+	char act[256], chk[256];
+	int idx;
 	pcb_layergrp_id_t gid;
 
+	memset(&props, 0,sizeof(props));
+	props.action = act;
+	props.checked = chk;
+	props.update_on = "";
+	props.cookie = ctx->anch;
 
-	memcpy(path, path_prefix, plen);
-	bn = path + plen;
-	if ((plen == 0) || (bn[-1] != '/')) {
-		*bn = '/';
-		bn++;
-		len_avail--;
-	}
+	pcb_hid_cfg_del_anchor_menus(node, ctx->anch);
 
-	for(gid = 0; gid < pcb_max_group(PCB); gid++) {
+	/* prepare for appending the strings at the end of the path, "under" the anchor */
+	*end = '/';
+	end++;
+
+	/* have to go reverse to keep order because this will insert items */
+
+	for(gid = pcb_max_group(PCB); gid >= 0; gid--) {
 		pcb_layergrp_t *g = &PCB->LayerGroups.grp[gid];
 		pcb_menu_prop_t props;
 		int n;
@@ -56,19 +73,29 @@ void pcb_layer_menu_create(const char *path_prefix, const char *cookie)
 		if (g->type & PCB_LYT_SUBSTRATE)
 			continue;
 
-		memset(&props, 0, sizeof(props));
-		props.action = "action";
-		props.cookie = cookie;
-
-		pcb_snprintf(bn, len_avail, "[%s]", g->name);
-		pcb_gui->create_menu(path, &props);
-
-		for(n = 0; n < g->len; n++) {
+		for(n = g->len-1; n >= 0; n--) {
 			pcb_layer_t *l = pcb_get_layer(PCB->Data, g->lid[n]);
 
-			pcb_snprintf(bn, len_avail, "  %s", l->name);
+			pcb_snprintf(end, len_avail, "  %s", l->name);
 			pcb_gui->create_menu(path, &props);
 		}
+
+		pcb_snprintf(end, len_avail, "[%s]", g->name);
+		pcb_gui->create_menu(path, &props);
+
 	}
 }
 
+
+void pcb_layer_install_menu(void)
+{
+	ly_ctx_t ctx;
+
+	ctx.view = 1;
+	ctx.anch = "@layerview";
+	pcb_hid_cfg_map_anchor_menus(ctx.anch, layer_install_menu, &ctx);
+
+	ctx.view = 0;
+	ctx.anch = "@layerpick";
+	pcb_hid_cfg_map_anchor_menus(ctx.anch, layer_install_menu, &ctx);
+}
