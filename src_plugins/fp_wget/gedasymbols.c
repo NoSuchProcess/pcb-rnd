3ea540c7fed2401c8d6c66eb3a6776eb1fa49888
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-04-10 07:07:41 +0000

    [fp_wget] -Add: missing copyright banners
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@16367 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/fp_wget/gedasymbols.c
+++ src_plugins/fp_wget/gedasymbols.c
@@ -1,3 +1,29 @@
+/*
+ *                            COPYRIGHT
+ *
+ *  pcb-rnd, interactive printed circuit board design
+ *  Copyright (C) 2016, 2017, 2018 Tibor 'Igor2' Palinkas
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  Contact:
+ *    Project page: http://repo.hu/projects/pcb-rnd
+ *    lead developer: email to pcb-rnd (at) igor2.repo.hu
+ *    mailing list: pcb-rnd (at) list.repo.hu (send "subscribe")
+ */
+
 #include "config.h"
 
 #include <stdlib.h>
