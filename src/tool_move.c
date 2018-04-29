27cd334c38e498d1117ef21e677db2fc7abea8dc
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-04-29 03:42:20 +0000

    [tool] -Fix: reset crosshair bounds after placing a drag&drop move so the crosshair move is not limited
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@16790 1d61de99-560f-4dba-8957-1e505d42358b

--- src/tool_move.c
+++ src/tool_move.c
@@ -88,6 +88,7 @@ void pcb_tool_move_notify_mode(void)
 		/* reset identifiers */
 		pcb_crosshair.AttachedObject.Type = PCB_OBJ_VOID;
 		pcb_crosshair.AttachedObject.State = PCB_CH_STATE_FIRST;
+		pcb_crosshair_set_range(0, 0, PCB->MaxWidth, PCB->MaxHeight);
 		break;
 	}
 }
