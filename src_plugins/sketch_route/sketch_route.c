fc98e59ac463fe0c772de54ff9d5210948975621
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-05-01 14:48:41 +0000

    [sketch_route] -Add: initial blank plugin
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@16922 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src_plugins/sketch_route/sketch_route.c
@@ -0,0 +1,67 @@
+/*
+ *                            COPYRIGHT
+ *
+ *  pcb-rnd, interactive printed circuit board design
+ *  Copyright (C) 2018 Tibor 'Igor2' Palinkas
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  Contact:
+ *    Project page: http://repo.hu/projects/pcb-rnd
+ *    lead developer: email to pcb-rnd (at) igor2.repo.hu
+ *    mailing list: pcb-rnd (at) list.repo.hu (send "subscribe")
+ */
+
+#include "config.h"
+
+#include "action_helper.h"
+#include "plugins.h"
+#include "hid_actions.h"
+
+
+const char *pcb_sketch_route_cookie = "sketch_route plugin";
+
+static const char pcb_acts_skroute[] = "skroute(TODO)";
+static const char pcb_acth_skroute[] = "TODO";
+int pcb_act_skroute(int argc, const char **argv, pcb_coord_t x, pcb_coord_t y)
+{
+	return 0;
+}
+
+pcb_hid_action_t sketch_route_action_list[] = {
+	{"skroute", 0, pcb_act_skroute, pcb_acth_skroute, pcb_acts_skroute},
+};
+
+PCB_REGISTER_ACTIONS(sketch_route_action_list, pcb_sketch_route_cookie)
+
+int pplg_check_ver_sketch_route(int ver_needed) { return 0; }
+
+void pplg_uninit_sketch_route(void)
+{
+	pcb_event_unbind_allcookie(pcb_sketch_route_cookie);
+	pcb_hid_remove_actions_by_cookie(pcb_sketch_route_cookie);
+}
+
+
+#include "dolists.h"
+int pplg_init_sketch_route(void)
+{
+	PCB_API_CHK_VER;
+	PCB_REGISTER_ACTIONS(sketch_route_action_list, pcb_sketch_route_cookie)
+
+	pcb_event_bind(PCB_EVENT_LAYERVIS_CHANGED, sketch_route_layer_chg, NULL, pcb_sketch_route_cookie);
+
+	return 0;
+}
