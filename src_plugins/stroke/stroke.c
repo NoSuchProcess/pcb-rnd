91864238c166041b5035512927035e8f86031d58
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-05-01 08:33:40 +0000

    [stroke] -Add: stroke() action with stroke(gesture, seq)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@16892 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/stroke/stroke.c
+++ src_plugins/stroke/stroke.c
@@ -30,6 +30,7 @@
 
 #include <stroke.h>
 
+#include "action_helper.h"
 #include "board.h"
 #include "conf_core.h"
 #include "crosshair.h"
@@ -48,22 +49,29 @@ conf_stroke_t conf_stroke;
 #define SIDE_X(x)  ((conf_core.editor.view.flip_x ? PCB->MaxWidth - (x) : (x)))
 #define SIDE_Y(y)  ((conf_core.editor.view.flip_y ? PCB->MaxHeight - (y) : (y)))
 
+static const char *pcb_stroke_cookie = "stroke plugin";
+
 static pcb_coord_t stroke_first_x, stroke_first_y, stroke_last_x, stroke_last_y;
+
+static int pcb_stroke_exec(const char *seq)
+{
+	conf_listitem_t *item;
+	int idx;
+
+	conf_loop_list(&conf_stroke.plugins.stroke.gestures, item, idx) {
+		if ((strcmp(seq, item->name) == 0) && (pcb_hid_parse_actions(item->val.string[0]) == 0))
+			return 0;
+	}
+	return -1;
+}
 
 static int pcb_stroke_finish(void)
 {
 	char msg[255];
 
 	pcb_mid_stroke = pcb_false;
-	if (stroke_trans(msg)) {
-		conf_listitem_t *item;
-		int idx;
-
-		conf_loop_list(&conf_stroke.plugins.stroke.gestures, item, idx) {
-			if ((strcmp(msg, item->name) == 0) && (pcb_hid_parse_actions(item->val.string[0]) == 0))
-				return 0;
-		}
-	}
+	if (stroke_trans(msg))
+		return pcb_stroke_exec(msg);
 	return -1;
 }
 
@@ -84,6 +92,33 @@ static void pcb_stroke_start(void)
 	stroke_first_y = SIDE_Y(pcb_crosshair.Y);
 }
 
+/*** action ***/
+
+static const char pcb_acts_stroke[] = "stroke(gesture, seq)";
+static const char pcb_acth_stroke[] = "Various gesture recognition related functions";
+static int pcb_act_stroke(int argc, const char **argv, pcb_coord_t x, pcb_coord_t y)
+{
+	if (argc < 1)
+		PCB_ACT_FAIL(stroke);
+
+	if (strcmp(argv[0], "gesture") == 0) {
+		if (argc < 2)
+			PCB_ACT_FAIL(stroke);
+		pcb_stroke_exec(argv[1]);
+	}
+	else
+		PCB_ACT_FAIL(stroke);
+	return 0;
+}
+
+/*** administration ***/
+
+pcb_hid_action_t stroke_action_list[] = {
+	{"stroke", 0, pcb_act_stroke, pcb_acth_stroke, pcb_acts_stroke}
+};
+PCB_REGISTER_ACTIONS(stroke_action_list, pcb_stroke_cookie)
+
+
 int pplg_check_ver_stroke(int ver_needed) { return 0; }
 
 int pplg_uninit_stroke(void)
@@ -92,6 +127,8 @@ int pplg_uninit_stroke(void)
 	return 0;
 }
 
+
+#include "dolists.h"
 int pplg_init_stroke(void)
 {
 	PCB_API_CHK_VER;
@@ -102,6 +139,7 @@ int pplg_init_stroke(void)
 	conf_reg_field(conf_stroke, field,isarray,type_name,cpath,cname,desc,flags);
 #include "stroke_conf_fields.h"
 
+	PCB_REGISTER_ACTIONS(stroke_action_list, pcb_stroke_cookie)
 
 	pcb_stub_stroke_record = pcb_stroke_record;
 	pcb_stub_stroke_start  = pcb_stroke_start;
