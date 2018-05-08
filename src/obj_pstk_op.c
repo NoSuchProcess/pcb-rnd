6e04e3b2decbfe8e2ad1a04ca46678e4d116d796
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-05-08 06:33:07 +0000

    [padstack] -Fix: make clearance minimum enforcement symmetric
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@16993 1d61de99-560f-4dba-8957-1e505d42358b

--- src/obj_pstk_op.c
+++ src/obj_pstk_op.c
@@ -294,7 +294,7 @@ void *pcb_pstkop_change_clear_size(pcb_opctx_t *ctx, pcb_pstk_t *ps)
 	value = MIN(PCB_MAX_LINESIZE, value);
 	if (!ctx->chgsize.is_absolute && (ctx->chgsize.value < 0) && (value < conf_core.design.bloat))
 		value = 0;
-	if (ctx->chgsize.is_absolute && (ctx->chgsize.value > 0) && (value < conf_core.design.bloat))
+	if ((ctx->chgsize.value > 0) && (value < conf_core.design.bloat))
 		value = conf_core.design.bloat + 1;
 	if (ps->Clearance == value)
 		return NULL;
