27a37a8f42fd1deed36f677831554948a3fccc76
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-05-13 05:43:27 +0000

    [core] -Del: dead code: no need to have a special case for subc parent on rats, the default "anything virtual won't have parent" part handles it
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@17051 1d61de99-560f-4dba-8957-1e505d42358b

--- src/obj_subc_parent.h
+++ src/obj_subc_parent.h
@@ -76,12 +76,6 @@ PCB_INLINE pcb_subc_t *pcb_obj_parent_subc(pcb_any_obj_t *obj)
 		case PCB_OBJ_ARC:
 			return pcb_lobj_parent_subc(obj->parent_type, &obj->parent);
 
-#if 0
-		case PCB_OBJ_RAT:
-			/* easy case: can not be in a subc at all */
-			return 0;
-#endif
-
 		default:
 			/* anything else: virtual */
 			return 0;
