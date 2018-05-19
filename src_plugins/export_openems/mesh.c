b119e2d9fa5a7924897048c21da4a39f6fb72bdd
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-05-19 09:28:04 +0000

    [export_openems] -Add: mesh lines on ports
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@17081 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/export_openems/mesh.c
+++ src_plugins/export_openems/mesh.c
@@ -91,11 +91,21 @@ static void mesh_add_obj(pcb_mesh_t *mesh, pcb_mesh_dir_t dir, pcb_coord_t c1, p
 /* generate edges and ranges looking at objects on the given layer */
 static int mesh_gen_obj(pcb_mesh_t *mesh, pcb_mesh_dir_t dir)
 {
+	pcb_data_t *data = mesh->layer->parent.data;
 	pcb_line_t *line;
 	pcb_line_t *arc;
 	pcb_poly_t *poly;
+	pcb_pstk_t *ps;
 	gdl_iterator_t it;
-	
+
+	padstacklist_foreach(&data->padstack, &it, ps) {
+		if (pcb_attribute_get(&ps->Attributes, "openems::vport") != 0) {
+			switch(dir) {
+				case PCB_MESH_HORIZONTAL: mesh_add_edge(mesh, dir, ps->y); break;
+				case PCB_MESH_VERTICAL:   mesh_add_edge(mesh, dir, ps->x); break;
+			}
+		}
+	}
 
 	linelist_foreach(&mesh->layer->Line, &it, line) {
 		pcb_coord_t x1 = line->Point1.X, y1 = line->Point1.Y, x2 = line->Point2.X, y2 = line->Point2.Y;
