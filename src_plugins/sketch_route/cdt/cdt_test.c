7d116112ef21afd052edd0ac6a1c133cc1a83a3d
wojciechk8 <wojciechk8@1d61de99-560f-4dba-8957-1e505d42358b>
2018-06-24 23:07:58 +0000

    [sketch_route] -Add: tricky test case for triangulate_polygon
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@17666 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/sketch_route/cdt/cdt_test.c
+++ src_plugins/sketch_route/cdt/cdt_test.c
@@ -6,14 +6,14 @@ cdt_t cdt;
 
 int main(void)
 {
-	point_t *p, *p1, *p2;
+	point_t *p, *p1, *p2, *pd[500];
 	edge_t *e;
 	int i;
 	clock_t t;
 	pointlist_node_t *p_violations = NULL;
 
-	cdt_init(&cdt, 1000, 1000, 5000, 5000);
-	/*cdt_init(&cdt, 0, 0, 100000, 100000);*/
+	/*cdt_init(&cdt, 1000, 1000, 5000, 5000);*/
+	cdt_init(&cdt, 0, 0, 100000, 100000);
 
 	/*
 	cdt_insert_point(&cdt, 2500, 3000);
@@ -94,6 +94,7 @@ int main(void)
 	*/
 
 	/* constrained edge 2 */
+	/*
 	p1 = cdt_insert_point(&cdt, 1500, 3800);
 	cdt_insert_point(&cdt, 2700, 3000);
 	cdt_insert_point(&cdt, 2800, 3000);
@@ -106,7 +107,27 @@ int main(void)
 	cdt_insert_point(&cdt, 3000, 4800);
 
 	e = cdt_insert_constrained_edge(&cdt, p1, p2);
+	*/
 
+	/*
+	for (i = 0; i < 500; i++) {
+		cdt_insert_point(&cdt, rand()%99999 + 1, rand()%99999 + 1);
+		pd[i] = cdt_insert_point(&cdt, rand()%99999 + 1, rand()%99999 + 1);
+	}
+	for (i = 0; i < 500; i++) {
+		cdt_delete_point(&cdt, pd[i]);
+	}
+	*/
+
+	/* tricky case for triangulate_polygon */
+	p = cdt_insert_point(&cdt, 98347, 51060);
+	cdt_insert_point(&cdt, 92086, 47852);
+	cdt_insert_point(&cdt, 95806, 44000);
+	cdt_insert_point(&cdt, 95030, 41242);
+	cdt_insert_point(&cdt, 94644, 55629);
+	cdt_insert_point(&cdt, 94000, 45300);
+
+	cdt_delete_point(&cdt, p);
 
 	if (cdt_check_delaunay(&cdt, &p_violations, NULL))
 		fprintf(stderr, "delaunay\n");
