dd3849c53ce336e373acbfb0129948fcf4e8824b
wojciechk8 <wojciechk8@1d61de99-560f-4dba-8957-1e505d42358b>
2018-06-25 17:59:51 +0000

    [sketch_route] -Del: 'unconstrain' func was supposed to do the same as delete_constrained_edge
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@17697 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/sketch_route/cdt/cdt.h
+++ src_plugins/sketch_route/cdt/cdt.h
@@ -33,7 +33,6 @@ point_t *cdt_insert_point(cdt_t *cdt, coord_t x, coord_t y);
 void cdt_delete_point(cdt_t *cdt, point_t *p);	/* any edge adjecent to this point cannot be constrained */
 edge_t *cdt_insert_constrained_edge(cdt_t *cdt, point_t *p1, point_t *p2);
 void cdt_delete_constrained_edge(cdt_t *cdt, edge_t *edge);
-void unconstrain(cdt_t *cdt, edge_t *edge);
 
 int cdt_check_delaunay(cdt_t *cdt, pointlist_node_t **point_violations, trianglelist_node_t **triangle_violations);
 void cdt_dump_animator(cdt_t *cdt, int show_circles, pointlist_node_t *point_violations, trianglelist_node_t *triangle_violations);
