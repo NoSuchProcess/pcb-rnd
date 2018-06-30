bfa1d12cb9956a6c06877f5f329d2b768785b003
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-06-30 14:05:52 +0000

    [fungw] -Update: switch over pcb_act_ChangeNonetlist()
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@17824 1d61de99-560f-4dba-8957-1e505d42358b

--- src/change_act.c
+++ src/change_act.c
@@ -684,12 +684,13 @@ Note that @code{Pins} means both pins and pads.
 
 %end-doc */
 
-static fgw_error_t pcb_act_ChangeNonetlist(fgw_arg_t *ores, int oargc, fgw_arg_t *oargv)
+static fgw_error_t pcb_act_ChangeNonetlist(fgw_arg_t *res, int argc, fgw_arg_t *argv)
 {
-	PCB_OLD_ACT_BEGIN;
-	const char *function = PCB_ACTION_ARG(0);
-	if (function) {
-		switch (pcb_funchash_get(function, NULL)) {
+	int op;
+
+	PCB_ACT_CONVARG(1, FGW_KEYWORD, ChangeJoin, op = fgw_keyword(&argv[1]));
+
+	switch(op) {
 		case F_ToggleObject:
 		case F_Object:
 		case F_Element:
@@ -711,10 +712,13 @@ static fgw_error_t pcb_act_ChangeNonetlist(fgw_arg_t *ores, int oargc, fgw_arg_t
 			if (pcb_chg_selected_nonetlist(PCB_OBJ_SUBC))
 				pcb_board_set_changed_flag(pcb_true);
 			break;
-		}
+
+		default:
+			return FGW_ERR_ARG_CONV;
 	}
+
+	PCB_ACT_IRES(0);
 	return 0;
-	PCB_OLD_ACT_END;
 }
 
 
