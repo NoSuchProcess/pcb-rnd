28d6a496ee1e3c5fd3970546487d37859d013694
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-07-02 03:55:46 +0000

    [fungw] -Update: pcb_gtk_act_logshowonappend() switched over, remove 'o' prefix from header
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@17883 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_gtk_common/dlg_log.h
+++ src_plugins/lib_gtk_common/dlg_log.h
@@ -15,6 +15,6 @@ void pcb_gtk_logv(int hid_active, enum pcb_message_level level, const char *fmt,
 /* Actions */
 extern const char pcb_gtk_acts_logshowonappend[];
 extern const char pcb_gtk_acth_logshowonappend[];
-fgw_error_t pcb_gtk_act_logshowonappend(fgw_arg_t *ores, int oargc, fgw_arg_t *oargv);
+fgw_error_t pcb_gtk_act_logshowonappend(fgw_arg_t *res, int argc, fgw_arg_t *argv);
 
 #endif /* PCB_GTK_DLG_LOG_H */
