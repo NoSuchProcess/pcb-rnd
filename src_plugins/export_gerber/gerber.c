f9e162655a7adce00d9c30a5cf78137abbfeb791
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-07-11 07:18:51 +0000

    [gerber] -Fix: reset lastgroup between the aperture mapping run and the actual export run - corner case: if there is only one group being exported, it's found "old" on the second run
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@18242 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/export_gerber/gerber.c
+++ src_plugins/export_gerber/gerber.c
@@ -650,6 +650,7 @@ static void gerber_do_export(pcb_hid_attr_val_t * options)
 	finding_apertures = 1;
 	pcb_hid_expose_all(&gerber_hid, &ctx);
 
+	lastgroup = -2;
 	layer_list_idx = 0;
 	finding_apertures = 0;
 	pcb_hid_expose_all(&gerber_hid, &ctx);
