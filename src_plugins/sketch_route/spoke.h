baf4a867e8d27fac7ad9b6c666c3b4a0e7558f8b
wojciechk8 <wojciechk8@1d61de99-560f-4dba-8957-1e505d42358b>
2018-07-15 12:06:40 +0000

    [sketch_route] -Add: spoke direction enum
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@18388 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/sketch_route/spoke.h
+++ src_plugins/sketch_route/spoke.h
@@ -7,6 +7,13 @@
 #include <genvector/vtp0.h>
 
 
+typedef enum {
+  SPOKE_DIR_1PI4 = 0,
+  SPOKE_DIR_3PI4 = 1,
+  SPOKE_DIR_5PI4 = 2,
+  SPOKE_DIR_7PI4 = 3
+} spoke_dir_t;
+
 struct spoke_s {
 	pcb_box_t bbox;
 	vtp0_t slots;
