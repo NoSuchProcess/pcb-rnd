11696f0da22cb3df4c487c2bd2aea2e2c90feb49
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-07-17 03:46:47 +0000

    [fp_wget] -Add: internal version of the config file, register dedicated user config
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@18444 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/fp_wget/fp_wget.c
+++ src_plugins/fp_wget/fp_wget.c
@@ -3,13 +3,18 @@
 #include "edakrill.h"
 #include "plugins.h"
 #include "fp_wget_conf.h"
+#include "../src_plugins/fp_wget/conf_internal.c"
+
 
 conf_fp_wget_t conf_fp_wget;
+
+#define FP_WGET_CONF_FN "fp_wget.conf"
 
 int pplg_check_ver_fp_wget(int ver_needed) { return 0; }
 
 void pplg_uninit_fp_wget(void)
 {
+	conf_unreg_file(FP_WGET_CONF_FN, fp_wget_conf_internal);
 	fp_gedasymbols_uninit();
 	fp_edakrill_uninit();
 	conf_unreg_fields("plugins/fp_wget/");
@@ -22,6 +27,8 @@ int pplg_init_fp_wget(void)
 #define conf_reg(field,isarray,type_name,cpath,cname,desc,flags) \
 	conf_reg_field(conf_fp_wget, field,isarray,type_name,cpath,cname,desc,flags);
 #include "fp_wget_conf_fields.h"
+
+	conf_reg_file(FP_WGET_CONF_FN, fp_wget_conf_internal);
 
 	fp_gedasymbols_init();
 	fp_edakrill_init();
