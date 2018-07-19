9e99f1b7cdecacbe841c64f95e9dd1132618f085
avigne <avigne@1d61de99-560f-4dba-8957-1e505d42358b>
2018-07-19 11:45:52 +0000

    [doc] -Add: TCL example.
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@18533 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ doc/user/06_feature/scripting/rosetta/10_hello/ex.tcl
@@ -0,0 +1,9 @@
+# An action without arguments that prints hello world in the log window
+# Returns 0 for success.
+proc hello {} {
+	message "Hello world!"
+	return 0
+}
+
+# register hello() action
+fgw_func_reg "hello"
