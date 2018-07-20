bd36bd8b9b4267c09444592cc46f90c2f0074a27
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-07-20 03:39:32 +0000

    [cli] -Change: gtk top window command entry prints the configure prompt instead of hardwired "Enter command"
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@18536 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_gtk_common/dlg_command.c
+++ src_plugins/lib_gtk_common/dlg_command.c
@@ -413,7 +413,7 @@ void ghid_handle_user_command(pcb_gtk_command_t *ctx, pcb_bool raise)
 		ghid_command_window_show(ctx, raise);
 	else {
 		command =
-			ghid_command_entry_get(ctx, _("Enter command:"), (conf_core.editor.save_last_command && previous) ? previous : (gchar *) "");
+			ghid_command_entry_get(ctx, pcb_cli_prompt(":"), (conf_core.editor.save_last_command && previous) ? previous : (gchar *) "");
 		if (command != NULL) {
 			/* copy new command line to save buffer */
 			g_free(previous);
