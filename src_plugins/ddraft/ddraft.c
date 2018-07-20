bdb567c6b2de8534bb8b2b112d413bd045f5ddc9
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-07-20 15:15:58 +0000

    [ddraft] -Add: trim(): dual get coords in the obj-obj setup
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@18563 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/ddraft/ddraft.c
+++ src_plugins/ddraft/ddraft.c
@@ -36,20 +36,76 @@
 #include "hid_init.h"
 #include "plugins.h"
 #include "actions.h"
+#include "funchash_core.h"
+#include "search.h"
 
 static const char *ddraft_cookie = "ddraft plugin";
 
-static const char pcb_acts_trim[] = "trim(selected|found|object, selected|found|object)\n";
+#define EDGE_TYPES (PCB_OBJ_LINE | PCB_OBJ_ARC)
+#define CUT_TYPES (PCB_OBJ_LINE | PCB_OBJ_ARC)
+
+static const char pcb_acts_trim[] = "trim([selected|found|object], [selected|found|object])\n";
 static const char pcb_acth_trim[] = "Use one or more objects as cutting edge and trim other objects. First argument is the cutting edge";
 static fgw_error_t pcb_act_trim(fgw_arg_t *res, int argc, fgw_arg_t *argv)
 {
-	const char *scut, *sobj;
-	int rs = -1;
+	int kwcut = F_Object, kwobj = F_Object;
+	pcb_objtype_t type;
+	void *ptr1, *ptr2, *ptr3;
+	pcb_any_obj_t *cedge;
+	pcb_coord_t x, y;
 
-	PCB_ACT_CONVARG(1, FGW_STR, trim, scut = argv[1].val.str);
-	PCB_ACT_CONVARG(2, FGW_STR, trim, sobj = argv[2].val.str);
+	PCB_ACT_MAY_CONVARG(1, FGW_KEYWORD, trim, kwcut = fgw_keyword(&argv[1]));
+	PCB_ACT_MAY_CONVARG(2, FGW_KEYWORD, trim, kwobj = fgw_keyword(&argv[2]));
 
-	PCB_ACT_IRES(rs);
+	switch(kwcut) {
+		case F_Object:
+			pcb_hid_get_coords("Select cutting edge object", &x, &y, 0);
+			type = pcb_search_screen(x, y, EDGE_TYPES, &ptr1, &ptr2, &ptr3);
+			if (type == 0) {
+				pcb_message(PCB_MSG_ERROR, "Invalid cutting edge object\n");
+				goto err;
+			}
+			cedge = ptr2;
+			switch(kwobj) {
+				case F_Object:
+					for(;;) {
+						x = PCB_MAX_COORD;
+						pcb_hid_get_coords("Select an object to cut or press esc", &x, &y, 1);
+						if (x == PCB_MAX_COORD)
+							break;
+
+						type = pcb_search_screen(x, y, CUT_TYPES, &ptr1, &ptr2, &ptr3);
+						if (type == 0) {
+							pcb_message(PCB_MSG_ERROR, "Can't cut that object\n");
+							continue;
+						}
+						pcb_printf("CUT! %p %p %mm %mm\n", cedge, ptr2, x, y);
+					}
+					break;
+				default: goto err2;
+			}
+			break;
+		case F_Selected:
+			switch(kwobj) {
+				case F_Selected:
+					pcb_message(PCB_MSG_ERROR, "Both cutting edge and objects to cut can't be selected\n");
+					goto err;
+				default: goto err2;
+			}
+			break;
+		default:
+			pcb_message(PCB_MSG_ERROR, "Invalid first argument\n");
+			goto err;
+	}
+
+	PCB_ACT_IRES(0);
+	return 0;
+
+	err2:;
+	pcb_message(PCB_MSG_ERROR, "Invalid second argument\n");
+
+	err:;
+	PCB_ACT_IRES(-1);
 	return 0;
 }
 
