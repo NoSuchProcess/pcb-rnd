a5dd46e1f099aa2ccce7e73f2a04c2c9d70e1b79
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-07-21 07:45:38 +0000

    [ddraft] -Add: low level for line-line trim
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@18576 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/ddraft/trim.c
+++ src_plugins/ddraft/trim.c
@@ -26,13 +26,50 @@
  *    mailing list: pcb-rnd (at) list.repo.hu (send "subscribe")
  */
 
+static int pcb_trim_with_line(pcb_line_t *cut_edge, pcb_any_obj_t *obj, pcb_coord_t rem_x, pcb_coord_t rem_y)
+{
+	int p;
+	double d1, d2;
+	pcb_box_t ip;
+
+	switch(obj->type) {
+		case PCB_OBJ_LINE:
+			{
+				pcb_line_t *l = (pcb_line_t *)obj;
+				p = pcb_intersect_cline_cline(cut_edge, l, &ip);
+				switch(p) {
+					case 0: return 0;
+					case 1:
+						/* do not trim if the result would be a zero-length line */
+						if ((ip.X1 == l->Point1.X) && (ip.Y1 == l->Point1.Y)) return 0;
+						if ((ip.X1 == l->Point2.X) && (ip.Y1 == l->Point2.Y)) return 0;
+
+						/* determine which endpoint to move (the one closer to the rem point) */
+						d1 = pcb_distance2(rem_x, rem_y, l->Point1.X, l->Point1.Y);
+						d2 = pcb_distance2(rem_x, rem_y, l->Point2.X, l->Point2.Y);
+						pcb_line_pre(l);
+						if (d1 < d2) {
+							l->Point1.X = ip.X1;
+							l->Point1.Y = ip.Y1;
+						}
+						else {
+							l->Point2.X = ip.X1;
+							l->Point2.Y = ip.Y1;
+						}
+						pcb_line_post(l);
+				}
+			}
+			break;
+		default: return -1;
+	}
+	return 0;
+}
+
 int pcb_trim(pcb_any_obj_t *cut_edge, pcb_any_obj_t *obj, pcb_coord_t rem_x, pcb_coord_t rem_y)
 {
 pcb_trace("trim: %p %p %mm %mm\n", cut_edge, obj, rem_x, rem_y);
 	switch(cut_edge->type) {
-/*
-		case PCB_OBJ_LINE: pcb_trim_with_line((pcb_line_t *)cut_edge, obj, rem_x, rem_y);
-*/
+		case PCB_OBJ_LINE: return pcb_trim_with_line((pcb_line_t *)cut_edge, obj, rem_x, rem_y);
 		default:
 			return -1;
 	}
