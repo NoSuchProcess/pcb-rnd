54b7ae80980e6fe784dfd2aff2769a991da6dfe7
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-07-21 06:44:43 +0000

    [ddraft] -Import: line-line intersection code from core
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@18570 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/ddraft/centgeo.c
+++ src_plugins/ddraft/centgeo.c
@@ -25,3 +25,53 @@
  *    lead developer: email to pcb-rnd (at) igor2.repo.hu
  *    mailing list: pcb-rnd (at) list.repo.hu (send "subscribe")
  */
+
+#include "obj_line.h"
+
+/* Same basic algo as in find_geo.c - see comment for the algo
+   description there */
+int pcb_intersect_cline_cline(pcb_line_t *Line1, pcb_line_t *Line2, pcb_box_t *ip)
+{
+	double s, r;
+	double line1_dx, line1_dy, line2_dx, line2_dy, point1_dx, point1_dy;
+
+	/* setup some constants */
+	line1_dx = Line1->Point2.X - Line1->Point1.X;
+	line1_dy = Line1->Point2.Y - Line1->Point1.Y;
+	line2_dx = Line2->Point2.X - Line2->Point1.X;
+	line2_dy = Line2->Point2.Y - Line2->Point1.Y;
+	point1_dx = Line1->Point1.X - Line2->Point1.X;
+	point1_dy = Line1->Point1.Y - Line2->Point1.Y;
+
+	/* If either line is a point, we have failed already, since the
+	 *   endpoint check above will have caught an "intersection". */
+	if ((line1_dx == 0 && line1_dy == 0)
+			|| (line2_dx == 0 && line2_dy == 0))
+		return pcb_false;
+
+	/* set s to cross product of Line1 and the line
+	 *   Line1.Point1--Line2.Point1 (as vectors) */
+	s = point1_dy * line1_dx - point1_dx * line1_dy;
+
+	/* set r to cross product of both lines (as vectors) */
+	r = line1_dx * line2_dy - line1_dy * line2_dx;
+
+	/* No cross product means parallel lines, or maybe Line2 is
+	 *  zero-length. In either case, since we did a bounding-box
+	 *  check before getting here, the above pcb_is_point_in_line() checks
+	 *  will have caught any intersections. */
+	if (r == 0.0)
+		return pcb_false;
+
+	s /= r;
+	r = (point1_dy * line2_dx - point1_dx * line2_dy) / r;
+
+	/* intersection is at least on AB */
+	if (r >= 0.0 && r <= 1.0)
+		return (s >= 0.0 && s <= 1.0);
+
+	/* intersection is at least on CD */
+	/* [removed this case since it always returns pcb_false --asp] */
+	return pcb_false;
+}
+
