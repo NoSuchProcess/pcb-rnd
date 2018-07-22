e12babfdfcc8207768e5725b38358dea1d7e84d2
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-07-22 09:22:37 +0000

    [ddraft] -Change: convert line-arc intersection code to centgeo API
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@18605 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/ddraft/centgeo.c
+++ src_plugins/ddraft/centgeo.c
@@ -30,6 +30,8 @@
 #include "centgeo.h"
 
 #include "obj_line.h"
+#include "obj_arc.h"
+#include "math_helper.h"
 #include "compat_misc.h"
 
 /* Same basic algo as in find_geo.c - see comment for the algo
@@ -135,8 +137,7 @@ double pcb_cline_pt_offs(pcb_line_t *line, pcb_coord_t px, pcb_coord_t py)
 	return (line_dx * pt_dx + line_dy * pt_dy) / (line_dx*line_dx + line_dy*line_dy);
 }
 
-#if 0
-pcb_bool pcb_intersect_cline_carc(pcb_line_t *Line, pcb_arc_t *Arc)
+int pcb_intersect_cline_carc(pcb_line_t *Line, pcb_arc_t *Arc, pcb_box_t *ip, double offs[2])
 {
 	double dx, dy, dx1, dy1, l, d, r, r2, Radius;
 	pcb_coord_t ex, ey;
@@ -150,39 +151,37 @@ pcb_bool pcb_intersect_cline_carc(pcb_line_t *Line, pcb_arc_t *Arc)
 	d *= d;
 
 	/* use the larger diameter circle first */
-	Radius = Arc->Width + MAX(0.5 * (Arc->Thickness + Line->Thickness) + Bloat, 0.0);
+	Radius = Arc->Width;
 	Radius *= Radius;
 	r2 = Radius * l - d;
 	/* projection doesn't even intersect circle when r2 < 0 */
 	if (r2 < 0)
 		return pcb_false;
-	/* check the ends of the line in case the projected point */
-	/* of intersection is beyond the line end */
-	if (pcb_is_point_on_arc(Line->Point1.X, Line->Point1.Y, MAX(0.5 * Line->Thickness + Bloat, 0.0), Arc))
+
+	/* check the ends of the line in case the projected point of intersection is beyond the line end */
+	if (pcb_is_point_on_arc(Line->Point1.X, Line->Point1.Y, 0, Arc))
 		return pcb_true;
-	if (pcb_is_point_on_arc(Line->Point2.X, Line->Point2.Y, MAX(0.5 * Line->Thickness + Bloat, 0.0), Arc))
+	if (pcb_is_point_on_arc(Line->Point2.X, Line->Point2.Y, 0, Arc))
 		return pcb_true;
 	if (l == 0.0)
 		return pcb_false;
+
 	r2 = sqrt(r2);
 	Radius = -(dx * dx1 + dy * dy1);
 	r = (Radius + r2) / l;
-	if (r >= 0 && r <= 1
-			&& pcb_is_point_on_arc(Line->Point1.X + r * dx, Line->Point1.Y + r * dy, MAX(0.5 * Line->Thickness + Bloat, 0.0) + 1, Arc))
+	if (r >= 0 && r <= 1 && pcb_is_point_on_arc(Line->Point1.X + r * dx, Line->Point1.Y + r * dy, 1, Arc))
 		return pcb_true;
 	r = (Radius - r2) / l;
-	if (r >= 0 && r <= 1
-			&& pcb_is_point_on_arc(Line->Point1.X + r * dx, Line->Point1.Y + r * dy, MAX(0.5 * Line->Thickness + Bloat, 0.0) + 1, Arc))
+	if (r >= 0 && r <= 1 && pcb_is_point_on_arc(Line->Point1.X + r * dx, Line->Point1.Y + r * dy, 1, Arc))
 		return pcb_true;
 
 	/* check arc end points */
 	pcb_arc_get_end(Arc, 0, &ex, &ey);
-	if (pcb_is_point_in_line(ex, ey, Arc->Thickness * 0.5 + Bloat, (pcb_any_line_t *) Line))
+	if (pcb_is_point_in_line(ex, ey, 1, (pcb_any_line_t *) Line))
 		return pcb_true;
 
 	pcb_arc_get_end(Arc, 1, &ex, &ey);
-	if (pcb_is_point_in_line(ex, ey, Arc->Thickness * 0.5 + Bloat, (pcb_any_line_t *) Line))
+	if (pcb_is_point_in_line(ex, ey, 1, (pcb_any_line_t *) Line))
 		return pcb_true;
 	return pcb_false;
 }
-#endif
--- src_plugins/ddraft/centgeo.h
+++ src_plugins/ddraft/centgeo.h
@@ -12,6 +12,14 @@
    2 = overlapping segments (overlap endpoitns are stored in X1;Y1 and X2;Y2 of ip) */
 int pcb_intersect_cline_cline(pcb_line_t *Line1, pcb_line_t *Line2, pcb_box_t *ip, double offs[2]);
 
+/* Calculate the intersection point(s) of a lines and an arc and store them
+   in ip and/or offs (on Line) if they are not NULL. Returns:
+   0 = no intersection
+   1 = one intersection (X1;Y1 of ip is loaded)
+   2 = two intersections (stored in X1;Y1 and X2;Y2 of ip) */
+int pcb_intersect_cline_carc(pcb_line_t *Line, pcb_arc_t *Arc, pcb_box_t *ip, double offs[2]);
+
+
 /* Calculate the point on a line corresponding to a [0..1] offset and store
    the result in dstx;dsty. */
 void pcb_cline_offs(pcb_line_t *line, double offs, pcb_coord_t *dstx, pcb_coord_t *dsty);
