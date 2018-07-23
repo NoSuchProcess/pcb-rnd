d65e1cde15f5be452a60918815559465b7bb8305
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-07-23 02:59:05 +0000

    [ddraft] -Cleanup: simplify indentation
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@18607 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/ddraft/trim.c
+++ src_plugins/ddraft/trim.c
@@ -58,20 +58,18 @@ static int pcb_trim_line(vtp0_t *cut_edges, pcb_line_t *line, pcb_coord_t rem_x,
 		pcb_any_obj_t *cut_edge = (pcb_any_obj_t *)cut_edges->array[n];
 		switch(cut_edge->type) {
 			case PCB_OBJ_LINE:
-				{
-					p = pcb_intersect_cline_cline(line, (pcb_line_t *)cut_edge, NULL, io);
-					switch(p) {
-						case 0: continue; /* no intersection, skip to the next potential cutting edge */
-						case 2: /* io[0] is the non-endpoint */
-						case 1:
-							if (io[0] < remo) {
-								if (io[0] > mino) mino = io[0];
-							}
-							else {
-								if (io[0] < maxo) maxo = io[0];
-							}
-							break;
-					}
+				p = pcb_intersect_cline_cline(line, (pcb_line_t *)cut_edge, NULL, io);
+				switch(p) {
+					case 0: continue; /* no intersection, skip to the next potential cutting edge */
+					case 2: /* io[0] is the non-endpoint */
+					case 1:
+						if (io[0] < remo) {
+							if (io[0] > mino) mino = io[0];
+						}
+						else {
+							if (io[0] < maxo) maxo = io[0];
+						}
+						break;
 				}
 				break;
 			default: return -1;
