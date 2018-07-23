b9219725f60ebb772d751f195ce470d5f134a567
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-07-23 05:32:46 +0000

    [ddraft] -Add: line trims arc: intersection calls
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@18622 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/ddraft/trim.c
+++ src_plugins/ddraft/trim.c
@@ -121,6 +121,87 @@ static int pcb_trim_line(vtp0_t *cut_edges, pcb_line_t *line, pcb_coord_t rem_x,
 	return 1;
 }
 
+static int pcb_trim_arc(vtp0_t *cut_edges, pcb_arc_t *arc, pcb_coord_t rem_x, pcb_coord_t rem_y)
+{
+	int p, n;
+	double io[2];
+	double mino = 0.0, maxo = 1.0, remo = pcb_carc_pt_offs(arc, rem_x, rem_y);
+	pcb_coord_t x, y;
+
+	for(n = 0; n < vtp0_len(cut_edges); n++) {
+		pcb_any_obj_t *cut_edge = (pcb_any_obj_t *)cut_edges->array[n];
+
+		p = 0;
+		switch(cut_edge->type) {
+			case PCB_OBJ_LINE:
+				p = pcb_intersect_carc_cline(arc, (pcb_line_t *)cut_edge, NULL, io);
+				break;
+/*			case PCB_OBJ_ARC:
+				p = pcb_intersect_carc_carc(arc, (pcb_arc_t *)cut_edge, NULL, io);
+				break;*/
+			default: return -1;
+		}
+
+
+pcb_trace("p=%d %f %f\n", p, io[0], io[1]);
+		switch(p) {
+			case 0: continue; /* no intersection, skip to the next potential cutting edge */
+			case 2:
+				if (io[1] < remo) {
+					if (io[1] > mino) mino = io[1];
+				}
+				else {
+					if (io[1] < maxo) maxo = io[1];
+				}
+			case 1:
+				if (io[0] < remo) {
+					if (io[0] > mino) mino = io[0];
+				}
+				else {
+					if (io[0] < maxo) maxo = io[0];
+				}
+				break;
+		}
+	}
+
+pcb_trace("mino=%f maxo=%f\n", mino, maxo);
+
+	if ((mino == 0.0) && (maxo == 1.0))
+		return 0; /* nothing to be done */
+
+	if (mino == maxo)
+		return 0; /* refuse to end up with 0-length lines */
+
+
+return 0;
+#if 0
+	/* mino and maxo holds the two endpoint offsets after the cuts, in respect
+	   to the original line. Cut/split the line using them. */
+	if ((mino > 0.0) && (maxo < 1.0)) { /* remove (shortest) middle section */
+		pcb_line_t *new_line = pcb_line_dup(line->parent.layer, line);
+
+		pcb_undo_add_obj_to_create(PCB_OBJ_LINE, new_line->parent.layer, new_line, new_line);
+
+		pcb_cline_offs(line, maxo, &x, &y);
+		move_lp(line, 1, x, y);
+
+		pcb_cline_offs(new_line, mino, &x, &y);
+		move_lp(new_line, 2, x, y);
+	}
+	else if ((mino == 0.0) && (maxo < 1.0)) { /* truncate at point1 (shortest overdue) */
+		pcb_cline_offs(line, maxo, &x, &y);
+		move_lp(line, 1, x, y);
+	}
+	else if ((mino > 0.0) && (maxo == 1.0)) { /* truncate at point2 (shortest overdue) */
+		pcb_cline_offs(line, mino, &x, &y);
+		move_lp(line, 2, x, y);
+	}
+	else
+		return -1;
+#endif
+	return 1;
+}
+
 static pcb_line_t *split_lp(pcb_line_t *line, double offs)
 {
 	pcb_coord_t x, y;
@@ -191,6 +272,12 @@ int pcb_trim_split(vtp0_t *cut_edges, pcb_any_obj_t *obj, pcb_coord_t rem_x, pcb
 				res = pcb_trim_line(cut_edges, (pcb_line_t *)obj, rem_x, rem_y);
 			else
 				res = pcb_split_line(cut_edges, (pcb_line_t *)obj, rem_x, rem_y);
+			break;
+		case PCB_OBJ_ARC:
+			if (trim)
+				res = pcb_trim_arc(cut_edges, (pcb_line_t *)obj, rem_x, rem_y);
+/*			else
+				res = pcb_split_arc(cut_edges, (pcb_line_t *)obj, rem_x, rem_y);*/
 			break;
 		default:
 			return -1;
