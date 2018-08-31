3aac73fd5567190c19883f98468834c8e5b697dd
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-08-31 06:13:39 +0000

    [lib_hid_common] -Add: cli central history load/save implementation
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@19051 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_hid_common/cli_history.c
+++ src_plugins/lib_hid_common/cli_history.c
@@ -26,7 +26,36 @@
 
 #include "config.h"
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <genlist/gendlist.h>
+
 #include "cli_history.h"
+#include "lib_hid_common.h"
+#include "safe_fs.h"
+#include "paths.h"
+
+#define CFG lib_hid_common_conf.plugins.lib_hid_common.cli_history
+
+typedef struct hist_t {
+	gdl_elem_t lst;
+	char cmd[1];
+} hist_t;
+
+/* Global CLI history: first entry is the oldest, last is the most recent one */
+static gdl_list_t *history;
+
+
+static void hist_append(const char *s)
+{
+	hist_t *h;
+	size_t len = strlen(s);
+	h = malloc(sizeof(hist_t) + len);
+	memcpy(h->cmd, s, len+1);
+	h->lst.parent = NULL;
+	gdl_append(history, h, lst);
+}
 
 void pcb_clihist_append(const char *cmd, pcb_clihist_append_cb_t *append, pcb_clihist_remove_cb_t *remove)
 {
@@ -35,11 +64,49 @@ void pcb_clihist_append(const char *cmd, pcb_clihist_append_cb_t *append, pcb_cl
 
 void pcb_clihist_load(void)
 {
+	FILE *f;
+	char *real_fn, *s, line[4096];
 
+	if ((CFG.file == NULL) || (CFG.slots < 1))
+		return;
+
+	real_fn = pcb_build_fn(CFG.file);
+	if (real_fn == NULL)
+		return;
+	f = pcb_fopen(real_fn, "r");
+	free(real_fn);
+	if (f == NULL)
+		return;
+
+	while((s = fgets(line, sizeof(line), f)) != NULL) {
+		while(isspace(*s)) s++;
+		if (*s == '\0')
+			continue;
+		hist_append(s);
+	}
+	fclose(f);
 }
 
 void pcb_clihist_save(void)
 {
+	FILE *f;
+	hist_t *h;
+	char *real_fn;
 
+	if ((CFG.file == NULL) || (CFG.slots < 1))
+		return;
+
+	real_fn = pcb_build_fn(CFG.file);
+	if (real_fn == NULL)
+		return;
+	f = pcb_fopen(real_fn, "w");
+	free(real_fn);
+	if (f == NULL)
+		return;
+
+	for(h = gdl_first(history); h != NULL; h = gdl_next(history, h))
+		fprintf(f, "%s\n", h->cmd);
+
+	fclose(f);
 }
 
