8ef549c18518c8410fa384ea9f45d36a4afd65ad
miloh <miloh@1d61de99-560f-4dba-8957-1e505d42358b>
2018-09-11 06:04:13 +0000

    [doc] -Update: reconsolidate the updated action dumping script into one
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@19302 1d61de99-560f-4dba-8957-1e505d42358b

--- doc/user/09_appendix/src/dump_actions_to_html.sh
+++ doc/user/09_appendix/src/dump_actions_to_html.sh
@@ -33,7 +33,47 @@ echo "<caption>\n" "<b>"
 echo $pcb_rnd_ver ", " $pcb_rnd_rev
 echo "</b>"
 echo  "<th>Action<th> Description <th> Syntax"
-./action_sorting.sh 2>/dev/null
+(
+	cd ../../../../src
+	./pcb-rnd --dump-actions 2>/dev/null
+) | awk '
+
+function flush_sd()
+{
+		if ( a != "" ||  s != "" || d != "" ) {
+			sub("^<br>", "", a)
+			sub("^<br>", "", d)
+			sub("^<br>", "", s)
+			print  "<tr><td>" a "</td>" "<td>" d "</td>" "<td>" s "</td>"
+			}
+		
+	a=""
+	s=""
+	d=""
+}
+
+
+/^A/ {
+	flush_sd()
+	sub("^A", "", $0)
+	a = a "<br>" $0
+	next
+}
+
+/^D/ {
+	sub("^D", "", $0)
+	d = d "<br>" $0
+	next
+}
+
+/^S/ {
+	sub("^S", "", $0)
+	s = s "<br>" $0
+	next
+}
+
+' | sort -u
+
 echo "
 </table>
 </body>
