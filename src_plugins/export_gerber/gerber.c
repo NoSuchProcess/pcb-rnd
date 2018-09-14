39a80cedd10fb9e3bb2b034876a23c99495434dd
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-09-14 06:42:19 +0000

    [export_gerber] -Cleanup: indentation
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@19334 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/export_gerber/gerber.c
+++ src_plugins/export_gerber/gerber.c
@@ -740,29 +740,28 @@ static int drill_sort(const void *va, const void *vb)
 
 static void drill_print_holes(ApertureList *apl, PendingDrills *pd, pcb_cardinal_t npd)
 {
-		pcb_cardinal_t i;
+	pcb_cardinal_t i;
 	Aperture *search;
 
-			/* We omit the ,TZ here because we are not omitting trailing zeros.  Our format is
-			   always six-digit 0.1 mil resolution (i.e. 001100 = 0.11") */
-			fprintf(f, "M48\r\n" "INCH\r\n");
-			for (search = apl->data; search; search = search->next)
-				pcb_fprintf(f, "T%02dC%.3mi\r\n", search->dCode, search->width);
-			fprintf(f, "%%\r\n");
-			/* FIXME */
+	/* We omit the ,TZ here because we are not omitting trailing zeros.  Our format is
+	   always six-digit 0.1 mil resolution (i.e. 001100 = 0.11") */
+	fprintf(f, "M48\r\n" "INCH\r\n");
+	for (search = apl->data; search; search = search->next)
+		pcb_fprintf(f, "T%02dC%.3mi\r\n", search->dCode, search->width);
+	fprintf(f, "%%\r\n");
 
-		/* dump pending drills in sequence */
-		qsort(pd, npd, sizeof(pd[0]), drill_sort);
-		for (i = 0; i < npd; i++) {
-			if (i == 0 || pd[i].diam != pd[i - 1].diam) {
-				Aperture *ap = findAperture(apl, pd[i].diam, ROUND);
-				fprintf(f, "T%02d\r\n", ap->dCode);
-			}
-			if (pd[i].is_slot)
-				pcb_fprintf(f, "X%06.0mkY%06.0mkG85X%06.0mkY%06.0mk\r\n", gerberDrX(PCB, pd[i].x), gerberDrY(PCB, pd[i].y), gerberDrX(PCB, pd[i].x2), gerberDrY(PCB, pd[i].y2));
-			else
-				pcb_fprintf(f, "X%06.0mkY%06.0mk\r\n", gerberDrX(PCB, pd[i].x), gerberDrY(PCB, pd[i].y));
+	/* dump pending drills in sequence */
+	qsort(pd, npd, sizeof(pd[0]), drill_sort);
+	for (i = 0; i < npd; i++) {
+		if (i == 0 || pd[i].diam != pd[i - 1].diam) {
+			Aperture *ap = findAperture(apl, pd[i].diam, ROUND);
+			fprintf(f, "T%02d\r\n", ap->dCode);
 		}
+		if (pd[i].is_slot)
+			pcb_fprintf(f, "X%06.0mkY%06.0mkG85X%06.0mkY%06.0mk\r\n", gerberDrX(PCB, pd[i].x), gerberDrY(PCB, pd[i].y), gerberDrX(PCB, pd[i].x2), gerberDrY(PCB, pd[i].y2));
+		else
+			pcb_fprintf(f, "X%06.0mkY%06.0mk\r\n", gerberDrX(PCB, pd[i].x), gerberDrY(PCB, pd[i].y));
+	}
 }
 
 static int gerber_set_layer_group(pcb_layergrp_id_t group, const char *purpose, int purpi, pcb_layer_id_t layer, unsigned int flags, int is_empty)
