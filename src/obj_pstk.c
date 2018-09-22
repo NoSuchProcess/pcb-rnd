1f50e532e430911e6c93a429909af2da68889ccf
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-09-22 05:29:50 +0000

    [pstk] -Split: poly shape draw from the solid poly draw into a local function (to be reused for thindraw later)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@19465 1d61de99-560f-4dba-8957-1e505d42358b

--- src/obj_pstk.c
+++ src/obj_pstk.c
@@ -362,6 +362,53 @@ static void set_ps_annot_color(pcb_hid_gc_t gc, pcb_pstk_t *ps)
 		conf_core.appearance.color.selected : conf_core.appearance.color.padstackmark);
 }
 
+static void pcb_pstk_draw_poly(pcb_draw_info_t *info, pcb_hid_gc_t gc, pcb_pstk_t *ps, pcb_pstk_shape_t *shape, int fill, pcb_coord_t dthick)
+{
+	if (shape->data.poly.pa == NULL)
+		pcb_pstk_shape_update_pa(&shape->data.poly);
+	pcb_hid_set_line_cap(gc, pcb_cap_round);
+	if (dthick != 0) {
+		/* slow - but would be used on export mostly, not on-screen drawing */
+		pcb_polo_t *p, p_st[32];
+		pcb_coord_t *x, *y, xy_st[64];
+		double vl = pcb_round(-dthick/2);
+		long n;
+
+		if (!shape->data.poly.inverted)
+			dthick = -dthick;
+		vl = pcb_round(dthick/2);
+		
+		if (shape->data.poly.len >= sizeof(p_st) / sizeof(p_st[0])) {
+			p = malloc(sizeof(pcb_polo_t) * shape->data.poly.len);
+			x = malloc(sizeof(pcb_coord_t) * shape->data.poly.len);
+		}
+		else {
+			p = p_st;
+			x = xy_st;
+		}
+		y = x + sizeof(p_st) / sizeof(p_st[0]);
+
+		/* relative: move each point radially */
+		for(n = 0; n < shape->data.poly.len; n++) {
+			p[n].x = shape->data.poly.x[n];
+			p[n].y = shape->data.poly.y[n];
+		}
+		pcb_polo_norms(p, shape->data.poly.len);
+		pcb_polo_offs(vl, p, shape->data.poly.len);
+		for(n = 0; n < shape->data.poly.len; n++) {
+			x[n] = pcb_round(p[n].x);
+			y[n] = pcb_round(p[n].y);
+		}
+		pcb_gui->fill_polygon_offs(gc, shape->data.poly.len, x, y, ps->x, ps->y);
+		if (p != p_st) {
+			free(p);
+			free(x);
+		}
+	}
+	else
+		pcb_gui->fill_polygon_offs(gc, shape->data.poly.len, shape->data.poly.x, shape->data.poly.y, ps->x, ps->y);
+}
+
 static void pcb_pstk_draw_shape_solid(pcb_draw_info_t *info, pcb_hid_gc_t gc, pcb_pstk_t *ps, pcb_pstk_shape_t *shape)
 {
 	pcb_coord_t r, dthick = 0;
@@ -370,49 +417,7 @@ static void pcb_pstk_draw_shape_solid(pcb_draw_info_t *info, pcb_hid_gc_t gc, pc
 
 	switch(shape->shape) {
 		case PCB_PSSH_POLY:
-			if (shape->data.poly.pa == NULL)
-				pcb_pstk_shape_update_pa(&shape->data.poly);
-			pcb_hid_set_line_cap(gc, pcb_cap_round);
-			if (dthick != 0) {
-				/* slow - but would be used on export mostly, not on-screen drawing */
-				pcb_polo_t *p, p_st[32];
-				pcb_coord_t *x, *y, xy_st[64];
-				double vl = pcb_round(-dthick/2);
-				long n;
-
-				if (!shape->data.poly.inverted)
-					dthick = -dthick;
-				vl = pcb_round(dthick/2);
-				
-				if (shape->data.poly.len >= sizeof(p_st) / sizeof(p_st[0])) {
-					p = malloc(sizeof(pcb_polo_t) * shape->data.poly.len);
-					x = malloc(sizeof(pcb_coord_t) * shape->data.poly.len);
-				}
-				else {
-					p = p_st;
-					x = xy_st;
-				}
-				y = x + sizeof(p_st) / sizeof(p_st[0]);
-
-				/* relative: move each point radially */
-				for(n = 0; n < shape->data.poly.len; n++) {
-					p[n].x = shape->data.poly.x[n];
-					p[n].y = shape->data.poly.y[n];
-				}
-				pcb_polo_norms(p, shape->data.poly.len);
-				pcb_polo_offs(vl, p, shape->data.poly.len);
-				for(n = 0; n < shape->data.poly.len; n++) {
-					x[n] = pcb_round(p[n].x);
-					y[n] = pcb_round(p[n].y);
-				}
-				pcb_gui->fill_polygon_offs(gc, shape->data.poly.len, x, y, ps->x, ps->y);
-				if (p != p_st) {
-					free(p);
-					free(x);
-				}
-			}
-			else
-				pcb_gui->fill_polygon_offs(gc, shape->data.poly.len, shape->data.poly.x, shape->data.poly.y, ps->x, ps->y);
+			pcb_pstk_draw_poly(info, gc, ps, shape, 1, dthick);
 			break;
 		case PCB_PSSH_LINE:
 			pcb_hid_set_line_cap(gc, shape->data.line.square ? pcb_cap_square : pcb_cap_round);
