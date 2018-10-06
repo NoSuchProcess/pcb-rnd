7f0750b353e7a26c92f8bb7acefca34c5981cb4f
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-10-06 08:11:38 +0000

    [io_dsn] -Add: write wires with layer name and net
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@19864 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_dsn/write.c
+++ src_plugins/io_dsn/write.c
@@ -32,18 +32,86 @@
 #include "error.h"
 #include "pcb_bool.h"
 #include "board.h"
+#include "data.h"
+#include "layer.h"
+#include "layer_grp.h"
 
 #include "write.h"
+
+#include "../src_plugins/lib_netmap/netmap.h"
 
 typedef struct {
 	FILE *f;
 	pcb_board_t *pcb;
+	pcb_netmap_t nmap;
 } dsn_write_t;
 
+#define GNAME_MAX 64
 
-int dsn_write_board(dsn_write_t *wctx)
+static void group_name(char *dst, const char *src, pcb_layergrp_id_t gid)
+{
+	int n;
+	char *d;
+	const char*s;
+
+	n = sprintf(dst, "%d__", (int)gid);
+
+	for(d = dst+n, s = src; (*s != '\0') && (n < GNAME_MAX - 1); s++,d++,n++) {
+		if ((*s == '"') || (*s < 32) || (*s > 126))
+			*d = '_';
+		else
+			*d = *s;
+	}
+	*d = '\0';
+}
+
+#define COORDX(x) (x)
+#define COORDY(y) (PCB->MaxHeight - (y))
+
+static int dsn_write_wiring(dsn_write_t *wctx)
+{
+	pcb_layer_id_t lid;
+	pcb_layer_t *ly;
+
+	fprintf(wctx->f, "  (wiring\n");
+	for(ly = wctx->pcb->Data->Layer, lid = 0; lid < wctx->pcb->Data->LayerN; lid++,ly++) {
+		char gname[GNAME_MAX];
+		pcb_layergrp_id_t gid = pcb_layer_get_group_(ly);
+		pcb_layergrp_t *lg = pcb_get_layergrp(wctx->pcb, gid);
+		pcb_lib_menu_t *net;
+
+		if ((lg == NULL) || !(lg->ltype & PCB_LYT_COPPER))
+			continue;
+
+		group_name(gname, lg->name, gid);
+
+		PCB_LINE_LOOP(ly);
+		{
+			net = htpp_get(&wctx->nmap.o2n, (pcb_any_obj_t *)line);
+			pcb_fprintf(wctx->f,"    (wire (path \"%s\" %[4] %[4] %[4] %[4] %[4])", gname, line->Thickness,
+				COORDX(line->Point1.X), COORDY(line->Point1.Y),
+				COORDX(line->Point2.X), COORDY(line->Point2.Y));
+			if (net != NULL)
+				fprintf(wctx->f, "(net \"%s\")", net->Name+2);
+			fprintf(wctx->f, "(type protect))\n");
+		}
+		PCB_END_LOOP;
+
+	}
+	fprintf(wctx->f, "  )\n");
+	return 0;
+}
+
+
+static int dsn_write_board(dsn_write_t *wctx)
 {
 	const char *s;
+	int res = 0;
+
+	if (pcb_netmap_init(&wctx->nmap, wctx->pcb) != 0) {
+		pcb_message(PCB_MSG_ERROR, "Can not set up net map\n");
+		return -1;
+	}
 
 	fprintf(wctx->f, "(pcb ");
 	if ((wctx->pcb->Name != NULL) && (*wctx->pcb->Name != '\0')) {
@@ -65,7 +133,17 @@ int dsn_write_board(dsn_write_t *wctx)
 	fprintf(wctx->f, "    (host_version \"%s\")\n", PCB_VERSION);
 	fprintf(wctx->f, "  )\n");
 
+	/* set units to mm with nm resolution */
+	fprintf(wctx->f, "  (resolution mm 1000000)\n");
+	fprintf(wctx->f, "  (unit mm)\n");
+	pcb_printf_slot[4] = "%.07mm";
+
+	res |= dsn_write_wiring(wctx);
+
 	fprintf(wctx->f, ")\n");
+
+	pcb_netmap_uninit(&wctx->nmap);
+	return res;
 }
 
 
