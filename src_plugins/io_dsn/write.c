58c36ac3bbbddf275ac7dc89223491fd3282f7b6
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-10-06 03:47:51 +0000

    [io_dsn] -Add: write minimal header
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@19862 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_dsn/write.c
+++ src_plugins/io_dsn/write.c
@@ -31,11 +31,50 @@
 #include "plug_io.h"
 #include "error.h"
 #include "pcb_bool.h"
+#include "board.h"
 
 #include "write.h"
 
+typedef struct {
+	FILE *f;
+	pcb_board_t *pcb;
+} dsn_write_t;
+
+
+int dsn_write_board(dsn_write_t *wctx)
+{
+	const char *s;
+
+	fprintf(wctx->f, "(pcb ");
+	if ((wctx->pcb->Name != NULL) && (*wctx->pcb->Name != '\0')) {
+		for(s = wctx->pcb->Name; *s != '\0'; s++) {
+			if (isalnum(*s))
+				fputc(*s, wctx->f);
+			else
+				fputc('_', wctx->f);
+		}
+		fputc('\n', wctx->f);
+	}
+	else
+		fprintf(wctx->f, "anon\n");
+
+	fprintf(wctx->f, "  (parser\n");
+	fprintf(wctx->f, "    (string_quote \")\n");
+	fprintf(wctx->f, "    (space_in_quoted_tokens on)\n");
+	fprintf(wctx->f, "    (host_cad \"pcb-rnd/io_dsn\")\n");
+	fprintf(wctx->f, "    (host_version \"%s\")\n", PCB_VERSION);
+	fprintf(wctx->f, "  )\n");
+
+	fprintf(wctx->f, ")\n");
+}
+
+
 int io_dsn_write_pcb(pcb_plug_io_t *ctx, FILE *FP, const char *old_filename, const char *new_filename, pcb_bool emergency)
 {
-	pcb_message(PCB_MSG_ERROR, "io_dsn_write_pcb() not yet implemented.\n");
-	return -1;
+	dsn_write_t wctx;
+
+	wctx.f = FP;
+	wctx.pcb = PCB;
+
+	return dsn_write_board(&wctx);
 }
