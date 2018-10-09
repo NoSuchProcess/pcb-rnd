ac6b6026616b0685b4f8d3ad5d8196f7385b1c2f
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-10-09 03:26:07 +0000

    -Update: copyright banners for all the new dad code
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@19917 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/dialogs/dialogs.c
+++ src_plugins/dialogs/dialogs.c
@@ -2,7 +2,7 @@
  *                            COPYRIGHT
  *
  *  pcb-rnd, interactive printed circuit board design
- *  Copyright (C) 2017 Tibor 'Igor2' Palinkas
+ *  Copyright (C) 2017, 2018 Tibor 'Igor2' Palinkas
  *
  *  This module, dialogs, was written and is Copyright (C) 2017 by Tibor Palinkas
  *  this module is also subject to the GNU GPL as described below
