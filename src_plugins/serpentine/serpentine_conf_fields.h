fb90f8b2458009ec95d060a7660581e5e4a72680
ade <ade@1d61de99-560f-4dba-8957-1e505d42358b>
2018-10-15 10:33:04 +0000

    [serpentine] -Del: serpentine_conf_fields.h is autogenerated and shouldn'e be in the repository
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@20089 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/serpentine/serpentine_conf_fields.h
+++
@@ -1,1 +0,0 @@
-conf_reg(plugins.serpentine.pitch,              scalar, CFN_REAL,     "plugins/serpentine",         "pitch",                "multiplier for line width to derive serpentine pitch", CFF_USAGE)
