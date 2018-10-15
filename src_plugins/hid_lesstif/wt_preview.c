5c41ca480ec761dcb32c6435a4261536b2ac62d4
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-10-15 03:51:48 +0000

    [dad] -Fix: reproduce the safe double buffered preview draw in the dad widget in lesstif
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@20082 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/hid_lesstif/wt_preview.c
+++ src_plugins/hid_lesstif/wt_preview.c
@@ -27,15 +27,23 @@
 #include "wt_preview.h"
 #include "hid_attrib.h"
 
+static int widget_depth(Widget w);
+
 void pcb_ltf_preview_callback(Widget da, pcb_ltf_preview_t *pd, XmDrawingAreaCallbackStruct *cbs)
 {
 	pcb_hid_preview_t *prv = (pcb_hid_preview_t *)pd->attr->enumerations;
 	int save_vx, save_vy, save_vw, save_vh;
 	int save_fx, save_fy;
 	double save_vz;
-	Pixmap save_px;
+	Pixmap save_px, save_main_px, save_mask_px, save_mask_bm;
 	int reason = cbs != NULL ? cbs->reason : 0;
 	pcb_hid_expose_ctx_t ex;
+	XGCValues gcv;
+	GC gc;
+
+	memset(&gcv, 0, sizeof(gcv));
+	gcv.graphics_exposures = 0;
+	gc = XtGetGC(da, GCGraphicsExposures, &gcv);
 
 	if ((reason == XmCR_RESIZE) || (pd->resized == 0)) {
 		Dimension w, h;
@@ -47,7 +55,6 @@ void pcb_ltf_preview_callback(Widget da, pcb_ltf_preview_t *pd, XmDrawingAreaCal
 		stdarg(XmNwidth, &w);
 		stdarg(XmNheight, &h);
 		XtGetValues(da, stdarg_args, stdarg_n);
-
 		pd->v_width = w;
 		pd->v_height = h;
 		pd->zoom = (pd->right - pd->left + 1) / (double) w;
@@ -59,6 +66,7 @@ void pcb_ltf_preview_callback(Widget da, pcb_ltf_preview_t *pd, XmDrawingAreaCal
 		pd->y = (pd->top + pd->bottom) / 2 - pd->v_height * pd->zoom / 2;
 	}
 
+	pinout = 0;
 	save_vx = view_left_x;
 	save_vy = view_top_y;
 	save_vz = view_zoom;
@@ -67,7 +75,13 @@ void pcb_ltf_preview_callback(Widget da, pcb_ltf_preview_t *pd, XmDrawingAreaCal
 	save_fx = conf_core.editor.view.flip_x;
 	save_fy = conf_core.editor.view.flip_y;
 	save_px = main_pixmap;
-	main_pixmap = pd->window;
+	save_main_px = main_pixmap;
+	save_mask_px = mask_pixmap;
+	save_mask_bm = mask_bitmap;
+	main_pixmap = XCreatePixmap(XtDisplay(da), XtWindow(da), pd->v_width, pd->v_height, widget_depth(da));
+	mask_pixmap = XCreatePixmap(XtDisplay(da), XtWindow(da), pd->v_width, pd->v_height, widget_depth(da));
+	mask_bitmap = XCreatePixmap(XtDisplay(da), XtWindow(da), pd->v_width, pd->v_height, 1);
+	pixmap = main_pixmap;
 	view_left_x = pd->x;
 	view_top_y = pd->y;
 	view_zoom = pd->zoom;
@@ -77,8 +91,9 @@ void pcb_ltf_preview_callback(Widget da, pcb_ltf_preview_t *pd, XmDrawingAreaCal
 	conf_force_set_bool(conf_core.editor.view.flip_y, 0);
 
 	XFillRectangle(display, pixmap, bg_gc, 0, 0, pd->v_width, pd->v_height);
+XDrawLine(display, pixmap, my_gc, 0, 0, pd->v_width, pd->v_height);
 
-/*	prv->user_expose_cb(pd->attr, pd->hid_ctx, my_gc, &ex);*/
+	memset(&ex, 0, sizeof(ex));
 	ex.view.X1 = view_left_x;
 	ex.view.Y1 = view_top_y;
 	ex.view.X2 = view_left_x + pd->v_width;
@@ -86,12 +101,21 @@ void pcb_ltf_preview_callback(Widget da, pcb_ltf_preview_t *pd, XmDrawingAreaCal
 
 	pcb_hid_expose_generic(&lesstif_hid, &ex);
 
+	XCopyArea(lesstif_display, pixmap, XtWindow(da), gc, 0, 0, pd->v_width, pd->v_height, 0, 0);
+	XtReleaseGC(da, gc);
+
 	view_left_x = save_vx;
 	view_top_y = save_vy;
 	view_zoom = save_vz;
 	view_width = save_vw;
 	view_height = save_vh;
-	main_pixmap = save_px;
+	XFreePixmap(lesstif_display, main_pixmap);
+	XFreePixmap(lesstif_display, mask_pixmap);
+	XFreePixmap(lesstif_display, mask_bitmap);
+	main_pixmap = save_main_px;
+	mask_pixmap = save_mask_px;
+	mask_bitmap = save_mask_bm;
+	pixmap = save_px;
 	conf_force_set_bool(conf_core.editor.view.flip_x, save_fx);
 	conf_force_set_bool(conf_core.editor.view.flip_y, save_fy);
 }
