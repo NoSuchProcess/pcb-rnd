3b99d099c07bb3d1f32962ef50cace384e3b84f0
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-10-20 05:47:43 +0000

    [fungw] -Add: events are translated to multi-call fungw function calls
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@20186 1d61de99-560f-4dba-8957-1e505d42358b

--- src/event.c
+++ src/event.c
@@ -2,8 +2,8 @@
  *                            COPYRIGHT
  *
  *  pcb-rnd, interactive printed circuit board design
- *  Copyright (C) 2016 Tibor 'Igor2' Palinkas
- * 
+ *  Copyright (C) 2016, 2018 Tibor 'Igor2' Palinkas
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
@@ -30,7 +30,41 @@
 #include "config.h"
 #include "event.h"
 #include "error.h"
+#include "actions.h"
 #include "fptr_cast.h"
+
+static const char *pcb_fgw_evnames[] = {
+	"pcbev_gui_init",
+	"pcbev_cli_enter",
+	"pcbev_save_pre",
+	"pcbev_save_post",
+	"pcbev_load_pre",
+	"pcbev_load_post",
+	"pcbev_board_changed",
+	"pcbev_board_meta_changed",
+	"pcbev_route_styles_changed",
+	"pcbev_netlist_changed",
+	"pcbev_layers_changed",
+	"pcbev_layervis_changed",
+	"pcbev_library_changed",
+	"pcbev_font_changed",
+	"pcbev_new_pstk",
+	"pcbev_busy",
+	"pcbev_rubber_reset",
+	"pcbev_rubber_remove_subc",
+	"pcbev_rubber_move",
+	"pcbev_rubber_move_draw",
+	"pcbev_rubber_rotate90",
+	"pcbev_rubber_rotate",
+	"pcbev_rubber_rename",
+	"pcbev_rubber_lookup_lines",
+	"pcbev_rubber_lookup_rats",
+	"pcbev_rubber_constrain_main_line",
+	"pcbev_gui_sync",
+	"pcbev_gui_sync_status",
+	"pcbev_user_input_post",
+	"pcbev_draw_crosshair_chatt"
+};
 
 typedef struct event_s event_t;
 
@@ -117,17 +151,25 @@ void pcb_event(pcb_event_id_t ev, const char *fmt, ...)
 {
 	va_list ap;
 	pcb_event_arg_t argv[EVENT_MAX_ARG], *a;
+	fgw_arg_t fargv[EVENT_MAX_ARG+1], *fa;
 	event_t *e;
 	int argc;
+
+	if (ev >= PCB_EVENT_last)
+		return;
 
 	a = argv;
 	a->type = PCB_EVARG_INT;
 	a->d.i = ev;
+
+	fa = fargv;
+	fa->type = FGW_INVALID; /* first argument will be the function, as filed in by fungw; we are not passing the event number as it is impossible to bind multiple events to the same function this way */
+
 	argc = 1;
 
 	if (fmt != NULL) {
 		va_start(ap, fmt);
-		for (a++; *fmt != '\0'; fmt++, a++, argc++) {
+		for (a++, fa++; *fmt != '\0'; fmt++, a++, fa++, argc++) {
 			if (argc >= EVENT_MAX_ARG) {
 				pcb_message(PCB_MSG_ERROR, "pcb_event(): too many arguments\n");
 				break;
@@ -135,31 +177,41 @@ void pcb_event(pcb_event_id_t ev, const char *fmt, ...)
 			switch (*fmt) {
 			case 'i':
 				a->type = PCB_EVARG_INT;
-				a->d.i = va_arg(ap, int);
+				fa->type = FGW_INT;
+				fa->val.nat_int = a->d.i = va_arg(ap, int);
 				break;
 			case 'd':
 				a->type = PCB_EVARG_DOUBLE;
-				a->d.d = va_arg(ap, double);
+				fa->type = FGW_DOUBLE;
+				fa->val.nat_double = a->d.d = va_arg(ap, double);
 				break;
 			case 's':
 				a->type = PCB_EVARG_STR;
+				fa->type = FGW_STR;
 				a->d.s = va_arg(ap, const char *);
+				fa->val.str = (char *)a->d.s;
 				break;
 			case 'p':
 				a->type = PCB_EVARG_PTR;
-				a->d.p = va_arg(ap, void *);
+				fa->type = FGW_PTR;
+				fa->val.ptr_void = a->d.p = va_arg(ap, void *);
 				break;
 			case 'c':
 				a->type = PCB_EVARG_COORD;
 				a->d.c = va_arg(ap, pcb_coord_t);
+				fa->type = FGW_LONG;
+				fa->val.nat_long = a->d.c;
+				fgw_arg_conv(&pcb_fgw, fa, FGW_COORD_);
 				break;
 			case 'a':
 				a->type = PCB_EVARG_ANGLE;
-				a->d.a = va_arg(ap, pcb_angle_t);
+				fa->type = FGW_DOUBLE;
+				fa->val.nat_double = a->d.a = va_arg(ap, pcb_angle_t);
 				break;
 			default:
 				a->type = PCB_EVARG_INT;
 				a->d.i = 0;
+				fa->type = FGW_INVALID;
 				pcb_message(PCB_MSG_ERROR, "pcb_event(): invalid argument type '%c'\n", *fmt);
 				break;
 			}
@@ -169,11 +221,16 @@ void pcb_event(pcb_event_id_t ev, const char *fmt, ...)
 
 	for (e = events[ev]; e != NULL; e = e->next)
 		e->handler(e->user_data, argc, argv);
+
+	fgw_call_all(&pcb_fgw, pcb_fgw_evnames[ev], argc, fargv);
 }
 
 void pcb_events_init(void)
 {
-
+	if ((sizeof(pcb_fgw_evnames) / sizeof(pcb_fgw_evnames[0])) != PCB_EVENT_last) {
+		fprintf(stderr, "INTERNAL ERROR: event.c: pcb_fgw_evnames and pcb_event_id_t are out of sync\n");
+		exit(1);
+	}
 }
 
 void pcb_events_uninit(void)
