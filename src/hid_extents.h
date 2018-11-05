434519987cb57c84076c019a0b7fabd8b9d45d3f
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-11-05 02:41:21 +0000

    -Add: mark hid_extents for removal
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@20728 1d61de99-560f-4dba-8957-1e505d42358b

--- src/hid_extents.h
+++ src/hid_extents.h
@@ -1,8 +1,7 @@
 #ifndef PCB_HID_EXTENTS_H
 #define PCB_HID_EXTENTS_H
 
-/* HID internal interfaces.  These may ONLY be called from the HID
-   modules, not from the common PCB code.  */
+/* DO NOT USE: will be removed soom. Use a normal bbox calculation */
 
 /* Convenience function that calls the expose callback for the item,
    and returns the extents of what was drawn.  */
