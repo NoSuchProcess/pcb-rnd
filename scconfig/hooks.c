3a0135e891393cc2ee403b3cf4775d5ca3c8e6ba
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-11-09 10:14:01 +0000

    [scconfig] -Fix: detect popen(), missing popen is fatal (make the dependency explicit)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@20846 1d61de99-560f-4dba-8957-1e505d42358b

--- scconfig/hooks.c
+++ scconfig/hooks.c
@@ -399,6 +399,7 @@ int hook_detect_target()
 	require("libs/fs/realpath/*",  0, 0);
 	require("libs/fs/readdir/*",  0, 1);
 	require("libs/io/fileno/*",  0, 1);
+	require("libs/io/popen/*",  0, 1);
 	require("libs/math/rint/*",  0, 0);
 	require("libs/math/round/*",  0, 0);
 	require("libs/userpass/getpwuid/*",  0, 0);
