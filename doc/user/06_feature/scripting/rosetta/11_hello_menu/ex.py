f94a6e29b8e1be2879724a2de0d664cd4cc1434e
avigne <avigne@1d61de99-560f-4dba-8957-1e505d42358b>
2018-11-11 09:29:35 +0000

    [rosetta] -Del: comment
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@20882 1d61de99-560f-4dba-8957-1e505d42358b

--- doc/user/06_feature/scripting/rosetta/11_hello_menu/ex.py
+++ doc/user/06_feature/scripting/rosetta/11_hello_menu/ex.py
@@ -2,7 +2,6 @@ def hello():
 	message("Hello world!");
 	return 0
 
-# register hello() action
 fgw_func_reg("hello")
 cookie = scriptcookie()
 createmenu("/main_menu/Plugins/script/hello", "hello()", "Prints hello! in the log", cookie)
