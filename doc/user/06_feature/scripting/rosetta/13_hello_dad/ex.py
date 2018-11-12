ea9e053654902811f15c9c0e02bfe6289a43d88d
avigne <avigne@1d61de99-560f-4dba-8957-1e505d42358b>
2018-11-12 07:51:06 +0000

    [rosetta] -Add: python version of  hello world with a vbox
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@20903 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ doc/user/06_feature/scripting/rosetta/13_hello_dad/ex.py
@@ -0,0 +1,10 @@
+def hello():
+	dad("heldi", "new")
+	dad("heldi", "begin_vbox")
+	dad("heldi",   "label", "Hello")
+	dad("heldi",   "label", "world!")
+	dad("heldi", "end")
+	dad("heldi", "run_modal", "hello-world", "")
+	return 0
+
+fgw_func_reg("hello")
