692679adc92da4971bc45794c33184215dc4b4ae
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-11-18 06:30:36 +0000

    [core] -Add: check hid_conf registration leftovers on uninit, in debug mode
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@20975 1d61de99-560f-4dba-8957-1e505d42358b

--- src/conf_hid.c
+++ src/conf_hid.c
@@ -44,6 +44,14 @@ static void conf_pcb_hid_init(void)
 
 void conf_pcb_hid_uninit(void)
 {
+#ifndef NDEBUG
+	{
+		htpp_entry_t *e;
+		for(e = htpp_first(conf_hid_ids); e != NULL; e = htpp_next(conf_hid_ids, e))
+			fprintf(stderr, "ERROR: conf_hid id left registered: '%s'\n", e->key);
+	}
+#endif
+
 	if (conf_hid_ids != NULL) {
 		htpp_free(conf_hid_ids);
 		conf_hid_ids = NULL;
