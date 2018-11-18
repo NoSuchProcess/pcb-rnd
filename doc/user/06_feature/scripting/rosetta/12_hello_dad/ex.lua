1dd5954b897bcbcad74d477015477cc44b65ab2a
avigne <avigne@1d61de99-560f-4dba-8957-1e505d42358b>
2018-11-18 07:34:56 +0000

    [rosetta] -Add: hello world dialog using DAD, in Lua
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@20977 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ doc/user/06_feature/scripting/rosetta/12_hello_dad/ex.lua
@@ -0,0 +1,8 @@
+function hello()
+	dad("heldi", "new")
+	dad("heldi", "label", "Hello world!")
+	dad("heldi", "run_modal", "hello-world", "Hello world dialog")
+	return 0
+end
+
+fgw_func_reg("hello")
