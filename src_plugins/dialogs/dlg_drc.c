22598c62175b4552bd26264ca554b0cfb2c62f9a
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-11-27 07:30:57 +0000

    [drc] -Change: rearrange the dialog box to save more screen space
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@21165 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/dialogs/dlg_drc.c
+++ src_plugins/dialogs/dlg_drc.c
@@ -161,6 +161,7 @@ static void pcb_dlg_drc(drc_ctx_t *ctx, const char *title)
 	const char *hdr[] = { "ID", "title", NULL };
 
 
+	PCB_DAD_BEGIN_VBOX(ctx->dlg);
 	PCB_DAD_BEGIN_HPANE(ctx->dlg);
 
 		/* left */
@@ -181,17 +182,13 @@ static void pcb_dlg_drc(drc_ctx_t *ctx, const char *title)
 
 			PCB_DAD_BEGIN_HBOX(ctx->dlg);
 				PCB_DAD_BUTTON(ctx->dlg, "Copy");
-				PCB_DAD_BUTTON(ctx->dlg, "Paste before");
-				PCB_DAD_BUTTON(ctx->dlg, "Paste after");
+				PCB_DAD_BUTTON(ctx->dlg, "Cut");
+				PCB_DAD_BUTTON(ctx->dlg, "Del");
 			PCB_DAD_END(ctx->dlg);
 
 			PCB_DAD_BEGIN_HBOX(ctx->dlg);
-				PCB_DAD_BUTTON(ctx->dlg, "Refresh");
-				PCB_DAD_BUTTON(ctx->dlg, "Remove entry");
-				PCB_DAD_BEGIN_HBOX(ctx->dlg);
-					PCB_DAD_COMPFLAG(ctx->dlg, PCB_HATF_EXPFILL);
-				PCB_DAD_END(ctx->dlg);
-				PCB_DAD_BUTTON(ctx->dlg, "Close");
+				PCB_DAD_BUTTON(ctx->dlg, "Paste before");
+				PCB_DAD_BUTTON(ctx->dlg, "Paste after");
 			PCB_DAD_END(ctx->dlg);
 		PCB_DAD_END(ctx->dlg);
 
@@ -206,7 +203,15 @@ static void pcb_dlg_drc(drc_ctx_t *ctx, const char *title)
 			PCB_DAD_LABEL(ctx->dlg, "(measure)");
 				ctx->wmeasure = PCB_DAD_CURRENT(ctx->dlg);
 		PCB_DAD_END(ctx->dlg);
+	PCB_DAD_END(ctx->dlg);
 
+		PCB_DAD_BEGIN_HBOX(ctx->dlg);
+			PCB_DAD_BUTTON(ctx->dlg, "Refresh");
+					PCB_DAD_BEGIN_HBOX(ctx->dlg);
+						PCB_DAD_COMPFLAG(ctx->dlg, PCB_HATF_EXPFILL);
+					PCB_DAD_END(ctx->dlg);
+					PCB_DAD_BUTTON(ctx->dlg, "Close");
+		PCB_DAD_END(ctx->dlg);
 	PCB_DAD_END(ctx->dlg);
 
 
