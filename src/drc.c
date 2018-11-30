8e8ecfc8ac0a86c6503f34d1224667d097e3013e
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-11-30 13:38:42 +0000

    [drc] -Add: text output in drc(): print, log and dump
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@21292 1d61de99-560f-4dba-8957-1e505d42358b

--- src/drc.c
+++ src/drc.c
@@ -29,6 +29,7 @@
 
 #include "actions.h"
 #include "drc.h"
+#include "conf_core.h"
 #include "compat_misc.h"
 
 
@@ -47,7 +48,7 @@ void pcb_drc_set_data(pcb_view_t *violation, const pcb_coord_t *measured_value, 
 }
 
 
-static const char pcb_acts_DRC[] = "DRC([list|simple])";
+static const char pcb_acts_DRC[] = "DRC([list|simple|print|log|dump])";
 static const char pcb_acth_DRC[] = "Invoke the DRC check. Results are presented as the argument requests.";
 /* DOC: drc.html */
 static fgw_error_t pcb_act_DRC(fgw_arg_t *res, int argc, fgw_arg_t *argv)
@@ -68,7 +69,65 @@ static fgw_error_t pcb_act_DRC(fgw_arg_t *res, int argc, fgw_arg_t *argv)
 		return pcb_actionv_bin("drcdialog", res, 2, args);
 	}
 
+
 	PCB_ACT_IRES(-1);
+	pcb_drc_all();
+
+	if (pcb_strcasecmp(dlg_type, "print") == 0) {
+		pcb_view_t *v;
+		for(v = pcb_view_list_first(&pcb_drc_lst); v != NULL; v = pcb_view_list_next(v)) {
+			printf("%ld: %s: %s\n", v->uid, v->type, v->title);
+			if (v->have_bbox)
+				pcb_printf("%m+within %$m4\n", conf_core.editor.grid_unit->allow, v->bbox.X1, v->bbox.Y1, v->bbox.X2, v->bbox.Y2);
+			if (v->have_xy)
+				pcb_printf("%m+at %$m2\n", conf_core.editor.grid_unit->allow, v->x, v->y);
+			pcb_printf("%m+required value %$mS\n", conf_core.editor.grid_unit->allow, v->data.drc.required_value);
+			if (v->data.drc.have_measured)
+				pcb_printf("%m+measured value %$mS\n", conf_core.editor.grid_unit->allow, v->data.drc.measured_value);
+			printf("%s\n\n", v->explanation);
+		}
+	}
+	else if (pcb_strcasecmp(dlg_type, "log") == 0) {
+		pcb_view_t *v;
+		for(v = pcb_view_list_first(&pcb_drc_lst); v != NULL; v = pcb_view_list_next(v)) {
+			pcb_message(PCB_MSG_INFO, "%ld: %s: %s\n", v->uid, v->type, v->title);
+			if (v->have_bbox)
+				pcb_message(PCB_MSG_INFO, "%m+within %$m4\n", conf_core.editor.grid_unit->allow, v->bbox.X1, v->bbox.Y1, v->bbox.X2, v->bbox.Y2);
+			if (v->have_xy)
+				pcb_message(PCB_MSG_INFO, "%m+at %$m2\n", conf_core.editor.grid_unit->allow, v->x, v->y);
+			pcb_printf("%m+required value %$mS\n", conf_core.editor.grid_unit->allow, v->data.drc.required_value);
+			if (v->data.drc.have_measured)
+				pcb_message(PCB_MSG_INFO, "%m+measured value %$mS\n", conf_core.editor.grid_unit->allow, v->data.drc.measured_value);
+			pcb_message(PCB_MSG_INFO, "%s\n\n", v->explanation);
+		}
+	}
+	if (pcb_strcasecmp(dlg_type, "dump") == 0) {
+		pcb_view_t *v;
+		char *s;
+		for(v = pcb_view_list_first(&pcb_drc_lst); v != NULL; v = pcb_view_list_next(v)) {
+			printf("V%ld\n", v->uid);
+			printf("T%s\n", v->type);
+			printf("t%s\n", v->title);
+			if (v->have_bbox)
+				pcb_printf("B%mm %mm %mm %mm mm\n", v->bbox.X1, v->bbox.Y1, v->bbox.X2, v->bbox.Y2);
+			if (v->have_xy)
+				pcb_printf("A%mm %mm mm\n", v->x, v->y);
+			pcb_printf("R%$mm\n", v->data.drc.required_value);
+			if (v->data.drc.have_measured)
+				pcb_printf("M%$mm\n", v->data.drc.measured_value);
+			for(s = v->explanation; *s != '\0'; s++)
+				if (*s == '\n')
+					*s = ' ';
+			printf("E%s\n\n", v->explanation);
+		}
+	}
+	else if (pcb_strcasecmp(dlg_type, "count") == 0)
+		PCB_ACT_IRES(pcb_view_list_length(&pcb_drc_lst));
+
+
+	pcb_view_list_free_fields(&pcb_drc_lst);
+
+
 	return 0;
 }
 
