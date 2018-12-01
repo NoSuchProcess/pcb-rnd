51e8fe1c2e67901ba05c5b043d7c02a4a8fc82d7
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-12-01 06:20:45 +0000

    [dialogs] -Add: finish the code move of manage plugins [dialogs] -Del: don't use the report dialog API for manage plugins - it's going to be removed
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@21311 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src_plugins/dialogs/dlg_plugins.c
@@ -0,0 +1,87 @@
+/*
+ *                            COPYRIGHT
+ *
+ *  pcb-rnd, interactive printed circuit board design
+ *  Copyright (C) 2015,2018 Tibor 'Igor2' Palinkas
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  Contact:
+ *    Project page: http://repo.hu/projects/pcb-rnd
+ *    lead developer: http://repo.hu/projects/pcb-rnd/contact.html
+ *    mailing list: pcb-rnd (at) list.repo.hu (send "subscribe")
+ */
+
+static const char pcb_acts_ManagePlugins[] = "ManagePlugins()\n";
+static const char pcb_acth_ManagePlugins[] = "Manage plugins dialog.";
+static fgw_error_t pcb_act_ManagePlugins(fgw_arg_t *res, int argc, fgw_arg_t *argv)
+{
+	PCB_DAD_DECL(dlg);
+	pup_plugin_t *p;
+	int nump = 0, numb = 0;
+	gds_t str;
+
+	gds_init(&str);
+
+	for (p = pcb_pup.plugins; p != NULL; p = p->next)
+		if (p->flags & PUP_FLG_STATIC)
+			numb++;
+		else
+			nump++;
+
+	gds_append_str(&str, "Plugins loaded:\n");
+	if (nump > 0) {
+		for (p = pcb_pup.plugins; p != NULL; p = p->next) {
+			if (!(p->flags & PUP_FLG_STATIC)) {
+				gds_append(&str, ' ');
+				gds_append_str(&str, p->name);
+				gds_append(&str, ' ');
+				gds_append_str(&str, p->path);
+				gds_append(&str, '\n');
+			}
+		}
+	}
+	else
+		gds_append_str(&str, " (none)\n");
+
+	gds_append_str(&str, "\n\nBuildins:\n");
+	if (numb > 0) {
+		for (p = pcb_pup.plugins; p != NULL; p = p->next) {
+			if (p->flags & PUP_FLG_STATIC) {
+				gds_append(&str, ' ');
+				gds_append_str(&str, p->name);
+				gds_append(&str, '\n');
+			}
+		}
+	}
+	else
+		gds_append_str(&str, " (none)\n");
+
+	gds_append_str(&str, "\n\nNOTE: this is the alpha version, can only list plugins/buildins\n");
+
+
+	PCB_DAD_BEGIN_VBOX(dlg);
+		PCB_DAD_COMPFLAG(dlg, PCB_HATF_SCROLL | PCB_HATF_EXPFILL);
+		PCB_DAD_LABEL(dlg, str.array);
+	PCB_DAD_END(dlg);
+
+	PCB_DAD_NEW(dlg, "Manage plugins", "", NULL, pcb_true, NULL);
+	PCB_DAD_RUN(dlg);
+	PCB_DAD_FREE(dlg);
+
+	gds_uninit(&str);
+	PCB_ACT_IRES(0);
+	return 0;
+}
