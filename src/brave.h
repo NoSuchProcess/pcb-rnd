645999a07ee43477d6d7a41678b6575db3a3c8ed
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-12-13 15:22:35 +0000

    [find] -Add: brave bit for using the new find
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@21764 1d61de99-560f-4dba-8957-1e505d42358b

--- src/brave.h
+++ src/brave.h
@@ -1,8 +1,9 @@
 #ifndef PCB_BRAVE_H
 #define PCB_BRAVE_H
-typedef enum {
+typedef enum { /* bitfield */
 	PCB_BRAVE_OFF = 0,
 	PCB_BRAVE_NOXOR = 1,
+	PCB_BRAVE_NEWFIND = 2,
 	PCB_BRACE_max
 } pcb_brave_t;
 
