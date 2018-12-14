7504aba420288965c5143c1239d18eea5b52fd56
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-12-14 07:23:59 +0000

    [find] -Update: replace the copyright banner on find_print.c: all code got replaced with pcb-rnd specific one already, the file doesn't contain verbatim lines from the original pcb source
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@21795 1d61de99-560f-4dba-8957-1e505d42358b

--- src/find_print.c
+++ src/find_print.c
@@ -1,10 +1,8 @@
 /*
- *
  *                            COPYRIGHT
  *
  *  pcb-rnd, interactive printed circuit board design
- *  (this file is based on PCB, interactive printed circuit board design)
- *  Copyright (C) 1994,1995,1996, 2005 Thomas Nau
+ *  Copyright (C) 2018 Tibor 'Igor2' Palinkas
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -18,13 +16,12 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  *  Contact:
  *    Project page: http://repo.hu/projects/pcb-rnd
  *    lead developer: http://repo.hu/projects/pcb-rnd/contact.html
  *    mailing list: pcb-rnd (at) list.repo.hu (send "subscribe")
- *
  */
 
 #include "data_it.h"
