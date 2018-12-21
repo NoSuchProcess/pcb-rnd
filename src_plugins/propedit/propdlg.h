f559bab643e05cd67086512ddd2b7a4f32df02a0
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-12-21 09:44:28 +0000

    [propedit] -Cleanup: propdlg.h header watchdog  and includes to be self contained
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@21981 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/propedit/propdlg.h
+++ src_plugins/propedit/propdlg.h
@@ -1,3 +1,10 @@
+#ifndef PCB_PROPDLG_H
+#define PCB_PROPDLG_H
+
+#include "actions.h"
+
 extern const char pcb_acts_propedit[];
 extern const char pcb_acth_propedit[];
 fgw_error_t pcb_act_propedit(fgw_arg_t *res, int argc, fgw_arg_t *argv);
+
+#endif
