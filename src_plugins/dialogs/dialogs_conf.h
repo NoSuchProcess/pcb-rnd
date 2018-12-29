e2c1e977d11eeec16bb0cd4f8d390e603af9021d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2018-12-29 06:57:35 +0000

    [wplc] -Add: include top window geometry in the conf struct as a template
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@22118 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/dialogs/dialogs_conf.h
+++ src_plugins/dialogs/dialogs_conf.h
@@ -12,6 +12,10 @@ typedef struct {
 				CFT_BOOLEAN to_user;
 			} auto_save_window_geometry;
 			const struct window_geometry {
+				CFT_INTEGER top_x;
+				CFT_INTEGER top_y;
+				CFT_INTEGER top_width;
+				CFT_INTEGER top_height;
 			} window_geometry;
 		} dialogs;
 	} plugins;
