f5797cec8bac47e63406b841cc90459bbe8a16cd
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-01-03 07:25:26 +0000

    [pstk] -Fix: combine rotation and side mirror for hash and eq
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@22253 1d61de99-560f-4dba-8957-1e505d42358b

--- src/obj_pstk.c
+++ src/obj_pstk.c
@@ -221,11 +221,13 @@ void pcb_pstk_copper_bbox(pcb_box_t *dst, pcb_pstk_t *ps)
 /* hash */
 int pcb_pstk_eq(const pcb_host_trans_t *tr1, const pcb_pstk_t *p1, const pcb_host_trans_t *tr2, const pcb_pstk_t *p2)
 {
+	double rotdir1 = tr1->on_bottom ? -1.0 : 1.0, rotdir2 = tr2->on_bottom ? -1.0 : 1.0;
+
 TODO("padstack: should compare shape by shape: a 180 deg rotated or mirrored rectangle is still just the same rectangle!")
 
 	if ((p1->smirror ^ tr1->on_bottom) != (p2->smirror ^ tr2->on_bottom)) return 0;
 	if ((p1->xmirror ^ tr1->on_bottom) != (p2->xmirror ^ tr2->on_bottom)) return 0;
-	if (floor(fmod(p1->rot + tr1->rot, 360.0)*10000) != floor(fmod(p2->rot + tr2->rot, 360.0)*10000)) return 0;
+	if (floor(fmod((p1->rot * rotdir1) + tr1->rot, 360.0)*10000) != floor(fmod((p2->rot * rotdir2) + tr2->rot, 360.0)*10000)) return 0;
 
 	if (!PCB_FLAG_TEST(PCB_FLAG_FLOATER, p1) && !PCB_FLAG_TEST(PCB_FLAG_FLOATER, p2)) {
 		pcb_coord_t x1, y1, x2, y2;
@@ -242,6 +244,7 @@ TODO("padstack: should compare shape by shape: a 180 deg rotated or mirrored rec
 unsigned int pcb_pstk_hash(const pcb_host_trans_t *tr, const pcb_pstk_t *p)
 {
 	unsigned int crd = 0;
+	double rotdir = tr->on_bottom ? -1.0 : 1.0;
 
 	if (!PCB_FLAG_TEST(PCB_FLAG_FLOATER, p)) {
 		pcb_coord_t x, y;
@@ -250,7 +253,7 @@ unsigned int pcb_pstk_hash(const pcb_host_trans_t *tr, const pcb_pstk_t *p)
 		crd = pcb_hash_coord(x) ^ pcb_hash_coord(y);
 	}
 
-	return pcb_hash_angle(tr, p->rot) ^ pcb_hash_coord(p->Clearance) ^
+	return pcb_hash_angle(tr, p->rot * rotdir) ^ pcb_hash_coord(p->Clearance) ^
 		pcb_hash_str(p->term) ^ crd ^
 		murmurhash(&p->smirror, sizeof(p->smirror)) ^
 		murmurhash(&p->xmirror, sizeof(p->xmirror));
