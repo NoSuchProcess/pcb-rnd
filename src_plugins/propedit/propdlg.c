19099b86980ceb012379fcb1bb01323b5982f74f
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-01-10 07:20:12 +0000

    [propedit] -Fix: don't print obj properties of invalid objects
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@22441 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/propedit/propdlg.c
+++ src_plugins/propedit/propdlg.c
@@ -178,9 +178,10 @@ static void prop_pcb2dlg(propdlg_t *ctx)
 		inv = 0;
 		for(idp = pcb_idpath_list_first(&ctx->pe.objs); idp != NULL; idp = pcb_idpath_list_next(idp)) {
 			pcb_any_obj_t *o = pcb_idpath2obj(ctx->pe.pcb->Data, idp);
-			if (o == NULL)
+			if (o != NULL)
+				pcb_append_printf(&scope, "%s #%ld, ", pcb_obj_type_name(o->type), o->ID);
+			else
 				inv++;
-			pcb_append_printf(&scope, "%s #%ld, ", pcb_obj_type_name(o->type), o->ID);
 		}
 		if (inv > 0)
 			pcb_append_printf(&scope, "%d invalid objects, ");
