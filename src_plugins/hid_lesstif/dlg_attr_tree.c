4d60227ab24cf00c7d5c15651d92afc065ff4c47
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-01-11 06:16:27 +0000

    [lesstif] -Add: placeholder for the tree table widget
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@22467 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/hid_lesstif/dlg_attr_tree.c
+++ src_plugins/hid_lesstif/dlg_attr_tree.c
@@ -1,4 +1,14 @@
 #include "xm_tree_table_widget.h"
+typedef struct {
+	gdl_list_t model;
+} ltf_tree_t;
+
+static void ltf_ttbl_xevent_cb(const tt_table_event_data_t *data)
+{
+	pcb_trace("ttbl x event\n");
+}
+
+
 static void ltf_tree_set(lesstif_attr_dlg_t *ctx, int idx, const char *val)
 {
 
@@ -6,6 +16,20 @@ static void ltf_tree_set(lesstif_attr_dlg_t *ctx, int idx, const char *val)
 
 static Widget ltf_tree_create(lesstif_attr_dlg_t *ctx, Widget parent, pcb_hid_attribute_t *attr)
 {
+#if 0
+	ltf_tree_t *lt = calloc(sizeof(ltf_tree_t), 1);
+	Widget table = xm_create_tree_table_widget_cb(parent, &lt->model, ltf_ttbl_xevent_cb, NULL, NULL);
 
+	XtManageChild(table);
+	return table;
+#else
+	Widget *w;
+	stdarg_n = 0;
+	stdarg(XmNalignment, XmALIGNMENT_BEGINNING);
+	stdarg(XmNlabelString, XmStringCreatePCB("todo: tree table"));
+	w = XmCreateLabel(parent, XmStrCast("todo"), stdarg_args, stdarg_n);
+
+	return w;
+#endif
 }
 
