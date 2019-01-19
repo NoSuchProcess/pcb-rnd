53d1c65330ff85e4211084e99251e6ad7458920d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-01-19 13:17:48 +0000

    [font] -Fix: typo in LoadFontFrom() second argument load caused it to be always 0
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@22654 1d61de99-560f-4dba-8957-1e505d42358b

--- src/font_act.c
+++ src/font_act.c
@@ -46,7 +46,7 @@ fgw_error_t pcb_act_load_font_from(fgw_arg_t *res, int argc, fgw_arg_t *argv)
 	int r;
 
 	PCB_ACT_MAY_CONVARG(1, FGW_STR, load_font_from, fname = argv[1].val.str);
-	PCB_ACT_MAY_CONVARG(2, FGW_STR, load_font_from, sid = argv[1].val.str);
+	PCB_ACT_MAY_CONVARG(2, FGW_STR, load_font_from, sid = argv[2].val.str);
 
 	if (sid != NULL) {
 		char *end;
