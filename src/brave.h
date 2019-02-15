47dc2c95da4f66584eb6f93d5d3eae7f12319c4e
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-02-15 16:53:03 +0000

    [netlist] -Add: brave setting for enabling the new netlist engine
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@23177 1d61de99-560f-4dba-8957-1e505d42358b

--- src/brave.h
+++ src/brave.h
@@ -5,7 +5,8 @@ typedef enum { /* bitfield */
 	PCB_BRAVE_NOXOR = 1,
 	PCB_BRAVE_CLIPBATCH = 2,
 	PCB_BRAVE_LESSTIF_TREETABLE = 4,
-	PCB_BRACE_max
+	PCB_BRAVE_NETLIST2 = 8,
+	PCB_BRAVE_max
 } pcb_brave_t;
 
 extern pcb_brave_t pcb_brave; /* cache generated from the config */
