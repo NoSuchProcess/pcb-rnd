57f1e9c4f3f9e1411ac35839908ebb085fddc8ba
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-02-16 04:19:55 +0000

    [core] -Fix: data dynflag clear shall be recursive so subc parts are cleared too
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@23186 1d61de99-560f-4dba-8957-1e505d42358b

--- src/data.c
+++ src/data.c
@@ -892,8 +892,10 @@ void pcb_data_dynflag_clear(pcb_data_t *data, pcb_dynf_t dynf)
 		PCB_DFLAG_CLR(&((pcb_any_obj_t *)n)->Flags, dynf);
 	pcb_r_end(&it);
 
-	for(n = pcb_r_first(data->subc_tree, &it); n != NULL; n = pcb_r_next(&it))
+	for(n = pcb_r_first(data->subc_tree, &it); n != NULL; n = pcb_r_next(&it)) {
 		PCB_DFLAG_CLR(&((pcb_any_obj_t *)n)->Flags, dynf);
+		pcb_data_dynflag_clear(((pcb_subc_t *)n)->data, dynf);
+	}
 	pcb_r_end(&it);
 
 	for(n = pcb_r_first(data->rat_tree, &it); n != NULL; n = pcb_r_next(&it))
