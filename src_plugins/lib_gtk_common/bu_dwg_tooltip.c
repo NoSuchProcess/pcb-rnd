25f8f443d248148ae7d5fa4a6a033ccfcf08ac7d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-02-21 04:10:18 +0000

    [netlist] -Add: gtk drawing area tooltip code for the new netlist API
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@23313 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_gtk_common/bu_dwg_tooltip.c
+++ src_plugins/lib_gtk_common/bu_dwg_tooltip.c
@@ -36,19 +36,23 @@
 #include "bu_dwg_tooltip.h"
 
 #include "layer.h"
-#include "library.h"
 #include "search.h"
 #include "find.h"
 #include "board.h"
 #include "obj_subc.h"
 #include "obj_subc_parent.h"
 #include "rats.h"
+#include "netlist2.h"
+
+#include "brave.h"
+#include "library.h"
 
 #define TOOLTIP_UPDATE_DELAY 200
 
 #define PCB_SILK_TYPE (PCB_OBJ_LINE | PCB_OBJ_ARC | PCB_OBJ_POLY)
 
-static char *describe_location(pcb_coord_t X, pcb_coord_t Y)
+TODO("netlist: remove this with the old netlist code");
+static char *describe_location_old(pcb_coord_t X, pcb_coord_t Y)
 {
 	void *ptr1, *ptr2, *ptr3;
 	int type;
@@ -114,6 +118,42 @@ static char *describe_location(pcb_coord_t X, pcb_coord_t Y)
 	return description;
 }
 
+static char *describe_location(pcb_coord_t X, pcb_coord_t Y)
+{
+	void *ptr1, *ptr2, *ptr3;
+	pcb_any_obj_t *obj;
+	int type;
+	pcb_subc_t *subc;
+	pcb_net_term_t *term;
+	gds_t desc;
+
+	/* check if there are any pins or pads at that position */
+	type = pcb_search_obj_by_location(PCB_OBJ_CLASS_TERM, &ptr1, &ptr2, &ptr3, X, Y, 0);
+	if (type == PCB_OBJ_VOID)
+		return NULL;
+
+	/* don't mess with silk objects! */
+	if ((type & PCB_SILK_TYPE) && (pcb_layer_flags_((pcb_layer_t *)ptr1) & PCB_LYT_SILK))
+		return NULL;
+
+	obj = ptr2;
+	if (obj->term == NULL)
+		return NULL;
+
+	subc = pcb_obj_parent_subc(ptr2);
+	if (subc == NULL)
+		return NULL;
+
+	term = pcb_net_find_by_refdes_term(&PCB->netlist[PCB_NETLIST_EDITED], subc->refdes, obj->term);
+
+	gds_init(&desc);
+	gds_append_str(&desc, "Subcircuit:\t"); gds_append_str(&desc, subc->refdes);
+	gds_append_str(&desc, "\nTerminal:  \t"); gds_append_str(&desc, obj->term);
+	gds_append_str(&desc, "\nNetlist:     \t"); gds_append_str(&desc, term == NULL ? "--" : term->parent.net->name);
+
+	return desc.array;
+}
+
 static int tooltip_update_timeout_id = 0;
 gboolean pcb_gtk_dwg_tooltip_check_object(GtkWidget *drawing_area, pcb_coord_t crosshairx, pcb_coord_t crosshairy)
 {
@@ -124,13 +164,19 @@ gboolean pcb_gtk_dwg_tooltip_check_object(GtkWidget *drawing_area, pcb_coord_t c
 	tooltip_update_timeout_id = 0;
 
 	/* check if there are any pins or pads at that position */
-	description = describe_location(crosshairx, crosshairy);
+	if (pcb_brave & PCB_BRAVE_NETLIST2)
+		description = describe_location(crosshairx, crosshairy);
+	else
+		description = describe_location_old(crosshairx, crosshairy);
 
 	if (description == NULL)
 		return FALSE;
 
 	gtk_widget_set_tooltip_text(drawing_area, description);
-	g_free(description);
+	if (pcb_brave & PCB_BRAVE_NETLIST2)
+		free(description);
+	else
+		g_free(description);
 
 	return FALSE;
 }
