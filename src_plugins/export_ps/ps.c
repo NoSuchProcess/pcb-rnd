2092d2ad1e340a466527fe8d99b6bab3a407daae
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-03-02 10:36:24 +0000

    [export_ps] -Cleanup: remove unused vars
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@23595 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/export_ps/ps.c
+++ src_plugins/export_ps/ps.c
@@ -619,8 +619,7 @@ static FILE *psopen(const char *base, const char *which)
    or eps.  */
 void ps_hid_export_to_file(FILE * the_file, pcb_hid_attr_val_t * options)
 {
-	static int saved_layer_stack[PCB_MAX_LAYER], i;
-	pcb_layergrp_t *g;
+	static int saved_layer_stack[PCB_MAX_LAYER];
 
 	conf_force_set_bool(conf_core.editor.thin_draw, 0);
 	conf_force_set_bool(conf_core.editor.thin_draw_poly, 0);
