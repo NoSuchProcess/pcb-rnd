21bb519b4b23e403fc6d98271c91eeb3281e9f94
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-03-03 15:56:11 +0000

    [fsd] -Add: gtk: hash table for history
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@23654 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_gtk_common/dlg_fileselect.c
+++ src_plugins/lib_gtk_common/dlg_fileselect.c
@@ -27,10 +27,36 @@
 #include "config.h"
 
 #include <gtk/gtk.h>
+#include <genht/htsp.h>
+#include <genht/hash.h>
 
 #include "hid.h"
+#include "compat_misc.h"
+
+static htsp_t history;
+static int inited = 0;
+
+#define MAX_HIST 8
+typedef struct file_history_s {
+	char *fn[MAX_HIST];
+} file_history_t;
 
 char *pcb_gtk_fileselect2(GtkWidget *top_window, const char *title, const char *descr, const char *default_file, const char *default_ext, const char *history_tag, pcb_hid_fsd_flags_t flags, pcb_hid_dad_subdialog_t *sub)
 {
+	file_history_t *hi;
+
+	if (!inited) {
+		htsp_init(&history, strhash, strkeyeq);
+		inited = 1;
+	}
+
+	if ((history_tag != NULL) && (*history_tag != '\0')) {
+		hi = htsp_get(&history, history_tag);
+		if (hi == NULL) {
+			hi = calloc(sizeof(file_history_t), 1);
+			htsp_set(&history, pcb_strdup(history_tag), hi);
+		}
+	}
+
 	return NULL;
 }
