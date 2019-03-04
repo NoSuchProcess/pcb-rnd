a383b4f400e82e44fc9ffae0b1beea7ed88141cb
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-03-04 06:34:16 +0000

    [core] -Fix: don't pop up an extra dialog for footprints-not-found in quiet mode
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@23670 1d61de99-560f-4dba-8957-1e505d42358b

--- src/object_act.c
+++ src/object_act.c
@@ -445,7 +445,7 @@ static fgw_error_t pcb_act_ElementList(fgw_arg_t *res, int argc, fgw_arg_t *argv
 			}
 		}
 		PCB_END_LOOP;
-		if (number_of_footprints_not_found > 0) {
+		if ((number_of_footprints_not_found > 0) && (!conf_core.rc.quiet)) {
 			pcb_hid_message_box("warning", "Footprint import", "Not all requested footprints were found.\nSee the message log for details", "ok", 0, NULL);
 		}
 		return 0;
