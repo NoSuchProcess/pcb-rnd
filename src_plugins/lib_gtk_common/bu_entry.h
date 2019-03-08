b24a7fae9fc86e78a297a38cf48f34be9a5087c7
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-03-08 13:56:50 +0000

    [gtk] -Del: ghid_table_coord_entry() - no user
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@23775 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_gtk_common/bu_entry.h
+++ src_plugins/lib_gtk_common/bu_entry.h
@@ -8,8 +8,3 @@ void ghid_coord_entry(GtkWidget * box, GtkWidget ** coord_entry, pcb_coord_t val
 											void (*cb_func) (pcb_gtk_coord_entry_t *, gpointer),
 											gpointer data, const gchar * string_pre, const gchar * string_post);
 
-void ghid_table_coord_entry(GtkWidget * table, gint row, gint column,
-														GtkWidget ** coord_entry, pcb_coord_t value,
-														pcb_coord_t low, pcb_coord_t high, enum ce_step_size step_size,
-														gint width, void (*cb_func) (pcb_gtk_coord_entry_t *, gpointer),
-														gpointer data, gboolean right_align, const gchar * string);
