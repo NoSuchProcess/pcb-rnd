77168ea892eb1c84376e358a0846daa1c3c8fd77
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-03-29 10:26:56 +0000

    [propedit] -Fix: accept NULL pcb in context, don't crash
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@24316 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/propedit/props.c
+++ src_plugins/propedit/props.c
@@ -80,7 +80,7 @@ void pcb_props_init(pcb_propedit_t *ctx, pcb_board_t *pcb)
 	memset(ctx, 0, sizeof(pcb_propedit_t));
 	htsp_init(&ctx->props, strhash, strkeyeq);
 	ctx->pcb = pcb;
-	ctx->data = pcb->Data;
+	ctx->data = pcb == NULL ? NULL : pcb->Data;
 }
 
 void pcb_props_reset(pcb_propedit_t *ctx)
