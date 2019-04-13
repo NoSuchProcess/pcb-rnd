b858833a02f1232c04b4aa9e1a0132ab87639e58
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-04-13 10:54:59 +0000

    [io_kicad] -Cleanup: copyright banner indentation
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@24591 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_kicad/read.c
+++ src_plugins/io_kicad/read.c
@@ -1,24 +1,24 @@
 /*
- *			COPYRIGHT
+ *                            COPYRIGHT
  *
- *	pcb-rnd, interactive printed circuit board design
- *	Copyright (C) 2016..2019 Tibor 'Igor2' Palinkas
- *	Copyright (C) 2016, 2017 Erich S. Heinzle
- *	Copyright (C) 2017 Miloh
+ *  pcb-rnd, interactive printed circuit board design
+ *  Copyright (C) 2016..2019 Tibor 'Igor2' Palinkas
+ *  Copyright (C) 2016, 2017 Erich S. Heinzle
+ *  Copyright (C) 2017 Miloh
  *
- *	This program is free software; you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation; either version 2 of the License, or
- *	(at your option) any later version.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
- *	GNU General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *	You should have received a copy of the GNU General Public License
- *	along with this program; if not, write to the Free Software
- *	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  *  Contact:
  *    Project page: http://repo.hu/projects/pcb-rnd
