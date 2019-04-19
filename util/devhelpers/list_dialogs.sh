d83aae21ea06bd125adcc46c8b114e7ae9dc0770
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-04-19 03:35:29 +0000

    [util] -Add: list_dialogs also prints dialog name (separator switched to \t because name can contain spaces)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@24685 1d61de99-560f-4dba-8957-1e505d42358b

--- util/devhelpers/list_dialogs.sh
+++ util/devhelpers/list_dialogs.sh
@@ -13,6 +13,7 @@ grep PCB_DAD_NEW $trunk/src/*.c $trunk/src_plugins/*/*.c | awk '
 		sub(".*src/", "src/", file)
 		sub(".*src_plugins/", "src_plugins/", file)
 		sub(".*PCB_DAD_NEW[^(]*[(]", "", name)
+		title=name
 		if (name ~ "^\"") {
 			sub("\"", "", name)
 			sub("\".*", "", name)
@@ -20,6 +21,14 @@ grep PCB_DAD_NEW $trunk/src/*.c $trunk/src_plugins/*/*.c | awk '
 		else
 			name = "<dyn>"
 
-		print name, file
+		sub("[^,]*,[^,]*, *", "", title)
+		if (title ~ "^\"") {
+			sub("\"", "", title)
+			sub("\".*", "", title)
+		}
+		else
+			title = "<dyn>"
+
+		print name "\t" title "\t" file
 	}
 '
