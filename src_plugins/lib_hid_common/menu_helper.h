e51ff7569c8b8285e15177c72d43f83fbcbe9965
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-04-25 04:36:23 +0000

    [lib_hid_common] -Fix: sort out the copyright banner: this code was all new for pcb-rnd's lihata based menu system
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@24866 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_hid_common/menu_helper.h
+++ src_plugins/lib_hid_common/menu_helper.h
@@ -1,3 +1,31 @@
+/*
+ *                            COPYRIGHT
+ *
+ *  pcb-rnd, interactive printed circuit board design
+ *  (this file is based on PCB, interactive printed circuit board design)
+ *  Copyright (C) 2016..2019 Tibor 'Igor2' Palinkas
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  Contact:
+ *    Project page: http://repo.hu/projects/pcb-rnd
+ *    lead developer: http://repo.hu/projects/pcb-rnd/contact.html
+ *    mailing list: pcb-rnd (at) list.repo.hu (send "subscribe")
+ *
+ */
+
 #ifndef PCB_HID_FLAGS_H
 #define PCB_HID_FLAGS_H
 
