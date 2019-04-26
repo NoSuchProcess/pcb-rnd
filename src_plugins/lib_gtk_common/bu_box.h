449004593a03c5d92f39ab8174a14267721a337c
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-04-26 14:47:50 +0000

    [gtk] -Move: category vbox from bu_box to the attribute dialog code - that's the only one legitimate user
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@24940 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_gtk_common/bu_box.h
+++ src_plugins/lib_gtk_common/bu_box.h
@@ -7,5 +7,3 @@
 GtkWidget *ghid_framed_vbox(GtkWidget * box, gchar * label, gint frame_border_width,
 														gboolean frame_expand, gint vbox_pad, gint vbox_border_width);
 
-GtkWidget *ghid_category_vbox(GtkWidget * box, const gchar * category_header,
-															gint header_pad, gint box_pad, gboolean pack_start, gboolean bottom_pad);
