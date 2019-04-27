2b253114a35955c8349dcf1b8b349f7422b5ac09
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-04-27 09:42:58 +0000

    [lib_hid_pcbui] -Cleanup: status bar: remove stale debug message
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@25000 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_hid_pcbui/status.c
+++ src_plugins/lib_hid_pcbui/status.c
@@ -182,6 +182,5 @@ void pcb_status_st_update_conf(conf_native_t *cfg, int arr_idx)
 
 void pcb_status_update_ev(void *user_data, int argc, pcb_event_arg_t argv[])
 {
-printf("JAJJ\n");
 	status_pcb2dlg();
 }
