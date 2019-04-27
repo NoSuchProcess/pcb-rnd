4a29178bb980e83be8d5f2f6581eacbf434c1be6
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-04-27 06:26:59 +0000

    [lib_hid_pcbui] -Change: toolbar: reorder the code to avoid forward declaration
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@24985 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_hid_pcbui/toolbar.c
+++ src_plugins/lib_hid_pcbui/toolbar.c
@@ -44,6 +44,22 @@ typedef struct {
 
 static toolbar_ctx_t toolbar;
 
+static void toolbar_pcb2dlg()
+{
+	pcb_toolid_t tid;
+
+	if (!toolbar.sub_inited)
+		return;
+
+	for(tid = 0; tid < toolbar.tid2wid.used; tid++) {
+		int st, wid = toolbar.tid2wid.array[tid];
+		if (wid == 0)
+			continue;
+		st = (tid == conf_core.editor.mode) ? 2 : 1;
+		pcb_gui->attr_dlg_widget_state(toolbar.sub.dlg_hid_ctx, wid, st);
+	}
+}
+
 static void toolbar_create_static(pcb_hid_cfg_t *cfg)
 {
 	const lht_node_t *t, *ts = pcb_hid_cfg_get_menu(cfg, "/toolbar_static");
@@ -72,22 +88,6 @@ static void toolbar_create_static(pcb_hid_cfg_t *cfg)
 	else {
 		PCB_DAD_LABEL(toolbar.sub.dlg, "No toolbar found in the menu file.");
 		PCB_DAD_HELP(toolbar.sub.dlg, "Check your menu file. If you use a locally modified or custom\nmenu file, make sure you merge upstream changes\n(such as the new toolbar subtree)");
-	}
-}
-
-static void toolbar_pcb2dlg()
-{
-	pcb_toolid_t tid;
-
-	if (!toolbar.sub_inited)
-		return;
-
-	for(tid = 0; tid < toolbar.tid2wid.used; tid++) {
-		int st, wid = toolbar.tid2wid.array[tid];
-		if (wid == 0)
-			continue;
-		st = (tid == conf_core.editor.mode) ? 2 : 1;
-		pcb_gui->attr_dlg_widget_state(toolbar.sub.dlg_hid_ctx, wid, st);
 	}
 }
 
