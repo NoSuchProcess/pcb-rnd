762d157fb732452f6173c3aa479229f57bafa72b
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-05-12 05:25:56 +0000

    [hidlib] -Fix: new layer sel: do not react on layer vis change event if the layer selector is not initialized
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@25429 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_hid_pcbui/layersel.c
+++ src_plugins/lib_hid_pcbui/layersel.c
@@ -669,7 +669,7 @@ void pcb_layersel_gui_init_ev(pcb_hidlib_t *hidlib, void *user_data, int argc, p
 
 void pcb_layersel_vis_chg_ev(pcb_hidlib_t *hidlib, void *user_data, int argc, pcb_event_arg_t argv[])
 {
-	if (layersel.lock_vis > 0)
+	if ((!layersel.sub_inited) || (layersel.lock_vis > 0))
 		return;
 	layersel_update_vis(&layersel, PCB);
 }
