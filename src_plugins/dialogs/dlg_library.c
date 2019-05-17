ec1668f2674ebe12a93a5bb370c71360b86bde58
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-05-17 15:08:53 +0000

    -Del: stale debug message
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@25536 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/dialogs/dlg_library.c
+++ src_plugins/dialogs/dlg_library.c
@@ -628,7 +628,6 @@ fgw_error_t pcb_act_LibraryDialog(fgw_arg_t *ores, int oargc, fgw_arg_t *oargv)
 
 static void library_changed_ev(pcb_hidlib_t *hidlib, void *user_data, int argc, pcb_event_arg_t argv[])
 {
-printf("d1 %d\n", library_ctx.active);
 	if (library_ctx.active)
 		library_lib2dlg(&library_ctx);
 }
