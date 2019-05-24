82816e69fd43e5471c316fcdd2a1791472297695
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-05-24 13:10:25 +0000

    [io_lihata] -Add: mark the config settings for omitting the font and config [dangerous] because it may ruin the portability of the resulting file
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@25688 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_lihata/lht_conf.h
+++ src_plugins/io_lihata/lht_conf.h
@@ -7,8 +7,8 @@ typedef struct {
 	const struct {
 		const struct {
 			CFT_STRING aux_pcb_pattern;  /* [obsolete] file name pattern to use when generating the .pcb backup */
-			CFT_BOOLEAN omit_font;       /* do not save the font subtree in board files */
-			CFT_BOOLEAN omit_config;     /* do not save the config subtree in board files */
+			CFT_BOOLEAN omit_font;       /* [dangerous] do not save the font subtree in board files */
+			CFT_BOOLEAN omit_config;     /* [dangerous] do not save the config subtree in board files */
 			CFT_BOOLEAN omit_styles;     /* do not save the routing styles subtree in board files */
 		} io_lihata;
 	} plugins;
