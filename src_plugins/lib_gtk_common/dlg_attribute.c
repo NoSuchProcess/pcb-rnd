abb0abd7411334199fd66834c4dbc5586a0bc2ab
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-05-25 12:38:28 +0000

    [gtk] -Add: transient-for call to mark DAD dialogs relation to the top window
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@25703 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_gtk_common/dlg_attribute.c
+++ src_plugins/lib_gtk_common/dlg_attribute.c
@@ -854,6 +854,7 @@ void *ghid_attr_dlg_new(pcb_gtk_common_t *com, const char *id, pcb_hid_attribute
 	pcb_event(com->hidlib, PCB_EVENT_DAD_NEW_DIALOG, "psp", ctx, ctx->id, plc);
 
 	ctx->dialog = gtk_dialog_new();
+	gtk_window_set_transient_for(GTK_WINDOW(ctx->dialog), GTK_WINDOW(com->top_window));
 
 	gtk_window_set_title(GTK_WINDOW(ctx->dialog), title);
 	gtk_window_set_role(GTK_WINDOW(ctx->dialog), id);
