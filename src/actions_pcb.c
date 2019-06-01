eab81820ed02379105771ce6d268d7c3cd3dfedf
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-06-01 14:02:18 +0000

    -Cleanup: indentation
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@25859 1d61de99-560f-4dba-8957-1e505d42358b

--- src/actions_pcb.c
+++ src/actions_pcb.c
@@ -129,28 +129,28 @@ pcb_data_t *pcb_data_by_name(pcb_board_t *pcb, const char **name)
 	if (name == NULL)
 		return NULL;
 
-			if (pcb_strncasecmp(*name, "pcb", 3) == 0) {
-				(*name) += 3;
-				if (**name == '/')
-					(*name)++;
-				return pcb->Data;
-			}
-			else if (pcb_strncasecmp(*name, "buffer#", 7) == 0) {
-				char *end;
-				long idx = strtol((*name)+7, &end, 10);
-				if ((*end == '\0') && (idx >= 0) && (idx < PCB_MAX_BUFFER)) {
-					*name = end;
-					if (**name == '/')
-						(*name)++;
-					return pcb_buffers[idx].Data;
-				}
-			}
-			else if (pcb_strncasecmp(*name, "buffer", 6) == 0) {
-				(*name) += 6;
-				if (**name == '/')
-					(*name)++;
-				return PCB_PASTEBUFFER->Data;
-			}
+	if (pcb_strncasecmp(*name, "pcb", 3) == 0) {
+		(*name) += 3;
+		if (**name == '/')
+			(*name)++;
+		return pcb->Data;
+	}
+	else if (pcb_strncasecmp(*name, "buffer#", 7) == 0) {
+		char *end;
+		long idx = strtol((*name)+7, &end, 10);
+		if ((*end == '\0') && (idx >= 0) && (idx < PCB_MAX_BUFFER)) {
+			*name = end;
+			if (**name == '/')
+				(*name)++;
+			return pcb_buffers[idx].Data;
+		}
+	}
+	else if (pcb_strncasecmp(*name, "buffer", 6) == 0) {
+		(*name) += 6;
+		if (**name == '/')
+			(*name)++;
+		return PCB_PASTEBUFFER->Data;
+	}
 	return NULL;
 }
 
@@ -165,12 +165,12 @@ const char *pcb_data_to_name(pcb_board_t *pcb, pcb_data_t *data, char *buf, int 
 	if (data == pcb->Data)
 		return "pcb";
 
-		for(n = 0; n < PCB_MAX_BUFFER; n++) {
-			if (data == pcb_buffers[n].Data) {
-				pcb_sprintf(buf, "buffer#%d", n);
-				return buf;
-			}
+	for(n = 0; n < PCB_MAX_BUFFER; n++) {
+		if (data == pcb_buffers[n].Data) {
+			pcb_sprintf(buf, "buffer#%d", n);
+			return buf;
 		}
+	}
 
 	return NULL;
 }
