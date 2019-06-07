92653867d2801352790eb012d56100746380845e
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-06-07 10:02:25 +0000

    [layer] -Cleanup: use the layer id type for iterating over layers in visibility save/restore, don't assume int is wide enough
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@26066 1d61de99-560f-4dba-8957-1e505d42358b

--- src/layer_vis.c
+++ src/layer_vis.c
@@ -314,7 +314,7 @@ pcb_layer_id_t pcb_layer_vis_last_lyt(pcb_layer_type_t target)
 
 void pcb_hid_save_and_show_layer_ons(int *save_array)
 {
-	int i;
+	pcb_layer_id_t i;
 	for (i = 0; i < pcb_max_layer; i++) {
 		save_array[i] = PCB->Data->Layer[i].meta.real.vis;
 		PCB->Data->Layer[i].meta.real.vis = 1;
@@ -330,7 +330,7 @@ void pcb_hid_save_and_show_layergrp_ons(int *save_array)
 
 void pcb_hid_restore_layer_ons(int *save_array)
 {
-	int i;
+	pcb_layer_id_t i;
 	for (i = 0; i < pcb_max_layer; i++)
 		PCB->Data->Layer[i].meta.real.vis = save_array[i];
 }
