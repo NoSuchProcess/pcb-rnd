a8224a84430521700038b5a8ebc8f60b6e8ac0b8
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-06-15 09:47:51 +0000

    -Fix: missing copyright banner: hid_init.h has the same history as hid_init.c
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@26206 1d61de99-560f-4dba-8957-1e505d42358b

--- src/hid_init.c
+++ src/hid_init.c
@@ -5,7 +5,7 @@
  *  (this file is based on PCB, interactive printed circuit board design)
  *  Copyright (C) 1994,1995,1996 Thomas Nau
  *  Copyright (C) 2004 harry eaton
- *  Copyright (C) 2016..2018 Tibor 'Igor2' Palinkas
+ *  Copyright (C) 2016..2019 Tibor 'Igor2' Palinkas
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
