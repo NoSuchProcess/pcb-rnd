2de8ba9fdb004be714f3e36f0e6454f067a29e1d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-06-17 03:31:43 +0000

    [propedit] -Cleanup: compiler warning on sign-mismatch
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@26272 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/propedit/propsel.c
+++ src_plugins/propedit/propsel.c
@@ -509,7 +509,8 @@ static void set_pstk(pcb_propset_ctx_t *st, pcb_pstk_t *ps)
 
 	if (set_common(st, (pcb_any_obj_t *)ps)) return;
 
-	ca = i = (st->c != 0);
+	ca = st->c;
+	i = (st->c != 0);
 	proto = pcb_pstk_get_proto(ps);
 
 	if (strncmp(st->name, "p/padstack/", 11) == 0) {
@@ -529,7 +530,7 @@ static void set_pstk(pcb_propset_ctx_t *st, pcb_pstk_t *ps)
 		if (st->c_valid && (strcmp(pn, "smirror") == 0) &&
 		    (pcb_pstk_change_instance(ps, NULL, NULL, NULL, NULL, &i) == 0)) DONE;
 		if (st->c_valid && (strcmp(pn, "proto") == 0) &&
-		    (pcb_pstk_change_instance(ps, &st->c, NULL, NULL, NULL, NULL) == 0)) DONE;
+		    (pcb_pstk_change_instance(ps, &ca, NULL, NULL, NULL, NULL) == 0)) DONE;
 		if (st->c_valid && (strcmp(pn, "hole") == 0) &&
 		    (pcb_pstk_proto_change_hole(proto, NULL, &st->c, NULL, NULL) == 0)) DONE;
 		if (st->c_valid && (strcmp(pn, "plated") == 0) &&
