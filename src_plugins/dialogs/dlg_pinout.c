d98cec5495af3d8c95b28fd569e5ae1024d293ae
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-06-28 12:23:23 +0000

    [dialog] -Fix: when pinout() doesn't find a subcircuit, print an error message
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@26497 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/dialogs/dlg_pinout.c
+++ src_plugins/dialogs/dlg_pinout.c
@@ -184,7 +184,9 @@ static fgw_error_t pcb_act_Pinout(fgw_arg_t *res, int argc, fgw_arg_t *argv)
 		pcb_dlg_pinout(PCB, PCB->Data, sc);
 		PCB_ACT_IRES(0);
 	}
-	else
+	else {
+		pcb_message(PCB_MSG_ERROR, "pinout dialog: there's no subcircuit there\n");
 		PCB_ACT_IRES(-1);
+	}
 	return 0;
 }
