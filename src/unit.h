c2f4c92e2f45820154920992848b8d4625a5c9aa
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-07-18 05:26:21 +0000

    [unit] -Update: bump copyright year
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@26686 1d61de99-560f-4dba-8957-1e505d42358b

--- src/unit.h
+++ src/unit.h
@@ -4,7 +4,7 @@
  *  pcb-rnd, interactive printed circuit board design
  *  (this file is based on PCB, interactive printed circuit board design)
  *  Copyright (C) 2011 Andrew Poelstra
- *  Copyright (C) 2016 Tibor 'Igor2' Palinkas
+ *  Copyright (C) 2016,2019 Tibor 'Igor2' Palinkas
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
