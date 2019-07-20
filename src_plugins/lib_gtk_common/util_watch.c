9777491dbbf22ca44ece8cf6952374198bb8ed30
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-07-20 04:21:31 +0000

    [gtk] -Bump: copyright year
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@26761 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_gtk_common/util_watch.c
+++ src_plugins/lib_gtk_common/util_watch.c
@@ -4,7 +4,7 @@
  *  pcb-rnd, interactive printed circuit board design
  *  (this file is based on PCB, interactive printed circuit board design)
  *  Copyright (C) 1994,1995,1996 Thomas Nau
- *  pcb-rnd Copyright (C) 2017 Tibor 'Igor2' Palinkas
+ *  pcb-rnd Copyright (C) 2017,2019 Tibor 'Igor2' Palinkas
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
