b994d22429a75d46b526f8cf9c11d25d575f115a
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-08-02 08:26:17 +0000

    [core] -Cleanup: remove obsolete comment
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@27007 1d61de99-560f-4dba-8957-1e505d42358b

--- src/hid_color.h
+++ src/hid_color.h
@@ -3,9 +3,6 @@
 
 #include "hid.h"
 
-/* HID internal interfaces.  These may ONLY be called from the HID
-   modules, not from the common PCB code.  */
-
 /* Used to cache color lookups.  If set is zero, it looks up the name
    and if found sets val and returns nonzero.  If not found, it
    returns zero.  If set is nonzero, name/val is added to the
