1b5833da851d13d254ee23d39286f1d2a666618d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-10-30 05:14:07 +0000

    [core] -Cleanup: allow arc radius to go down to 0 - don't constraint it with a random value when nothing breaks
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@27930 1d61de99-560f-4dba-8957-1e505d42358b

--- src/globalconst.h
+++ src/globalconst.h
@@ -34,15 +34,15 @@
 #define PCB_LARGE_VALUE      (COORD_MAX / 2 - 1) /* maximum extent of board and elements */
 
 #define PCB_MAX_COORD        ((pcb_coord_t)PCB_LARGE_VALUE) /* coordinate limits */
-#define PCB_MIN_SIZE         PCB_MIL_TO_COORD(10) /* lowest width and height of the board */
+#define PCB_MIN_SIZE         0
 
 #define PCB_MAX_LAYER        38    /* max number of layer, check source code for more changes, a *lot* more changes */
 /* new array size that allows substrate layers */
 #define PCB_MAX_LAYERGRP     ((PCB_MAX_LAYER+8)*2)    /* max number of layer groups, a.k.a. physical layers: a few extra outer layers per side, pluse one substrate per real layer */
 #define PCB_MIN_THICKNESS    PCB_MIN_SIZE
 #define PCB_MAX_THICKNESS    PCB_MAX_COORD
-#define PCB_MIN_ARCRADIUS    PCB_MIL_TO_COORD(0.01)
-#define PCB_MAX_ARCRADIUS    ((pcb_coord_t)PCB_LARGE_VALUE)
+#define PCB_MIN_ARCRADIUS    PCB_MIN_SIZE
+#define PCB_MAX_ARCRADIUS    PCB_MAX_COORD
 #define PCB_MIN_TEXTSCALE    10 /* scaling of text objects in percent */
 #define PCB_MAX_TEXTSCALE    10000
 #define PCB_MIN_PINORVIASIZE PCB_MIL_TO_COORD(20)	/* size of a pin or via */
