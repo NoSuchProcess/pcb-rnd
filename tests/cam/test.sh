77c5457f19a40bc0e85f3c296d85a0ea7b84ac92
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2019-12-06 09:04:45 +0000

    [test] -Add: cam test framework [test] -Add: cam test for named groups
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@28604 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ tests/cam/test.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#DBG=cdb
+
+TESTS=grp_name
+
+do_test()
+{
+	local testcase="$1" refs r o
+	$DBG pcb-rnd -C $testcase/cam.conf -x cam test --outfile $testcase/out layers.lht
+
+	(
+		cd $testcase
+		refs=`ls ref.*.svg 2>/dev/null`
+		if test -z "$refs"
+		then
+			tar -zxf ref.tar.gz
+		fi
+		for r in ref.*.svg
+		do
+			o=out.${r#ref.}
+			diff -u $r $o
+		done
+	)
+}
+
+do_clean()
+{
+	local testcase="$1"
+	rm -f $testcase/ref.*.svg $testcase/out.*.svg
+}
+
+cmd="$1"
+shift 1
+
+tests="$@"
+if test -z "$tests"
+then
+	tests=$TESTS
+fi
+
+for n in $tests
+do
+	case "$cmd" in
+		test) do_test $n ;;
+		clean) do_clean $n ;;
+	esac
+done
