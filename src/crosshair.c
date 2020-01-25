6442a2e72dd6db50de4c0558147c955609842cd9
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-01-25 11:08:53 +0000

    -Update: bump copyright year
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@29331 1d61de99-560f-4dba-8957-1e505d42358b

--- src/crosshair.c
+++ src/crosshair.c
@@ -5,6 +5,7 @@
  *  (this file is based on PCB, interactive printed circuit board design)
  *  Copyright (C) 1994,1995,1996 Thomas Nau
  *  15 Oct 2008 Ineiev: add different crosshair shapes
+ *  Copyright (C) 2020 Tibor 'Igor2' Palinkas
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
