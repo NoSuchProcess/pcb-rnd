f2d2b0b5887c21909f33fd82ebf85e93bb2065a3
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-01-28 07:17:26 +0000

    [core] -Fix: don't crash on NULL xform when drawing attached text
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@29430 1d61de99-560f-4dba-8957-1e505d42358b

--- src/obj_text.c
+++ src/obj_text.c
@@ -1416,9 +1416,13 @@ void pcb_text_invalidate_draw(pcb_layer_t *Layer, pcb_text_t *Text)
 	pcb_draw_invalidate(Text);
 }
 
+static pcb_draw_info_t txor_info;
+static pcb_xform_t txor_xform;
+
 void pcb_text_draw_xor(pcb_text_t *text, pcb_coord_t x, pcb_coord_t y)
 {
-	DrawTextLowLevel_(NULL, text, 0, 1, x, y, PCB_TXT_TINY_CHEAP);
+	txor_info.xform = &txor_xform;
+	DrawTextLowLevel_(&txor_info, text, 0, 1, x, y, PCB_TXT_TINY_CHEAP);
 	if ((conf_core.appearance.text_host_bbox) && (text->BoundingBox.X1 != text->BoundingBox.X2)) {
 		pcb_render->draw_line(pcb_crosshair.GC, x + text->BoundingBox.X1, y + text->BoundingBox.Y1, x + text->BoundingBox.X1, y + text->BoundingBox.Y2);
 		pcb_render->draw_line(pcb_crosshair.GC, x + text->BoundingBox.X1, y + text->BoundingBox.Y1, x + text->BoundingBox.X2, y + text->BoundingBox.Y1);
