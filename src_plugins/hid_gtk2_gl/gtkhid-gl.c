e57d7cee749e056f809a02d22d0862eb2874ffac
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-02-20 18:24:05 +0000

    [gl] -Fix: always draw crosshair end-to-end of screen view area
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@29862 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/hid_gtk2_gl/gtkhid-gl.c
+++ src_plugins/hid_gtk2_gl/gtkhid-gl.c
@@ -761,10 +761,10 @@ static gboolean ghid_gl_drawing_area_expose_cb(GtkWidget *widget, pcb_gtk_expose
 
 	ghid_gl_start_drawing(port);
 
-	ctx.view.X1 = MIN(0, Pz(ev->area.x) + ghidgui->port.view.x0);
-	ctx.view.Y1 = MIN(0, Pz(ev->area.y) + ghidgui->port.view.y0);
-	ctx.view.X2 = MAX(hidlib->size_x, Pz(ev->area.x + ev->area.width) + ghidgui->port.view.x0);
-	ctx.view.Y2 = MAX(hidlib->size_y, Pz(ev->area.y + ev->area.height) + ghidgui->port.view.y0);
+	ctx.view.X1 = MIN(Px(ev->area.x), Px(ev->area.x + ev->area.width + 1));
+	ctx.view.Y1 = MIN(Py(ev->area.y), Py(ev->area.y + ev->area.height + 1));
+	ctx.view.X2 = MAX(Px(ev->area.x), Px(ev->area.x + ev->area.width + 1));
+	ctx.view.Y2 = MAX(Py(ev->area.y), Py(ev->area.y + ev->area.height + 1));
 
 	pcb_gl_draw_expose_init(&gtk2_gl_hid, allocation.width, allocation.height, ev->area.x, allocation.height - ev->area.height - ev->area.y, ev->area.width, ev->area.height, &priv->offlimits_color);
 
