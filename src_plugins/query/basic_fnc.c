829f5bc08a40b773463f80920f6f139904f5df3a
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-03-25 05:11:05 +0000

    [query] -Add: action(): convert return value
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@30168 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/query/basic_fnc.c
+++ src_plugins/query/basic_fnc.c
@@ -26,6 +26,7 @@
 #include "config.h"
 
 #include <librnd/core/actions.h>
+#include <libfungw/fungw_conv.h>
 
 #include "board.h"
 #include "data.h"
@@ -154,7 +155,7 @@ static int fnc_subcobjs(pcb_qry_exec_t *ectx, int argc, pcb_qry_val_t *argv, pcb
 static int fnc_action(pcb_qry_exec_t *ectx, int argc, pcb_qry_val_t *argv, pcb_qry_val_t *res)
 {
 	int n;
-	fgw_arg_t resa, arga[PCB_QRY_MAX_FUNC_ARGS];
+	fgw_arg_t tmp, resa, arga[PCB_QRY_MAX_FUNC_ARGS], *arg;
 	fgw_error_t e;
 
 	if (argv[0].type != PCBQ_VT_STRING) {
@@ -173,7 +174,6 @@ static int fnc_action(pcb_qry_exec_t *ectx, int argc, pcb_qry_val_t *argv, pcb_q
 					long i;
 					pcb_idpath_list_t *list = calloc(sizeof(pcb_idpath_list_t), 1);
 					for(i = 0; i < argv[n].data.lst.used; i++) {
-						fgw_arg_t tmp;
 						fgw_ptr_reg(&pcb_fgw, &tmp, PCB_PTR_DOMAIN_IDPATH, FGW_PTR | FGW_STRUCT, pcb_obj2idpath(argv[n].data.lst.array[i]));
 						pcb_idpath_list_append(list, fgw_idpath(&tmp));
 					}
@@ -202,11 +202,64 @@ static int fnc_action(pcb_qry_exec_t *ectx, int argc, pcb_qry_val_t *argv, pcb_q
 		}
 	}
 
-	if (pcb_actionv_bin(&ectx->pcb->hidlib, argv[0].data.str, &res, argc, &arga) != 0)
+	if (pcb_actionv_bin(&ectx->pcb->hidlib, argv[0].data.str, &resa, argc, arga) != 0)
 		return -1;
 
 	/* convert action result to query result */
 
+#	define FGW_TO_QRY_NUM(lst, val)   res->type = PCBQ_VT_LONG; res->data.lng = val; goto fin;
+#	define FGW_TO_QRY_STR(lst, val)   res->type = PCBQ_VT_STRING; res->data.str = pcb_strdup(val == NULL ? "" : val); goto fin;
+#	define FGW_TO_QRY_NIL(lst, val)   res->type = PCBQ_VT_VOID; goto fin;
+
+/* has to free idpath and idpathlist return, noone else will have the chance */
+#	define FGW_TO_QRY_PTR(lst, val) \
+	if (val == NULL) { \
+		res->type = PCBQ_VT_VOID; \
+		goto fin; \
+	} \
+	else if (fgw_ptr_in_domain(&pcb_fgw, val, PCB_PTR_DOMAIN_IDPATH)) { \
+		pcb_idpath_t *idp = val; \
+		res->type = PCBQ_VT_OBJ; \
+		res->data.obj = pcb_idpath2obj(ectx->pcb, idp); \
+		pcb_idpath_list_remove(idp); \
+		fgw_ptr_unreg(&pcb_fgw, &resa, PCB_PTR_DOMAIN_IDPATH); \
+		free(idp); \
+		goto fin; \
+	} \
+	else if (fgw_ptr_in_domain(&pcb_fgw, val, PCB_PTR_DOMAIN_IDPATH_LIST)) { \
+		pcb_message(PCB_MSG_ERROR, "query action(): can not convert object list yet\n"); \
+		res->type = PCBQ_VT_VOID; \
+		goto fin; \
+	} \
+	else { \
+		pcb_message(PCB_MSG_ERROR, "query action(): can not convert unknown pointer\n"); \
+		res->type = PCBQ_VT_VOID; \
+		goto fin; \
+	}
+
+	arg = &resa;
+	if (FGW_IS_TYPE_CUSTOM(resa.type))
+		fgw_arg_conv(&pcb_fgw, &resa, FGW_AUTO);
+
+	switch(FGW_BASE_TYPE(resa.type)) {
+		ARG_CONV_CASE_LONG(lst, FGW_TO_QRY_NUM);
+		ARG_CONV_CASE_LLONG(lst, FGW_TO_QRY_NUM);
+		ARG_CONV_CASE_DOUBLE(lst, FGW_TO_QRY_NUM);
+		ARG_CONV_CASE_LDOUBLE(lst, FGW_TO_QRY_NUM);
+		ARG_CONV_CASE_PTR(lst, FGW_TO_QRY_PTR);
+		ARG_CONV_CASE_STR(lst, FGW_TO_QRY_STR);
+		ARG_CONV_CASE_CLASS(lst, FGW_TO_QRY_NIL);
+		ARG_CONV_CASE_INVALID(lst, FGW_TO_QRY_NIL);
+	}
+	if (arg->type & FGW_PTR) {
+		FGW_TO_QRY_PTR(lst, arg->val.ptr_void);
+	}
+	else {
+		FGW_TO_QRY_NIL(lst, 0);
+	}
+
+	fin:;
+	fgw_arg_free(&pcb_fgw, &resa);
 	return 0;
 }
 
