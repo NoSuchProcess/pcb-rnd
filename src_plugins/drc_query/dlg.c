e0a40dbaea36aa988927a08adf6ec418d3fb6f75
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-04-03 09:54:41 +0000

    [drc_query] -Add: merge all changes after a rule save
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@30361 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/drc_query/dlg.c
+++ src_plugins/drc_query/dlg.c
@@ -191,7 +191,7 @@ static void rule_btn_save_cb(void *hid_ctx, void *caller_data, pcb_hid_attribute
 	MKDIR_ND_SET_TEXT(nd, "title", ctx->dlg[ctx->wtitle].val.str);
 	MKDIR_ND_SET_TEXT(nd, "query", txt->hid_get_text(atxt, hid_ctx));
 
-	pcb_conf_update(DRC_CONF_PATH_PLUGIN, 0);
+	pcb_conf_update(NULL, -1);
 }
 
 #undef MKDIR_ND
