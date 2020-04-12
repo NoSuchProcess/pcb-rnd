ddac926ab0ff606a5ffab9cc1c4f07eed016c421
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-04-12 14:03:47 +0000

    [io_bxl] -Add: tester: dummy glue layer for line
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@30583 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ tests/io_bxl/glue.c
@@ -0,0 +1,9 @@
+#include "bxl.h"
+
+void pcb_bxl_pattern_begin(pcb_bxl_ctx_t *ctx, const char *name) {}
+void pcb_bxl_pattern_end(pcb_bxl_ctx_t *ctx) {}
+void pcb_bxl_reset(pcb_bxl_ctx_t *ctx) {}
+void pcb_bxl_set_layer(pcb_bxl_ctx_t *ctx, const char *layer_name) {}
+void pcb_bxl_set_coord(pcb_bxl_ctx_t *ctx, int idx, pcb_coord_t val) {}
+void pcb_bxl_add_line(pcb_bxl_ctx_t *ctx) {}
+
