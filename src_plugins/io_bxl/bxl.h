2fa4a35797f98457160d6ccde133c22aa5d23617
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-04-12 12:47:14 +0000

    [io_bxl] -Add: context struct for remembering data and the subc being created
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@30575 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_bxl/bxl.h
+++ src_plugins/io_bxl/bxl.h
@@ -1,6 +1,13 @@
 #ifndef PCB_BXL_H
 #define PCB_BXL_H
+
+#include "data.h"
+#include "obj_subc.h"
+
 typedef struct pcb_bxl_ctx_s {
-	int dummy;
+	pcb_data_t *data;
+	pcb_subc_t *sc;
+	char in_target_fp; /* 1 if we are parsing the target footprint; else skip */
 } pcb_bxl_ctx_t;
+
 #endif
