8739ff7ed9eba3012f8aaadb4ddfe46399045bc7
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-04-13 08:38:51 +0000

    [io_bxl] -Update: parser test glue layer to the new API
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@30602 1d61de99-560f-4dba-8957-1e505d42358b

--- tests/io_bxl/glue.c
+++ tests/io_bxl/glue.c
@@ -4,7 +4,6 @@ void pcb_bxl_pattern_begin(pcb_bxl_ctx_t *ctx, const char *name) {}
 void pcb_bxl_pattern_end(pcb_bxl_ctx_t *ctx) {}
 void pcb_bxl_reset(pcb_bxl_ctx_t *ctx) {}
 void pcb_bxl_set_layer(pcb_bxl_ctx_t *ctx, const char *layer_name) {}
-void pcb_bxl_set_coord(pcb_bxl_ctx_t *ctx, int idx, pcb_coord_t val) {}
 void pcb_bxl_add_line(pcb_bxl_ctx_t *ctx) {}
 void pcb_bxl_add_arc(pcb_bxl_ctx_t *ctx) {}
 void pcb_bxl_poly_begin(pcb_bxl_ctx_t *ctx) {}
