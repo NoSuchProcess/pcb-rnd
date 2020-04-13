47f8bc66410c2d759d36ad9343aadf959258de64
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-04-13 13:33:27 +0000

    [io_bxl] -Update: parser test: glue layer for padstacks
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@30613 1d61de99-560f-4dba-8957-1e505d42358b

--- tests/io_bxl/glue.c
+++ tests/io_bxl/glue.c
@@ -7,6 +7,8 @@ void pcb_bxl_set_layer(pcb_bxl_ctx_t *ctx, const char *layer_name) {}
 void pcb_bxl_set_justify(pcb_bxl_ctx_t *ctx, const char *str) {}
 void pcb_bxl_add_line(pcb_bxl_ctx_t *ctx) {}
 void pcb_bxl_add_arc(pcb_bxl_ctx_t *ctx) {}
+void pcb_bxl_add_text(pcb_bxl_ctx_t *ctx) {}
+void pcb_bxl_set_text_str(pcb_bxl_ctx_t *ctx, char *str) { free(str); }
 void pcb_bxl_poly_begin(pcb_bxl_ctx_t *ctx) {}
 void pcb_bxl_poly_end(pcb_bxl_ctx_t *ctx) {}
 void pcb_bxl_poly_add_vertex(pcb_bxl_ctx_t *ctx, pcb_coord_t x, pcb_coord_t y) {}
@@ -15,3 +17,11 @@ void pcb_bxl_add_property(pcb_bxl_ctx_t *ctx, pcb_any_obj_t *obj, const char *ke
 static pcb_bxl_test_style_t dummy_ts;
 void pcb_bxl_text_style_begin(pcb_bxl_ctx_t *ctx, char *name) { free(name); ctx->state.text_style = &dummy_ts; }
 void pcb_bxl_text_style_end(pcb_bxl_ctx_t *ctx) {}
+void pcb_bxl_set_text_style(pcb_bxl_ctx_t *ctx, const char *name) {}
+
+
+void pcb_bxl_padstack_begin(pcb_bxl_ctx_t *ctx, char *name) { free(name); }
+void pcb_bxl_padstack_end(pcb_bxl_ctx_t *ctx) { }
+void pcb_bxl_padstack_begin_shape(pcb_bxl_ctx_t *ctx, const char *name) {}
+void pcb_bxl_padstack_end_shape(pcb_bxl_ctx_t *ctx) {}
+
