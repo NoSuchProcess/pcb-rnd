abd7a9149c1bd5f47a72bd23d02b00ae249e17c5
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-04-14 08:07:23 +0000

    [io_bxl] -Update: test parser: pattern reset glue
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@30631 1d61de99-560f-4dba-8957-1e505d42358b

--- tests/io_bxl/glue.c
+++ tests/io_bxl/glue.c
@@ -6,6 +6,7 @@ pcb_coord_t pcb_bxl_coord_y(pcb_coord_t c) { return -c; }
 void pcb_bxl_pattern_begin(pcb_bxl_ctx_t *ctx, const char *name) {}
 void pcb_bxl_pattern_end(pcb_bxl_ctx_t *ctx) {}
 void pcb_bxl_reset(pcb_bxl_ctx_t *ctx) {}
+void pcb_bxl_reset_pattern(pcb_bxl_ctx_t *ctx) {}
 void pcb_bxl_set_layer(pcb_bxl_ctx_t *ctx, const char *layer_name) {}
 void pcb_bxl_set_justify(pcb_bxl_ctx_t *ctx, const char *str) {}
 void pcb_bxl_add_line(pcb_bxl_ctx_t *ctx) {}
@@ -16,6 +17,7 @@ void pcb_bxl_poly_begin(pcb_bxl_ctx_t *ctx) {}
 void pcb_bxl_poly_end(pcb_bxl_ctx_t *ctx) {}
 void pcb_bxl_poly_add_vertex(pcb_bxl_ctx_t *ctx, pcb_coord_t x, pcb_coord_t y) {}
 void pcb_bxl_add_property(pcb_bxl_ctx_t *ctx, pcb_any_obj_t *obj, const char *keyval) {}
+void pcb_bxl_set_attr_val(pcb_bxl_ctx_t *ctx, char *key, char *val) { free(key); free(val); }
 
 static pcb_bxl_test_style_t dummy_ts;
 void pcb_bxl_text_style_begin(pcb_bxl_ctx_t *ctx, char *name) { free(name); ctx->state.text_style = &dummy_ts; }
