0ac86dc04c7b8c3c5f0a9e996f655df33e9396f9
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-04-30 14:09:35 +0000

    [librnd] -Cleanup: math_helper whitespace
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@31004 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/core/math_helper.h
+++ src/librnd/core/math_helper.h
@@ -31,40 +31,40 @@
 /* ---------------------------------------------------------------------------
  * some math constants
  */
-#ifndef	M_PI
-#define	M_PI			3.14159265358979323846
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
 #endif
 #ifndef M_SQRT1_2
-#define M_SQRT1_2 		0.707106781	/* 1/sqrt(2) */
+#define M_SQRT1_2 0.707106781 /* 1/sqrt(2) */
 #endif
-#define	RND_M180			(M_PI/180.0)
-#define RND_RAD_TO_DEG		(180.0/M_PI)
-#define	RND_TAN_22_5_DEGREE_2	0.207106781	/* 0.5*tan(22.5) */
-#define RND_COS_22_5_DEGREE		0.923879533	/* cos(22.5) */
-#define	RND_TAN_30_DEGREE		0.577350269	/* tan(30) */
-#define	RND_TAN_60_DEGREE		1.732050808	/* tan(60) */
-#define RND_LN_2_OVER_2		0.346573590
-#define RND_TO_RADIANS(degrees)	(RND_M180 * (degrees))
-#define RND_SQRT2	1.41421356237309504880	/* sqrt(2) */
+#define RND_M180                 (M_PI/180.0)
+#define RND_RAD_TO_DEG           (180.0/M_PI)
+#define RND_TAN_22_5_DEGREE_2    0.207106781  /* tan(22.5)/2 */
+#define RND_COS_22_5_DEGREE      0.923879533  /* cos(22.5) */
+#define RND_TAN_30_DEGREE        0.577350269  /* tan(30) */
+#define RND_TAN_60_DEGREE        1.732050808  /* tan(60) */
+#define RND_LN_2_OVER_2          0.346573590
+#define RND_TO_RADIANS(degrees)  (RND_M180 * (degrees))
+#define RND_SQRT2                1.41421356237309504880 /* sqrt(2) */
 
 #define RND_CLAMP(x, low, high)  (((x) > (high)) ? (high) : (((x) < (low)) ? (low) : (x)))
-#define RND_ABS(a)	   (((a) < 0) ? -(a) : (a))
-#define RND_SQUARE(x)          ((float) (x) * (float) (x))
+#define RND_ABS(a)               (((a) < 0) ? -(a) : (a))
+#define RND_SQUARE(x)            ((float) (x) * (float) (x))
 
 /* ---------------------------------------------------------------------------
  * misc macros, some might already be defined by <limits.h>
  */
-#ifndef	MIN
-#define	MIN(a,b)		((a) < (b) ? (a) : (b))
-#define	MAX(a,b)		((a) > (b) ? (a) : (b))
+#ifndef MIN
+#define MIN(a,b)  ((a) < (b) ? (a) : (b))
+#define MAX(a,b)  ((a) > (b) ? (a) : (b))
 #endif
 #ifndef SGN
-#define SGN(a)			((a) >0 ? 1 : ((a) == 0 ? 0 : -1))
+#define SGN(a)  ((a) >0 ? 1 : ((a) == 0 ? 0 : -1))
 #endif
-#define RND_SGNZ(a)                 ((a) >=0 ? 1 : -1)
+#define RND_SGNZ(a)                  ((a) >=0 ? 1 : -1)
 #define RND_MAKE_MIN(a,b)            if ((b) < (a)) (a) = (b)
 #define RND_MAKE_MAX(a,b)            if ((b) > (a)) (a) = (b)
 
 
-#define	RND_SWAP_SIGN_X(x)		(x)
-#define	RND_SWAP_SIGN_Y(y)		(-(y))
+#define RND_SWAP_SIGN_X(x)  (x)
+#define RND_SWAP_SIGN_Y(y)  (-(y))
