f9017057197d0683e972670f7fdfd3e6805069a8
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-05-15 10:59:22 +0000

    [libuirc] -Add: make trace optional
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@31395 1d61de99-560f-4dba-8957-1e505d42358b

--- src_3rd/libuirc/libuirc.c
+++ src_3rd/libuirc/libuirc.c
@@ -254,7 +254,9 @@ static uirc_event_t uirc_parse(uirc_t *ctx, char *line)
 	char *end, *arg, *cmd, *from = line;
 	uirc_event_t res = 0;
 
-/*printf("line='%s'\n", line);*/
+#ifdef LIBUIRC_TRACE
+printf("line='%s'\n", line);
+#endif
 
 	if (strncmp(line, "PING", 4) == 0) {
 		from[1] = 'O';
