e4f31ec15c2a9116f9812cb94f250b40644cd26d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-05-15 07:07:00 +0000

    [irc] -Add: login dialog
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@31379 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/irc/irc.c
+++ src_plugins/irc/irc.c
@@ -31,11 +31,87 @@
 #include <librnd/core/hid_dad.h>
 #include <librnd/core/plugins.h>
 
-extern const char pcb_acts_irc[] = "irc()";
-extern const char pcb_acth_irc[] = "non-modal, single-instance, single-server, single-channel irc window for online support";
-extern fgw_error_t pcb_act_irc(fgw_arg_t *res, int argc, fgw_arg_t *argv)
+#include <libuirc/libuirc.h>
+
+typedef enum {
+	IRC_OFF,
+	IRC_LOGIN,
+	IRC_ONLINE,
+	IRC_DISCONNECTED
+} irc_state_t;
+
+typedef struct{
+	RND_DAD_DECL_NOINIT(dlg)
+	RND_DAD_DECL_NOINIT(dlg_login)
+	irc_state_t state;
+	uirc_t irc;
+} irc_ctx_t;
+
+irc_ctx_t irc_ctx;
+
+static void irc_login_close_cb(void *caller_data, rnd_hid_attr_ev_t ev)
 {
-	rnd_trace("irc: not yet\n");
+	irc_ctx_t *ctx = caller_data;
+
+	if (ctx->dlg_login_ret_override->valid && (ctx->dlg_login_ret_override->value == IRC_LOGIN)) {
+		/* connect */
+	}
+	else {
+		ctx->state = IRC_OFF;
+	}
+	RND_DAD_FREE(ctx->dlg_login);
+}
+
+static int pcb_dlg_login_irc_login(void)
+{
+	char *nick;
+	rnd_hid_dad_buttons_t clbtn[] = {{"Connect!", IRC_LOGIN}, {"Cancel", 0}, {NULL, 0}};
+	if (irc_ctx.state != IRC_OFF)
+		return -1; /* do not open another */
+
+	nick = rnd_get_user_name();
+	if ((nick == NULL) || (*nick == '\0') || (strcmp(nick, "Unknown") == 0)) {
+		int r = rnd_getpid();
+		free(nick);
+		r ^= rand();
+		r ^= time(NULL);
+		nick = rnd_strdup_printf("pcb-rnd-%d", r);
+	}
+
+	RND_DAD_BEGIN_VBOX(irc_ctx.dlg_login);
+		RND_DAD_COMPFLAG(irc_ctx.dlg_login, RND_HATF_EXPFILL);
+		RND_DAD_LABEL(irc_ctx.dlg_login, "On-line help: IRC login");
+		RND_DAD_BEGIN_TABLE(irc_ctx.dlg_login, 2);
+			RND_DAD_LABEL(irc_ctx.dlg_login, "nickname:");
+			RND_DAD_STRING(irc_ctx.dlg_login);
+				RND_DAD_DEFAULT_PTR(irc_ctx.dlg_login, nick);
+
+			RND_DAD_LABEL(irc_ctx.dlg_login, "channel:");
+			RND_DAD_STRING(irc_ctx.dlg_login);
+				RND_DAD_DEFAULT_PTR(irc_ctx.dlg_login, "#pcb-rnd");
+
+			RND_DAD_LABEL(irc_ctx.dlg_login, "server:");
+			RND_DAD_STRING(irc_ctx.dlg_login);
+				RND_DAD_DEFAULT_PTR(irc_ctx.dlg_login, "irc.repo.hu");
+
+		RND_DAD_END(irc_ctx.dlg_login);
+		RND_DAD_BUTTON_CLOSES(irc_ctx.dlg_login, clbtn);
+	RND_DAD_END(irc_ctx.dlg_login);
+
+	/* set up the context */
+	irc_ctx.state = IRC_LOGIN;
+
+	RND_DAD_NEW("irc_login", irc_ctx.dlg_login, "pcb-rnd online-help: IRC", &irc_ctx, rnd_false, irc_login_close_cb);
+	return 0;
+}
+
+
+const char pcb_acts_irc[] = "irc()";
+const char pcb_acth_irc[] = "non-modal, single-instance, single-server, single-channel irc window for online support";
+fgw_error_t pcb_act_irc(fgw_arg_t *res, int argc, fgw_arg_t *argv)
+{
+	RND_ACT_IRES(pcb_dlg_login_irc_login());
+	return 0;
 }
 
 
