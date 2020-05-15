ab8cfda9185c0271fd837c60a04d63f6799c76f5
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-05-15 03:42:57 +0000

    -Move: concat'd net code from work/net to trunk so it can be integrated
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@31363 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src_3rd/libporty_net/concat.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+tmp=net
+
+fixinc()
+{
+	grep -v "^#include \"\|^#warning"
+}
+
+svn checkout svn://repo.hu/libporty/trunk/src/libporty/net $tmp
+
+(echo '#include "os_includes.h"'
+cat $tmp/network.h $tmp/os_dep.h $tmp/tcp4.h $tmp/dns4.h $tmp/uninit_chain.h | fixinc) > libportytcp4.h
+
+(echo '#include "libportytcp4.h"'
+cat $tmp/os_dep.c $tmp/tcp4.c $tmp/dns4.c $tmp/uninit_chain.c | fixinc) > libportytcp4.c
+
+
