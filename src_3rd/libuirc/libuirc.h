09ffc0b57f83d28709b5031d0138699d0a0a2da1
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-05-15 06:06:05 +0000

    [libuirc] -Add: license text
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@31375 1d61de99-560f-4dba-8957-1e505d42358b

--- src_3rd/libuirc/libuirc.h
+++ src_3rd/libuirc/libuirc.h
@@ -1,3 +1,25 @@
+/*
+    libuirc - minimal IRC protocol
+    Copyright (C) 2020  Tibor 'Igor2' Palinkas
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+    Author: libporty (at) igor2.repo.hu
+*/
+
+
 #include <genvector/gds_char.h>
 #include <libporty_net/libportytcp4.h>
 
