5d127ed955c04861703d92d7fd164664f9e7b4e7
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-06-04 09:28:36 +0000

    [librnd] -Add: #define M_E for c89-portability
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@31761 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/core/math_helper.h
+++ src/librnd/core/math_helper.h
@@ -34,6 +34,9 @@
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
+#ifndef M_E
+#define M_E 2.7182818284590452354
+#endif
 #ifndef M_SQRT1_2
 #define M_SQRT1_2 0.707106781 /* 1/sqrt(2) */
 #endif
