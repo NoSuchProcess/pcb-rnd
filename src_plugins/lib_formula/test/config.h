02c49bf92e6363db60ba6a37a4e86e8970938c9a
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-06-04 08:46:34 +0000

    [formula] -Add: minimal cli tester
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@31759 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src_plugins/lib_formula/test/config.h
@@ -0,0 +1,4 @@
+typedef int rnd_coord_t;
+
+#define RND_COORD_TO_MM(x) ((x)/1000000)
+#define RND_MM_TO_COORD(x) ((x)*1000000)
---
+++ src_plugins/lib_formula/test/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "../impedance.c"
+int main()
+{
+	double tt, th, sh, d, res;
+
+	if (scanf("%lf %lf %lf %lf", &tt, &th, &sh, &d) != 4) {
+		fprintf(stderr, "need: trace_width, trace_height, subst_height, dielect\n");
+		return 1;
+	}
+
+	res = pcb_impedance_microstrip(RND_MM_TO_COORD(tt), RND_MM_TO_COORD(th), RND_MM_TO_COORD(sh), d);
+	printf("res=%f\n", res);
+	return 0;
+}
