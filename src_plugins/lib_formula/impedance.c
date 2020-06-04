c93ceb4ebfddfb26b455c5bebbe1590071183e15
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-06-04 08:40:44 +0000

    [formula] -Fix: impedance: calculate in mm for best double performance [formula] -Fix: typo
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@31758 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_formula/impedance.c
+++ src_plugins/lib_formula/impedance.c
@@ -27,18 +27,18 @@
 #include "config.h"
 #include <math.h>
 
-#define TOM(x) (RND_COORD_TO_MM(x) / 1000.0)
+#define TOM(x) RND_COORD_TO_MM(x)
 #define SQR(x) ((x)*(x))
 #define N0 377
 
-double pcb_impedance_microstrip(rnd_coord_t trace_thick, rnd_coord_t trace_height, rnd_coord_t subst_height, double dielect)
+double pcb_impedance_microstrip(rnd_coord_t trace_width, rnd_coord_t trace_height, rnd_coord_t subst_height, double dielect)
 {
-	double w = TOM(trace_thick), t = TOM(trace_height), h = TOM(subst_height), Er = dielect;
+	double w = TOM(trace_width), t = TOM(trace_height), h = TOM(subst_height), Er = dielect;
 	double weff, x1, x2;
 
 	weff = w + (t/M_PI) * log(4*M_E / sqrt(SQR(t/h)+SQR(t/(w*M_PI+1.1*t*M_PI)))) * (Er+1)/(2*Er);
 	x1 = 4 * ((14*Er+8)/(11*Er)) * (h/weff);
-	x2 = sqrt(16 * SQR(h/weff) * SQR((14*Er+8)/(11*Er)) + ((Er+1)/(2*Er))*M_PI);
+	x2 = sqrt(16 * SQR(h/weff) * SQR((14*Er+8)/(11*Er)) + ((Er+1)/(2*Er))*M_PI*M_PI);
 	return N0 / (2*M_PI * sqrt(2) * sqrt(Er+1)) * log(1+4*(h/weff)*(x1+x2));
 }
 
--- src_plugins/lib_formula/impedance.h
+++ src_plugins/lib_formula/impedance.h
@@ -1,2 +1,2 @@
-double pcb_impedance_microstrip(rnd_coord_t trace_thick, rnd_coord_t trace_height, rnd_coord_t subst_height, double dielect);
+double pcb_impedance_microstrip(rnd_coord_t trace_width, rnd_coord_t trace_height, rnd_coord_t subst_height, double dielect);
 
