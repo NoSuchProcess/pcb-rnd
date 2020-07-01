f49c3e252a92b2068609bb38f390f109d6f84fda
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-07-01 02:24:41 +0000

    -Bump: version to 2.2.2
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@32087 1d61de99-560f-4dba-8957-1e505d42358b

--- scconfig/hooks.c
+++ scconfig/hooks.c
@@ -11,7 +11,7 @@
 #include "util/arg_auto_set.h"
 #include "Rev.h"
 
-#define version "2.2.1"
+#define version "2.2.2"
 
 #include "../src_3rd/puplug/scconfig_hooks.h"
 #include "../src_3rd/libfungw/scconfig_hooks.h"
