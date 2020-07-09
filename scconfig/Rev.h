81442b5c3b5102d956def1c134d09723e6618040
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-07-09 08:41:54 +0000

    [scconfig] -Update: lesstif hidlib state change
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@32169 1d61de99-560f-4dba-8957-1e505d42358b

--- scconfig/Rev.h
+++ scconfig/Rev.h
@@ -1,1 +1,1 @@
-static const int myrev = 32120;
+static const int myrev = 32169;
--- scconfig/hooks.c
+++ scconfig/hooks.c
@@ -345,7 +345,7 @@ int hook_generate()
 
 	print_sum_setting("/local/pcb/want_parsgen",   "Regenerating languages with bison & flex");
 	print_sum_setting("/local/pcb/debug",          "Compilation for debugging");
-	print_sum_setting_or("/local/pcb/symbols",        "Include debug symbols", istrue(get("/local/pcb/debug")));
+	print_sum_setting_or("/local/pcb/symbols",     "Include debug symbols", istrue(get("/local/pcb/debug")));
 	print_sum_cfg_val("/local/pcb/coord_bits",     "Coordinate type bits");
 	print_sum_cfg_val("/local/pcb/dot_pcb_rnd",    ".pcb_rnd config dir under $HOME");
 
