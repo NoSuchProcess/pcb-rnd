3dfeb33f1409f06621d29e51deba5e2af3bce1a1
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-07-16 07:37:44 +0000

    [menu] -Add: preferences: print description when a menu row is selected
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@32227 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/dialogs/dlg_pref_menu.c
+++ src_plugins/dialogs/dlg_pref_menu.c
@@ -87,13 +87,40 @@ void pcb_dlg_pref_menu_close(pref_ctx_t *ctx)
 {
 }
 
+static void menu_select(rnd_hid_attribute_t *attrib, void *hid_ctx, rnd_hid_row_t *row)
+{
+	rnd_hid_attr_val_t hv;
+	rnd_hid_tree_t *tree = attrib->wdata;
+	pref_ctx_t *ctx = tree->user_ctx;
+	rnd_menu_patch_t *m;
+
+	if ((row == NULL) || (row->user_data == NULL)) {
+		hv.str = "";
+		rnd_gui->attr_dlg_set_value(ctx->dlg_hid_ctx, ctx->menu.wdesc, &hv);
+		return;
+	}
+
+	m = row->user_data;
+	hv.str = m->desc == NULL ? "" : m->desc;
+	rnd_gui->attr_dlg_set_value(ctx->dlg_hid_ctx, ctx->menu.wdesc, &hv);
+
+
+}
+
 
 void pcb_dlg_pref_menu_create(pref_ctx_t *ctx)
 {
-	static const char *hdr[] = {"uid", "type", "prio", "cookie", "file", NULL};
+	static const char *hdr[] = {"uid", "type", "prio", "cookie", "file name", NULL};
 
 	RND_DAD_COMPFLAG(ctx->dlg, RND_HATF_EXPFILL);
 	RND_DAD_TREE(ctx->dlg, 5, 0, hdr);
 		RND_DAD_COMPFLAG(ctx->dlg, RND_HATF_EXPFILL | RND_HATF_SCROLL);
 		ctx->menu.wlist = RND_DAD_CURRENT(ctx->dlg);
+		RND_DAD_TREE_SET_CB(ctx->dlg, selected_cb, menu_select);
+		RND_DAD_TREE_SET_CB(ctx->dlg, ctx, ctx);
+	RND_DAD_BEGIN_HBOX(ctx->dlg);
+		RND_DAD_LABEL(ctx->dlg, "Description:");
+		RND_DAD_LABEL(ctx->dlg, "");
+		ctx->menu.wdesc = RND_DAD_CURRENT(ctx->dlg);
+	RND_DAD_END(ctx->dlg);
 }
--- src_plugins/dialogs/dlg_pref_menu.h
+++ src_plugins/dialogs/dlg_pref_menu.h
@@ -2,7 +2,7 @@
 #define PCB_DLG_PREF_MENU_H
 
 typedef struct {
-	int wlist;
+	int wlist, wdesc;
 } pref_menu_t;
 
 void pcb_dlg_pref_menu_create(pref_ctx_t *ctx);
