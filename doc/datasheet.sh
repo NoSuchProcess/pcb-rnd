f1bbd7d0a0496590e715663447755c17e9eb5789
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-08-21 16:48:53 +0000

    [doc] -Split: pup file format extractor from the datasheet generator so it can be reused
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@32532 1d61de99-560f-4dba-8957-1e505d42358b

--- doc/datasheet.sh
+++ doc/datasheet.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+awk_on_formats()
+{
 (for n in ../src_plugins/*/*.pup
 do
 	echo "@@@ $n"
 	cat $n
 done
 echo "@@@@@@"
-cat datasheet.html
+cat
 ) | awk '
 
 BEGIN {
@@ -63,15 +65,20 @@ function add(name, dir   ,type)
 	next
 }
 
-(mode == 2) && ($1 == "<!--") && ($2 == "begin") && ($3 == "fmt") { ignore = 1; print $0; next }
-(mode == 2) && ($1 == "<!--") && ($2 == "end") && ($3 == "fmt") {
+(mode != 2) { next }
+'"$1"
+
+}
+
+
+awk_on_formats  '
+($1 == "<!--") && ($2 == "begin") && ($3 == "fmt") { ignore = 1; print $0; next }
+($1 == "<!--") && ($2 == "end") && ($3 == "fmt") {
 	print FMTS[$4, $5]
 	ignore = 0;
 	print $0
 	next
 }
 
-
-(mode == 2) && (!ignore) { print $0 }
-
-' > datasheet2.html && mv datasheet2.html datasheet.html
+(!ignore) { print $0 }
+' < datasheet.html > datasheet2.html && mv datasheet2.html datasheet.html
