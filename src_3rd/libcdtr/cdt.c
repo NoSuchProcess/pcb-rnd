006a37b1df6f879414f967d1ac903f15cd23c9f6
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-09-13 07:41:48 +0000

    [libcdtr] -Add: license banner: LGPL2 with permission from Wojciech via email
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@32653 1d61de99-560f-4dba-8957-1e505d42358b

--- src_3rd/libcdtr/cdt.c
+++ src_3rd/libcdtr/cdt.c
@@ -1,11 +1,27 @@
 /*
- *  Copyright (C) 2018 Wojciech Krutnik
- *
- *  Constrained Delaunay Triangulation using incremental algorithm, as described in:
- *    Yizhi Lu and Wayne Wei-Ming Dai, "A numerical stable algorithm for constructing
- *    constrained Delaunay triangulation and application to multichip module layout,"
- *    China., 1991 International Conference on Circuits and Systems, Shenzhen, 1991,
- *    pp. 644-647 vol.2.
+    libcdtr - Constrained Delaunay Triangulation using incremental algorithm
+
+    Copyright (C) 2018 Wojciech Krutnik
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+    Constrained Delaunay Triangulation using incremental algorithm, as described in:
+      Yizhi Lu and Wayne Wei-Ming Dai, "A numerical stable algorithm for constructing
+      constrained Delaunay triangulation and application to multichip module layout,"
+      China., 1991 International Conference on Circuits and Systems, Shenzhen, 1991,
+      pp. 644-647 vol.2.
  */
 
 #include <assert.h>
