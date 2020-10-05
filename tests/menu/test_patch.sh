70ed0430e3705f60a3bfbae44a636b3d1e9848d2
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-10-05 06:13:50 +0000

    [tests] -Fix: menu test shouldn't depend on installed pcb-rnd
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@32983 1d61de99-560f-4dba-8957-1e505d42358b

--- tests/menu/test_patch.sh
+++ tests/menu/test_patch.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+SRC=../../src
+CFG="-c rc/library_search_paths=. -c rc/quiet=1"
+
 #	MenuPatch(load, test, "insert.lht");
 
+cp $SRC/default_font $SRC/default4.lht .
 rm d0.lht d1.lht >/dev/null 2>&1
 
 echo '
@@ -9,8 +13,8 @@ echo '
 	MenuDebug(save, "d0.lht");
 '$1'
 	MenuDebug(save, "d1.lht");
-' | pcb-rnd -c rc/menu_file=base --gui batch 2>&1 | grep -v "^menu debug"
+' | $SRC/pcb-rnd $CFG -c rc/menu_file=base --gui batch 2>&1 | grep -v "^menu debug"
 
 diff -U1 d0.lht d1.lht | grep "^[ +-][^+-]"
 
-rm d0.lht d1.lht >/dev/null 2>&1
+rm d0.lht d1.lht default_font default4.lht >/dev/null 2>&1
