8ee0c6b08c716a1170a48027f47abddad0c43660
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-10-05 12:23:03 +0000

    [query] -Add: coord() function that attempts to convert a string to coordinates (useful for reading coords from attributes)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@32986 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/query/fnc.c
+++ src_plugins/query/fnc.c
@@ -31,6 +31,7 @@
 #include <librnd/core/actions.h>
 #include <librnd/core/conf.h>
 #include <librnd/core/compat_misc.h>
+#include <librnd/core/misc_util.h>
 #include <libfungw/fungw_conv.h>
 
 #include "board.h"
@@ -74,6 +75,20 @@ static int fnc_abs(pcb_qry_exec_t *ectx, int argc, pcb_qry_val_t *argv, pcb_qry_
 	PCB_QRY_RET_INV(res);
 }
 
+static int fnc_coord(pcb_qry_exec_t *ectx, int argc, pcb_qry_val_t *argv, pcb_qry_val_t *res)
+{
+	if (argc == 1) {
+		switch(argv[0].type) {
+			case PCBQ_VT_COORD: PCB_QRY_RET_COORD(res, argv[0].data.crd);
+			case PCBQ_VT_LONG:  PCB_QRY_RET_COORD(res, argv[0].data.lng);
+			case PCBQ_VT_DOUBLE: PCB_QRY_RET_COORD(res, argv[0].data.dbl);
+			case PCBQ_VT_STRING: PCB_QRY_RET_COORD(res, (rnd_coord_t)rnd_get_value(argv[0].data.str, NULL, NULL, NULL));
+			default: break;
+		}
+	}
+	PCB_QRY_RET_INV(res);
+}
+
 #include "fnc_glue.c"
 #include "fnc_list.c"
 #include "fnc_geo.c"
@@ -83,6 +98,7 @@ void pcb_qry_basic_fnc_init(void)
 {
 	pcb_qry_fnc_reg("llen", fnc_llen);
 	pcb_qry_fnc_reg("abs", fnc_abs);
+	pcb_qry_fnc_reg("coord", fnc_coord);
 	pcb_qry_fnc_reg("distance", fnc_distance);
 	pcb_qry_fnc_reg("mklist", fnc_mklist);
 	pcb_qry_fnc_reg("violation", fnc_violation);
