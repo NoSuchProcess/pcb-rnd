ebc310a6fb704b95fa30f6b324935919f676a27e
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-11-11 03:43:45 +0000

    [tests] -Fix: menu tests: run pcb-rnd from src/, with plugins it won't work from tests/menu
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@33239 1d61de99-560f-4dba-8957-1e505d42358b

--- tests/menu/test_patch.sh
+++ tests/menu/test_patch.sh
@@ -1,20 +1,30 @@
 #!/bin/sh
 
 SRC=../../src
-CFG="-c rc/library_search_paths=. -c rc/quiet=1"
+CFG="-c rc/library_search_paths=dummy_lib -c rc/quiet=1"
 
-#	MenuPatch(load, test, "insert.lht");
+# call:
+# insert.lht 'MenuPatch(load, test, "insert.lht");'
 
-cp $SRC/default_font $SRC/default4.lht .
 rm d0.lht d1.lht >/dev/null 2>&1
 
+cp pcb-menu-base.lht $1 $SRC
+(
+cd $SRC
+mkdir dummy_lib
 echo '
 	MenuDebug(force-enable);
 	MenuDebug(save, "d0.lht");
-'$1'
+'$2'
 	MenuDebug(save, "d1.lht");
-' | $SRC/pcb-rnd $CFG -c rc/menu_file=base --gui batch 2>&1 | grep -v "^menu debug"
+' | ./pcb-rnd $CFG -c rc/menu_file=base --gui batch 2>&1 | grep -v "^menu debug"
+rmdir dummy_lib
+)
+
+mv $SRC/d0.lht .
+mv $SRC/d1.lht .
+rm $SRC/pcb-menu-base.lht $SRC/$1
 
 diff -U1 d0.lht d1.lht | grep "^[ +-][^+-]"
 
-rm d0.lht d1.lht default_font default4.lht >/dev/null 2>&1
+rm d0.lht d1.lht >/dev/null 2>&1
