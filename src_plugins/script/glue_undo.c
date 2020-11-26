349e570c6193fcf05f35d3f01b7491a9eff1751d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-11-26 06:36:25 +0000

    [script] -Add: use the action API for arranging undo with the host app instead of hardwiring pcb-rnd undo calls
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@33388 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/script/glue_undo.c
+++ src_plugins/script/glue_undo.c
@@ -1,25 +1,44 @@
-#include "undo.h"
-#include "globalconst.h"
-
 /* Live scripting needs to undo using the host app; to make this host app
    API independent, the action interface should be used */
 
+static long script_undo_action(rnd_hidlib_t *hl, const char *cmd)
+{
+	fgw_arg_t res, argv[2];
+
+	argv[1].type = FGW_STR;
+	argv[1].val.cstr = cmd;
+	if (rnd_actionv_bin(hl, "Undo", &res, 2, argv) != 0)
+		return -1;
+
+	if (fgw_arg_conv(&rnd_fgw, &res, FGW_LONG) != 0)
+		return -1;
+
+	return res.val.nat_long;
+}
+
 static long get_undo_serial(rnd_hidlib_t *hl)
 {
-	return pcb_undo_serial();
+	return script_undo_action(hl, "GetSerial");
 }
 
 static long get_num_undo(rnd_hidlib_t *hl)
 {
-	return pcb_num_undo();
+	return script_undo_action(hl, "GetNum");
 }
 
 static void inc_undo_serial(rnd_hidlib_t *hl)
 {
-	pcb_undo_inc_serial();
+	script_undo_action(hl, "IncSerial");
 }
 
 static void undo_above(rnd_hidlib_t *hl, long ser)
 {
-	pcb_undo_above(ser);
+	fgw_arg_t res, argv[3];
+
+	argv[1].type = FGW_STR;
+	argv[1].val.cstr = "Above";
+	argv[2].type = FGW_LONG;
+	argv[2].val.nat_long = ser;
+	rnd_actionv_bin(hl, "Undo", &res, 3, argv);
+	fgw_arg_free(&rnd_fgw, &res);
 }
