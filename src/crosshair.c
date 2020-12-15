edd036adde82152a416feb77388acff85215eefd
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-12-15 07:50:34 +0000

    -Fix: typo
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@33518 1d61de99-560f-4dba-8957-1e505d42358b

--- src/crosshair.c
+++ src/crosshair.c
@@ -1048,7 +1048,7 @@ void pcb_crosshair_grid_fit(pcb_board_t *pcb, rnd_coord_t X, rnd_coord_t Y)
 	enfmode  = (rnd_conf.editor.mode == pcb_crosshair.tool_line) && pcb_crosshair.AttachedLine.State != PCB_CH_STATE_FIRST;
 	if (pcb_brave & PCB_BRAVE_ENFORCE_CLR_MOVE)
 		enfmode |= (rnd_conf.editor.mode == pcb_crosshair.tool_move);
-	if (enfmode &&conf_core.editor.auto_drc)
+	if (enfmode && conf_core.editor.auto_drc)
 		pcb_line_enforce_drc(pcb);
 
 	rnd_gui->set_crosshair(rnd_gui, pcb_crosshair.X, pcb_crosshair.Y, HID_SC_DO_NOTHING);
