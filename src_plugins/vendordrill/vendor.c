4e442dbf911c06d219c6a482e5d545383c2e85a9
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-02-04 03:59:46 +0000

    [vendordrill] -Add: errors and warnings for the obsolete drc subtree
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@34057 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/vendordrill/vendor.c
+++ src_plugins/vendordrill/vendor.c
@@ -201,7 +201,7 @@ fgw_error_t pcb_act_LoadVendorFrom(fgw_arg_t *res, int argc, fgw_arg_t *argv)
 	const char *fname = NULL, *spure = NULL;
 	static char *default_file = NULL;
 	const char *sval;
-	int pure = 0;
+	int pure = 0, warn_drc = 0;
 	lht_doc_t *doc;
 	lht_node_t *drlres;
 	rnd_bool free_fname = rnd_false;
@@ -314,40 +314,46 @@ fgw_error_t pcb_act_LoadVendorFrom(fgw_arg_t *res, int argc, fgw_arg_t *argv)
 	if (sval != NULL) {
 		load_meta_coord("design/bloat", floor(sf * atof(sval) + 0.5));
 		rnd_message(RND_MSG_INFO, "Set DRC minimum copper spacing to %ml mils\n", conf_core.design.bloat);
+		warn_drc = 1;
 	}
 
 	sval = rnd_hid_cfg_text_value(doc, "/drc/copper_overlap");
 	if (sval != NULL) {
 		load_meta_coord("design/shrink", floor(sf * atof(sval) + 0.5));
-		rnd_message(RND_MSG_INFO, "Set DRC minimum copper overlap to %ml mils\n", conf_core.design.shrink);
+		rnd_message(RND_MSG_ERROR, "drc/copper_overlap is not supported anymore, please use the new DRC instead\n");
 	}
 
 	sval = rnd_hid_cfg_text_value(doc, "/drc/copper_width");
 	if (sval != NULL) {
 		load_meta_coord("design/min_wid", floor(sf * atof(sval) + 0.5));
 		rnd_message(RND_MSG_INFO, "Set DRC minimum copper spacing to %ml mils\n", conf_core.design.min_wid);
+		warn_drc = 1;
 	}
 
 	sval = rnd_hid_cfg_text_value(doc, "/drc/silk_width");
 	if (sval != NULL) {
 		load_meta_coord("design/min_slk", floor(sf * atof(sval) + 0.5));
 		rnd_message(RND_MSG_INFO, "Set DRC minimum silk width to %ml mils\n", conf_core.design.min_slk);
+		warn_drc = 1;
 	}
 
 	sval = rnd_hid_cfg_text_value(doc, "/drc/min_drill");
 	if (sval != NULL) {
 		load_meta_coord("design/min_drill", floor(sf * atof(sval) + 0.5));
-		rnd_message(RND_MSG_INFO, "Set DRC minimum drill diameter to %ml mils\n", conf_core.design.min_drill);
+		rnd_message(RND_MSG_ERROR, "drc/min_drill is not supported anymore, please use the new DRC instead\n");
 	}
 
 	sval = rnd_hid_cfg_text_value(doc, "/drc/min_ring");
 	if (sval != NULL) {
 		load_meta_coord("design/min_ring", floor(sf * atof(sval) + 0.5));
-		rnd_message(RND_MSG_INFO, "Set DRC minimum annular ring to %ml mils\n", conf_core.design.min_ring);
+		rnd_message(RND_MSG_ERROR, "drc/min_ring is not supported anymore, please use the new DRC instead\n");
 	}
 
 	rnd_message(RND_MSG_INFO, "Loaded %d vendor drills from %s\n", n_vendor_drills, fname);
 	rnd_message(RND_MSG_INFO, "Loaded %ld skips for %d different attributes\n", num_skips, skips.used);
+
+	if (warn_drc)
+		rnd_message(RND_MSG_WARNING, "Vendordrill: %s contains a drc subtree. Please use the new DRC instead. This feature is going to be removed.\n", fname);
 
 	rnd_conf_set(RND_CFR_DESIGN, "plugins/vendor/enable", -1, "0", RND_POL_OVERWRITE);
 
