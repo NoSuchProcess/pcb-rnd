b89ae3ae52caa9ce18f76e7da3fdefbe0ad3383e
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-02-04 04:40:52 +0000

    [tests] -Add: automated tests for vendordrill skips
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@34059 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ tests/vendordrill/test_all.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+ROOT="../.."
+SRC="$ROOT/src"
+PCBRND="./pcb-rnd"
+GLOBARGS="-c rc/library_search_paths=../tests/RTT/lib -c rc/quiet=1"
+
+
+run_pcb_rnd() {
+local brd="`pwd`/$1"
+
+shift 1
+
+(
+echo '
+message(*** PRE  ***)
+Report(DrillReport)
+'
+
+while test $# -gt 0
+do
+	echo "LoadVendorFrom(\"`pwd`/$1\", pure)"
+	shift 1
+done
+
+echo '
+ApplyVendor()
+
+message(*** POST ***)
+Report(DrillReport)
+') | (
+	cd $SRC
+	pcb-rnd $GLOBARGS --gui batch $brd
+)
+}
+
+run_test()
+{
+	local tname="$1"
+	shift 1
+
+	run_pcb_rnd "$@" > $tname.out
+
+	diff -u $tname.ref $tname.out
+	if test "$?" = "0"
+	then
+		rm $tname.out
+		return 0
+	fi
+	return 1
+}
+
+run_test skip_attr    testbrd.lht skip_attr.lht
+run_test skip_descr   testbrd.lht skip_descr.lht
+run_test skip_refdes  testbrd.lht skip_refdes.lht
+run_test skip_value   testbrd.lht skip_value.lht
+
