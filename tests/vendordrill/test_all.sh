ac09d5d25e1dd575e01ddde4d7b5da4fcc9640c3
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-02-04 04:46:48 +0000

    [tests] -Add: vendordrill tests for all different rounding methods and pure loading
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@34060 1d61de99-560f-4dba-8957-1e505d42358b

--- tests/vendordrill/test_all.sh
+++ tests/vendordrill/test_all.sh
@@ -55,3 +55,10 @@ run_test skip_descr   testbrd.lht skip_descr.lht
 run_test skip_refdes  testbrd.lht skip_refdes.lht
 run_test skip_value   testbrd.lht skip_value.lht
 
+run_test pure         testbrd.lht pure1.lht pure2.lht
+
+run_test round        testbrd.lht round.lht
+run_test round_up     testbrd.lht round_up.lht
+run_test round_down   testbrd.lht round_down.lht
+run_test round_near   testbrd.lht round_near.lht
+
