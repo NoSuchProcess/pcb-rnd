0ae74ebddfefc7b17912cef6f825d25c8390570a
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-04-08 09:58:42 +0000

    [core] -Cleanup: remove PCB_MIN_GRID (hardwired size limit not in use)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@34328 1d61de99-560f-4dba-8957-1e505d42358b

--- src/globalconst.h
+++ src/globalconst.h
@@ -41,7 +41,6 @@
 #define PCB_MIN_TEXTSCALE    1 /* scaling of text objects in percent (must be an integer greater than 0) */
 #define PCB_MAX_TEXTSCALE    10000
 #define PCB_MIN_PINORVIACOPPER RND_MIL_TO_COORD(4)	/* min difference outer-inner diameter */
-#define PCB_MIN_GRID         1
 #define PCB_MAX_FONTPOSITION 255 /* upper limit of characters in my font */
 
 #define PCB_MAX_BUFFER       5 /* number of pastebuffers additional changes in menu.c are also required to select more buffers */
