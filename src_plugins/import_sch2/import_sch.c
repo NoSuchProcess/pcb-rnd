0f00478b2d745b808eb3d4c02ab1d05a3ded573b
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-04-09 03:30:59 +0000

    [import_sch2] -Add: code comments to the stages of reimport
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@34341 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/import_sch2/import_sch.c
+++ src_plugins/import_sch2/import_sch.c
@@ -152,14 +152,18 @@ static int do_import(void)
 	if (!nonempty)
 		return do_dialog();
 
+	/* build the argument list in a[] */
 	a = malloc((len+1) * sizeof(char *));
 	for(n = 0, ci = rnd_conflist_first((rnd_conflist_t *)&conf_import_sch.plugins.import_sch.args); ci != NULL; ci = rnd_conflist_next(ci), n++)
 		a[n] = ci->val.string[0];
+
+	/* perform a reimport */
 	rnd_message(RND_MSG_DEBUG, "import_sch2: reimport with %s -> %p\n", imp_name, p);
 	pcb_undo_freeze_serial();
 	res = p->import(p, IMPORT_ASPECT_NETLIST, a, len);
 	pcb_undo_unfreeze_serial();
 	pcb_undo_inc_serial();
+
 	free(a);
 	return res;
 }
