c0efefeb8e1c893cf607335c4697c0466798c1a1
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-06-28 12:03:50 +0000

    [lib_netmap] -Add: document the API and purpose of placement.[ch]
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@35036 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_netmap/placement.h
+++ src_plugins/lib_netmap/placement.h
@@ -2,12 +2,17 @@
 #include "data.h"
 #include "ht_subc.h"
 
+/* Build a hash of subcircuit-to-prototype. Effectively a local library
+   of unique footprint prototypes */
+
 typedef struct {
-	htscp_t subcs;
+	htscp_t subcs;   /* value is a normalized subc prototype */
 	pcb_board_t *pcb;
 	pcb_data_t data; /* temp buffer to place prototype subcs in */
 } pcb_placement_t;
 
 void pcb_placement_init(pcb_placement_t *ctx, pcb_board_t *pcb);
 void pcb_placement_uninit(pcb_placement_t *ctx);
+
+/* Iterate all subcircuits in data and build ctx->subcs hash */
 void pcb_placement_build(pcb_placement_t *ctx, pcb_data_t *data);
