a5c4b9225ac2fcb3fea75f0c01368e5886c555cd
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-07-22 10:10:46 +0000

    [io_altium] -Fix: sphash prefix [io_altium] -Fix: multiple definitions of the main pcb parser function
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@35290 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_altium/pcbdoc.h
+++ src_plugins/io_altium/pcbdoc.h
@@ -1,5 +1,2 @@
-int io_altium_parse_pcbdoc_ascii(rnd_hidlib_t *hidlib, altium_tree_t *tree, const char *fn)
-{
-
-}
+int io_altium_parse_pcbdoc_ascii(rnd_hidlib_t *hidlib, altium_tree_t *tree, const char *fn);
 
