003830086a25b0db34a9668d79e0e76e16f9b1f8
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-07-22 10:14:54 +0000

    [io_altium] -Cleanup: parser test doesn't depend on static inline (C99)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@35292 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_altium/parser_test/main.c
+++ src_plugins/io_altium/parser_test/main.c
@@ -7,6 +7,17 @@ FILE *rnd_fopen(rnd_hidlib_t *hidlib, const char *fn, const char *mode)
 	return fopen(fn, mode);
 }
 
+long rnd_file_size(rnd_hidlib_t *hidlib, const char *path)
+{
+	struct stat st;
+	if (stat(path, &st) != 0)
+		return -1;
+	if (st.st_size > LONG_MAX)
+		return -1;
+	return st.st_size;
+}
+
+
 static void dump_tree(altium_tree_t *tree)
 {
 	altium_record_t *rec;
--- src_plugins/io_altium/parser_test/plug_io.h
+++ src_plugins/io_altium/parser_test/plug_io.h
@@ -17,16 +17,7 @@ typedef enum {
 	pcb_plug_iot_dummy
 } pcb_plug_iot_t;
 
-static inline long rnd_file_size(rnd_hidlib_t *hidlib, const char *path)
-{
-	struct stat st;
-	if (stat(path, &st) != 0)
-		return -1;
-	if (st.st_size > LONG_MAX)
-		return -1;
-	return st.st_size;
-}
-
+long rnd_file_size(rnd_hidlib_t *hidlib, const char *path);
 FILE *rnd_fopen(rnd_hidlib_t *hidlib, const char *fn, const char *mode);
 
 #endif
