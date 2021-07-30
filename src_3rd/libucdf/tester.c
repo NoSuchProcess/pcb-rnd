79988b8444c62deacee5e6b78e17bf48adb8c1b3
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-07-30 09:29:57 +0000

    [libucdf] -Add: preapre for test-printing a long file
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@35427 1d61de99-560f-4dba-8957-1e505d42358b

--- src_3rd/libucdf/tester.c
+++ src_3rd/libucdf/tester.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "ucdf.h"
 
 static void print_dir(ucdf_ctx_t *ctx, ucdf_direntry_t *dir, int level)
@@ -9,6 +10,27 @@ static void print_dir(ucdf_ctx_t *ctx, ucdf_direntry_t *dir, int level)
 	printf("%s [%d] %c%ld @%ld\n", dir->name, dir->type, (dir->is_short ? 'S' : 'L'), dir->size, dir->first);
 	for(d = dir->children; d != NULL; d = d->next)
 		print_dir(ctx, d, level+1);
+}
+
+/* return the direntry for /name/Data */
+static ucdf_direntry_t *de_find(ucdf_ctx_t *ctx, const char *name)
+{
+	ucdf_direntry_t *d, *n;
+	for(d = ctx->root->children; d != NULL; d = d->next) {
+		if (strcmp(d->name, name) == 0) {
+			for(n = d->children; n != NULL; n = n->next)
+				if (strcmp(n->name, "Data") == 0)
+					return n;
+			return NULL;
+		}
+	}
+
+	return NULL;
+}
+
+static void dump_file(ucdf_ctx_t *ctx, ucdf_direntry_t *d)
+{
+	
 }
 
 int main(int argc, char *argv[])
@@ -29,6 +51,14 @@ int main(int argc, char *argv[])
 	printf("  sat_len: %ld; dir1=%ld ssat=%ld+%ld msat=%ld+%ld\n", ctx.sat_len, ctx.dir_first, ctx.ssat_first, ctx.ssat_len, ctx.msat_first, ctx.msat_len);
 
 	print_dir(&ctx, ctx.root, 0);
+
+	printf("--NETS:\n");
+	{
+		ucdf_direntry_t *de = de_find(&ctx, "Nets6");
+		print_dir(&ctx, de, 0);
+		printf("==\n");
+		dump_file(&ctx, de);
+	}
 
 	ucdf_close(&ctx);
 	return 0;
