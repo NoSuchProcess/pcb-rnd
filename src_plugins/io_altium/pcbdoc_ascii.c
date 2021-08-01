9429b32b262303cd677f74dcfaa276e6ec5a1d0c
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-08-01 05:40:33 +0000

    [io_altium] -Cleanup: unify file short description format
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@35498 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_altium/pcbdoc_ascii.c
+++ src_plugins/io_altium/pcbdoc_ascii.c
@@ -3,7 +3,7 @@
  *
  *  pcb-rnd, interactive printed circuit board design
  *
- *  altium pcbdoc ASCII plugin - low level read: parse into a tree in mem
+ *  altium plugin - pcbdoc low level read: parse ASCII into a tree in mem
  *  pcb-rnd Copyright (C) 2021 Tibor 'Igor2' Palinkas
  *
  *  This program is free software; you can redistribute it and/or modify
