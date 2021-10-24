6caaf6183c7b29ce5e954c579b9be322af059f3f
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-10-24 08:35:07 +0000

    [tests] -Fix: also ignore empty lines printed as part of error messages for the menu test
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@35778 1d61de99-560f-4dba-8957-1e505d42358b

--- tests/pupfilter.sh
+++ tests/pupfilter.sh
@@ -3,8 +3,16 @@
 # ignore plugin loading errors: this way we do not need to install all
 # librnd deps
 
-exec awk '
+exec awk -v "how=$1" '
+	BEGIN {
+		ws = "ws" ~ how;
+	}
 	/^E: Some of the dynamic/ { next }
 	/^E: puplug:/ { next}
+	/^pcb-rnd conf ERROR: conf node .*not unregistered/ { next }
+	/^ERROR: hid_actions_uninit: action.*left registered/ { next }
+	/^pcb_plug_fp_chain is not empty;/  { next }
+	/^... Log produced during uninitialization:/ { next }
+	(ws && (/^[ \t]*$/)) { next }
 	{ print $0 }
 '
