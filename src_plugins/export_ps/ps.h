b039caace4cbd231c08de611cdc83799249188e1
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-04-22 11:30:05 +0000

    [export_ps] -Cleanup: sort out what's really needed in ps.h and why
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@36234 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/export_ps/ps.h
+++ src_plugins/export_ps/ps.h
@@ -1,11 +1,14 @@
-extern const char *ps_cookie;
+/* required by lpr */
 extern rnd_hid_t ps_hid;
 extern void ps_hid_export_to_file(FILE *, rnd_hid_attr_val_t *, rnd_xform_t *);
-extern void hid_eps_init();
-extern void hid_eps_uninit();
 void ps_ps_init(rnd_hid_t * hid);
 
+/* required for ps<->eps cross call */
+extern void hid_eps_init();
+extern void hid_eps_uninit();
+extern const char *ps_cookie;
 
+/* will be the low level */
 typedef struct rnd_ps_s {
 	FILE *outf;
 } rnd_ps_t;
