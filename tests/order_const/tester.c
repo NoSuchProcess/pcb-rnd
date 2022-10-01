0c43239497aa0473cf0ccf1fecafbf5698d802b6
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-10-01 12:22:30 +0000

    [tests] -Add: order_const: load and parse a script
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@36860 1d61de99-560f-4dba-8957-1e505d42358b

--- tests/order_const/tester.c
+++ tests/order_const/tester.c
@@ -1,5 +1,40 @@
+#include <stdio.h>
 #include <src_plugins/order/constraint.h>
+
+int verbose = 0;
+extern int pcb_ordc_parse_verbose;
+
 int main(int argc, char *argv[])
 {
+	pcb_ordc_ctx_t ctx;
+	char script[8192], *fn = argv[1];
+	FILE *f;
+	int r;
 
+	if (strcmp(argv[1], "-v") == 0) {
+		verbose = pcb_ordc_parse_verbose = 1;
+		fn = argv[2];
+	}
+
+	if (fn == NULL) {
+		fprintf(stderr, "Error: need script file name\n");
+		return 1;
+	}
+
+	f = fopen(fn, "r");
+	if (f == NULL) {
+		fprintf(stderr, "Error: failed to open the script file (%s)\n", fn);
+		return 1;
+	}
+	fread(script, 1, sizeof(script), f);
+	fclose(f);
+
+	r = pcb_ordc_parse_str(&ctx, script);
+	if (r != 0) {
+		fprintf(stderr, "Error: failed to parse the script (exiting)\n");
+		return 1;
+	}
+
+
+	return 0;
 }
