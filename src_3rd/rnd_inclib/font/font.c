2f82c62e533cc64233ed33ee0a92782a64c0e20f
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-11-18 09:24:01 +0000

    -Add: license banner in reimplemented font engine
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@36983 1d61de99-560f-4dba-8957-1e505d42358b

--- src_3rd/rnd_inclib/font/font.c
+++ src_3rd/rnd_inclib/font/font.c
@@ -1,3 +1,30 @@
+/*
+ *                            COPYRIGHT
+ *
+ *  librnd, modular 2D CAD framework - vector font rendering
+ *  librnd Copyright (C) 2022 Tibor 'Igor2' Palinkas
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  Contact:
+ *    Project page: http://repo.hu/projects/librnd
+ *    lead developer: http://repo.hu/projects/librnd/contact.html
+ *    mailing list: pcb-rnd (at) list.repo.hu (send "subscribe")
+ *
+ */
+
 #include <ctype.h>
 #include <librnd/core/config.h>
 #include <librnd/core/compat_misc.h>
