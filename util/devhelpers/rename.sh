d8026aeeb1936978dca68c661309f48759c784e2
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-11-21 13:37:49 +0000

    [util] -Del: rename scripts - use the ones in librnd
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@37023 1d61de99-560f-4dba-8957-1e505d42358b

--- util/devhelpers/rename.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-find . -name '*.[chly]' -exec sed -i "$@" {} \;
-find . -name 'config.h.in' -exec sed -i "$@" {} \;
-
--- util/devhelpers/renamef.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Rename a function and log
-
-./rename.sh "s/\\([^a-zA-Z0-9_>.]\\)${1}[ \t]*[(]/\\1$2(/g"
-./rename.sh "s/^${1}[ \t]*[(]/$2(/g"
-
-#echo "#define $1 $2" >> src/librnd/pcb_compat.h
