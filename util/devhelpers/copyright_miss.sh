c1e66b3bf793a0c0897f4fe84297b59e98696545
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-11-21 13:34:43 +0000

    [util] -Del: include, comment and copyright source crawler scripts - use the one in librnd
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@37022 1d61de99-560f-4dba-8957-1e505d42358b

--- util/devhelpers/copyright_miss.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# This file is placed in the Public Domain.
-
-# Print the path of source files that may miss the copyright banner among
-# with their length in line
-
-find . -name '*.[chly]' -exec sh -c 'grep "^ [*]  Copyright [(]C[)]" {} >/dev/null || wc -l "{}"' \;
