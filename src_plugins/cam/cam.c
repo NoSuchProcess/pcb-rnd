2dbac6cdbc5bfd9b67f68026cd076f47b0235d3d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-11-30 15:02:25 +0000

    [librnd4] -Fix: set cam context ->hidlib before doing the export from the command line, as the low level export code doesn't get it from PCB anymore
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@37069 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/cam/cam.c
+++ src_plugins/cam/cam.c
@@ -266,6 +266,8 @@ static int export_cam_parse_arguments(rnd_hid_t *hid, int *argc, char ***argv)
 
 static void export_cam_do_export(rnd_hid_t *hid, rnd_design_t *design, rnd_hid_attr_val_t *options, void *appspec)
 {
+	cam_export_ctx.hidlib = design;
+
 	if (!cam_export_has_outfile)
 		cam_init_inst_fn(&cam_export_ctx);
 
