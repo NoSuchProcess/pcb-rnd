ccff0967acc041acdf66e1d591e983587838999f
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-12-04 14:33:34 +0000

    [librnd4] -Update: follow API change on RND_EVENT_DESIGN_META_CHANGED_LIB
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@37086 1d61de99-560f-4dba-8957-1e505d42358b

--- src/board.c
+++ src/board.c
@@ -470,14 +470,14 @@ rnd_coord_t pcb_board_thickness(pcb_board_t *pcb, const char *namespace, pcb_boa
 	return pcb_stack_thickness(pcb, namespace, flags, 0, rnd_true, pcb->LayerGroups.len-1, rnd_true, PCB_LYT_COPPER | PCB_LYT_SUBSTRATE);
 }
 
-void pcb_board_changed_lib_ev(rnd_design_t *hidlib, void *user_data, int argc, rnd_event_arg_t argv[])
+void pcb_board_changed_meta_ev(rnd_design_t *hidlib, void *user_data, int argc, rnd_event_arg_t argv[])
 {
 	pcb_board_replaced();
 }
 
 void pcb_board_init(void)
 {
-	rnd_event_bind(RND_EVENT_DESIGN_META_CHANGED_LIB, pcb_board_changed_lib_ev, NULL, core_board_cookie);
+	rnd_event_bind(RND_EVENT_DESIGN_META_CHANGED, pcb_board_changed_meta_ev, NULL, core_board_cookie);
 }
 
 void pcb_board_uninit(void)
