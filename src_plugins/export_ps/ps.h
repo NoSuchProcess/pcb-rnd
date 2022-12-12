e966230a942e04e8c30822b21913bb41dc361f19
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-12-12 04:27:34 +0000

    [librnd4] -Update: API change on export_ps (getting appspec from lpr)
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@37099 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/export_ps/ps.h
+++ src_plugins/export_ps/ps.h
@@ -1,6 +1,6 @@
 /* required by lpr */
 extern rnd_hid_t ps_hid;
-extern void ps_hid_export_to_file(rnd_design_t *dsg, FILE *, rnd_hid_attr_val_t *, rnd_xform_t *);
+extern void ps_hid_export_to_file(rnd_design_t *dsg, FILE *, rnd_hid_attr_val_t *, rnd_xform_t *, void *);
 void ps_ps_init(rnd_hid_t * hid);
 
 /* required for ps<->eps cross call */
