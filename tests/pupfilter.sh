2198ed45e4c9b1595774187848b111752224661d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2023-02-15 06:21:38 +0000

    [test] -Fix: filter out pup soft uninit warning to reduce noise
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@37269 1d61de99-560f-4dba-8957-1e505d42358b

--- tests/pupfilter.sh
+++ tests/pupfilter.sh
@@ -13,6 +13,7 @@ exec awk -v "how=$1" '
 	/^ERROR: hid_actions_uninit: action.*left registered/ { next }
 	/^pcb_plug_fp_chain is not empty;/  { next }
 	/^... Log produced during uninitialization:/ { next }
+	/some plugins are not soft-unloaded/ { next }
 	(ws && (/^[ \t]*$/)) { next }
 	{ print $0 }
 '
