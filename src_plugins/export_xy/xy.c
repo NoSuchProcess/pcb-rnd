01b1f85cfad2d8019435f52b889ea33be8fed8e4
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2023-05-10 11:37:58 +0000

    [export_xy] -Fix: get the format enum initialized when called from the export() action
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@37510 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/export_xy/xy.c
+++ src_plugins/export_xy/xy.c
@@ -989,6 +989,11 @@ static int xy_usage(rnd_hid_t *hid, const char *topic)
 static int xy_parse_arguments(rnd_hid_t *hid, int *argc, char ***argv)
 {
 	rnd_export_register_opts2(hid, xy_options, sizeof(xy_options) / sizeof(xy_options[0]), xy_cookie, 0);
+
+	/* when called from the export() command this field may be uninitialized yet */
+	if (xy_options[HA_format].enumerations == NULL)
+		xy_get_export_options(hid, NULL, NULL, NULL);
+
 	return rnd_hid_parse_command_line(argc, argv);
 }
 
