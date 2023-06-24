0e44d967f0590fffdc58abd21d73d85aa6c98b87
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2023-06-24 07:35:17 +0000

    [io_eagle] -Cleanup: replace compat rtree entry insert with native
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@37969 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_eagle/read.c
+++ src_plugins/io_eagle/read.c
@@ -1384,7 +1384,7 @@ TODO("subc: revise this: are we loading an instance here? do we need to place it
 	if (st->pcb != NULL) {
 		if (st->pcb->Data->subc_tree == NULL)
 			rnd_rtree_init(st->pcb->Data->subc_tree = malloc(sizeof(rnd_rtree_t)));
-		rnd_r_insert_entry(st->pcb->Data->subc_tree, (rnd_box_t *)subc);
+		rnd_rtree_insert(st->pcb->Data->subc_tree, subc, (rnd_rtree_box_t *)subc);
 		pcb_subc_rebind(st->pcb, subc);
 	}
 
