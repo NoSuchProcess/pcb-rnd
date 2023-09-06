bf2b6dbe66a1450956d7043cc977ae5235b80e25
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2023-09-06 05:26:56 +0000

    [core] -Fix: rats patch fwd annot verify: if a terminal is not on a net the call may return -1 (e.g. the net is missing) - accept thta as connection broken
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@38257 1d61de99-560f-4dba-8957-1e505d42358b

--- src/rats_patch.c
+++ src/rats_patch.c
@@ -415,7 +415,7 @@ int pcb_rats_patch_already_done(pcb_board_t *pcb, pcb_ratspatch_line_t *n)
 			}
 			break;
 		case RATP_DEL_CONN:
-			if (pcb_net_is_term_on_net_by_name(pcb, &pcb->netlist[PCB_NETLIST_INPUT], n->arg1.net_name, n->id) == 0) {
+			if (pcb_net_is_term_on_net_by_name(pcb, &pcb->netlist[PCB_NETLIST_INPUT], n->arg1.net_name, n->id) < 1) {
 				rnd_message(RND_MSG_INFO, "rats patch: 'del connection' %s to %s is done on forward annotation, removing.\n", n->id, n->arg1.net_name);
 				return 1;
 			}
