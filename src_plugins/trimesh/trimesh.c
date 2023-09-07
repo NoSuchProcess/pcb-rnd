c08a5bfb6d707e2bb3633e9708ea42d308d81835
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2023-09-07 14:35:36 +0000

    [trimesh] -Add: TriMesh() action and infra
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@38291 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/trimesh/trimesh.c
+++ src_plugins/trimesh/trimesh.c
@@ -33,19 +33,87 @@
 #include <librnd/hid/hid_attrib.h>
 #include <librnd/core/actions.h>
 
+#include "board.h"
+#include "data.h"
+#include "flag.h"
+#include "obj_line.h"
+
 #include <src_plugins/lib_polyhelp/triangulate.h>
 
 static const char *trimesh_cookie = "trimesh";
+
+typedef enum {
+	PT_EDGE,
+	PT_MAIN,
+	PT_AUX,
+	PT_max
+} point_t;
+
+typedef struct {
+	pcb_board_t *pcb;
+	rnd_rtree_t pts[PT_max];
+} trimesh_t;
+
+
+static void trimesh_init(trimesh_t *ctx, pcb_board_t *pcb)
+{
+	int n;
+
+	ctx->pcb = pcb;
+	for(n = 0; n < PT_max; n++)
+		rnd_rtree_init(&ctx->pts[n]);
+}
+
+static void trimesh_uninit(trimesh_t *ctx)
+{
+	int n;
+
+	for(n = 0; n < PT_max; n++)
+		rnd_rtree_uninit(&ctx->pts[n]);
+
+}
+
+static void trimesh_selected_lines(trimesh_t *ctx)
+{
+	PCB_LINE_ALL_LOOP(ctx->pcb->Data); {
+		if (!PCB_FLAG_TEST(PCB_FLAG_SELECTED, line))
+			continue;
+
+	} PCB_ENDALL_LOOP;
+}
+
+static const char pcb_acts_TriMesh[] = "TriMesh()\n";
+static const char pcb_acth_TriMesh[] = "replicate the outer contour of the selected polygon(s) with growing or shrinking them by offset; the new polygon is drawn on the current layer";
+static fgw_error_t pcb_act_TriMesh(fgw_arg_t *res, int argc, fgw_arg_t *argv)
+{
+	trimesh_t ctx = {0};
+
+	trimesh_init(&ctx, PCB_ACT_BOARD);
+	trimesh_selected_lines(&ctx);
+	trimesh_uninit(&ctx);
+
+	RND_ACT_IRES(0);
+	return 0;
+}
+
+
+static rnd_action_t trimesh_action_list[] = {
+	{"TriMesh", pcb_act_TriMesh, pcb_acth_TriMesh, pcb_acts_TriMesh}
+};
+
 
 int pplg_check_ver_trimesh(int ver_needed) { return 0; }
 
 void pplg_uninit_trimesh(void)
 {
+	rnd_remove_actions_by_cookie(trimesh_cookie);
 }
 
 int pplg_init_trimesh(void)
 {
 	RND_API_CHK_VER;
+
+	RND_REGISTER_ACTIONS(trimesh_action_list, trimesh_cookie);
 
 	return 0;
 }
