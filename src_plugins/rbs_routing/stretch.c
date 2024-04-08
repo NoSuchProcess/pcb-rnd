0b874506d2a9641332baa1fd85c19947a7c1bfae
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-04-08 07:22:10 +0000

    [rbs_routing] -Add: line stretch: remove the target line from the grbs model
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@38769 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/rbs_routing/stretch.c
+++ src_plugins/rbs_routing/stretch.c
@@ -8,10 +8,17 @@ int rbsr_stretch_line_begin(rbsr_stretch_t *rbss, pcb_board_t *pcb, rnd_layer_id
 	rbsr_map_debug_dump(&rbss->map, "rbss1.dump");
 
 	gl = htpp_get(&rbss->map.robj2grbs, line);
+rnd_trace("line=%p -> %p\n", line, gl);
 	if (gl == NULL) {
 		rnd_message(RND_MSG_ERROR, "rbsr_stretch_line_begin(): can't stretch this line (not in the grbs map)\n");
 		return -1;
 	}
+
+	htpp_pop(&rbss->map.robj2grbs, line);
+	grbs_path_remove_line(&rbss->map.grbs, gl);
+
+	rbsr_map_debug_draw(&rbss->map, "rbss2.svg");
+	rbsr_map_debug_dump(&rbss->map, "rbss2.dump");
 
 	return 0;
 }
