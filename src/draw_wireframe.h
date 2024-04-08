c9f1e28813679ea4f63086fbbf66489064136785
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-04-08 09:49:55 +0000

    [core] -Add: low level wireframe inline draw: option for drawing arc and line without any end-cap
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@38777 1d61de99-560f-4dba-8957-1e505d42358b

--- src/draw_wireframe.h
+++ src/draw_wireframe.h
@@ -3,6 +3,7 @@
  *
  *  pcb-rnd, interactive printed circuit board design
  *  (this file is based on PCB, interactive printed circuit board design)
+ *  Copyright (C) 2024 Tibor 'Igor2' Palinkas
  *  Copyright (C) 1994,1995,1996 Thomas Nau
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -31,10 +32,16 @@
 #include "config.h"
 #include <librnd/hid/hid.h>
 
+RND_INLINE void pcb_draw_wireframe_arc_(rnd_hid_gc_t gc, pcb_arc_t *arc, rnd_coord_t thick, int enable_cap);
+RND_INLINE void pcb_draw_wireframe_arc(rnd_hid_gc_t gc, pcb_arc_t *arc, rnd_coord_t thick)
+{
+	pcb_draw_wireframe_arc_(gc, arc, thick, 1);
+}
+
 /*-----------------------------------------------------------
  * Draws the outline of an arc
  */
-RND_INLINE void pcb_draw_wireframe_arc(rnd_hid_gc_t gc, pcb_arc_t *arc, rnd_coord_t thick)
+RND_INLINE void pcb_draw_wireframe_arc_(rnd_hid_gc_t gc, pcb_arc_t *arc, rnd_coord_t thick, int enable_cap)
 {
 	rnd_coord_t wid = thick / 2;
 	rnd_coord_t x1, y1, x2, y2;
@@ -47,18 +54,23 @@ RND_INLINE void pcb_draw_wireframe_arc(rnd_hid_gc_t gc, pcb_arc_t *arc, rnd_coor
 	pcb_arc_get_end(arc, 0, &x1, &y1);
 	pcb_arc_get_end(arc, 1, &x2, &y2);
 
+	/* outer (longer) */
 	rnd_render->draw_arc(gc, arc->X, arc->Y, arc->Width + wid, arc->Height + wid, arc->StartAngle, arc->Delta);
 	if (wid > rnd_pixel_slop) {
 		rnd_coord_t w = (wid < arc->Width) ? (arc->Width - wid) : 0;
 		rnd_coord_t h = (wid < arc->Height) ? (arc->Height - wid) : 0;
+		/* inner (shorter) */
 		rnd_render->draw_arc(gc, arc->X, arc->Y, w, h, arc->StartAngle, arc->Delta);
-		rnd_render->draw_arc(gc, x1, y1, wid, wid, arc->StartAngle, -180 * SGN(arc->Delta));
-		rnd_render->draw_arc(gc, x2, y2, wid, wid, arc->StartAngle + arc->Delta, 180 * SGN(arc->Delta));
+		if (enable_cap) {
+			rnd_render->draw_arc(gc, x1, y1, wid, wid, arc->StartAngle, -180 * SGN(arc->Delta));
+			rnd_render->draw_arc(gc, x2, y2, wid, wid, arc->StartAngle + arc->Delta, 180 * SGN(arc->Delta));
+		}
 	}
 }
 
 /*-----------------------------------------------------------
- * Draws the outline of a line
+ * Draws the outline of a line; if square is
+ * -1 assume round cap but don't draw it
  */
 RND_INLINE void pcb_draw_wireframe_line(rnd_hid_gc_t gc, rnd_coord_t x1, rnd_coord_t y1, rnd_coord_t x2, rnd_coord_t y2, rnd_coord_t thick, int square)
 {
@@ -68,7 +80,7 @@ RND_INLINE void pcb_draw_wireframe_line(rnd_hid_gc_t gc, rnd_coord_t x1, rnd_coo
 		double h = 0.5 * thick / sqrt(RND_SQUARE(dx) + RND_SQUARE(dy));
 		rnd_coord_t ox = dy * h + 0.5 * SGN(dy);
 		rnd_coord_t oy = -(dx * h + 0.5 * SGN(dx));
-		if (square) {
+		if (square > 0) {
 			/* make the line longer by cap */
 			x1 -= dx * h;
 			x2 += dx * h;
@@ -80,12 +92,12 @@ RND_INLINE void pcb_draw_wireframe_line(rnd_hid_gc_t gc, rnd_coord_t x1, rnd_coo
 			rnd_render->draw_line(gc, x1 - ox, y1 - oy, x2 - ox, y2 - oy);
 
 			/* draw caps */
-			if (!square) {
+			if (square == 0) {
 				rnd_angle_t angle = atan2(dx, dy) * 57.295779;
 				rnd_render->draw_arc(gc, x1, y1, thick / 2, thick / 2, angle - 180, 180);
 				rnd_render->draw_arc(gc, x2, y2, thick / 2, thick / 2, angle, 180);
 			}
-			else {
+			else if (square > 0) {
 				rnd_render->draw_line(gc, x1 + ox, y1 + oy, x1 - ox, y1 - oy);
 				rnd_render->draw_line(gc, x2 + ox, y2 + oy, x2 - ox, y2 - oy);
 			}
@@ -94,7 +106,7 @@ RND_INLINE void pcb_draw_wireframe_line(rnd_hid_gc_t gc, rnd_coord_t x1, rnd_coo
 			rnd_render->draw_line(gc, x1, y1, x2, y2);
 	}
 	else {
-		if (square) {
+		if (square > 0) {
 			/* square cap 0 long line does not have an angle -> always draw it axis aligned */
 			rnd_coord_t cx1 = x1 - thick/2, cx2 = x1 + thick/2, cy1 = y1 - thick/2, cy2 = y1 + thick/2;
 			rnd_render->draw_line(gc, cx1, cy1, cx2, cy1);
@@ -102,8 +114,8 @@ RND_INLINE void pcb_draw_wireframe_line(rnd_hid_gc_t gc, rnd_coord_t x1, rnd_coo
 			rnd_render->draw_line(gc, cx2, cy2, cx1, cy2);
 			rnd_render->draw_line(gc, cx1, cy2, cx1, cy1);
 		}
-		else
-			rnd_render->draw_arc(gc, x1, y1, thick/2, thick/2, 0, 360); 
+		else if (square == 0)
+			rnd_render->draw_arc(gc, x1, y1, thick/2, thick/2, 0, 360);
 	}
 }
 
