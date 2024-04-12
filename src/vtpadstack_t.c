aed64eeb2a89b949dffbdbe8d2b93095116fb9ca
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-04-12 15:35:02 +0000

    [core] -Cleanup: avoid librnd font2 GVT redeclarations
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@38797 1d61de99-560f-4dba-8957-1e505d42358b

--- src/vtpadstack_t.c
+++ src/vtpadstack_t.c
@@ -1,6 +1,7 @@
 #include <genvector/vtp0.h>
 #include <genvector/gds_char.h>
 #include <librnd/core/color.h>
+#include "obj_pstk_shape.h"
 #define GVT_DONT_UNDEF
 #include "vtpadstack_t.h"
 #include <genvector/genvector_impl.c>
--- src/vtroutestyle.c
+++ src/vtroutestyle.c
@@ -1,3 +1,4 @@
+#include <librnd/font2/font.h>
 #define GVT_DONT_UNDEF
 #include "vtroutestyle.h"
 #include <genvector/genvector_impl.c>
