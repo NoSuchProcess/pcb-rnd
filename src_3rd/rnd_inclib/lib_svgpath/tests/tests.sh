5daf320dca59ae368838d701658e534075cbb0f9
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-06-23 08:30:13 +0000

    [lib_svgpath] -Add: tester announces results
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@38989 1d61de99-560f-4dba-8957-1e505d42358b

--- src_3rd/rnd_inclib/lib_svgpath/tests/tests.sh
+++ src_3rd/rnd_inclib/lib_svgpath/tests/tests.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
+
+bad=0
 for n in *.in
 do
 	bn=${n%%.in}
+	echo -n "$bn... "
 	out=$bn.svg
 	ref=$bn.ref
 	./tester < $n > $out
 	diff -u $ref $out
+	if test $? = 0
+	then
+		echo "ok"
+		rm $out
+	else
+		echo "!! BROKEN !!"
+		bad=1
+	fi
 done
+
+if test $bad = 0
+then
+	echo ""
+	echo "QC PASS"
+	echo ""
+fi
