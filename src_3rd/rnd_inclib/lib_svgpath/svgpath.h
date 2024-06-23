4024c0681c7c2c170b29ec727c9d5cf7e9bf52ae
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-06-23 08:11:38 +0000

    [lib_svgpath] -Add: parse M, m, L and l [lib_svgpath] -Add: stdio tester
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@38981 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src_3rd/rnd_inclib/lib_svgpath/svgpath.h
@@ -0,0 +1,43 @@
+#ifndef SVGPATH_H
+#define SVGPATH_H
+
+/* Rendering callback; uctx is user context passed to the rendering call */
+typedef struct svgpath_cfg_s {
+
+	/*** config ***/
+
+	/* max length of a line segment used in curve/arc approximation; if 0
+	   or negative fall back to hardwired default */
+	double curve_approx_seglen;
+
+
+	/*** mandatory - may be NULL only if no rendering is to be done (syntax check) ***/
+
+	/* draw a straight line segment betwen x1;y1 and x2;y2 */
+	void (*line)(void *uctx, double x1, double y1, double x2, double y2);
+
+
+	/*** optional - when NULL, emulated using lines instead ***/
+
+	/* draw a cubic bezier between sx;sy and ex;ey using control points cx*;cy* */
+	void (*bezier_cubic)(void *uctx, double sx, double sy, double cx1, double cy1, double cx2, double cy2, double ex, double ey);
+
+	/* draw a quadratic bezier between sx;sy and ex;ey using control point cx;cy */
+	void (*bezier_quadratic)(void *uctx, double sx, double sy, double cx, double cy, double ex, double ey);
+
+	/* draw a rotated elliptical arc; center is cx;cy with radius rx;ry, from
+	   start angle sa (in radian) to delta angle da (in radian). Radii and angles
+	   are interpreted in an ellipse-local coord system that is then rotated
+	   by rot radians */
+	void (*earc)(void *uctx, double cx, double cy, double rx, double ry, double sa, double da, double rot);
+
+	/*** optional - silently skipped when NULL ***/
+	/* report an error in path; offs is the byte offset within the path string */
+	void (*error)(void *uctx, const char *errmsg, long offs);
+
+} svgpath_cfg_t;
+
+/* Render a path using callbacks from cb. Returns 0 on success. */
+int svgpath_render(const svgpath_cfg_t *cfg, void *uctx, const char *path);
+
+#endif
---
+++ src_3rd/rnd_inclib/lib_svgpath/tests/tester.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "../svgpath.c"
+
+static void svg_print_line(void *uctx, double x1, double y1, double x2, double y2)
+{
+	fprintf((FILE *)uctx, "<line x1=\"%.3f\" y1=\"%.3f\" x2=\"%.3f\" y2=\"%.3f\" stroke=\"black\" />\n", x1, y1, x2, y2);
+}
+
+static void svg_print_error(void *uctx, const char *errmsg, long offs)
+{
+	fprintf((FILE *)uctx, "<!-- ERROR %s at %ld -->\n", errmsg, offs);
+}
+
+
+int main()
+{
+	FILE *fout;
+	char path[8192];
+	int len, res;
+	svgpath_cfg_t cfg = {0};
+
+	len = fread(path, 1, sizeof(path), stdin);
+	if (len <= 0) {
+		fprintf(stderr, "Failed to read path from stdin\n");
+		return 1;
+	}
+
+	cfg.line = svg_print_line;
+	cfg.error = svg_print_error;
+	fout = stdout;
+
+	fprintf(fout, "<?xml version=\"1.0\"?>\n<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.0\" width=\"1000\" height=\"1000\" viewBox=\"0 0 1000 1000\">\n");
+	fprintf(fout, "<path d=\"%s\" style=\"stroke:green;stroke-width:3\" />\n\n\n", path);
+
+	res = svgpath_render(&cfg, fout, path);
+
+	fprintf(fout, "\n<!-- res=%d -->\n", res);
+	fprintf(fout, "</svg>");
+
+	return res;
+}
