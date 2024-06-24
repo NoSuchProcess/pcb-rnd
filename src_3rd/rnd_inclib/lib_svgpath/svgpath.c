39c1d56bb416d6070b1199c30ff6e643f8926620
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-06-24 06:55:36 +0000

    [lib_svgpath] -Fix: missing #includes for NULL and sscanf to work
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@39017 1d61de99-560f-4dba-8957-1e505d42358b

--- src_3rd/rnd_inclib/lib_svgpath/svgpath.c
+++ src_3rd/rnd_inclib/lib_svgpath/svgpath.c
@@ -19,6 +19,8 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <ctype.h>
 #include <math.h>
 #include "svgpath.h"
