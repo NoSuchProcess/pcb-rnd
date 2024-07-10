228612dd9f03741438683194b0748b435f07689d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-07-10 03:52:24 +0000

    [io_easyeda] -Import: unzip configuration from sch-rnd
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@39051 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_easyeda/io_easyeda_conf.h
+++ src_plugins/io_easyeda/io_easyeda_conf.h
@@ -6,7 +6,8 @@
 typedef struct {
 	const struct {
 		const struct {
-			RND_CFT_BOOLEAN dummy;
+			RND_CFT_STRING zip_list_cmd;           /* shell command that lists the content of a zip file to stdout; %s is replaced by path to the file; noise (headers and file sizes) is accepted as long as file names are not cut by newlines */
+			RND_CFT_STRING zip_extract_cmd;        /* shell command that extracts a zip file in current working directory; %s is replaced by path to the file */
 		} io_easyeda;
 	} plugins;
 } conf_io_easyeda_t;
