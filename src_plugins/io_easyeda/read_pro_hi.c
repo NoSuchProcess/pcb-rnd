3c2660a39b79282e2a964543a4c9b9f4d812af59
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-08-02 06:36:28 +0000

    [io_easyeda] -Add: pro: missing code TODO() for rect slot
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@39354 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/io_easyeda/read_pro_hi.c
+++ src_plugins/io_easyeda/read_pro_hi.c
@@ -866,6 +866,7 @@ static int pro_draw_polyobj(easy_read_ctx_t *ctx, gdom_node_t *path, pcb_layer_t
 					*shp_tx = x;
 					*shp_ty = y;
 */
+					TODO("make an example and support this");
 					error_at(ctx, path, ("rect in slot not yet supported\n"));
 					return -1;
 				}
