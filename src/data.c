707a4ada5ddfd34d1c938c886e1ad9e13e4df413
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-09-07 05:11:09 +0000

    [polybool2] -Fix: avoid null ptr deref for polygon cleared out of existence
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@39461 1d61de99-560f-4dba-8957-1e505d42358b

--- src/data.c
+++ src/data.c
@@ -832,12 +832,14 @@ static void data_clip_all_cb(void *ctx_)
 		/* create all missing pline edge trees */
 		PCB_POLY_ALL_LOOP(data); {
 			rnd_polyarea_t *pa = polygon->Clipped;
+			if (pa != NULL) {
 			do {
 				rnd_pline_t *pl;
 				for(pl = pa->contours; pl != NULL; pl = pl->next)
 					if (pl->tree == NULL)
 						pl->tree = rnd_poly_make_edge_tree(pl);
 			} while((pa = pa->f) != polygon->Clipped);
+			}
 		} PCB_ENDALL_LOOP;
 	}
 #else
