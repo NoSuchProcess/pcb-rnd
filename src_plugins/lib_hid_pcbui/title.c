204d1a7cb3332138abac89b8aa9474191c88c2dd
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-11-02 09:04:36 +0000

    [lib_hid_pcbui] -Fix: also update the title on design meta change if design name changed
    git-svn-id: svn://repo.hu/pcb-rnd/trunk@39620 1d61de99-560f-4dba-8957-1e505d42358b

--- src_plugins/lib_hid_pcbui/title.c
+++ src_plugins/lib_hid_pcbui/title.c
@@ -65,12 +65,15 @@ static void pcb_title_board_changed_ev(rnd_design_t *hidlib, void *user_data, in
 	update_title(hidlib, pcb->Changed, pcb->is_footprint);
 }
 
+static char last_hl_name;
+
 static void pcb_title_meta_changed_ev(rnd_design_t *hidlib, void *user_data, int argc, rnd_event_arg_t argv[])
 {
 	pcb_board_t *pcb = (pcb_board_t *)hidlib;
-	if (brd_changed == pcb->Changed)
+	if ((brd_changed == pcb->Changed) && (last_hl_name == hidlib->name))
 		return;
 	brd_changed = pcb->Changed;
+	last_hl_name = hidlib->name;
 	update_title(hidlib, pcb->Changed, pcb->is_footprint);
 }
 
